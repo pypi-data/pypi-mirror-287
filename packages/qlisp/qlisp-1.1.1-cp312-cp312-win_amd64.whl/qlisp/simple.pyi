555a8fb672a1c55fe81d08d24704b3e4c96fe2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:03:09 +0000

    Add package qlisp/1.1.1/qlisp-1.1.1-cp312-cp312-win_amd64.whl

---
+++ packages/qlisp/qlisp-1.1.1-cp312-cp312-win_amd64.whl/qlisp/simple.pyi
@@ -0,0 +1,18 @@
+from typing import Callable
+
+import numpy as np
+
+
+def regesterGateMatrix(gate: str,
+                       mat: Callable | np.ndarray,
+                       N: int | None = ...,
+                       docs: str = ...):
+    ...
+
+
+def applySeq(seq: list[tuple], psi0=None, ignores:list[str]=...) -> np.ndarray:
+    ...
+
+
+def seq2mat(seq: list[tuple], U=None, ignores:list[str]=...) -> np.ndarray:
+    ...
