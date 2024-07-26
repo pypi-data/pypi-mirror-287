e472ea2815c2cf1daef2861792987d9d32982c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:03:09 +0000

    Add package os-charts/0.0.1/os_charts-0.0.1.tar.gz

---
+++ packages/os-charts/os_charts-0.0.1.tar.gz/os_charts-0.0.1/README.md
@@ -0,0 +1,26 @@
+## DbCharts
+
+> OS 相关的指标计算库
+
+
+## 入口文件
+
+```
+dbchart -> charts_data.py
+```
+
+## 使用
+
+#### 安装依赖包
+
+1. 配置数据源
+
+```bash
+pass
+```
+
+2. 调用
+
+```bash
+pass
+```
---
+++ packages/os-charts/os_charts-0.0.1.tar.gz/os_charts-0.0.1/oschart/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/3/7 6:35 PM
+# @Author  : zy
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+"""
+文件功能:
+
+"""
+from oschart.charts_data import DbChart
+from oschart.database.database_init import set_db_chart_config
