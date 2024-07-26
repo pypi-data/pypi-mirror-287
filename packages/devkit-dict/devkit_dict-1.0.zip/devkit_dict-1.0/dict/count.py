26927d09cad3790f94838e1a0e76981393aefd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:04:01 +0000

    Add package devkit-dict/1.0/devkit_dict-1.0.zip

---
+++ packages/devkit-dict/devkit_dict-1.0.zip/devkit_dict-1.0/dict/count.py
@@ -0,0 +1,9 @@
+from list.count import elemcount
+def count(d, val):
+    if type(d) == type(dict()):
+        return [v for k, v in d.items()].count(val)
+    return None
+def valuecount()
+    if type(d) == type(dict()):
+        return elemcount([v for k, v in d.items()])
+    return None
