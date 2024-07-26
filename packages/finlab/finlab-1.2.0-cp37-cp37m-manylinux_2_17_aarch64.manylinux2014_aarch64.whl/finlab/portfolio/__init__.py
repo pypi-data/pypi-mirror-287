906746632e7874dfafef7e3466342af3c2b7950a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:03:15 +0000

    Add package finlab/1.2.0/finlab-1.2.0-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/finlab/finlab-1.2.0-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/finlab/portfolio/__init__.py
@@ -0,0 +1,4 @@
+from .portfolio import Portfolio
+from .cloud_report import create_report_from_cloud
+from .custom_report import create_custom_report
+from .portfolio_sync_manager import PortfolioSyncManager
