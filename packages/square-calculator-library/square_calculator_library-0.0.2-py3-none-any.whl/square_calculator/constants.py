e43bce78fe7730f136e764d876a273e77e7a31ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:03:08 +0000

    Add package square-calculator-library/0.0.2/square_calculator_library-0.0.2-py3-none-any.whl

---
+++ packages/square-calculator-library/square_calculator_library-0.0.2-py3-none-any.whl/square_calculator/constants.py
@@ -0,0 +1,9 @@
+PI = 3.1415926535897932384626433832795028841971693993751058209749445923
+FIGURE_TYPES = (
+    'circle',
+    'ellipse',
+    'rectangle',
+    'rectangular triangle',
+    'trapezoid',
+    'triangle',
+)
