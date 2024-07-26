ad22692d04fa1f4a89d7bc8d62eac728f1cce62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:03:12 +0000

    Add package dbldatagen/0.4.0.post1/dbldatagen-0.4.0.post1-py3-none-any.whl

---
+++ packages/dbldatagen/dbldatagen-0.4.0.post1-py3-none-any.whl/dbldatagen/distributions/data_distribution.py
@@ -0,0 +1,93 @@
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+This file defines the base class for statistical distributions
+
+Each inherited version of the DataDistribution object is used to generate random numbers drawn from
+a specific distribution.
+
+As the test data generator needs to scale the set of values generated across different data ranges,
+the generate function is intended to generate values scaled to values between 0 and 1.
+
+AS some distributions don't have easily predicted bounds, we scale the random data sets
+by taking the minimum and maximum value of each generated data set and using that as the range for the generated data.
+
+For some distributions, there may be alternative more efficient mechanisms for scaling the data to the [0, 1] interval.
+
+Some data distributions are scaled to the [0,1] interval as part of their data generation
+and no further scaling is needed.
+"""
+import copy
+from abc import ABC, abstractmethod
+
+import numpy as np
+import pyspark.sql.functions as F
+
+
+class DataDistribution(ABC):
+    """ Base class for all distributions"""
+
+    def __init__(self):
+        self._rounding = False
+        self._randomSeed = None
+
+    @staticmethod
+    def get_np_random_generator(random_seed):
+        """ Get numpy random number generator
+
+        :param random_seed: Numeric random seed to use. If < 0, then no random
+        :return:
+        """
+        assert random_seed is None or type(random_seed) in [np.int32, np.int64, int], \
+            f"`randomSeed` must be int or int-like not {type(random_seed)}"
+        from numpy.random import default_rng
+        if random_seed not in (-1, -1.0):
+            rng = default_rng(random_seed)
+        else:
+            rng = default_rng()
+
+        return rng
+
+    @abstractmethod
+    def generateNormalizedDistributionSample(self):
+        """ Generate sample of data for distribution
+
+        :return: random samples from distribution scaled to values between 0 and 1
+
+        Note implementors should provide implementation for this,
+
+        Return value is expected to be a Pyspark SQL column expression such as F.expr("rand()")
+        """
+        pass
+
+    def withRounding(self, rounding):
+        """ Create copy of object and set the rounding attribute
+
+        :param rounding: rounding value to set. Should be True or False
+        :return: new instance of data distribution object with rounding set
+        """
+        new_distribution_instance = copy.copy(self)
+        new_distribution_instance._rounding = rounding
+        return new_distribution_instance
+
+    @property
+    def rounding(self):
+        """get the `rounding` attribute """
+        return self._rounding
+
+    def withRandomSeed(self, seed):
+        """ Create copy of object and set the random seed attribute
+
+        :param seed: random generator seed value to set. Should be integer,  float or None
+        :return: new instance of data distribution object with rounding set
+        """
+        new_distribution_instance = copy.copy(self)
+        new_distribution_instance._randomSeed = seed
+        return new_distribution_instance
+
+    @property
+    def randomSeed(self):
+        """get the `randomSeed` attribute """
+        return self._randomSeed
