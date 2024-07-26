731868b496240197cc6c51b76228e004fda9759d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:03:08 +0000

    Add package biobb-morphIVD/1.2/biobb_morphIVD-1.2.tar.gz

---
+++ packages/biobb-morphIVD/biobb_morphIVD-1.2.tar.gz/biobb_morphIVD-1.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_morphIVD",
+    version="1.2",
+    author="Biobb developers - Maria Paola Ferri",
+    author_email="maria.ferri@bsc.es",
+    description="Biobb_3DShaper is use case to run a simulation with Abaqus.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Bioinformatics Workflows BioExcel Compatibility",
+    url="https://github.com/mapoferri/Biobb_MorphIVD",
+    project_urls={
+        "Documentation": "http://biobb_template.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/"
+    },
+    packages=setuptools.find_packages(exclude=['adapters', 'docs', 'test']),
+    install_requires=['biobb_common==3.7.0','biobb_md>=3.7.0', 'biobb_analysis==3.7.0'],
+    python_requires='==3.8.*',
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+    ),
+)
