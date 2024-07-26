649cb865b08d5b009b416346ffcd239857d83074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:03:09 +0000

    Add package shancx/1.8.1/shancx-1.8.1-py3-none-any.whl

---
+++ packages/shancx/shancx-1.8.1-py3-none-any.whl/shancx/tool.py
@@ -0,0 +1,14 @@
+
+import netCDF4 as nc
+import numpy as np
+def getPoint(pre, df, lat0, lon0, resolution, decimal=1):
+    latIdx = ((lat0 - df["Lat"]) / resolution + 0.5).astype(np.int64)
+    lonIdx = ((df["Lon"] - lon0) / resolution + 0.5).astype(np.int64)
+    return pre[...,latIdx, lonIdx].round(decimals=decimal)
+def Get_Lat_Lon_QPF(path,Lon_data,Lat_data):
+    with nc.Dataset(path) as dataNC:
+        latArr = dataNC["lat"][:]
+        lonArr = dataNC["lon"][:]
+        pre = dataNC[list(dataNC.variables.keys())[3]][:] 
+    data = getPoint(pre , {"Lon":Lon_data,"Lat":Lat_data} , latArr[0], lonArr[0], 0.01)
+    return data
