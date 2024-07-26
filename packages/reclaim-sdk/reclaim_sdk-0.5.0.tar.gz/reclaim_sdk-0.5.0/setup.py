6f1c91cde30444386949d82fe11b0e17555037fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:02:55 +0000

    Add package reclaim-sdk/0.5.0/reclaim_sdk-0.5.0.tar.gz

---
+++ packages/reclaim-sdk/reclaim_sdk-0.5.0.tar.gz/reclaim_sdk-0.5.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_namespace_packages
+
+# Load the requirements from the requirements.txt file
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+# Read the README.md file for the long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="reclaim-sdk",
+    version="0.5.0",
+    description="Unofficial Reclaim.ai Python API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/llabusch93/reclaim-sdk",
+    author="Laurence Lars Labusch",
+    author_email="llabusch@labusch-it.com",
+    license="MIT",
+    packages=find_namespace_packages(),
+    zip_safe=False,
+    scripts=[],
+    python_version=">=3.7",
+    install_requires=requirements,
+)
