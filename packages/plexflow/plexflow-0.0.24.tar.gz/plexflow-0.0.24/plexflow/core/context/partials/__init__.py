4fe2263eb0005419762deae92cd0d8c88d1ac4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:03:51 +0000

    Add package plexflow/0.0.24/plexflow-0.0.24.tar.gz

---
+++ packages/plexflow/plexflow-0.0.24.tar.gz/plexflow-0.0.24/plexflow/core/context/partials/__init__.py
@@ -0,0 +1,4 @@
+from plexflow.core.context.partials.ids import Ids
+from plexflow.core.context.partials.watchlist import Watchlist
+from plexflow.core.context.partials.cache import LRUCache
+from plexflow.core.context.partials.movie import Movie
