dbe981f9ac3aceb6bd777d9d2faa0af6f86ee849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:03:04 +0000

    Add package frii-site/0.1.1/frii_site-0.1.1-py3-none-any.whl

---
+++ packages/frii-site/frii_site-0.1.1-py3-none-any.whl/frii_site/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidDomain(Exception):
+    pass
+
+class InvalidToken(Exception):
+    pass
+
+class InvalidType(Exception):
+    pass
+
+class InvalidPerms(Exception):
+    pass
+
+class BackendException(Exception):
+    pass
