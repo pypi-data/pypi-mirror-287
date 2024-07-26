f84c4f5eecaa932f0612edc5d7d003fc3efb9581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:02:59 +0000

    Add package Jarvislib/1.3/Jarvislib-1.3.tar.gz

---
+++ packages/Jarvislib/Jarvislib-1.3.tar.gz/Jarvislib-1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Jarvislib',
+      version='1.3',
+      description='no',
+      packages=['Jarvislib'],
+      author_email='babadey.pl@gmail.com',
+      zip_safe=False)
