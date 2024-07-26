fd8247b93f36cf5d877b94cde707e73249234728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 15:03:07 +0000

    Add package codenrock/0.8/codenrock-0.8.tar.gz

---
+++ packages/codenrock/codenrock-0.8.tar.gz/codenrock-0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codenrock',
+    version='0.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'tuspy',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'codenrock=codenrock.cli:main',
+        ],
+    },
+)
