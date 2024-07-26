dbf37bc477e476c9518087625bb4219404cb5157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:03:26 +0000

    Add package odoo14-addon-ssi-general-audit/14.0.2.1.0/odoo14_addon_ssi_general_audit-14.0.2.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-general-audit/odoo14_addon_ssi_general_audit-14.0.2.1.0-py3-none-any.whl/odoo/addons/ssi_general_audit/models/client_account_group.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import fields, models
+
+
+class ClientAccountGroup(models.Model):
+    _name = "client_account_group"
+    _inherit = [
+        "mixin.master_data",
+    ]
+    _description = "Client Account Group"
+    _order = "sequence, id"
+
+    sequence = fields.Integer(
+        string="Sequence",
+        required=True,
+        default=5,
+    )
