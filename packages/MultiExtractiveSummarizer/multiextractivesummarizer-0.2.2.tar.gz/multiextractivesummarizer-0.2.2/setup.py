76e04e7c44ef9ba6f24b02ccec4215f357dbf8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:03:36 +0000

    Add package MultiExtractiveSummarizer/0.2.2/multiextractivesummarizer-0.2.2.tar.gz

---
+++ packages/MultiExtractiveSummarizer/multiextractivesummarizer-0.2.2.tar.gz/multiextractivesummarizer-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MultiExtractiveSummarizer',
+    version='0.2.2',
+    packages=find_packages(),
+    install_requires=[
+        "nltk",
+        "sentence-transformers",
+        "scipy",
+        "scikit-learn"
+        
+    ],
+    author='Arshraj Randhawa',
+    author_email='arshraj.randhawa@gmail.com',
+    description='Python package for extractive text summarization using various embeddings and methods.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/arshraj-r/MultiExtractiveSummarizer.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
