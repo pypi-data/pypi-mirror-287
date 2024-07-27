cd504855ce3392516903053c282123307d65f341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:04 +0000

    Add package rotarysaw/666.38.2067.44/rotarysaw-666.38.2067.44-py3-none-any.whl

---
+++ packages/rotarysaw/rotarysaw-666.38.2067.44-py3-none-any.whl/rotarysaw/measure.py
@@ -0,0 +1,136 @@
+import logging as log
+from collections import defaultdict
+from functools import partial
+from time import monotonic
+
+from tabulate import tabulate
+from statistics import mean
+
+
+"""
+Measure static class.
+Defaults to reports every 5 seconds.
+"""
+class Measure():
+    data = defaultdict(dict)
+    lastreport = monotonic()
+    timeout = 5
+
+    def __init__(self):
+        pass
+
+    """
+    Timeout in seconds
+    """
+    @staticmethod
+    def setInterval(timeout:int):
+        Measure.timeout = timeout
+        Measure.lastreport = monotonic()
+
+    @staticmethod
+    def measurer(index):
+        return Measurer(index)
+
+    @staticmethod
+    def prettytime(tms):
+        s = 1000
+        m = s*60
+        h = m*60
+        if tms >= h:
+            return f"{tms/h:.1f}h"
+        elif tms >= m:
+            return f"{tms/m:.1f}m"
+        if tms > s:
+            return f"{tms / s:.1f}s"
+        else:
+            return f"{tms:.1f}"
+
+    @staticmethod
+    def report():
+        log.info("Measure report")
+        rows = []
+        rows += [['Index','Count','t/exec','calls/sec','total']]
+        for k, v in Measure.data.items():
+            try:
+                row = []
+                row.append(k)
+                row.append(v['count'])
+                if len(v['runs']) > 3:
+                    row.append(Measure.prettytime(mean(v['runs'])))
+                else:
+                    if v['count'] == 0:
+                        row.append('-')
+                    else:
+                        row.append(Measure.prettytime(v['ms']/v['count']))
+
+                try:
+                    row.append(round(len(v['flares'])/(v['flares'][-1]-v['flares'][0]),1))
+                except ZeroDivisionError:
+                    row.append('-')
+
+                row.append(Measure.prettytime(v['ms']))
+                rows.append(row)
+            except KeyError:
+                pass
+
+        log.info(tabulate(rows))
+
+    @staticmethod
+    def report_if_time():
+        if Measure.timeout is not None:
+            if (monotonic() - Measure.lastreport) > Measure.timeout:
+                Measure.report()
+                Measure.lastreport = monotonic()
+
+    @staticmethod
+    def start(index):
+        Measure.report_if_time()
+
+        mi = Measure.data[index]
+        if 'ms' not in mi:
+            mi['ms'] = 0
+        if 'count' not in mi:
+            mi['count'] = 0
+        if 'flares' not in mi:
+            mi['flares'] = []
+        if 'runs' not in mi:
+            mi['runs'] = []
+        mi['flares'].append(monotonic())
+
+        while monotonic() - mi['flares'][0] > 15:
+            mi['flares'].pop(0)
+
+    @staticmethod
+    def stop(index, measuredms):
+        mi = Measure.data[index]
+
+        mi['count'] += 1
+        mi['ms'] += measuredms
+        mi['runs'].append(measuredms)
+        if len(mi['runs']) > 10:
+            mi['runs'].pop(0)
+
+        Measure.report_if_time()
+
+
+
+class Measurer():
+    """
+    Use as a context manager to measure. It's a very light layer on the Measure static object.
+        with Measurer('socalledfastdenoise'):
+            for x in range(1000):
+                socalled_fast_denoise()
+
+    """
+    def __init__(self, index):
+        self.start = partial(Measure.start, index=index)
+        self.stop = partial(Measure.stop, index=index)
+
+    def __enter__(self):
+        self.t = monotonic()
+        self.start()
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop(measuredms=(monotonic() - self.t)*1000)
---
+++ packages/rotarysaw/rotarysaw-666.38.2067.44-py3-none-any.whl/rotarysaw/test_rotarysaw.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+import sys
+import io
+
+import pytest
+import pytest_mock
+import logging
+import basic
+
+from basic import *
+
+from measure import Measure, Measurer
+
+class TestMeasure():
+    def test_basic(self,mocker):
+        Measure.setInterval(1.1)
+        for i in range(1,10):
+            with Measurer('uhuh') as f:
+                sleep(0.05)
+        sleep(1)
+        info = mocker.spy(logging, 'info')
+        Measure.report_if_time()
+        assert logging.info.call_count == 2
+        with Measurer('tuuba') as f:
+            sleep(0.1)
+        sleep(1)
+        Measure.report_if_time()
+
+    def test_report(self,mocker):
+
+        assert 'h' in Measure.prettytime(12312314234)
+        assert 'm' in Measure.prettytime(60*1001)
+        assert 's' in Measure.prettytime(1005)
+        assert '500.0' == Measure.prettytime(500)
+
+
+
+
+
+class TestSimples():
+    def test_file_datum(self):
+        a = file_datum()
+        log.info(a)
+        b = from_datum(a)
+        assert isinstance(b, datetime)
+        assert str(datetime.now().year) in a
+        assert isinstance(a, str)
+
+        dt = datetime.now()-timedelta(days=123)
+        a = file_datum(t=dt, accuracy=True)
+        log.info(a)
+        b = from_datum(a)
+        assert isinstance(b, datetime)
+        assert isinstance(a, str)
+        assert str(datetime.now().year) in a
+        assert dt == b
+
+    def test_force(self):
+        td = 'testdir'
+        if os.path.exists('testdir'):
+            if os.path.isfile(td):
+                os.unlink(td)
+            else:
+                os.rmdir('testdir')
+
+        force_mkdir('testdir')
+        assert os.path.exists('testdir')
+        os.rmdir('testdir')
+
+        open('testdir','w').write('moi')
+
+        with pytest.raises(FileExistsError):
+            force_mkdir('testdir',critical=True)
+
+    def test_hostname(self):
+        hname = hostname()
+        assert isinstance(hname, str)
+        assert len(hname) > 0
+        assert hname in ['F','opal','turrican','turrican.q.uraanikaivos.com','alex','alexlinux']
+
+    def test_log_trace(self):
+        log_trace()
+
+        backup = sys.stderr
+        sys.stderr = io.StringIO()
+        # Setting because using it for sensing binary mode.
+        sys.stderr.mode = 'w'
+
+        log_trace(stderr=True)
+
+        assert len(sys.stderr.getvalue())>50
+        assert 'pytest' in sys.stderr.getvalue()
+
+        sys.stderr = backup
+        del backup
+
+    def test_sigint_handler(self):
+
+        unlink_if_exists('debug')
+        install_log_trace()
+        assert(log_trace.count > 4)
+
+        if sys.platform != 'win32':
+            from signal import SIGINT
+            os.kill(os.getpid(), SIGINT)
+        else:
+            pytest.skip("Cannot sigint on windows")
+
+    def test_keyboard_interrupt(self):
+        unlink_if_exists('debug')
+        with pytest.raises(KeyboardInterrupt):
+            sigint_handler()
+            sigint_handler()
+            sigint_handler()
+            sigint_handler()
+            sigint_handler()
+
+    def test_debug_mode(self, mocker):
+
+        f = open('debug', 'w')
+        f.write('moi')
+        f.close()
+
+        import pdb
+        success = False
+
+        def kraut(*argk):
+            nonlocal success
+            success = True
+
+        backup = sys.stderr
+        sys.stderr = io.StringIO()
+        # Setting because using it for sensing binary mode.
+        sys.stderr.mode = 'w'
+
+        log_trace(stderr=True)
+
+        assert len(sys.stderr.getvalue()) > 50
+        assert 'pytest' in sys.stderr.getvalue()
+
+        st = pdb.set_trace
+        pdb.set_trace = kraut
+        sigint_handler()
+        pdb.set_trace = st
+
+        assert success
+
+        unlink_if_exists('debug')
+
+    def test_chdirabs(self):
+        assert len(absdirname('/humppa/kraut')) > 0
+
+        old = os.getcwd()
+        r = chdirabsdirname()
+        assert r.lower() == os.getcwd().lower()
+        assert os.getcwd() == absdirname(__file__)
+        assert os.getcwd() == old
+
+
+
+class TestTimer():
+    def test_timer(self):
+        wasrun = False
+        def test():
+            nonlocal wasrun
+            wasrun = True
+        rerun_delay(test, 0.05)
+        assert wasrun
+        wasrun = False
+        rerun_delay(test, 0.05)
+        assert not wasrun
+        sleep(0.03)
+        rerun_delay(test, 0.05)
+        assert not wasrun
+        sleep(0.05)
+        rerun_delay(test, 0.05)
+        assert wasrun
+
+        def test2(para, koo):
+            assert para == 1
+            assert koo == 2
+            nonlocal wasrun
+            wasrun = True
+
+        rerun_delay(test2, 0.05, 1, 2)
+        sleep(0.08)
+        rerun_delay(test2, 0.05, 1, 2)
+
+
+
