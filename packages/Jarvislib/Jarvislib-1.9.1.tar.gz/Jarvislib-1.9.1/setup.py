d3f69ea77be3861c5d37af3b962a4fe749f8f4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:20 +0000

    Add package Jarvislib/1.9.1/Jarvislib-1.9.1.tar.gz

---
+++ packages/Jarvislib/Jarvislib-1.9.1.tar.gz/Jarvislib-1.9.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Jarvislib',
+      version='1.9.1',
+      description='Импорт функции: from Jarvislib import start. Запуск главного цикла: start()',
+      packages=['Jarvislib'],
+      author_email='babadey.pl@gmail.com',
+      zip_safe=False)
