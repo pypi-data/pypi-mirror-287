7d66dad0380cb848808219178e389d1ef47692ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:04:12 +0000

    Add package ms-toollib/1.4.10/ms_toollib-1.4.10.tar.gz

---
+++ packages/ms-toollib/ms_toollib-1.4.10.tar.gz/ms_toollib-1.4.10/base/src/videos/mod.rs
@@ -0,0 +1,17 @@
+pub mod avf_video; 
+pub use avf_video::{AvfVideo};
+pub mod rmv_video; 
+pub use rmv_video::{RmvVideo};
+pub mod evf_video; 
+pub use evf_video::{EvfVideo};
+pub mod mvf_video; 
+pub use mvf_video::{MvfVideo};
+pub mod base_video; 
+pub use base_video::{BaseVideo};
+pub mod minesweeper_board; 
+pub use minesweeper_board::{MinesweeperBoard, GameBoardState, MouseState};
+mod analyse_methods;
+
+
+
+
