19f6c8869057a9840606eec189ae59629dd9f42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:56 +0000

    Add package cosmopharm/0.0.30/cosmopharm-0.0.30.tar.gz

---
+++ packages/cosmopharm/cosmopharm-0.0.30.tar.gz/cosmopharm-0.0.30/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Dynamically set the long_description from readme_pypi.md, if available
+try:
+    with open('./docs/README_pypi.md', encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = None
+
+setup(
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
