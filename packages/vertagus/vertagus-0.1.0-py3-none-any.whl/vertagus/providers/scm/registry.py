4bbc761c96505ac04d0e44168d26095ea1469e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:25 +0000

    Add package vertagus/0.1.0/vertagus-0.1.0-py3-none-any.whl

---
+++ packages/vertagus/vertagus-0.1.0-py3-none-any.whl/vertagus/providers/scm/registry.py
@@ -0,0 +1,17 @@
+import typing as T
+from .git_ import GitScm
+from vertagus.core.scm_base import ScmBase
+
+_scm_types = {
+    GitScm.scm_type: GitScm,
+}
+
+
+def get_scm_cls(scm_type: str) -> T.Type[ScmBase]:
+    if scm_type not in _scm_types:
+        raise ValueError(f"Unknown scm type: {scm_type}")
+    return _scm_types[scm_type]
+
+
+def register_scm_cls(scm_cls: T.Type[ScmBase]):
+    _scm_types[scm_cls.scm_type] = scm_cls
