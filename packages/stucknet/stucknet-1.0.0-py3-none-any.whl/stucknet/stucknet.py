5ee119c8897d8a22954300c5bbde6f19229805b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:05:09 +0000

    Add package stucknet/1.0.0/stucknet-1.0.0-py3-none-any.whl

---
+++ packages/stucknet/stucknet-1.0.0-py3-none-any.whl/stucknet/stucknet.py
@@ -0,0 +1,20 @@
+import random
+
+
+def generate_random_256_bit_passkey():
+    # I hope you didn't try a brute force
+    return random.randbytes(32)
+
+def get_hint():
+    # if you already reached the source code, perhaps you don't need the hint
+    return 'pip!'
+
+def get_flag():
+    # whoa, that was risky!
+    # fortunately, I censored the flag before you captured it and permanently deleted it from pypi
+    return 'SUS{n0t_th3_f14g}'
+
+
+# print hacked message in red
+message = 'YOU HAVE BEEN HACKED!!!'
+print(f'\033[38;2;255;0;0m{message} \033[38;2;255;255;255m')
