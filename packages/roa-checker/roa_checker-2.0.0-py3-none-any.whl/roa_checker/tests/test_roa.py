b338fe2c14c23f7781360329f13fe9c66643610b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 14:03:38 +0000

    Add package roa-checker/2.0.0/roa_checker-2.0.0-py3-none-any.whl

---
+++ packages/roa-checker/roa_checker-2.0.0-py3-none-any.whl/roa_checker/tests/test_roa.py
@@ -0,0 +1,60 @@
+from ipaddress import ip_network
+
+from roa_checker import ROA, ROARouted, ROAValidity
+
+
+def test_roa_outcome_valid():
+    roa = ROA(ip_network("1.2.0.0/16"), 1)
+    validity = roa.get_validity(ip_network("1.2.0.0/16"), 1)
+    assert validity == ROAValidity.VALID
+    outcome = roa.get_outcome(ip_network("1.2.0.0/16"), 1)
+    assert outcome.validity == ROAValidity.VALID
+    assert outcome.routed == ROARouted.ROUTED
+    assert roa.routed == ROARouted.ROUTED
+
+
+def test_roa_outcome_unknown():
+    roa = ROA(ip_network("1.3.0.0/16"), 1)
+    validity = roa.get_validity(ip_network("1.2.0.0/16"), 2)
+    assert validity == ROAValidity.UNKNOWN
+    outcome = roa.get_outcome(ip_network("1.2.0.0/16"), 2)
+    assert outcome.validity == ROAValidity.UNKNOWN
+    assert outcome.routed == ROARouted.ROUTED
+    assert roa.routed == ROARouted.ROUTED
+
+
+def test_roa_validity_invalid_length():
+    roa = ROA(ip_network("1.2.0.0/16"), 1)
+    validity = roa.get_validity(ip_network("1.2.3.0/24"), 1)
+    assert validity == ROAValidity.INVALID_LENGTH
+    outcome = roa.get_outcome(ip_network("1.2.3.0/24"), 1)
+    assert outcome.validity == ROAValidity.INVALID_LENGTH
+    assert outcome.routed == ROARouted.ROUTED
+    assert roa.routed == ROARouted.ROUTED
+
+
+def test_roa_validity_invalid_origin():
+    roa = ROA(ip_network("1.2.0.0/16"), 1)
+    validity = roa.get_validity(ip_network("1.2.0.0/16"), 0)
+    assert validity == ROAValidity.INVALID_ORIGIN
+    outcome = roa.get_outcome(ip_network("1.2.0.0/16"), 0)
+    assert outcome.validity == ROAValidity.INVALID_ORIGIN
+    assert outcome.routed == ROARouted.ROUTED
+    assert roa.routed == ROARouted.ROUTED
+
+
+def test_roa_validity_invalid_length_and_origin():
+    roa = ROA(ip_network("1.2.0.0/16"), 1)
+    validity = roa.get_validity(ip_network("1.2.3.0/24"), 2)
+    assert validity == ROAValidity.INVALID_LENGTH_AND_ORIGIN
+    outcome = roa.get_outcome(ip_network("1.2.3.0/24"), 2)
+    assert outcome.validity == ROAValidity.INVALID_LENGTH_AND_ORIGIN
+    assert outcome.routed == ROARouted.ROUTED
+    assert roa.routed == ROARouted.ROUTED
+
+
+def test_roa_non_routed():
+    roa = ROA(ip_network("1.2.0.0/16"), 0)
+    outcome = roa.get_outcome(ip_network("1.2.3.0/24"), 2)
+    assert outcome.validity == ROAValidity.INVALID_LENGTH_AND_ORIGIN
+    assert outcome.routed == ROARouted.NON_ROUTED
