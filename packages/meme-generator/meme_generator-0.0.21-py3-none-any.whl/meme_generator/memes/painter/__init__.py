14885c9ff0bc4ba2e9222fab5190c24362369eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:04:05 +0000

    Add package meme-generator/0.0.21/meme_generator-0.0.21-py3-none-any.whl

---
+++ packages/meme-generator/meme_generator-0.0.21-py3-none-any.whl/meme_generator/memes/painter/__init__.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+
+img_dir = Path(__file__).parent / "images"
+
+
+def painter(images: list[BuildImage], texts, args):
+    img = (
+        images[0].convert("RGBA").resize((240, 345), keep_ratio=True, direction="north")
+    )
+    frame = BuildImage.open(img_dir / "0.png")
+    frame.paste(img, (125, 91), below=True)
+    return frame.save_jpg()
+
+
+add_meme("painter", painter, min_images=1, max_images=1, keywords=["小画家"])
---
+++ packages/meme-generator/meme_generator-0.0.21-py3-none-any.whl/meme_generator/memes/this_chicken/__init__.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.exception import TextOverLength
+
+img_dir = Path(__file__).parent / "images"
+
+
+def this_chicken(images: list[BuildImage], texts, args):
+    text = texts[0] if texts else "这是十二生肖中的鸡"
+    img = images[0].convert("RGBA").resize((640, 640), keep_ratio=True)
+
+    frame = BuildImage.open(img_dir / "0.png")
+    try:
+        frame.draw_text(
+            (0, 900, 1440, 1080),
+            text,
+            max_fontsize=80,
+            min_fontsize=40,
+            fill="white",
+            stroke_ratio=1 / 15,
+            stroke_fill="black",
+        )
+    except ValueError:
+        raise TextOverLength(text)
+    frame.paste(
+        img.perspective(((507, 0), (940, 351), (383, 625), (0, 256))),
+        (201, 201),
+        below=True,
+    )
+    return frame.save_jpg()
+
+
+add_meme(
+    "this_chicken",
+    this_chicken,
+    min_images=1,
+    max_images=1,
+    max_texts=1,
+    default_texts=["这是十二生肖中的鸡"],
+    keywords=["这是鸡", "🐔"],
+)
