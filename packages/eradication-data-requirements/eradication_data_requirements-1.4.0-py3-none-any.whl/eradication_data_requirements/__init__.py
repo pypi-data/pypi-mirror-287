b67ad5fedcc86bb2a2fe8a4cfe3a88cc1912ed2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:03:33 +0000

    Add package eradication-data-requirements/1.4.0/eradication_data_requirements-1.4.0-py3-none-any.whl

---
+++ packages/eradication-data-requirements/eradication_data_requirements-1.4.0-py3-none-any.whl/eradication_data_requirements/__init__.py
@@ -0,0 +1,10 @@
+"""A template Python module"""
+
+__version__ = "1.4.0"
+from .api import *  # noqa
+from .cli import *  # noqa
+from .data_requirements_plot import *  # noqa
+from .fit_ramsey_time_series import *  # noqa
+from .plot_progress_probability import *  # noqa
+from .resample_raw_data import *  # noqa
+from .plot_cpue_series import *  # noqa
