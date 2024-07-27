4898ae790a788115006730be9b7f623ee6acc654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:44 +0000

    Add package gee-package/0.1/gee_package-0.1-py3-none-any.whl

---
+++ packages/gee-package/gee_package-0.1-py3-none-any.whl/gee_package/arithmetic.py
@@ -0,0 +1,15 @@
+# my_simple_package/my_simple_package/arithmetic.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    return a / b
