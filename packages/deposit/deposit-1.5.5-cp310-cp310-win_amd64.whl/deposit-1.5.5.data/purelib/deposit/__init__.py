e041949b6682a1d51205a7ecc0a354867a65093a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:06 +0000

    Add package deposit/1.5.5/deposit-1.5.5-cp310-cp310-win_amd64.whl

---
+++ packages/deposit/deposit-1.5.5-cp310-cp310-win_amd64.whl/deposit-1.5.5.data/purelib/deposit/__init__.py
@@ -0,0 +1,12 @@
+version_info = (1, 5, 5)
+
+__version__ = '.'.join(map(str, version_info))
+__title__ = 'Deposit'
+__date__ = "26.7.2024"
+
+from deposit.store.store import Store
+
+from deposit.store.abstract_dtype import AbstractDType
+from deposit.store.ddatetime import DDateTime
+from deposit.store.dgeometry import DGeometry
+from deposit.store.dresource import DResource
