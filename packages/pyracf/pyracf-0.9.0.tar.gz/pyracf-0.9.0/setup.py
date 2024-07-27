7ca7a6f66259cb4e62899699c7449977b9699d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:02:55 +0000

    Add package pyracf/0.9.0/pyracf-0.9.0.tar.gz

---
+++ packages/pyracf/pyracf-0.9.0.tar.gz/pyracf-0.9.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyracf",
+    version="0.9.0",
+    author="Wizard of z/OS",
+    author_email="wizard@zdevops.com",
+    description="Parsing IRRDBU00 unloads in panda dataframes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wizardofzos/pyracf",
+    project_urls={
+        "Bug Tracker": "https://github.com/wizardofzos/pyracf/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    install_requires=[
+        'wheel',
+        'pandas>=1.5.2',
+        'xlsxwriter>=3.1.0'
+    ],
+    python_requires=">=3.6",
+)
