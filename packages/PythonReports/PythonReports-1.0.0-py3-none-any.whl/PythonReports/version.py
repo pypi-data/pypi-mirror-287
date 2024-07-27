a12c0282bafe8d7aba56f32d486d3bdb429d48ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 14:04:36 +0000

    Add package PythonReports/1.0.0/PythonReports-1.0.0-py3-none-any.whl

---
+++ packages/PythonReports/PythonReports-1.0.0-py3-none-any.whl/PythonReports/version.py
@@ -0,0 +1,9 @@
+"""PythonReports package version declaration"""
+
+from datetime import date
+
+__version__ = "1.0.0"
+__date__ = date(2024, 7, 27)
+__all__ = ["__version__", "__date__"]
+
+# vim: set et sts=4 sw=4 :
