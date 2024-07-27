ac695213e76636229851a52518fd5eb54a631cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:03:20 +0000

    Add package socialdataanalysis/0.0.57/socialdataanalysis-0.0.57-py3-none-any.whl

---
+++ packages/socialdataanalysis/socialdataanalysis-0.0.57-py3-none-any.whl/socialdataanalysis/__init__.py
@@ -0,0 +1,30 @@
+from .association import (
+    analisar_independencia_variaveis_tabela_contingencia,
+    calcular_odds_ratio_razao_risco_discrepancia,
+    calcular_distribuicao_probabilidades_e_decisao_hipotese,
+    complementar_tabela_contingencia_com_analise_estatistica,
+    resolver_sistema_equacoes_dada_variavel_tabela_contingencia,
+    decompor_tabela_contingencia,
+    gerar_tabela_contingencia,
+    complementar_e_filtrar_tabelas_contingencia,
+    avaliar_homogeneidade_odds_ratio,
+    avaliar_independencia_condicional,
+    avaliar_associacao_condicional,
+    analisar_frequencias_esperadas,
+    eliminacao_reversa_com_comparacao_llm,
+    calcular_e_exibir_odds_ratios_llm,
+    criar_tabela_contingencia_expandida_llm,
+    plot_stacked_bar_chart,
+    realizar_analise_correspondencia,
+    detalhar_resultados_analise_correspondencia,
+    analise_correspondencia_e_grafico,
+)
+from .exploratoryfactoranalysis import (
+    inverter_escala,
+    transformar_escala,
+    analisar_consistencia_interna,
+)
+from .generalfunctions import (
+    download_and_load_sav_file,
+    generate_frequency_table
+)
