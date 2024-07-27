6f40d039cbef6b8e4b63aa7ea7b5c319c5c95438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:05:07 +0000

    Add package danila-lib/2.3.4/danila_lib-2.3.4-py3-none-any.whl

---
+++ packages/danila-lib/danila_lib-2.3.4-py3-none-any.whl/data/neuro/models.py
@@ -0,0 +1,34 @@
+RAMA_CLASSIFY_MODEL_ADDRESS = 'https://disk.yandex.ru/d/4xtNt0zftHFFVg'
+
+RAMA_BEGICKAYA_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/dld9TFyEQGWBcw'
+RAMA_RUZHIMMASH_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/42sWB5wc7iCZgQ'
+RAMA_ALTAI_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/q3FDt5wzdjCg7Q'
+RAMA_BALAKOVO_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/HlcViPdjY2hS5g'
+RAMA_PROMLIT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/wRdF0UOdGoXlVQ'
+RAMA_TIHVIN_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/8xJz_9PsUDH41Q'
+RAMA_URALVAGON_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/QK7sDnJDisNJiQ'
+
+RAMA_ALTAI_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/Pb4cNl3JxcUw6Q'
+RAMA_BALAKOVO_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/bitLjF_wboq26Q'
+RAMA_BEGICKAYA_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/nQaiB6YheVSc7Q'
+RAMA_PROMLIT_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/lu11fYVSW64k6A'
+RAMA_RUZHIMMASH_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/B5brahwfJhy9mg'
+RAMA_TIHVIN_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/fbQL7mq1wV6ueA'
+RAMA_URALVAGON_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/HZH6SabldzFSkQ'
+
+# TEXT_RECOGNIZE_RUZHIMMASH_MODEL_ADDRESS = 'https://disk.yandex.ru/d/C2hDMsbTZMnL8Q' -7500_b7500_r
+# TEXT_RECOGNIZE_BEGICKAYA_MODEL_ADDRESS = 'https://disk.yandex.ru/d/C2hDMsbTZMnL8Q'
+TEXT_RECOGNIZE_RUZHIMMASH_MODEL_ADDRESS = 'https://disk.yandex.ru/d/GcI-mAtwpb2I-g' # 11000_b_11000_r
+TEXT_RECOGNIZE_BEGICKAYA_MODEL_ADDRESS = 'https://disk.yandex.ru/d/GcI-mAtwpb2I-g'  # 11000_b_11000_r
+
+RAMA_PROD_CLASSIFY_MODEL_ADDRESS = 'https://disk.yandex.ru/d/8FI8TaTwaCfbSA'
+RAMA_PROD_CLASSIFY_MODEL_ADDRESS_2 = 'https://disk.yandex.ru/d/P7rKubXWHJjgCQ'
+VAGON_NUMBER_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/ZGS0sdk1syJmcQ'
+VAGON_NUMBER_DETECT_MODEL_ADDRESS_2 = 'https://disk.yandex.ru/d/3artWauJULaWXw'
+DETAIL_CLASSIFY_MODEL_ADDRESS = 'https://disk.yandex.ru/d/Xamk_OYzYAbGhg'
+DETAIL_CLASSIFY_MODEL_ADDRESS_2 = 'https://disk.yandex.ru/d/_t4r3N7a2Uy3cA'
+
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS = 'https://disk.yandex.ru/d/eh714_PrjFyLHg'
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS_2 = 'https://disk.yandex.ru/d/iZxGW6ZaXZDoQQ'
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS_3 = 'https://disk.yandex.ru/d/wd697DtHumhsjA'
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS_4 = 'https://disk.yandex.ru/d/4UqcWigCF5hCqw' 
