fb50eb6cf2cda52694708ae9cd252171f49b8022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 15:03:51 +0000

    Add package ceylon/0.15.5/ceylon-0.15.5-py3-none-win_amd64.whl

---
+++ packages/ceylon/ceylon-0.15.5-py3-none-win_amd64.whl/ceylon/workspace/worker.py
@@ -0,0 +1,19 @@
+from ceylon.ceylon import WorkerAgent, WorkerAgentConfig, Processor, \
+    MessageHandler
+
+
+class Worker(WorkerAgent, Processor, MessageHandler):
+
+    def __init__(self, name="admin", workspace_id="admin", admin_peer=None, admin_port=8888, role="worker"):
+        super().__init__(config=WorkerAgentConfig(name=name,
+                                                  role=role,
+                                                  admin_peer=admin_peer,
+                                                  admin_port=admin_port,
+                                                  work_space_id=workspace_id), processor=self, on_message=self)
+
+    async def run(self, inputs: "bytes"):
+        pass
+
+    async def on_message(self, agent_id: "str", data: "bytes", time: "int"):
+        pass
+   
