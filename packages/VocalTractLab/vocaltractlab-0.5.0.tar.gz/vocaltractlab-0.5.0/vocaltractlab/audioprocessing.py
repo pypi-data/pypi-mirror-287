c8afb459ba4724122c1465105c0f905fef27d9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:03:39 +0000

    Add package VocalTractLab/0.5.0/vocaltractlab-0.5.0.tar.gz

---
+++ packages/VocalTractLab/vocaltractlab-0.5.0.tar.gz/vocaltractlab-0.5.0/vocaltractlab/audioprocessing.py
@@ -0,0 +1,160 @@
+
+
+
+import os
+import torch
+import torchaudio
+import torchaudio.functional as F
+#import torchaudio.transforms as T
+import numpy as np
+#import librosa
+#from scipy import interpolate as ip
+#import parselmouth
+#import matplotlib.pyplot as plt
+from vocaltractlab_cython import get_constants
+
+from typing import Tuple
+from typing import Union
+from typing import Optional
+from typing import Dict
+from numpy.typing import ArrayLike
+
+
+
+torch.set_num_threads(1)
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+MAX_WAV_VALUE = 32768.0
+
+def to_float(
+          x: Union[torch.Tensor, ArrayLike],
+    ) -> torch.Tensor:
+    """Converts a tensor of ints into floats in the range [-1, 1].
+    Args:
+        x (Union[torch.Tensor, ArrayLike]): Tensor of ints with arbitrary shape.
+    Returns:
+        torch.Tensor: Tensor of floats with same shape as x.
+    """
+    # Convert to torch.Tensor if needed.
+    if not isinstance(x, torch.Tensor):
+        x = torch.tensor(x)
+    # Convert to float if needed.
+    if not x.dtype == torch.float:
+        input_dtype = x.dtype
+        x = x.float()
+        if input_dtype == torch.int:
+            x /= MAX_WAV_VALUE
+    return x
+
+def to_int(
+        waveform,
+    ):
+    """
+    Convert any audio array to Torch int tensor.
+    :param waveform: Audio to convert.
+    :return: Audio as int16.
+    """
+    # Convert to torch tensor
+    if not isinstance(waveform, torch.Tensor):
+        waveform = torch.tensor(waveform)
+    # Conver to int   
+    if not waveform.dtype == torch.int16:
+        waveform = waveform * 32768
+        waveform = waveform.short()
+    return waveform
+
+def normalize_audio_amplitude(
+        x,
+        dBFS = -1,
+        ): #normalisation in dB
+	norm_factor = 10**( -1 * dBFS * 0.05 ) -1
+	norm_max = torch.max( torch.abs( x ) )#, axis=0)
+	x /= ( norm_max + ( norm_max * norm_factor ) )
+	return x
+
+def postprocess(
+        x: ArrayLike,
+        sr_out: int,
+        dBFS: int = -1,
+        file_path: str = None,
+        to_numpy: bool = False,
+        ) -> np.ndarray:
+    
+    vtl_constants = get_constants()
+
+    x = torch.tensor( x ).unsqueeze( 0 )
+
+    if sr_out is None:
+        sr_out = vtl_constants[ 'sr_audio' ]
+    elif sr_out != vtl_constants[ 'sr_audio' ]:
+        x = resample_like_librosa(
+            x = x,
+            sr_in = vtl_constants[ 'sr_audio' ],
+            sr_out = sr_out,
+            )
+    
+    if dBFS is not None:
+        x = normalize_audio_amplitude(
+            x = x,
+            dBFS = dBFS,
+            )
+        
+    if file_path is not None:
+        if not os.path.exists(
+            os.path.dirname( file_path )
+            ):
+            os.makedirs(
+                os.path.dirname( file_path ),
+                exist_ok = True,
+                )
+        torchaudio.save(
+            file_path,
+            x,
+            sr_out,
+            )
+        
+    if to_numpy:
+        x = x.numpy()
+    
+    return x
+
+def resample_like_librosa(
+        x: Union[torch.Tensor, ArrayLike],
+        sr_in: int,
+        sr_out: int,
+    ) -> torch.Tensor:
+    """
+    Resample a time series, similar to librosa
+    with 'kaiser_best' resampling method.
+    Args:
+        x (Union[torch.Tensor, ArrayLike]): Tensor of ints with arbitrary shape.
+        sr_in (int): Input sampling rate.
+        sr_out (int): Output sampling rate.
+    Returns:
+        torch.Tensor: Tensor of floats with same shape as x.
+    """
+    # Convert to torch.Tensor if needed.
+    x = to_float(x)
+    if sr_in != sr_out:
+        x = F.resample(
+            waveform = x,
+            orig_freq = sr_in,
+            new_freq = sr_out,
+            lowpass_filter_width = 64,
+            rolloff = 0.9475937167399596,
+            resampling_method = 'sinc_interp_kaiser',
+            beta = 14.769656459379492,
+            )
+    return x
+
+def hz_to_st(
+        frequency_hz,
+        reference = 1.0,
+    ):
+    return 12.0*np.log( frequency_hz / reference ) / np.log(2.0)
+
+def st_to_hz(
+        frequency_st,
+        reference = 1.0,
+    ):
+    return reference*pow( 2, frequency_st / 12.0 )
---
+++ packages/VocalTractLab/vocaltractlab-0.5.0.tar.gz/vocaltractlab-0.5.0/vocaltractlab/core.py
@@ -0,0 +1,413 @@
+
+
+
+import os
+import numpy as np
+
+from vocaltractlab_cython import get_constants
+from vocaltractlab_cython import gesture_file_to_audio
+from vocaltractlab_cython import synth_block
+from target_approximation.vocaltractlab import MotorSequence, MotorSeries
+
+from typing import Union, List, Tuple, Dict, Any, Optional, Callable, Iterable, Sequence
+from numpy.typing import ArrayLike
+
+from tools_mp import multiprocess
+
+from .utils import make_iterable
+from .audioprocessing import postprocess
+
+          
+
+def gesture_to_audio(
+        gesture_data: Union[ Iterable[ str ], str ],
+        audio_files: Optional[ Union[ Iterable[ str ], str ] ],
+        normalize_audio: int = -1,
+        sr: int = None,
+        return_data: bool = False,
+        workers: int = None,
+        verbose: bool = True,
+        ) -> None:
+
+    gesture_data = make_iterable( gesture_data )
+    audio_files = make_iterable( audio_files )
+    if len( gesture_data ) != len( audio_files ):
+        raise ValueError(
+            f"""
+            The number of gesture file paths: {len(gesture_data)}
+            does not match the number of audio file paths: {len(audio_files)}.
+            """
+            )
+    
+    args = [
+        dict(
+            gesture_data = gd,
+            audio_file_path = audio_file_path,
+            verbose_api = False,
+            normalize_audio = normalize_audio,
+            sr = sr,
+            )
+        for gd, audio_file_path in zip(
+            gesture_data,
+            audio_files,
+            )
+        ]
+    audio_data = multiprocess(
+        _gesture_to_audio,
+        args = args,
+        return_data = return_data,
+        workers = workers,
+        verbose = verbose,
+        mp_threshold = 4,
+        )
+    return audio_data
+
+def _gesture_to_audio(
+        gesture_data,
+        audio_file_path,
+        verbose_api,
+        normalize_audio,
+        sr,
+        ) -> np.ndarray:
+    if isinstance( gesture_data, str ):
+        #gesture_file = gesture_data.to_gesture_file( file_path = None )
+        gesture_file = gesture_data
+    else:
+        raise TypeError(
+            f"""
+            The specified gesture data type: '{type(gesture_data)}'
+            is not supported. Type must be one of the following:
+            - str
+            """
+            )
+    audio = gesture_file_to_audio(
+        ges_file_path = gesture_file,
+        audio_file_path = None,
+        verbose_api = verbose_api,
+    )
+    
+    audio = postprocess(
+        x = audio,
+        sr_out = sr,
+        dBFS = normalize_audio,
+        file_path = audio_file_path,
+        to_numpy = True,
+        )
+    
+    return audio
+    
+'''
+def gesture_to_motor(
+        gesture_files: Union[ Iterable[ str ], str ],
+        motor_files: Optional[ Union[ Iterable[ str ], str ] ],
+        return_data: bool = False,
+        workers: int = None,
+        verbose: bool = True,
+        ) -> None:
+
+    gesture_files = make_iterable( gesture_files )
+    motor_files = make_iterable( motor_files )
+    if len( gesture_files ) != len( motor_files ):
+        raise ValueError(
+            f"""
+            The number of gesture file paths: {len(gesture_files)}
+            does not match the number of motor file paths: {len(motor_files)}.
+            """
+            )
+    
+    args = [
+        dict(
+            motor_data = md,
+            audio_file_path = audio_file_path,
+            normalize_audio = normalize_audio,
+            sr = sr,
+            )
+        for md, audio_file_path in zip(
+            motor_data,
+            audio_file_path_list,
+            )
+        ]
+    audio_data = multiprocess(
+        _motor_to_audio,
+        args = args,
+        return_data = return_data,
+        workers = workers,
+        verbose = verbose,
+        mp_threshold = 4,
+        )
+    return
+'''
+
+def motor_to_audio(
+        motor_data: Union[ MotorSequence, MotorSeries, str ],
+        audio_files: Optional[ Union[ Iterable[str], str ] ] = None,
+        normalize_audio: int = -1,
+        sr: int = None,
+        return_data: bool = False,
+        workers: int = None,
+        verbose: bool = True,
+        ) -> np.ndarray:
+    """
+    Convert motor data into audio signals.
+
+    Parameters
+    ----------
+    motor_data : Union[MotorScore, MotorSeries, str]
+        Input data representing motor scores or series.
+        Can be a MotorScore object, MotorSeries object, or a path to a file.
+
+    audio_files : Optional[Union[Iterable[str], str]], optional
+        Path or list of paths to store the generated audio files.
+        If None, audio files will not be saved. Default is None.
+
+    normalize_audio : int, optional
+        Amplitude normalization factor.
+        -1 indicates no normalization. Default is -1.
+
+    sr : int, optional
+        Sampling rate of the output audio.
+        If None, defaults to the system's default audio sampling rate.
+
+    return_data : bool, optional
+        Flag indicating whether to return the generated audio data.
+        Default is False.
+
+    workers : int, optional
+        Number of worker processes for parallel processing.
+        If None, uses the system's default number of CPU cores.
+        Default is None.
+
+    verbose : bool, optional
+        Verbosity mode. If True, displays progress information.
+        Default is True.
+
+    Returns
+    -------
+    np.ndarray
+        If 'return_data' is True, returns a NumPy array of the generated audio data.
+
+    Raises
+    ------
+    ValueError
+        If the number of audio file paths doesn't match the number of motor data.
+
+    FileNotFoundError
+        If the specified motor file path does not exist.
+
+    TypeError
+        If the specified motor data type is not supported.
+        Supported types include str, MotorScore, and MotorSeries.
+
+    Notes
+    -----
+    This function converts motor data into audio signals using the VocalTractLab synthesizer.
+    It processes the motor parameters and generates corresponding audio signals.
+
+    Examples
+    --------
+    # Example 1: Convert MotorScore object to audio without saving files
+    >>> motor_data = MotorScore(...)  # Replace '...' with actual MotorScore data
+    >>> audio_data = motor_to_audio(motor_data)
+
+    # Example 2: Convert MotorSeries object to audio and save the files
+    >>> motor_series = MotorSeries(...)  # Replace '...' with actual MotorSeries data
+    >>> audio_files = ['audio1.wav', 'audio2.wav']  # List of paths to save audio files
+    >>> motor_to_audio(motor_series, audio_files=audio_files, return_data=False)
+
+    # Example 3: Convert motor data from a file to audio with normalization
+    >>> motor_file_path = 'path/to/motor_data.csv'  # Replace with the actual file path
+    >>> audio_data = motor_to_audio(motor_file_path, normalize_audio=0.5, return_data=True)
+    """
+
+    motor_data = make_iterable( motor_data )
+    if audio_files is None:
+        audio_files = [ None ] * len( motor_data )
+    else:
+        audio_files = make_iterable( audio_files )
+    if len( audio_files ) != len( motor_data ):
+        raise ValueError(
+            f"""
+            The number of audio file paths: {len(audio_files)}
+            does not match the number of motor data: {len(motor_data)}.
+            """
+            )
+    
+    args = [
+        dict(
+            motor_data = md,
+            audio_file_path = audio_file_path,
+            normalize_audio = normalize_audio,
+            sr = sr,
+            )
+        for md, audio_file_path in zip(
+            motor_data,
+            audio_files,
+            )
+        ]
+    audio_data = multiprocess(
+        _motor_to_audio,
+        args = args,
+        return_data = return_data,
+        workers = workers,
+        verbose = verbose,
+        #mp_threshold = 4,
+        )
+    return audio_data
+
+def _motor_to_audio(
+        motor_data,
+        audio_file_path,
+        normalize_audio,
+        sr,
+        state_samples = None,
+        ):
+    """
+    Generate audio from motor data.
+
+    Parameters
+    ----------
+    motor_data : Union[MotorScore, MotorSeries, str]
+        Input data representing motor scores or series.
+        Can be a MotorScore object, MotorSeries object, or a path to a file.
+
+    audio_file_path : Optional[str]
+        Path to store the generated audio file. If None, audio will not be saved.
+
+    normalize_audio : int
+        Amplitude normalization factor. Use -1 for no normalization.
+
+    sr : int
+        Sampling rate of the output audio.
+
+    state_samples : int, optional
+        Number of samples for state duration.
+        If None, defaults to a predefined constant value.
+
+    Returns
+    -------
+    torch.Tensor
+        A tensor representing the generated audio.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the specified motor file path does not exist.
+
+    TypeError
+        If the specified motor data type is not supported.
+        Supported types include str, MotorScore, and MotorSeries.
+
+    Notes
+    -----
+    This function generates audio signals from motor data using the VocalTractLab synthesizer.
+    It processes the motor parameters and synthesizes corresponding audio signals.
+
+    Examples
+    --------
+    # Example 1: Generate audio from MotorScore object without saving the file
+    >>> motor_data = MotorScore(...)  # Replace '...' with actual MotorScore data
+    >>> audio_tensor = _motor_to_audio(motor_data, audio_file_path=None, normalize_audio=0, sr=44100)
+
+    # Example 2: Generate audio from MotorSeries object and save the audio file
+    >>> motor_series = MotorSeries(...)  # Replace '...' with actual MotorSeries data
+    >>> audio_path = 'output_audio.wav'  # Path to save the audio file
+    >>> _motor_to_audio(motor_series, audio_file_path=audio_path, normalize_audio=-1, sr=22050)
+
+    # Example 3: Generate audio from a file containing motor data with custom state samples
+    >>> motor_file_path = 'path/to/motor_data.csv'  # Replace with the actual file path
+    >>> audio_tensor = _motor_to_audio(motor_file_path, audio_file_path=None, normalize_audio=0.8, sr=44100, state_samples=120)
+    """
+
+    if isinstance( motor_data, str ):
+        if not os.path.exists( motor_data ):
+            raise FileNotFoundError( 
+                f"""
+                The specified motor file path: '{motor_data}'
+                does not exist.
+                """
+            )
+        motor_series = MotorSeries.load(
+            motor_data,
+            sr = 441,
+            )
+    elif isinstance( motor_data, MotorSequence ):
+        motor_series = motor_data.to_series(
+            sr = 441,
+        )
+    elif isinstance( motor_data, MotorSeries ):
+        motor_series = motor_data
+    else:
+        raise TypeError(
+            f"""
+            The specified motor data type: '{type(motor_data)}'
+            is not supported. Type must be one of the following:
+            - str
+            - MotorSequence
+            - MotorSeries
+            """
+            )
+    vtl_constants = get_constants()
+    if state_samples is None:
+        #state_samples = vtl_constants[ 'n_samples_per_state' ]
+        state_samples = int(
+            vtl_constants[ 'sr_audio' ] / motor_series.sr
+        )
+        
+
+    #print( motor_series.to_numpy( part='tract' ) )
+
+    tract_params = motor_series.tract().to_numpy( transpose = False )
+    glottal_params = motor_series.glottis().to_numpy( transpose = False )
+    #print( tract_params.shape )
+    #print( glottal_params.shape )
+    #print( state_samples )
+
+    
+    audio = synth_block(
+        tract_parameters = tract_params,
+        glottis_parameters = glottal_params,
+        state_samples = state_samples,
+        verbose_api = False,
+        )
+    
+    audio = postprocess(
+        x = audio,
+        sr_out = sr,
+        dBFS = normalize_audio,
+        file_path = audio_file_path,
+        to_numpy = True,
+        )
+    
+    return audio
+
+def phoneme_to_gesture(
+        phoneme: str,
+        ) -> np.ndarray:
+    return
+
+def _phoneme_to_gesture(
+        phoneme: str,
+        ) -> np.ndarray:
+    return
+
+def phoneme_to_motor():
+    return
+
+def _phoneme_to_motor():
+    return
+
+
+#def _supra_glottal_state_to_svg_str( args ):
+#    supra_glottal_state = args
+#    svgStr = ( ' ' * 10000 ).encode()
+#    constants = get_constants()
+#    cdef np.ndarray[np.float64_t, ndim = 1] tractParams = np.zeros(
+#        constants['n_tract_params'],
+#        dtype = 'float64',
+#        )
+#    tractParams = supra_glottal_state.ravel()
+#    vtlExportTractSvgToStr(
+#        &tractParams[0],
+#        svgStr,
+#        )
+#    return svgStr.decode()
