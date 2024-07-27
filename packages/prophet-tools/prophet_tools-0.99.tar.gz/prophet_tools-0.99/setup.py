93fc502b3351bd0f87e62cb06f3a7d234be5ee5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:38 +0000

    Add package prophet-tools/0.99/prophet_tools-0.99.tar.gz

---
+++ packages/prophet-tools/prophet_tools-0.99.tar.gz/prophet_tools-0.99/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='prophet_tools',
+      version='0.99',
+      description='ProPHet personal tools',
+      packages=['prophet_tools'],
+      author_email='prophet.incorporated@gmail.com',
+      zip_safe=False)
