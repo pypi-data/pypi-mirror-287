331317c6eb7c91ebbf28c5f132348ce6eee17405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 14:03:57 +0000

    Add package corals/0.1.7/corals-0.1.7-py2.py3-none-any.whl

---
+++ packages/corals/corals-0.1.7-py2.py3-none-any.whl/corals/correlation/topkdiff/base.py
@@ -0,0 +1,8 @@
+def topkdiff(
+        D1, 
+        D2, 
+        threshold=None,
+        k=None, 
+        correlation_function="pearson", 
+        **kwargs):
+    raise NotImplementedError("Interface specification only.")
