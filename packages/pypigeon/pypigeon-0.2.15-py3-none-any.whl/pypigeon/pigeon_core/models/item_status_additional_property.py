27ac7ac22d43afd3adb98147c138a6573932e386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:13 +0000

    Add package pypigeon/0.2.15/pypigeon-0.2.15-py3-none-any.whl

---
+++ packages/pypigeon/pypigeon-0.2.15-py3-none-any.whl/pypigeon/pigeon_core/models/item_status_additional_property.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ItemStatusAdditionalProperty(str, Enum):
+    FAILED = "failed"
+    NOT_APPLICABLE = "not-applicable"
+    OK = "ok"
+    PENDING = "pending"
+    SERVER_ERROR = "server-error"
+
+    def __str__(self) -> str:
+        return str(self.value)
