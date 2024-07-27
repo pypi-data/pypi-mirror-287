43819b4f7419bf419a4f54310e342e0051cb13ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:05:07 +0000

    Add package danila-lib/2.3.4/danila-lib-2.3.4.tar.gz

---
+++ packages/danila-lib/danila-lib-2.3.4.tar.gz/danila-lib-2.3.4/data/neuro/prods.py
@@ -0,0 +1,9 @@
+from data.result import Rama_prod
+
+PRODS = {Rama_prod.Rama_Prod.altai:'1923',
+         Rama_prod.Rama_Prod.balakovo:'1487',
+         Rama_prod.Rama_Prod.begickaya:'12',
+         Rama_prod.Rama_Prod.promlit:'33',
+         Rama_prod.Rama_Prod.ruzhimmash:'1275',
+         Rama_prod.Rama_Prod.tihvin:'1378',
+         Rama_prod.Rama_Prod.uralvagon:'5'}
