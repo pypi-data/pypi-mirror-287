af80c87a9a5381b9017e4101b97fb4909a5893d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:31 +0000

    Add package dynax/0.0.1/dynax-0.0.1.tar.gz

---
+++ packages/dynax/dynax-0.0.1.tar.gz/dynax-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='dynax',
+    version='0.0.1',
+    py_modules=['dynax'],
+    # description='',
+    author='Muchen Sun',
+    author_email='sunmch15@gmail.com',
+    # url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+    python_requires='>=3.8',
+)
