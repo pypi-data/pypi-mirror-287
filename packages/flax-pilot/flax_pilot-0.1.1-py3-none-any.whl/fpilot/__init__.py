c8102489edb49328e8853cd012043a00dd4cf7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 14:04:31 +0000

    Add package flax-pilot/0.1.1/flax_pilot-0.1.1-py3-none-any.whl

---
+++ packages/flax-pilot/flax_pilot-0.1.1-py3-none-any.whl/fpilot/__init__.py
@@ -0,0 +1,6 @@
+from training.trainer import Trainer
+import trackers.basic_trackers as BasicTrackers
+import checkpoints.checkpoint as SaveLoad
+
+
+__all__ = ['Trainer', 'BasicTrackers', 'SaveLoad']
---
+++ packages/flax-pilot/flax_pilot-0.1.1-py3-none-any.whl/fpilot/checkpoints/__init__.py
@@ -0,0 +1,4 @@
+import orbax.checkpoint as ocp
+from ..common import replicate, unreplicate
+
+__all__ = ['ocp', 'replicate', 'unreplicate']
