7b8dd43b6ec70e8da182da52ec5574dc2b22c4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:03:56 +0000

    Add package cloudpss-hydrogen/4.0.0a14/cloudpss_hydrogen-4.0.0a14-py3-none-any.whl

---
+++ packages/cloudpss-hydrogen/cloudpss_hydrogen-4.0.0a14-py3-none-any.whl/cloudpss/utils/graphqlUtil.py
@@ -0,0 +1,10 @@
+# coding=UTF-8
+import json
+from cloudpss.utils import request
+
+
+def graphql_request(query, variables=None):
+    payload = {'query': query, 'variables': variables}
+    r = request('POST', 'graphql', data=json.dumps(payload))
+
+    return json.loads(r.text)
