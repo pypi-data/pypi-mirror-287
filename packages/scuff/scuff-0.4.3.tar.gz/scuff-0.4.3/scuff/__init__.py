e9660dca6c71b02265e69afa3721b92468973417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 15:03:59 +0000

    Add package scuff/0.4.3/scuff-0.4.3.tar.gz

---
+++ packages/scuff/scuff-0.4.3.tar.gz/scuff-0.4.3/scuff/__init__.py
@@ -0,0 +1,36 @@
+'''
+Scuff:
+    A data serialization language and transpiler suite written in Python.
+'''
+
+
+__title__ = 'scuff'
+__description__ = "A slick config file format for humans."
+__url__ = "https://github.com/akyuute/scuff"
+__version__ = '0.4.3'
+__author__ = "akyuute"
+__license__ = 'MIT'
+__copyright__ = "Copyright (c) 2023-present akyuute"
+
+
+ENDMARKER = ''
+NEWLINE = '\n'
+KEYWORDS = ()
+
+
+from .tools import (
+    to_json,
+    to_py,
+    parse,
+    unparse,
+    py_to_scuff,
+    dump,
+)
+from .parser import (
+    FileParser,
+    RecursiveDescentParser,
+    PyParser,
+    Unparser,
+)
+from .compiler import Compiler
+
