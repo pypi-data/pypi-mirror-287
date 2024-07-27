ce48139e2ca43bd32aced892b4ce6311236d8862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:03:44 +0000

    Add package helloworldplayerpython/0.0.1/helloworldplayerpython-0.0.1-py3-none-any.whl

---
+++ packages/helloworldplayerpython/helloworldplayerpython-0.0.1-py3-none-any.whl/helloworldplayerpython/player.py
@@ -0,0 +1,26 @@
+"""
+Este es el modulo que incluye la clase
+de reporductor de música
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproductor de
+    música
+    """
+
+    def play(self, song):
+        """
+        Reproduce la canción que recibió como parámetro
+
+        Parameters:
+        song (str): este es un string con el path de la canción
+
+        Returns:
+        int: devuelve 1 si reproduce con éxito, en el caso de fracaso devuelve 0
+        """
+        print("reporducinedo canción")
+
+    def stop(self):
+        print("stopping")
