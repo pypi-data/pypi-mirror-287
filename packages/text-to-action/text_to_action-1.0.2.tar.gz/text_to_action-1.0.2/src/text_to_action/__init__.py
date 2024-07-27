43fe97f6622c0a60d018baec4faf2a2ebfe33ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 14:03:47 +0000

    Add package text-to-action/1.0.2/text_to_action-1.0.2.tar.gz

---
+++ packages/text-to-action/text_to_action-1.0.2.tar.gz/text_to_action-1.0.2/src/text_to_action/__init__.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+# Load environment variables from the .env file located in the project root
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+# Construct the path to the action_embeddings directory
+EMBEDDINGS_DIR = os.path.join(os.path.dirname(__file__), "action_embeddings")
+ACTIONS_DIR = os.path.join(os.path.dirname(__file__), "actions")
+from .action_dispatcher import ActionDispatcher
