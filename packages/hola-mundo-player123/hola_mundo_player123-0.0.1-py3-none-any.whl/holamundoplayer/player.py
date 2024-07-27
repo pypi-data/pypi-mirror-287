1d92b1d0f4ac31c97888e124f93d69b488651c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:03:23 +0000

    Add package hola-mundo-player123/0.0.1/hola_mundo_player123-0.0.1-py3-none-any.whl

---
+++ packages/hola-mundo-player123/hola_mundo_player123-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,25 @@
+""""
+Esta es el modulo que incluye la clase de 
+reproductor de musica
+"""
+
+
+class Player:
+    """"
+    Esta clase crea un reproductor de muscia
+    """
+
+    def play(self, song):
+        """"
+        Reproduce la cancion que reciibo como parametro
+
+        Parameteres:
+        song(str): es e sun string con el path de la cancion
+
+        Returns:
+        int: devuelve 1 si reproduce con exito de lo contrario 0
+        """
+        print("Reproduciendo cancion")
+
+    def stop(self):
+        print("Stopping")
