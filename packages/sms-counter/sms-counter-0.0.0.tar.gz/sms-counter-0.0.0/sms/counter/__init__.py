9bd32c3637dd4ee5d66ec4c558771ead7c98d047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:03:43 +0000

    Add package sms-counter/0.0.0/sms-counter-0.0.0.tar.gz

---
+++ packages/sms-counter/sms-counter-0.0.0.tar.gz/sms-counter-0.0.0/sms/counter/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__version__ = '0.0.0';
+
+from .sms_counter import SMSCounter;
+
+from .sms_counter import GSM_7BIT, SEGMENT_LEN_GSM_7BIT, SEGMENT_LEN_GSM_7BIT_MULTIPART;
+from .sms_counter import GSM_7BIT_EX, SEGMENT_LEN_GSM_7BIT_EX, SEGMENT_LEN_GSM_7BIT_EX_MULTIPART;
+from .sms_counter import UTF16, SEGMENT_LEN_UTF16, SEGMENT_LEN_UTF16_MULTIPART;
+from .sms_counter import MAP_CHARS_7BIT, MAP_CHARS_7BIT_EX;
