2f68dc893a72f8d0210bd934ee4b8ce8afe56f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:03:04 +0000

    Add package ansys-motorcad-core/0.5.4/ansys_motorcad_core-0.5.4-py3-none-any.whl

---
+++ packages/ansys-motorcad-core/ansys_motorcad_core-0.5.4-py3-none-any.whl/ansys/motorcad/core/methods/rpc_methods_lab.py
@@ -0,0 +1,115 @@
+"""RPC methods for Motor-CAD Lab."""
+
+
+class _RpcMethodsLab:
+    def __init__(self, mc_connection):
+        self.connection = mc_connection
+
+    def calculate_test_performance_lab(self):
+        """Calculate the test performance.
+
+        Results are saved in the MOT file results folder as ``MotorLAB_caldata.mat``.
+        """
+        method = "CalculateTestPerformance_Lab"
+        return self.connection.send_and_receive(method)
+
+    def export_duty_cycle_lab(self):
+        """Export the calculated duty cycle data to the thermal model."""
+        method = "ExportDutyCycle_Lab"
+        return self.connection.send_and_receive(method)
+
+    def get_model_built_lab(self):
+        """Test if the Lab model must be built or rebuilt before running calculations.
+
+        Returns
+        -------
+        bool
+            ``True`` if the Lab model has been built and is valid for the current settings,
+            ``False`` otherwise.
+        """
+        method = "GetModelBuilt_Lab"
+        return self.connection.send_and_receive(method)
+
+    def show_results_viewer_lab(self, calculation_type):
+        """Load the results viewer for the specified Lab calculation type.
+
+        Parameters
+        ----------
+        calculation_type : str
+            Type of calculation. Options are ``"Electromagnetic"``, ``"Thermal"``,
+            ``"Generator"``, ``"Duty Cycle"``, and ``"Calibration"``.
+        """
+        method = "ShowResultsViewer_Lab"
+        params = [calculation_type]
+        return self.connection.send_and_receive(method, params)
+
+    def export_figure_lab(self, calculation_type, variable, file_name):
+        """Export an image of the Lab results graph.
+
+        Parameters
+        ----------
+        calculation_type : str
+            Type of calculation. Options are ``"Electromagnetic"``, ``"Thermal"``,
+            ``"Generator"``, ``"Duty Cycle"``, and ``"Calibration"``.
+        variable : str
+           Variable to plot on the Y axis (2D graphs) or Z axis (3D graphs). For
+           example, ``"Shaft Torque"``.
+        file_name : str
+            Name of the image file.
+        """
+        method = "ExportFigure_Lab"
+        params = [calculation_type, variable, file_name]
+        return self.connection.send_and_receive(method, params)
+
+    def calculate_generator_lab(self):
+        """Calculate generator performance.
+
+        Results are saved in the MOT file results folder as ``LabResults_Generator.mat``.
+        """
+        method = "CalculateGenerator_Lab"
+        return self.connection.send_and_receive(method)
+
+    def load_external_model_lab(self, file_path):
+        """Load an external model data file.
+
+        This parameter is used when the Lab link type is set to ``Custom`` or ``Ansys Maxwell``.
+
+        Parameters
+        ----------
+        file_path : str
+            Full path to the data file, including the file name. Use the ``r'filepath'``
+            syntax to force Python to ignore special characters.
+        """
+        method = "LoadExternalModel_Lab"
+        params = [file_path]
+        return self.connection.send_and_receive(method, params)
+
+    def clear_model_build_lab(self):
+        """Clear the Lab model build."""
+        method = "ClearModelBuild_Lab"
+        return self.connection.send_and_receive(method)
+
+    def build_model_lab(self):
+        """Build the Lab model."""
+        method = "BuildModel_Lab"
+        return self.connection.send_and_receive(method)
+
+    def calculate_operating_point_lab(self):
+        """Run the Lab operating point calculation."""
+        method = "CalculateOperatingPoint_Lab"
+        return self.connection.send_and_receive(method)
+
+    def calculate_magnetic_lab(self):
+        """Run the Lab magnetic calculation."""
+        method = "CalculateMagnetic_Lab"
+        return self.connection.send_and_receive(method)
+
+    def calculate_thermal_lab(self):
+        """Run the Lab thermal calculation."""
+        method = "CalculateThermal_Lab"
+        return self.connection.send_and_receive(method)
+
+    def calculate_duty_cycle_lab(self):
+        """Run the Lab duty cycle."""
+        method = "CalculateDutyCycle_Lab"
+        return self.connection.send_and_receive(method)
---
+++ packages/ansys-motorcad-core/ansys_motorcad_core-0.5.4-py3-none-any.whl/ansys/motorcad/core/methods/rpc_methods_materials.py
@@ -0,0 +1,141 @@
+"""RPC methods for materials."""
+
+
+class _RpcMethodsMaterials:
+    def __init__(self, mc_connection):
+        self.connection = mc_connection
+
+    def set_fluid(self, cooling_type, fluid):
+        """Set the fluid for a cooling type.
+
+        Parameters
+        ----------
+        cooling_type : str
+            Type of the cooling. Options are ``"InternalFluid"``,
+            ``"ExternalFluid"``, ``"ShaftSGFluid"``, ``"RotorWJFluid"``,
+            ``"SlotWJFluid"``, ``"HousingWJFluid"``, ``"WetRotorFluid"``,
+            ``"SprayCoolingFluid"``, ``"TVentFluid"``,
+            ``"Spray_RadialHousing_Fluid"``, ``"Spray_RadialRotor_Fluid"``,
+            ``"Spray_RadialHousing_Fluid_F"``, ``"Spray_RadialHousing_Fluid_R"``,
+            ``"Spray_RadialRotor_Fluid_F"`` and ``"Spray_RadialRotor_Fluid_R"``.
+        fluid :
+        """
+        method = "SetFluid"
+        params = [cooling_type, fluid]
+        return self.connection.send_and_receive(method, params)
+
+    def set_component_material(self, component_name, material_name):
+        """Set the solid material properties of the component from the materials database.
+
+        Parameters
+        ----------
+        component_name : str
+            Component name. In Motor-CAD, you can select **Input Data -> Materials**
+            and view the **Component** column to see component names.
+        material_name : str
+            Name of the solid material.
+        """
+        method = "SetComponentMaterial"
+        params = [component_name, material_name]
+        return self.connection.send_and_receive(method, params)
+
+    def get_component_material(self, component_name):
+        """Get the solid material name of the component.
+
+        Parameters
+        ----------
+        component_name : str
+            Component name. In Motor-CAD, you can select **Input Data -> Materials**
+            and view the **Component** column to see component names.
+        """
+        method = "GetComponentMaterial"
+        params = [component_name]
+        return self.connection.send_and_receive(method, params)
+
+    def import_solid_material(self, file_name, material_name):
+        """Import the solid material from the materials database.
+
+        Parameters
+        ----------
+        file_name : str
+            Name of the materials database.
+        material_name : str
+            Name of the solid material.
+        """
+        method = "ImportSolidMaterial"
+        params = [file_name, material_name]
+        return self.connection.send_and_receive(method, params)
+
+    def export_solid_material(self, file_name, material_name):
+        """Export the solid material to the materials database.
+
+        Parameters
+        ----------
+        file_name : str
+            Name of the materials database.
+        material_name : str
+            Name of the solid material.
+        """
+        method = "ExportSolidMaterial"
+        params = [file_name, material_name]
+        return self.connection.send_and_receive(method, params)
+
+    def delete_solid_material(self, material_name):
+        """Delete the solid material from the materials database.
+
+        Parameters
+        ----------
+        material_name : str
+            Name of the solid material.
+        """
+        method = "DeleteSolidMaterial"
+        params = [material_name]
+        return self.connection.send_and_receive(method, params)
+
+    def calculate_iron_loss_coefficients(self, material_name):
+        """Calculate and return the iron loss coefficients for the solid material.
+
+        Parameters
+        ----------
+        material_name : str
+            Name of the solid material.
+        """
+        method = "CalculateIronLossCoefficients"
+        params = [material_name]
+        return self.connection.send_and_receive(method, params)
+
+    def save_iron_loss_coefficients(self, material_name):
+        """Save the calculated iron loss coefficients to the materials database.
+
+        Parameters
+        ----------
+        material_name : str
+            Name of the solid material.
+        """
+        method = "SaveIronLossCoefficients"
+        params = [material_name]
+        return self.connection.send_and_receive(method, params)
+
+    def calculate_magnet_parameters(self, material_name):
+        """Calculate parameters for the nonlinear demagnetization model.
+
+        Parameters
+        ----------
+        material_name : str
+            Name of the solid material.
+        """
+        method = "CalculateMagnetParameters"
+        params = [material_name]
+        return self.connection.send_and_receive(method, params)
+
+    def save_magnet_parameters(self, material_name):
+        """Save the calculated magnet parameters of the solid material to the materials database.
+
+        Parameters
+        ----------
+        material_name : str
+            Name of the solid material.
+        """
+        method = "SaveMagnetParameters"
+        params = [material_name]
+        return self.connection.send_and_receive(method, params)
