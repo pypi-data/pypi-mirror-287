5e6e65a4fcaffa5accc850f3367b44986836aa70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:03:30 +0000

    Add package cryptonitbot/0.0.1/cryptonitbot-0.0.1.tar.gz

---
+++ packages/cryptonitbot/cryptonitbot-0.0.1.tar.gz/cryptonitbot-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='cryptonitbot',
+    version='0.0.1',
+    description="A bot for encryption and decryption using Telegram",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="ruslanlap",
+    author_email="your-email@example.com",
+    url="https://github.com/your-username/cryptonit-bot",
+    packages=find_packages(),
+    install_requires=[req.strip() for req in open('cryptonit_bot/requirements.txt')],
+    entry_points={
+        'console_scripts': [
+            'cryptonit-bot=cryptonit_bot.bot:run_bot',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
