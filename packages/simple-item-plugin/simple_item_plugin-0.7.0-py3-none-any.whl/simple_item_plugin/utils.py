7c7d85283eb6fbcb69386f0280c73039cda96c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 15:03:51 +0000

    Add package simple-item-plugin/0.7.0/simple_item_plugin-0.7.0-py3-none-any.whl

---
+++ packages/simple-item-plugin/simple_item_plugin-0.7.0-py3-none-any.whl/simple_item_plugin/utils.py
@@ -0,0 +1,25 @@
+import random
+from beet import Context, Language, Generator
+from simple_item_plugin.types import Lang, TranslatedString, NAMESPACE
+from typing import Union
+
+
+def generate_uuid() -> list[int]:
+    return [
+        random.randint(0, 0xFFFFFFFF),
+        random.randint(0, 0xFFFFFFFF),
+        random.randint(0, 0xFFFFFFFF),
+        random.randint(0, 0xFFFFFFFF),
+    ]
+
+
+def export_translated_string(ctx: Union[Context, Generator], translation: TranslatedString):
+    # create default languages files if they don't exist
+    for lang in Lang:
+        if lang.namespaced not in ctx.assets.languages:
+            ctx.assets.languages[lang.namespaced] = Language({})
+
+    for lang, translate in translation[1].items():
+        ctx.assets.languages[f"{NAMESPACE}:{lang.value}"].data[
+            translation[0]
+        ] = translate
