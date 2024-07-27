8bf3b23e6794e11373d9524f6faa7c532223830a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:10 +0000

    Add package evlatools/0.1.21/evlatools-0.1.21.tar.gz

---
+++ packages/evlatools/evlatools-0.1.21.tar.gz/evlatools-0.1.21/evlatools/comm/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time    : 2024/4/29 17:51
+from evlatools.comm._cache import CacheUtils
+from evlatools.comm._mock import MockUtils
+from evlatools.comm._date import DateUtils
+
+cache = CacheUtils()
+
+
+mock = MockUtils()
+
+
+dateutil = DateUtils()
+
