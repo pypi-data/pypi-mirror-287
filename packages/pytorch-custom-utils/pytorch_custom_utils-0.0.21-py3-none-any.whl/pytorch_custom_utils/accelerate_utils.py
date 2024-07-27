1454c8090d6f8f9089792895a695659f40300b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 14:03:56 +0000

    Add package pytorch-custom-utils/0.0.21/pytorch_custom_utils-0.0.21-py3-none-any.whl

---
+++ packages/pytorch-custom-utils/pytorch_custom_utils-0.0.21-py3-none-any.whl/pytorch_custom_utils/accelerate_utils.py
@@ -0,0 +1,121 @@
+from functools import partial, wraps
+from typing import Optional, Callable
+from contextlib import nullcontext, contextmanager
+
+from torch.nn import Module
+
+from accelerate import Accelerator
+from accelerate.tracking import WandBTracker
+
+# helper functions
+
+def exists(v):
+    return v is not None
+
+@contextmanager
+def combine_contexts(a, b):
+    with a() as c1, b() as c2:
+        yield (c1, c2)
+
+def find_first(cond: Callable, arr):
+    for el in arr:
+        if cond(el):
+            return el
+
+    return None
+
+# adds a context manager for wandb tracking with a specific project and experiment name
+
+def add_wandb_tracker_contextmanager(
+    accelerator_instance_name = 'accelerator',
+    tracker_hps_instance_name = 'tracker_hps'
+):
+    def decorator(klass):
+
+        @contextmanager
+        def wandb_tracking(
+            self,
+            project: str,
+            run: Optional[str] = None,
+            hps: Optional[dict] = None
+        ):
+            maybe_accelerator = getattr(self, accelerator_instance_name, None)
+
+            assert exists(maybe_accelerator) and isinstance(maybe_accelerator, Accelerator), f'Accelerator instance not found at self.{accelerator_instance_name}'
+
+            hps = getattr(self, tracker_hps_instance_name, hps)
+
+            maybe_accelerator.init_trackers(project, config = hps)
+
+            wandb_tracker = find_first(lambda el: isinstance(el, WandBTracker), maybe_accelerator.trackers)
+
+            assert exists(wandb_tracker), 'wandb tracking was not enabled. you need to set `log_with = "wandb"` on your accelerate kwargs'
+
+            if exists(run):
+                assert exists(wandb_tracker)
+                wandb_tracker.run.name = run
+
+            yield
+
+            maybe_accelerator.end_training() 
+
+        if not hasattr(klass, 'wandb_tracking'):
+            klass.wandb_tracking = wandb_tracking
+
+        return klass
+
+    return decorator
+
+# automatically unwrap model when attribute cannot be found on the maybe ddp wrapped main model
+
+class ForwardingWrapper:
+  def __init__(self, parent, child):
+    self.parent = parent
+    self.child = child
+
+  def __getattr__(self, key):
+    if hasattr(self.parent, key):
+      return getattr(self.parent, key)
+
+    return getattr(self.child, key)
+
+  def __call__(self, *args, **kwargs):
+    call_fn = self.__getattr__('__call__')
+    return call_fn(*args, **kwargs)
+
+def auto_unwrap_model(
+    accelerator_instance_name = 'accelerator',
+    model_instance_name = 'model'
+):
+    def decorator(klass):
+        _orig_init = klass.__init__
+
+        @wraps(_orig_init)
+        def __init__(self, *args, **kwargs):
+            _orig_init(self, *args, **kwargs)
+            model = getattr(self, model_instance_name)
+            accelerator = getattr(self, accelerator_instance_name)
+
+            assert isinstance(accelerator, Accelerator)
+            forward_wrapped_model = ForwardingWrapper(model, accelerator.unwrap_model(model))
+            setattr(self, model_instance_name, forward_wrapped_model)
+
+        klass.__init__ = __init__
+        return klass
+
+    return decorator
+
+# gradient accumulation context manager
+# for no_sync context on all but the last iteration
+
+def model_forward_contexts(
+    accelerator: Accelerator,
+    model: Module,
+    grad_accum_steps: int = 1
+):
+    for i in range(grad_accum_steps):
+        is_last_step = i == grad_accum_steps - 1
+
+        maybe_no_sync = partial(accelerator.no_sync, model) if not is_last_step else nullcontext
+
+        yield partial(combine_contexts, accelerator.autocast, maybe_no_sync)
---
+++ packages/pytorch-custom-utils/pytorch_custom_utils-0.0.21-py3-none-any.whl/pytorch_custom_utils/module_device.py
@@ -0,0 +1,86 @@
+from functools import wraps
+from typing import List
+
+import torch
+from torch import is_tensor
+from torch.nn import Module
+from torch.utils._pytree import tree_flatten, tree_unflatten
+
+# provides a .device for your model
+# uses a dummy scalar tensor
+
+def module_device(
+    device_property_name = 'device'
+):
+    def decorator(klass):
+        assert issubclass(klass, Module), 'should decorate a subclass of torch.nn.Module'
+
+        _orig_init = klass.__init__
+
+        @wraps(_orig_init)
+        def __init__(self, *args, **kwargs):
+            _orig_init(self, *args, **kwargs)
+
+            self.register_buffer('_dummy', torch.tensor(0), persistent = False)
+
+        @property
+        def _device_property(self):
+            return self._dummy.device
+
+        klass.__init__ = __init__
+        setattr(klass, device_property_name, _device_property)
+        return klass
+
+    return decorator
+
+# a decorator that automatically casts all tensors coming into .forward to the proper device
+
+def autocast_device(
+    methods: List[str] = ['forward']
+):
+    def decorator(klass):
+        assert issubclass(klass, Module), 'should decorate a subclass of torch.nn.Module'
+
+        orig_fns = [getattr(klass, method) for method in methods]
+
+        for method, orig_fn in zip(methods, orig_fns):
+
+            @wraps(orig_fn)
+            def fn(self, *args, **kwargs):
+
+                # determine device
+                # use dummy from decorator above
+                # otherwise look for parameters and use the device on that
+
+                if hasattr(self, '_dummy'):
+                    device = self._dummy.device
+                else:
+                    device = next(self.parameters()).device
+
+                # flatten
+
+                flattened_args, tree_spec = tree_flatten([args, kwargs])
+
+                # transform args
+
+                maybe_transformed_args = []
+
+                for flattened_arg in flattened_args:
+                    if is_tensor(flattened_arg):
+                        flattened_arg = flattened_arg.to(device)
+
+                    maybe_transformed_args.append(flattened_arg)
+
+                # unflatten
+
+                args, kwargs = tree_unflatten(maybe_transformed_args, tree_spec)
+
+                # call original fn
+
+                orig_fn(self, *args, **kwargs)
+
+            setattr(klass, method, fn)
+
+        return klass
+
+    return decorator
