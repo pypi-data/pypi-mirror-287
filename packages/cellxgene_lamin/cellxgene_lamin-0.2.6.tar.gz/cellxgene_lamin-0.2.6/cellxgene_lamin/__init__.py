d609096ff1b9f01e7dffbf18b75e83f88411e82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:49 +0000

    Add package cellxgene_lamin/0.2.6/cellxgene_lamin-0.2.6.tar.gz

---
+++ packages/cellxgene_lamin/cellxgene_lamin-0.2.6.tar.gz/cellxgene_lamin-0.2.6/cellxgene_lamin/__init__.py
@@ -0,0 +1,22 @@
+"""Manage cellxgene metadata using LaminDB.
+
+Import the package::
+
+   import cellxgene_lamin
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   Curate
+   CellxGeneFields
+   datasets
+"""
+
+__version__ = "0.2.6"  # denote a pre-release for 0.1.0 with 0.1rc1
+
+from . import datasets
+from ._curate import Curate
+from ._cxg_rest import get_collections_from_cxg, get_datasets_from_cxg
+from ._fields import CellxGeneFields
---
+++ packages/cellxgene_lamin/cellxgene_lamin-0.2.6.tar.gz/cellxgene_lamin-0.2.6/noxfile.py
@@ -0,0 +1,71 @@
+import shutil
+from pathlib import Path
+
+import nox
+from laminci import upload_docs_artifact
+from laminci.nox import build_docs, login_testuser1, run_pre_commit
+
+nox.options.default_venv_backend = "none"
+
+GROUPS = {
+    "census": ["query-census.ipynb"],
+    "validator": ["cellxgene.ipynb", "cellxgene-curate.ipynb"],
+}
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session
+@nox.parametrize(
+    "group",
+    ["census", "validator", "docs"],
+)
+def install(session: nox.Session, group: str) -> None:
+    extra = ""
+    session.run(*"uv pip install --system scipy>=1.12.0,<1.13.0rc1".split())
+    if group == "census":
+        extra = ",jupyter,aws"
+        session.run(*"uv pip install --system tiledbsoma".split())
+    elif group == "validator":
+        extra = ",jupyter,aws,zarr"
+        session.run(*"uv pip install --system cellxgene-schema==5.0.2".split())
+    session.run(*"uv pip install --system .[dev]".split())
+    session.run(
+        "uv",
+        "pip",
+        "install",
+        "--system",
+        f"lamindb[bionty{extra}] @ git+https://github.com/laminlabs/lamindb@release",
+    )
+
+
+@nox.session
+@nox.parametrize(
+    "group",
+    ["census", "validator"],
+)
+@nox.session
+def build(session, group):
+    login_testuser1(session)
+    session.run(*f"pytest -s ./tests/test_notebooks.py::test_{group}".split())
+
+    # Move executed notebooks temporarily to recover them for docs building
+    target_dir = Path(f"./docs_{group}")
+    target_dir.mkdir(exist_ok=True)
+    for filename in GROUPS[group]:
+        shutil.copy(Path("docs") / filename, target_dir / filename)
+
+
+@nox.session
+def docs(session):
+    # Recover executed notebooks
+    for group in ["census", "validator"]:
+        for path in Path(f"./docs_{group}").glob("*"):
+            path.rename(f"./docs/{path.name}")
+
+    session.run(*"lamin init --storage ./docsbuild --schema bionty".split())
+    build_docs(session, strict=True)
+    upload_docs_artifact(aws=True)
