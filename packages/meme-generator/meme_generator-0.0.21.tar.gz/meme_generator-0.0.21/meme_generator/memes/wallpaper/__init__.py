0a370fdaf639e28dc79b296cc7dea5090c5b7860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 03:04:05 +0000

    Add package meme-generator/0.0.21/meme_generator-0.0.21.tar.gz

---
+++ packages/meme-generator/meme_generator-0.0.21.tar.gz/meme_generator-0.0.21/meme_generator/memes/wallpaper/__init__.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from PIL.Image import Image as IMG
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.utils import save_gif
+
+img_dir = Path(__file__).parent / "images"
+
+
+def wallpaper(images: list[BuildImage], texts, args):
+    img = images[0].convert("RGBA").resize((515, 383), keep_ratio=True)
+    frames: list[IMG] = []
+    for i in range(8):
+        frames.append(BuildImage.open(img_dir / f"{i}.png").image)
+    for i in range(8, 20):
+        frame = BuildImage.open(img_dir / f"{i}.png")
+        frame.paste(img, (176, -9), below=True)
+        frames.append(frame.image)
+    return save_gif(frames, 0.07)
+
+
+add_meme("wallpaper", wallpaper, min_images=1, max_images=1, keywords=["墙纸"])
