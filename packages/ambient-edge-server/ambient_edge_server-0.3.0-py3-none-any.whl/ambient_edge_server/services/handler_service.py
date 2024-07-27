7b04b9a1b66dab936eea98890a8459a17f80d87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 14:04:23 +0000

    Add package ambient-edge-server/0.3.0/ambient_edge_server-0.3.0-py3-none-any.whl

---
+++ packages/ambient-edge-server/ambient_edge_server-0.3.0-py3-none-any.whl/ambient_edge_server/services/handler_service.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from docker import DockerClient
+
+from ambient_edge_server.event_handlers.authorize_to_registry_handler import (
+    AuthorizeRegistryHandler,
+)
+from ambient_edge_server.event_handlers.base_handler import BaseHandler
+from ambient_edge_server.event_handlers.deploy_service_handler import (
+    DeployServiceHandler,
+)
+from ambient_edge_server.event_handlers.test_handler import TestHandler
+from ambient_edge_server.services.event_service import EventService
+from ambient_edge_server.services.registry_service import RegistryServiceFactory
+
+
+class HandlerService:
+    def __init__(
+        self,
+        event_service: EventService,
+        docker_client: DockerClient,
+        registry_svc_factory: RegistryServiceFactory,
+    ) -> None:
+        self._handlers: List[BaseHandler] = [
+            TestHandler(event_service),
+            DeployServiceHandler(event_service, docker_client=docker_client),
+            AuthorizeRegistryHandler(
+                event_svc=event_service, registry_svc_factory=registry_svc_factory
+            ),
+        ]
+
+    async def start(self):
+        for handler in self._handlers:
+            await handler.subscribe()
+
+    def get_handlers(self):
+        return self._handlers
