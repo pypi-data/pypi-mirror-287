1d0eebffa968afab8cfc8340a41a22334e6b8d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 15:04:22 +0000

    Add package daniel-fav-distributions/1.0/daniel_fav_distributions-1.0.tar.gz

---
+++ packages/daniel-fav-distributions/daniel_fav_distributions-1.0.tar.gz/daniel_fav_distributions-1.0/daniel_fav_distributions/example.py
@@ -0,0 +1,4 @@
+from gaussiandistribution import Gaussian
+
+gaussian_one = Gaussian(22, 2)
+print(gaussian_one.mean)
---
+++ packages/daniel-fav-distributions/daniel_fav_distributions-1.0.tar.gz/daniel_fav_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="daniel_fav_distributions", 
+ version="1.0",
+ description="Gaussian and Binomial distributions", 
+ packages=['daniel_fav_distributions'],
+ author = "Daniel de la Riva Massaad",
+ author_email = "danieldelariva97@gmail.com",
+ zip_safe=False)
