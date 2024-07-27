702c1b3f59018a04b2cb1630eabb48add8c4a5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:03:33 +0000

    Add package dynamic-valuation/1.999000000000009000000000000000009999990000009999909090909090999900099990009999000090099900009999900000099999900009999099990009999990009990009990009/dynamic_valuation-1.999000000000009000000000000000009999990000009999909090909090999900099990009999000090099900009999900000099999900009999099990009999990009990009990009.tar.gz

---
+++ packages/dynamic-valuation/dynamic_valuation-1.999000000000009000000000000000009999990000009999909090909090999900099990009999000090099900009999900000099999900009999099990009999990009990009990009.tar.gz/dynamic_valuation-1.999000000000009000000000000000009999990000009999909090909090999900099990009999000090099900009999900000099999900009999099990009999990009990009990009/dynamic_valuation/series_approx.py
@@ -0,0 +1,14 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+def series_vapprox(cl,p)
+#sGrid = np.linspace(cl.s_min, cl.s_max, cl.n)
+A = np.array([[cl.δ * s**j - sdot(s) * j * s**(j - 1) for j in range(p + 1)] for s in cl.s_grid])
+coeffs = np.linalg.solve(A.T @ A, A.T @ cl.W(cl.s_grid))
+
+V_approx = np.sum([coeffs[i] * s**i for i in range(p + 1)], axis=0)
+
+plt.plot(s, y_approx, 'r--', lw=1, label=f"Least Squares Collocation: {p+1} Monomials on {Uniform Nodes")
+plt.legend()
+plt.show()
+
---
+++ packages/dynamic-valuation/dynamic_valuation-1.999000000000009000000000000000009999990000009999909090909090999900099990009999000090099900009999900000099999900009999099990009999990009990009990009.tar.gz/dynamic_valuation-1.999000000000009000000000000000009999990000009999909090909090999900099990009999000090099900009999900000099999900009999099990009999990009990009990009/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dynamic_valuation",
+    version="1.999000000000009000000000000000009999990000009999909090909090999900099990009999000090099900009999900000099999900009999099990009999990009990009990009",
+    author="Eric Larson",
+    author_email="ericl3@illinois.edu",
+    description="Find the present value of a capital asset stocks and/or\
+    flows subject to an equation of motion.",
+    packages=['dynamic_valuation'],
+    install_requires=["numpy","matplotlib","scipy","pandas"]
+)
