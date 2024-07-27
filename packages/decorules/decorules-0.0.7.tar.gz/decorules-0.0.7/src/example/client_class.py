c3ac80cb60e112a55a0bd4ecd6304cd7982fbcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:03:37 +0000

    Add package decorules/0.0.7/decorules-0.0.7.tar.gz

---
+++ packages/decorules/decorules-0.0.7.tar.gz/decorules-0.0.7/src/example/client_class.py
@@ -0,0 +1,121 @@
+import types
+import sys
+from library_class import LibraryClass
+from decorules.decorators import raise_if_false_on_class, raise_if_false_on_instance, enforces_instance_rules
+
+is_multiplier_larger_than_1 = lambda x: x.MULTIPLIER > 1.0
+smallest_coordinate_times_multiplier_larger_than_largest = lambda x: min(x.coordinates) * x.MULTIPLIER > max(
+    x.coordinates)
+
+
+@raise_if_false_on_instance(smallest_coordinate_times_multiplier_larger_than_largest,
+                            ValueError,
+                            "Checks that the multiplier times the smallest coordinate is larger than the largest coordinate")
+@raise_if_false_on_class(is_multiplier_larger_than_1,
+                         AttributeError,
+                         "Checks that the multiplier is larger than 2.0")
+class ClientClass(LibraryClass):
+    MULTIPLIER = 2.5
+
+    @enforces_instance_rules
+    def append(self, value=0.0):
+        self.coordinates.append(value)
+
+
+class SomeBaseClass:
+    def foo(self):
+        print(f"Executing method {sys._getframe().f_code.co_name}")
+        pass
+
+
+class MultipleInheritanceClass(SomeBaseClass, ClientClass):
+    @enforces_instance_rules
+    def multiply_multiplier(self, value=1.0):
+        self.MULTIPLIER = self.MULTIPLIER * value
+
+
+class SomeOtherClass(SomeBaseClass):
+    @enforces_instance_rules
+    def foo(self):
+        print(f"Executing method {sys._getframe().f_code.co_name}")
+        pass
+
+class LayerClass1(ClientClass):
+    pass
+
+class LayerClass2(LayerClass1):
+    pass
+
+class LayerClass3(LayerClass2):
+    pass
+
+class LayerClass4(LayerClass3):
+    pass
+
+
+
+def main():
+    a = LibraryClass("a")
+    a.testme()
+    b = LibraryClass("b", 0.2, 0.5)
+    b.testme()
+    try:
+        c = LibraryClass("c", -2.0, 0.5)
+        c.testme()
+    except ValueError as ve:
+        print(ve)
+        pass
+    d = ClientClass("d", 0.1, 0.1, 0.05)
+    d.testme()
+    try:
+        e = ClientClass("e", 0.1, 0.1, 0.025)
+        e.testme()
+    except ValueError as ve:
+        print(ve)
+        pass
+    try:
+        f = ClientClass("f", 0.8, 0.8, 0.4)
+        f.testme()
+    except ValueError as ve:
+        print(ve)
+        pass
+    g = ClientClass("g", 0.1, 0.1, 0.05)
+    g.testme()
+    g.append(0.06)  # should not break an instance rule
+    g.testme()
+    try:
+        g.append(max(g.coordinates) * ClientClass.MULTIPLIER)  # should break
+        g.testme()
+    except ValueError as ve:
+        print(ve)
+        pass
+    h = MultipleInheritanceClass("h", 0.1, 0.1, 0.05)
+    h.multiply_multiplier(1.0)
+    h.testme()
+    h.multiply_multiplier(10.0)
+    h.testme()
+    try:
+        h.multiply_multiplier(0.001)
+        h.testme()
+    except ValueError as ve:
+        print(ve)
+        pass
+    i = SomeOtherClass()
+    try:
+        i.foo()
+    except TypeError as te:
+        print(te)
+        pass
+
+    j = LayerClass4("j", 0.1, 0.1, 0.05)
+    j.append(0.06)  # should not break an instance rule#
+    print(j.coordinates)
+    try:
+        j.append(max(j.coordinates) * LayerClass4.MULTIPLIER)  # should break
+    except ValueError as ve:
+        print(ve)
+        pass
+    print(j.coordinates)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/decorules/decorules-0.0.7.tar.gz/decorules-0.0.7/tests/test_decs.py
@@ -0,0 +1,182 @@
+import pytest
+import types
+import sys
+from functools import partial
+from collections import Counter
+from collections.abc import Iterable
+# make sure to add to python path!
+from decorules.has_enforced_rules import HasEnforcedRules
+from decorules.decorators import raise_if_false_on_class, raise_if_false_on_instance, enforces_instance_rules
+from decorules.predicates import key_type_enforcer, min_value, min_list_type_counter
+
+
+def test_class_type_wrong_fails_1():
+    with pytest.raises(TypeError):
+        check_method_present = partial(key_type_enforcer,
+                                       enforced_type=types.FunctionType,
+                                       enforced_key='foo')
+
+        @raise_if_false_on_class(check_method_present, AttributeError, 'Checks if foo method exists')
+        class NotRightTypeClass:
+            def foo(self):
+                print(f"Executing method {sys._getframe().f_code.co_name}")
+
+
+def test_class_method_ok_1():
+    check_method_present = partial(key_type_enforcer,
+                                   enforced_type=types.FunctionType,
+                                   enforced_key='library_functionality')
+    try:
+        @raise_if_false_on_class(check_method_present, AttributeError)
+        class HasCorrectMethodClass(metaclass=HasEnforcedRules):
+            def library_functionality(self):
+                return 1
+    except AttributeError as ex:
+        pytest.fail(f"Failed {sys._getframe().f_code.co_name} with AttributeError {str(ex)}")
+
+
+def test_class_method_fails_1():
+    with pytest.raises(AttributeError):
+        check_method_present = partial(key_type_enforcer,
+                                       enforced_type=types.FunctionType,
+                                       enforced_key='library_functionality')
+
+        @raise_if_false_on_class(check_method_present, AttributeError)
+        class MissingCorrectMethodClass(metaclass=HasEnforcedRules):
+            pass
+
+
+def test_class_attribute_ok_2():
+    geq_type_dict = {str: 1, int: 2, float: 1}
+    static_member_name = "STATIC_SET"
+    check_has_enough_of_type = partial(min_list_type_counter,
+                                       list_name=static_member_name,
+                                       min_counter=Counter(geq_type_dict))
+    try:
+        @raise_if_false_on_class(check_has_enough_of_type,
+                                 AttributeError,
+                                 f"f{static_member_name} must have these or more {geq_type_dict}")
+        class HasSufficientStaticSetClass(metaclass=HasEnforcedRules):
+            STATIC_SET = ("Test", 10, 40, 50, 45.5, 60.0, '3', 'i', BaseException())
+            pass
+    except AttributeError as ex:
+        pytest.fail(f"Failed {sys._getframe().f_code.co_name} with AttributeError {str(ex)}")
+
+
+def test_class_method_fails_2():
+    geq_type_dict = {str: 1, int: 2, float: 1}
+    with pytest.raises(AttributeError):
+        @raise_if_false_on_class(partial(min_list_type_counter,
+                                         list_name='STATIC_LIST',
+                                         min_counter=Counter({str: 1, int: 2, float: 1})),
+                                 AttributeError,
+                                 f"STATIC_LIST member must have these or more {geq_type_dict}")
+        class HasInsufficientStaticSetClass(metaclass=HasEnforcedRules):
+            STATIC_LIST = ("Test", 10, 40, 50, '3', 'i', BaseException())
+            pass
+
+
+def test_instance_method_ok_1():
+    try:
+        @raise_if_false_on_instance(partial(key_type_enforcer,
+                                            enforced_type=int,
+                                            enforced_key='x'),
+                                    AttributeError)
+        class HasCorrectInstanceMemberVariable(metaclass=HasEnforcedRules):
+            def __init__(self, value=20):
+                self.x = value
+
+        a = HasCorrectInstanceMemberVariable()
+    except AttributeError as ex:
+        pytest.fail(f"Failed {sys._getframe().f_code.co_name} with AttributeError {str(ex)}")
+    except BaseException as ex:
+        pytest.fail(f"Failed {sys._getframe().f_code.co_name} with Exception {type(ex)} {str(ex)}")
+
+
+def test_instance_method_fails_1():
+    with pytest.raises(AttributeError):
+        @raise_if_false_on_instance(partial(key_type_enforcer,
+                                            enforced_type=int,
+                                            enforced_key='x'),
+                                    AttributeError)
+        class HasIncorrectInstanceMemberVariable(metaclass=HasEnforcedRules):
+            def __init__(self, value=20):
+                self.y = value
+
+        a = HasIncorrectInstanceMemberVariable()
+
+
+def test_instance_method_fails_2():
+    with pytest.raises(AttributeError):
+        @raise_if_false_on_instance(partial(key_type_enforcer, enforced_type=int, enforced_key='x'), AttributeError)
+        class HasDeletedInstanceMemberVariable(metaclass=HasEnforcedRules):
+            def __init__(self, value=20):
+                self.x = value
+                del self.x
+
+        a = HasDeletedInstanceMemberVariable()
+
+def test_instance_on_method_ok_1():
+    @raise_if_false_on_instance(partial(key_type_enforcer,
+                                        enforced_type=int,
+                                        enforced_key='y'), AttributeError)
+    @raise_if_false_on_instance(lambda x: x.y < 10, ValueError)
+    class HasMethodCheckedOKAfterCall(metaclass=HasEnforcedRules):
+        def __init__(self, value=20):
+            self.y = value
+
+        @enforces_instance_rules
+        def add(self, value=0):
+            self.y += value
+
+    a = HasMethodCheckedOKAfterCall(0)
+    a.add(1)
+    a.add(1)
+    a.add(1)
+
+
+def test_instance_on_method_fails_1():
+    @raise_if_false_on_instance(partial(key_type_enforcer,
+                                        enforced_type=int,
+                                        enforced_key='y'), AttributeError)
+    @raise_if_false_on_instance(lambda x: x.y<10, ValueError)
+    class HasMethodCheckedAndFailsAfterCall(metaclass=HasEnforcedRules):
+        def __init__(self, value=20):
+            self.y = value
+        @enforces_instance_rules
+        def add(self, value=0):
+            self.y += value
+
+    a = HasMethodCheckedAndFailsAfterCall(0)
+    a.add(1)
+    a.add(1)
+    a.add(1)
+    with pytest.raises(ValueError):
+        a.add(10)
+
+def test_instance_on_method_ok_2():
+    @raise_if_false_on_instance(partial(key_type_enforcer,
+                                        enforced_type=int,
+                                        enforced_key='y'), AttributeError)
+    @raise_if_false_on_instance(lambda x: x.y < 10, ValueError)
+    class HasMethodCheckedOKAfterCall2(metaclass=HasEnforcedRules):
+        def __init__(self, value=20):
+            self.y = value
+
+        @enforces_instance_rules
+        def add(self, value=0):
+            self.y += value
+
+    a = HasMethodCheckedOKAfterCall2(0)
+    a.add(1)
+    a.add(1)
+    a.add(1)
+    a.add(1)
+    a.add(-10)
+    a.add(1)
+    a.add(10)
+
+
+
+
+
