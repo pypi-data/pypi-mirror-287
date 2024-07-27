1205c23623f31b07effd84730ac1e7bfb5f639b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:04:13 +0000

    Add package subdomain-takeover-tools/0.41.0/subdomain_takeover_tools-0.41.0-py3-none-any.whl

---
+++ packages/subdomain-takeover-tools/subdomain_takeover_tools-0.41.0-py3-none-any.whl/subdomain_takeover_tools/helper/prepare.py
@@ -0,0 +1,31 @@
+import re
+
+import dns.resolver
+
+
+def prepare_domain_name(host_in):
+    host = re.sub(r'https?://', '', host_in)
+    host = re.sub(r'/.*$', '', host)
+    host = re.sub(r'\.$', '', host)
+    return host
+
+
+def process_subtake_output(is_valid, line, check, inverse, strict):
+    (parts, domain) = line.split('\t\t')
+    if ': ]' in parts:
+        target = ''
+    else:
+        (_, target) = parts[1:-2].split(': ')
+    check(is_valid, domain, target, inverse, strict)
+
+
+def resolve_cname(hostname):
+    try:
+        cnames = dns.resolver.resolve(hostname, 'CNAME')
+
+        if len(cnames) == 0:
+            raise ResourceWarning("CNAME not found")
+        else:
+            return prepare_domain_name(str(cnames[0]))
+    except dns.resolver.NXDOMAIN:
+        return None
