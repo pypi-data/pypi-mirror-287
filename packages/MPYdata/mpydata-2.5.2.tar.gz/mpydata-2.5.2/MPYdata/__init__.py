a87cbd6517c4cac971fc39a7e8c071e12ea979dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:03:04 +0000

    Add package MPYdata/2.5.2/mpydata-2.5.2.tar.gz

---
+++ packages/MPYdata/mpydata-2.5.2.tar.gz/mpydata-2.5.2/MPYdata/__init__.py
@@ -0,0 +1,8 @@
+#MPYdata
+#Mahendra Sai Phaneeswar Yerramsetti
+#https://github.com/MahendraYerramsetti/PYdata
+
+from .MPYdata import MPYdata
+
+__all__ = ['MPYdata']
+
---
+++ packages/MPYdata/mpydata-2.5.2.tar.gz/mpydata-2.5.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name="MPYdata",
+    version="2.5.2",
+    author="Mahendra Sai Phaneeswar Yerramsetti",
+    author_email="mahendrayerramsetti@gmail.com",
+    description="""A Python Package for saving data in database.""",
+    url="https://github.com/MahendraYerramsetti/PYdata",
+    long_description="""A Python Package for saving data in database.
+    it uses simple syntaxes to perform advanced queries in python.
+    To use this package, simply import it, to kow how to use it please 
+    visit https://github.com/MahendraYerramsetti/PYdata.
+    where you could know about codes and more.""",
+    project_urls={
+        "Bug Tracker": "https://github.com/MahendraYerramsetti/PYdata/issues",
+        "Source Code": "https://github.com/MahendraYerramsetti/PYdata",
+        "Documentation": "https://github.com/MahendraYerramsetti/PYdata",
+        "Website": "https://github.com/MahendraYerramsetti/PYdata",
+    },
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
