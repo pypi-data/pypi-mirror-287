0e45810f2d5161b95b4eb91b78c6963f267a51c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:49 +0000

    Add package rotarysaw/666.32.1979.0/rotarysaw-666.32.1979.0.tar.gz

---
+++ packages/rotarysaw/rotarysaw-666.32.1979.0.tar.gz/rotarysaw-666.32.1979.0/rotarysaw/gl.py
@@ -0,0 +1,8 @@
+
+
+import OpenGL
+import OpenGL.GL as gl        # python wrapping of OpenGL
+from OpenGL.GL import shaders
+
+OpenGL.FULL_LOGGING = True
+OpenGL.ERROR_ON_COPY = True
---
+++ packages/rotarysaw/rotarysaw-666.32.1979.0.tar.gz/rotarysaw-666.32.1979.0/rotarysaw/pyqt6network.py
@@ -0,0 +1,12 @@
+from PyQt6.QtNetwork import QNetworkDatagram, QNetworkRequest, QNetworkReply, QNetworkProxy, QNetworkProxyFactory
+from PyQt6.QtNetwork import QNetworkProxyQuery, QNetworkCacheMetaData, QNetworkAddressEntry, QNetworkCookieJar, QAbstractNetworkCache
+from PyQt6.QtNetwork import QNetworkInterface, QNetworkDiskCache, QNetworkInformation, QNetworkAccessManager, QNetworkCookie
+from PyQt6.QtNetwork import QSsl, QSslSocket, QSslError, QSslEllipticCurve, QSslCertificateExtension
+from PyQt6.QtNetwork import QSslDiffieHellmanParameters, QSslCertificate, QSslConfiguration, QSslPreSharedKeyAuthenticator, QSslCipher
+from PyQt6.QtNetwork import QSslKey, QSslServer, QDnsMailExchangeRecord, QDnsTextRecord, QDnsServiceRecord
+from PyQt6.QtNetwork import QDnsHostAddressRecord, QDnsLookup, QDnsDomainNameRecord, QTcpSocket, QAbstractSocket
+from PyQt6.QtNetwork import QUdpSocket, QLocalSocket, QHttpMultiPart, QHttp1Configuration, QHttp2Configuration
+from PyQt6.QtNetwork import QHttpPart, QHostAddress, QHostInfo, QLocalServer, QTcpServer
+from PyQt6.QtNetwork import QOcspResponse, QOcspRevocationReason, QOcspCertificateStatus, QAuthenticator, QHstsPolicy
+from PyQt6.QtNetwork import QPasswordDigestor
+from PyQt6.QtWebSockets import QWebSocket, QWebSocketServer, QWebSocketHandshakeOptions, QWebSocketProtocol, QWebSocketCorsAuthenticator, QMaskGenerator
