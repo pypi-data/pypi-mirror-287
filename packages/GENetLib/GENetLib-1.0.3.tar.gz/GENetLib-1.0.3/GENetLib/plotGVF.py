8eb34df14c30f4999638988ed5b31c206d801409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:03:15 +0000

    Add package GENetLib/1.0.3/GENetLib-1.0.3.tar.gz

---
+++ packages/GENetLib/GENetLib-1.0.3.tar.gz/GENetLib-1.0.3/GENetLib/plotGVF.py
@@ -0,0 +1,10 @@
+from plotFD import plotFD
+
+
+def plotGVF(x, y = None, xlab = None, ylab = None):
+    gvf = x
+    if y == None:
+        plotFD(gvf, xlab, ylab)
+    else:
+        plotFD(gvf, y, xlab, ylab)
+
