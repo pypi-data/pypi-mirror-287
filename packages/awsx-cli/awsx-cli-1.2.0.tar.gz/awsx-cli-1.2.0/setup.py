2960fa5bdc4663a8c52cea753d6e327e1255e0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:59 +0000

    Add package awsx-cli/1.2.0/awsx-cli-1.2.0.tar.gz

---
+++ packages/awsx-cli/awsx-cli-1.2.0.tar.gz/awsx-cli-1.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name='awsx-cli',
+    version='1.2.0',
+    author='Mert Öngengil, Mehmet Eraslan, Engin Can Höke',
+    description='AWS credential management tool',
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    url='https://github.com/mertongngl/awsx',
+    py_modules=['awsx'],
+    install_requires=[
+        'argparse'
+    ],
+    entry_points={
+        'console_scripts': [
+            'awsx = awsx:main',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
