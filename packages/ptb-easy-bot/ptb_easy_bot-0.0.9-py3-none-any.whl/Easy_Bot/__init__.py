197e48e9a7848c00af95c7e5d4c19106476ab1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 04:03:52 +0000

    Add package ptb-easy-bot/0.0.9/ptb_easy_bot-0.0.9-py3-none-any.whl

---
+++ packages/ptb-easy-bot/ptb_easy_bot-0.0.9-py3-none-any.whl/Easy_Bot/__init__.py
@@ -0,0 +1,13 @@
+from ._easyBot import Client, COMMANDS , MESSAGE_HANDLER ,CALLBACKS , Filters 
+from ._keyboard import InlineReplyMarkup
+
+
+__author__ = "@pamod_madubashana"
+__all__ = (
+    "Client",
+    "COMMANDS",
+    "MESSAGE_HANDLER",
+    "CALLBACKS",
+    "Filters",
+    "InlineReplyMarkup",
+)
