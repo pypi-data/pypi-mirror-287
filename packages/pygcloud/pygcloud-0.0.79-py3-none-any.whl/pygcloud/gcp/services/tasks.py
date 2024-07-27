0fd59ee0c8ee1993729d5ea036bfd30d2d085d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:17 +0000

    Add package pygcloud/0.0.79/pygcloud-0.0.79-py3-none-any.whl

---
+++ packages/pygcloud/pygcloud-0.0.79-py3-none-any.whl/pygcloud/gcp/services/tasks.py
@@ -0,0 +1,45 @@
+"""
+Cloud Tasks
+
+https://cloud.google.com/sdk/gcloud/reference/tasks
+
+@author: jldupont
+"""
+
+from pygcloud.models import Params, GCPServiceUpdatable
+from pygcloud.gcp.models import TaskQueue
+from pygcloud.gcp.services.iam import IAMBindingCapableMixin
+
+
+class TasksQueues(GCPServiceUpdatable, IAMBindingCapableMixin):
+
+    LISTING_CAPABLE = True
+    LISTING_REQUIRES_LOCATION = True
+    DEPENDS_ON_API = "cloudtasks.googleapis.com"
+    REQUIRES_UPDATE_AFTER_CREATE = False
+    LISTING_REQUIRES_LOCATION = True
+    SPEC_CLASS = TaskQueue
+    GROUP = ["tasks", "queues"]
+
+    def __init__(
+        self,
+        name: str,
+        location: str,
+        params_create: Params = [],
+        params_update: Params = []
+    ):
+        super().__init__(name=name, ns="queues")
+        self._params_create = params_create
+        self._params_update = params_update
+        self.location = location
+
+    def params_describe(self):
+        return ["describe", self.name, "--format", "json", "--location", self.location]
+
+    def params_create(self):
+        return ["create", self.name, "--format", "json", "--location", self.location] \
+            + self._params_create
+
+    def params_update(self):
+        return ["update", self.name, "--format", "json", "--location", self.location] \
+            + self._params_update
