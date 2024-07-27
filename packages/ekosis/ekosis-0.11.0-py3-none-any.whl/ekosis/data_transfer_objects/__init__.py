7ad91f12faa084624e38dbd2049f42f3f2d48d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 05:04:19 +0000

    Add package ekosis/0.11.0/ekosis-0.11.0-py3-none-any.whl

---
+++ packages/ekosis/ekosis-0.11.0-py3-none-any.whl/ekosis/data_transfer_objects/__init__.py
@@ -0,0 +1,7 @@
+from .empty import EmptyDto
+
+from .json_protocol import RequestDTO
+from .json_protocol import ResponseDTO
+
+from .queued_endpoint_response import QueuedEndpointResponseDTO
+from .statistics import StatsRequestDto, StatsResponseDto
