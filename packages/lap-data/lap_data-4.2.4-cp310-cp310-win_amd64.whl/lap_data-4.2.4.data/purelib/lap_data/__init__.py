393cd6581909162db6077984dd8a659dab86ba99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:34 +0000

    Add package lap-data/4.2.4/lap_data-4.2.4-cp310-cp310-win_amd64.whl

---
+++ packages/lap-data/lap_data-4.2.4-cp310-cp310-win_amd64.whl/lap_data-4.2.4.data/purelib/lap_data/__init__.py
@@ -0,0 +1,7 @@
+version_info = (4, 2, 4)
+
+__version__ = '.'.join(map(str, version_info))
+__title__ = 'Laser Aided Profiler Data Access Library'
+__date__ = "26.7.2024"
+
+from lap_data.lcmodel import LCModel
