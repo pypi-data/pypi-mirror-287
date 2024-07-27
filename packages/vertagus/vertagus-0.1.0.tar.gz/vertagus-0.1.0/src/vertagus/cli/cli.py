9b804ab99c0616e7a6839a4f06ca1be7e31cdbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-27 06:04:25 +0000

    Add package vertagus/0.1.0/vertagus-0.1.0.tar.gz

---
+++ packages/vertagus/vertagus-0.1.0.tar.gz/vertagus-0.1.0/src/vertagus/cli/cli.py
@@ -0,0 +1,28 @@
+import logging
+import os
+
+logging.basicConfig(
+    level=os.environ.get("VERTAGUS_LOG_LEVEL", "INFO"),
+    format="{message}",
+    style="{"
+)
+
+
+import click
+from .commands import (
+    validate_cmd,
+    create_tag_cmd,
+    create_aliases_cmd,
+    list_rules_cmd
+)
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(validate_cmd)
+cli.add_command(create_tag_cmd)
+cli.add_command(create_aliases_cmd)
+cli.add_command(list_rules_cmd)
