cc5c2b847679c8c95072086e4fc4eea7989d4f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:45 +0000

    Add package py-htmlbuilder/0.1.2/py_htmlbuilder-0.1.2.tar.gz

---
+++ packages/py-htmlbuilder/py_htmlbuilder-0.1.2.tar.gz/py_htmlbuilder-0.1.2/py_html/contrib/bootstrap/_types.py
@@ -0,0 +1,28 @@
+import typing as t
+
+BVariants = t.Literal[
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+]
+
+BTextVariant = t.Literal[
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+    "body",
+    "muted",
+    "white",
+    "black-50",
+    "white-50",
+]
