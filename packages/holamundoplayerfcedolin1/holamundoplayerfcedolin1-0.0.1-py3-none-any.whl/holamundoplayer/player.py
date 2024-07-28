9e9c0efb1deeacc771be75aba6adf6d1ace25a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:30 +0000

    Add package holamundoplayerfcedolin1/0.0.1/holamundoplayerfcedolin1-0.0.1-py3-none-any.whl

---
+++ packages/holamundoplayerfcedolin1/holamundoplayerfcedolin1-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,23 @@
+"""
+Esta es la doc de Player
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproduc
+    de música
+    """
+
+    def play(self, song):
+        """
+        Reproduce la canción que
+        recibió en el construct
+
+        Parameters:
+        song (str): Este es un str con el path de la song
+
+        Returns:
+        int: devuelve 1 o 0 en caso de fracaso
+        """
+    print("reproduciendo")
