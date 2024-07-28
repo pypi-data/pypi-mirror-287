cb0be8ce2a2dc4b6a188c3f022cd5283631baf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:03:34 +0000

    Add package Py2NL/0.1.4/Py2NL-0.1.4.tar.gz

---
+++ packages/Py2NL/Py2NL-0.1.4.tar.gz/Py2NL-0.1.4/PyNLI/main.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+from .solara_interface import Page, Board, Chat, get_data_container, View
+
+def NLICall(key: str = None, **kwargs) -> Any:
+    data = get_data_container(key)
+    data.vars.update(kwargs)
+    for varname in kwargs:
+        data.var_descriptions[varname] = 'Passed as parameter'
+
+
+    return View(data)
