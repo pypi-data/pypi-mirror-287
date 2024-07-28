0a0c8d18c6831ff8e1115937be08256430986c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:04:38 +0000

    Add package fs-store-cli/1.1/fs_store_cli-1.1.tar.gz

---
+++ packages/fs-store-cli/fs_store_cli-1.1.tar.gz/fs_store_cli-1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fs_store_cli',
+    version='1.1',
+    license='proprietary',
+    description='A CLI tool to interact with the file server',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'fs-store=fs_store_cli.cli:cli',
+        ],
+    },
+    author='Gajendra Waghmare',
+    author_email='gajendraw1237@gmail.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
