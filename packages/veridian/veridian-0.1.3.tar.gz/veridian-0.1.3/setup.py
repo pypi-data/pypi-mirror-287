e7f5478ce17a47bb5925823469b3e9a27fe279e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:03:35 +0000

    Add package veridian/0.1.3/veridian-0.1.3.tar.gz

---
+++ packages/veridian/veridian-0.1.3.tar.gz/veridian-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read_long_description():
+    with open("README.md", "r", encoding="utf-8") as fh:
+        return fh.read()
+
+
+setup(
+    name="veridian",
+    version="0.1.3",
+    description="A simple SDK to clean tabular data using the Veridian API.",
+    long_description=read_long_description(),
+    long_description_content_type="text/markdown",
+    author="Arav Kumar",
+    author_email="veridiandata@gmail.com",
+    url="https://github.com/A-Ravioli/veridian-api-python-sdk",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
