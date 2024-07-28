ca52993c1f068042cf21fe5ff1ded0f1b28c05ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:04:29 +0000

    Add package ngwidgets/0.16.1/ngwidgets-0.16.1-py3-none-any.whl

---
+++ packages/ngwidgets/ngwidgets-0.16.1-py3-none-any.whl/ngwidgets/log_view.py
@@ -0,0 +1,25 @@
+"""
+Created on 2023-11-15
+
+@author: wf
+"""
+import logging
+
+from nicegui import ui
+
+
+class LogElementHandler(logging.Handler):
+    """A logging handler that emits messages to a log element."""
+
+    def __init__(self, element: ui.log, level: int = logging.NOTSET) -> None:
+        self.element = element
+        super().__init__(level)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        try:
+            msg = self.format(record)
+            self.element.push(msg)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception:
+            self.handleError(record)
