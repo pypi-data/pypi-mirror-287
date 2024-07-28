e04bdaa072a488970c4927f0eaedc343d117e1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:03:43 +0000

    Add package monisha/0.0.86/monisha-0.0.86.tar.gz

---
+++ packages/monisha/monisha-0.0.86.tar.gz/monisha-0.0.86/Monisha/functions/function19.py
@@ -0,0 +1,19 @@
+from ..scripts import Numeo
+from decimal import Decimal, ROUND_DOWN
+#==================================================================================
+
+def views(sizes):
+    if not sizes.isdigit() or sizes == 0 or sizes < 0:
+        return sizes
+    indeu = 0
+    power = 1000
+    sizes = int(sizes)
+    while sizes >= power and indeu < len(Numeo.DATA01) - 1:
+        sizes /= power
+        indeu += 1
+    sized = float(Decimal(sizes).quantize(Decimal('0.01'), rounding=ROUND_DOWN))
+    sizeo = str(int(sized)) if sized.is_integer() else str(round(sized, 2))
+    moonu = sizeo + Numeo.DATA01[indeu]
+    return moonu
+
+#==================================================================================
