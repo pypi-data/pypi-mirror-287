9a8ecd747bab7b3edb1d8c6d95f0d3f132004976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:55 +0000

    Add package img2social/0.1.1/img2social-0.1.1.tar.gz

---
+++ packages/img2social/img2social-0.1.1.tar.gz/img2social-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='img2social',
+    version='0.1.1',
+    description='Create engaging images for socials quicker.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='dodo',
+    author_email='dodothedeveloper@gmail.com',
+    url='https://github.com/dodothedeveloper/img2social',
+    packages=find_packages(),  # Automatically find your packages
+    install_requires=[ ],
+    entry_points={
+        'console_scripts': [
+            'img2social=file.py:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
