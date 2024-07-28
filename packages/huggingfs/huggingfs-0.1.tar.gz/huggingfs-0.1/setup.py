34c502791c2dd141fb8d742cbca3525e01aa42af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:51 +0000

    Add package huggingfs/0.1/huggingfs-0.1.tar.gz

---
+++ packages/huggingfs/huggingfs-0.1.tar.gz/huggingfs-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+    name="huggingfs",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "gradio",
+        "huggingface_hub[hf_transfer]"
+    ],
+    entry_points={
+        "console_scripts": [
+            "huggingfs=huggingfs.main:main",
+        ],
+    },
+    description="huggingface downloader web ui",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cocktailpeanut/huggingfs",
+    python_requires=">=3.6",
+)
+
