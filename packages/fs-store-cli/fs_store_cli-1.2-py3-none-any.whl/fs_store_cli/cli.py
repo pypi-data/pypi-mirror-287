dd2c37dc2e26bac4fb9a510140d706c5da124e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:44 +0000

    Add package fs-store-cli/1.2/fs_store_cli-1.2-py3-none-any.whl

---
+++ packages/fs-store-cli/fs_store_cli-1.2-py3-none-any.whl/fs_store_cli/cli.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import json
+
+import click
+import requests
+import os
+
+
+@click.group()
+def cli():
+    pass
+
+
+url = 'http://localhost:8080'
+
+
+@cli.command()
+@click.argument('file_path', type=click.Path(exists=True))
+def upload_file(file_path):
+    """Upload a file to the server."""
+
+    files = {'file': open(file_path, 'rb')}
+    file_name = os.path.basename(file_path)
+    try:
+        response = requests.post(f"{url}/v1/files/{file_name}", files=files)
+        response.raise_for_status()
+        click.echo(f"{file_name} uploaded successfully.")
+    except requests.exceptions.RequestException as e:
+        click.echo(f"Failed to upload file: {e}", err=True)
+
+
+@cli.command()
+def list_files():
+    """Get all file names from the server."""
+    try:
+        response = requests.get(f"{url}/v1/files")
+        response.raise_for_status()
+        for file in json.loads(response.text):
+            click.echo(file)
+    except requests.exceptions.RequestException as e:
+        click.echo(f"Failed to get list of files: {e}", err=True)
+
+
+@cli.command()
+@click.argument('file_name')
+def delete_file(file_name):
+    """Delete a file from the server."""
+
+    try:
+        response = requests.delete(f"{url}/v1/files/{file_name}")
+        response.raise_for_status()
+        click.echo(f"{file_name} deleted successfully.")
+    except requests.exceptions.RequestException as e:
+        click.echo(f"Failed to delete file: {e}", err=True)
+
+
+if __name__ == '__main__':
+    cli()
