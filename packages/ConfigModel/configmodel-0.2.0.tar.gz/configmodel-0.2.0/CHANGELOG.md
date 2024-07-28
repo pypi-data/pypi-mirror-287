6dc5e1649ea40ac236a2f4d29b6b0695dee582e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:39 +0000

    Add package ConfigModel/0.2.0/configmodel-0.2.0.tar.gz

---
+++ packages/ConfigModel/configmodel-0.2.0.tar.gz/configmodel-0.2.0/CHANGELOG.md
@@ -0,0 +1,61 @@
+# CHANGELOG
+
+## v0.2.0 (2024-07-27)
+
+### Chore
+
+* chore(coverage): added Codecov and Code Climate ([`0a585b8`](https://github.com/umanamente/py-configmodel/commit/0a585b8a5b4c4af6451c72367ccc95fb68594b88))
+
+* chore: Send coverage report to Coveralls after all checks completed ([`d8f767d`](https://github.com/umanamente/py-configmodel/commit/d8f767d245bdac1d6a7309814440c5c2bfd8bcdf))
+
+* chore: Skip build setup steps if version was not changed ([`86d2c8d`](https://github.com/umanamente/py-configmodel/commit/86d2c8d58081188d50460da6a11092e8361b2ff2))
+
+* chore: Run tests for multiple python versions ([`8258249`](https://github.com/umanamente/py-configmodel/commit/8258249a1a3f15d241bcbfbfd3bcc8ff20940165))
+
+### Documentation
+
+* docs: readme cleanup ([`8dba323`](https://github.com/umanamente/py-configmodel/commit/8dba3233fd6d07d4ca2397358cb38521c11f200d))
+
+* docs: Update pypi badge color (blue) ([`feb756a`](https://github.com/umanamente/py-configmodel/commit/feb756aed73599efa40859f5a3d4069e01c71ee9))
+
+* docs: Set coveralls badge branch to &#34;master&#34; in README.rst ([`5fcbfb7`](https://github.com/umanamente/py-configmodel/commit/5fcbfb7d5f77f4f493a91cc82089177027f224e6))
+
+### Feature
+
+* feat: support annotated fields without default values ([`f4bf2dd`](https://github.com/umanamente/py-configmodel/commit/f4bf2dd2df320d3ae8453a4353c29de49fdb95be))
+
+### Refactor
+
+* refactor: added tests, improved coverage ([`9dff209`](https://github.com/umanamente/py-configmodel/commit/9dff20937ddacef03ac0b6808c855d40e61203ff))
+
+* refactor: added tests, improved coverage ([`cbd80e7`](https://github.com/umanamente/py-configmodel/commit/cbd80e79a2a11580e3ec89b7f1b8aafec4af3349))
+
+* refactor: using assert instead of unreachable exception ([`11444cb`](https://github.com/umanamente/py-configmodel/commit/11444cbcc2501d1bc0388fe445559178e525261b))
+
+* refactor: replaced unreachable exceptions with asserts ([`b2cdc88`](https://github.com/umanamente/py-configmodel/commit/b2cdc8842a628fe477bcf0e72c387451da118297))
+
+## v0.1.2 (2024-01-16)
+
+### Chore
+
+* chore: Fixed semantic-release condition (don&#39;t attempt to publish to PYPI if version wasn&#39;t changed) ([`dcd5d93`](https://github.com/umanamente/py-configmodel/commit/dcd5d93bd9f5903a7b6d6deefaf81370229a4431))
+
+* chore: Run unit tests with coverage in GH Actions ([`5acc851`](https://github.com/umanamente/py-configmodel/commit/5acc8512bc53ac1d6684665b5847c4ba86c03fcc))
+
+### Fix
+
+* fix: If default values were removed from ini file during execution, write default values again.
+test: Added tests and improved coverage
+docs: Docs theme tweaks ([`05d5de2`](https://github.com/umanamente/py-configmodel/commit/05d5de2b07dce1f342ee07bda924e453477b7458))
+
+## v0.1.1 (2024-01-15)
+
+### Fix
+
+* fix: Changed Changelog format (rst to md) to make it compatible with semantic-release ([`3ad98f0`](https://github.com/umanamente/py-configmodel/commit/3ad98f0d7dd4410e44c5b755ae8082fa9a3f9e0c))
+
+## v0.1.0 (2024-01-15)
+
+### Feature
+
+* feat: First version ([`9a11678`](https://github.com/umanamente/py-configmodel/commit/9a11678a2420a17dd4215210b40acc69e11b39e5))
---
+++ packages/ConfigModel/configmodel-0.2.0.tar.gz/configmodel-0.2.0/tests/test_ConfigModel.py
@@ -0,0 +1,373 @@
+import unittest
+
+__author__ = "Vasily Maslyukov"
+__license__ = "MIT"
+
+from unittest.mock import patch
+
+from configmodel import ConfigModel, config_file, nested_field
+from configmodel.FieldBase import FieldBase
+from configmodel.Logger import Log
+from mock_Serializer import mock_return_path_as_value, MockSerializer
+
+TEST_CONFIG_FILE = "config.mock"
+ANOTHER_CONFIG_FILE = "second_config.mock"
+
+# enable logging
+Log.logging_enabled = True
+
+
+class TestConfigModel(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        # add mock serializer for *.mock extension
+        MockSerializer.register_in_factory()
+
+    def test_forbid_instances_of_static_config(self):
+        """
+        If @config_file decorator is used, ConfigModel should not be instantiated
+        """
+        @config_file(TEST_CONFIG_FILE)
+        class StaticConfig(ConfigModel):
+            product_key = "1234567890"
+
+        with self.assertRaises(Exception):
+            # this should fail
+            StaticConfig()
+
+    def test_allow_instances_of_non_static_config(self):
+        """
+        If @config_file decorator is not used, ConfigModel could be instantiated
+        """
+        class NonStaticConfig(ConfigModel):
+            value1 = "default value"
+
+        instance = NonStaticConfig(TEST_CONFIG_FILE)
+        self.assertEqual("default value", instance.value1)
+
+    def test_create_instances(self):
+        """
+        Create two instances of the same class.
+        Check that they are different objects, and don't share values
+        """
+        class ConfigInstance(ConfigModel):
+            value1 = "1001"
+            value2 = "1002"
+
+            class NestedConfig(ConfigModel):
+                color = "2003"
+                saturation = "2004"
+
+            purple_config = NestedConfig()
+            red_config = NestedConfig()
+
+        # create two instances
+        instance1 = ConfigInstance(TEST_CONFIG_FILE)
+        instance2 = ConfigInstance(ANOTHER_CONFIG_FILE)
+
+        # set different values
+        instance1.purple_config.color = "orange"
+        instance2.purple_config.color = "green"
+
+        # check how many fields are defined in total (including nested)
+        all_fields = instance1._get_all_fields_recursive()
+        self.assertEqual(6, len(all_fields))
+
+        # if a decorator is not used, classes should not act as singletons
+        self.assertIsNone(instance1.NestedConfig._get_instance())
+        self.assertIsNone(instance2.NestedConfig._get_instance())
+
+        # check that instances are different
+        self.assertIsNot(instance1._fields, instance2._fields)
+        self.assertIsNot(instance1.purple_config, instance2.purple_config)
+
+        self.assertNotEqual(instance1.purple_config.color, instance2.purple_config.color)
+
+        # check that static variables are not changed (only instance variables shall be changed)
+        self.assertEqual("2003", ConfigInstance.purple_config.color)
+
+    def test_nested_instance_of_outer_class(self):
+        """
+        Define a nested class outside the main class.
+        Check that it's possible to create an instance of the nested class.
+        """
+        class OuterNestedConfig(ConfigModel):
+            value1 = "1001"
+            value2 = "1002"
+
+        class MainConfig(ConfigModel):
+            value1 = "2001"
+            value2 = "2002"
+            outer_nested_config = OuterNestedConfig()
+
+        instance1 = MainConfig(TEST_CONFIG_FILE)
+        instance2 = MainConfig(ANOTHER_CONFIG_FILE)
+        self.assertEqual(instance1.outer_nested_config.value1, "1001")
+
+        # assign different values
+        instance1.outer_nested_config.value1 = "red value"
+        instance2.outer_nested_config.value1 = "green value"
+
+        # check that instances are different
+        self.assertIsNot(instance1.outer_nested_config, instance2.outer_nested_config)
+        self.assertNotEqual(instance1.outer_nested_config.value1, instance2.outer_nested_config.value1)
+        self.assertEqual("red value", instance1.outer_nested_config.value1)
+        self.assertEqual("green value", instance2.outer_nested_config.value1)
+        # check that static variables are not changed (only instance variables shall be changed)
+        self.assertEqual("1001", MainConfig.outer_nested_config.value1)
+
+    def test_forbid_both_nested_class_decorator_and_instance(self):
+        """
+        If nested class is decorated with @nested_field, it's not allowed to define an instance of the same class.
+        """
+        with self.assertRaises(Exception):
+            @config_file(TEST_CONFIG_FILE)
+            class RootConfig(ConfigModel):
+                # use decorator
+                @nested_field("nested_config")
+                class NestedConfig(ConfigModel):
+                    pass
+                # also define instance (not allowed with decorator)
+                other_instance = NestedConfig()
+
+    def test_initialize_fields_decorated(self):
+        """
+        Check that fields are initialized correctly when using @nested_field decorator
+        """
+        # define config
+        @config_file(TEST_CONFIG_FILE)
+        class StaticConfig(ConfigModel):
+            product_key = "1234567890"
+
+            @nested_field("magenta_config")
+            class NestedConfig(ConfigModel):
+                parameter1 = "default value"
+
+        # an instance of StaticConfig should be created because it's decorated
+        root_instance = StaticConfig._get_instance()
+        self.assertIsNotNone(root_instance)
+
+        all_fields = root_instance._get_all_fields_recursive()
+        self.assertEqual(2, len(all_fields))
+
+        # check paths
+        with patch.object(MockSerializer, "get_value", side_effect=mock_return_path_as_value) as mock_get_value:
+            val = StaticConfig.product_key
+            mock_get_value.assert_called_with(["product_key"])
+            # accessing field using static class, but serializer should receive @nested_field path
+            val = StaticConfig.NestedConfig.parameter1
+            mock_get_value.assert_called_with(["magenta_config", "parameter1"])
+
+    def test_initialize_fields_empty_values(self):
+        """
+        Check that fields are initialized correctly when default values are not specified
+        """
+        # define config
+        @config_file(TEST_CONFIG_FILE)
+        class StaticConfig(ConfigModel):
+            product_key: str
+
+            @nested_field("purple_config")
+            class NestedConfig(ConfigModel):
+                parameter1: str
+                parameter2: int
+                parameter3: str = "default value"
+
+        # an instance of StaticConfig should be created because it's decorated
+        root_instance = StaticConfig._get_instance()
+        self.assertIsNotNone(root_instance)
+
+        all_fields = root_instance._get_all_fields_recursive()
+        self.assertEqual(4, len(all_fields))
+
+        # check paths
+        with patch.object(MockSerializer, "get_value", side_effect=mock_return_path_as_value) as mock_get_value:
+            # accessing field using static class, but serializer should receive @nested_field path
+            val = StaticConfig.NestedConfig.parameter1
+            mock_get_value.assert_called_with(["purple_config", "parameter1"])
+            val = StaticConfig.NestedConfig.parameter2
+            mock_get_value.assert_called_with(["purple_config", "parameter2"])
+
+    def test_paths(self):
+        """
+        Test complex config with multiple levels of nesting
+        """
+        # define config
+        @config_file(TEST_CONFIG_FILE)
+        class StaticConfig(ConfigModel):
+            string_value = "1234567890"
+            integer_value = 1234567890
+            float_value = 1234567890.0
+            boolean_value_true = True
+            boolean_value_false = False
+
+            class ApiKey(ConfigModel):
+                client_id = "8298"
+                secret = "98297821"
+
+            class MultilevelConfig(ConfigModel):
+                root_parameter = "root value"
+                other_parameter = "other value"
+
+                class RedConfig(ConfigModel):
+                    red_parameter = "red value"
+                    pink_parameter = "pink value"
+
+                @nested_field("green_config")
+                class RenamedConfig(ConfigModel):
+                    green_parameter = "green value"
+                    grass_price = 17
+
+        # test get_value
+        with patch.object(MockSerializer, "get_value", side_effect=mock_return_path_as_value) as mock_get_value:
+            val = StaticConfig.string_value
+            mock_get_value.assert_called_with(["string_value"])
+            val = StaticConfig.ApiKey.client_id
+            mock_get_value.assert_called_with(["api_key", "client_id"])
+            val = StaticConfig.MultilevelConfig.RedConfig.red_parameter
+            mock_get_value.assert_called_with(["multilevel_config", "red_config", "red_parameter"])
+            val = StaticConfig.MultilevelConfig.RenamedConfig.green_parameter
+            mock_get_value.assert_called_with(["multilevel_config", "green_config", "green_parameter"])
+
+        # test set_value
+        with patch.object(MockSerializer, "set_value") as mock_set_value:
+            StaticConfig.string_value = "new value"
+            mock_set_value.assert_called_with(["string_value"], "new value")
+            StaticConfig.ApiKey.client_id = "new client id"
+            mock_set_value.assert_called_with(["api_key", "client_id"], "new client id")
+            StaticConfig.MultilevelConfig.RedConfig.red_parameter = "new red value"
+            mock_set_value.assert_called_with(["multilevel_config", "red_config", "red_parameter"], "new red value")
+            StaticConfig.MultilevelConfig.RenamedConfig.green_parameter = "new green value"
+            mock_set_value.assert_called_with(["multilevel_config", "green_config", "green_parameter"], "new green value")
+
+    def test_nested_classes_are_added_to_fields(self):
+        """
+        Check that nested classes are added to fields
+        """
+        @config_file(TEST_CONFIG_FILE)
+        class StaticConfig(ConfigModel):
+            string_value = "1234567890"
+
+            class NestedConfig(ConfigModel):
+                parameter1 = "default value"
+
+        # an instance of StaticConfig should be created because it's decorated
+        root_instance = StaticConfig._get_instance()
+        self.assertIsNotNone(root_instance)
+
+        root_fields = root_instance._fields
+        self.assertIn("NestedConfig", root_fields)
+
+    def test_unsupported_fields(self):
+        """
+        Check that unsupported fields are not allowed
+        """
+        with self.assertRaises(Exception):
+            # this should fail
+            @config_file(TEST_CONFIG_FILE)
+            class UnsupportedDict(ConfigModel):
+                unsupported_field = {"key1": "value1", "key2": "value2"}
+
+        with self.assertRaises(Exception):
+            # this should fail
+            @config_file(TEST_CONFIG_FILE)
+            class UnsupportedList(ConfigModel):
+                unsupported_field = ["value1", "value2"]
+
+        with self.assertRaises(Exception):
+            # this should fail
+            @config_file(TEST_CONFIG_FILE)
+            class UnsupportedSet(ConfigModel):
+                unsupported_field = {"value1", "value2"}
+
+    def test_uninitialized_fields(self):
+        """
+        Check that accessing uninitialized fields doesn't raise an exception
+        """
+        class UninitializedConfig(ConfigModel):
+            parameter1 = "1234"
+
+            class NestedConfig(ConfigModel):
+                parameter2 = "5678"
+
+        # ConfigModel is not initialized yet
+        # assign to uninitialized fields
+        UninitializedConfig.parameter1 = "7777"
+        UninitializedConfig.NestedConfig.parameter2 = "8888"
+
+    def test_access_undefined_properties_1(self):
+        """
+        Check that accessing undefined properties doesn't raise an exception
+        """
+        @config_file(TEST_CONFIG_FILE)
+        class UndefinedConfig(ConfigModel):
+            parameter1 = "1234"
+
+            class NestedConfig(ConfigModel):
+                parameter2 = "5678"
+
+        UndefinedConfig.NestedConfig.undefined_property = "new value"
+
+    def test_access_undefined_properties_2(self):
+        """
+        Check that accessing undefined properties doesn't raise an exception
+        """
+        # config is not initialized yet
+        class UndefinedConfig(ConfigModel):
+            parameter1 = "1234"
+
+            @nested_field("nested_instance")
+            class NestedConfig(ConfigModel):
+                parameter2 = "5678"
+
+        UndefinedConfig.NestedConfig.undefined_property = "new value"
+
+    def test_filename_not_specified(self):
+        """
+        Check that exception is raised if config file is not specified
+        """
+        class UndefinedConfig(ConfigModel):
+            parameter1 = "1234"
+
+        instance = UndefinedConfig()
+        instance.parameter1 = "new value"
+        val = instance.parameter1
+        self.assertEqual("new value", val)
+
+    def test_assign_to_nested_class(self):
+        """
+        Check that assigning to nested class raises an exception
+        """
+        @config_file(TEST_CONFIG_FILE)
+        class UndefinedConfig(ConfigModel):
+            parameter1 = "1234"
+
+            class NestedConfig(ConfigModel):
+                parameter2 = "5678"
+
+        with self.assertRaises(Exception):
+            UndefinedConfig.NestedConfig = "new value"
+
+    def test_fields_base(self):
+        """
+        Check that fields are initialized correctly
+        """
+        @config_file(TEST_CONFIG_FILE)
+        class RootConfig(ConfigModel):
+            generic_value = FieldBase("renamed_generic_value", "111")
+
+            class NestedConfig(ConfigModel):
+                nested_value = FieldBase("renamed_nested_value", "222")
+
+        # check that serializer receives correct paths
+        with patch.object(MockSerializer, "get_value", side_effect=mock_return_path_as_value) as mock_get_value:
+            val = RootConfig.generic_value
+            mock_get_value.assert_called_with(["renamed_generic_value"])
+            val = RootConfig.NestedConfig.nested_value
+            mock_get_value.assert_called_with(["nested_config", "renamed_nested_value"])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
