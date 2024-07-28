ea9992a74f5f3e5be3f75cbd39f2f0bf72b2116e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:37 +0000

    Add package presentpy/1.1.0/presentpy-1.1.0.tar.gz

---
+++ packages/presentpy/presentpy-1.1.0.tar.gz/presentpy-1.1.0/src/presentpy/__main__.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from typing import Any, List, Tuple
+
+import click
+import nbformat
+
+from presentpy.code_slide_source import CodeSlideSource
+from presentpy.namespaces import Namespaces
+from presentpy.writer.presentation import Presentation
+from presentpy.writer.theme import Theme
+
+odf_namespaces = {
+    "dom": "http://www.w3.org/2001/xml-events",
+    "draw": "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0",
+    "fo": "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0",
+    "office": "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
+    "presentation": "urn:oasis:names:tc:opendocument:xmlns:presentation:1.0",
+    "script": "urn:oasis:names:tc:opendocument:xmlns:script:1.0",
+    "smil": "urn:oasis:names:tc:opendocument:xmlns:smil-compatible:1.0",
+    "style": "urn:oasis:names:tc:opendocument:xmlns:style:1.0",
+    "svg": "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0",
+    "table": "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
+    "text": "urn:oasis:names:tc:opendocument:xmlns:text:1.0",
+    "xlink": "http://www.w3.org/1999/xlink",
+}
+
+
+@click.command()
+@click.argument("notebook", type=click.Path(exists=True))
+@click.option(
+    "--output",
+    default=".",
+    help="Directory or file path where the output ODP file will be saved. Defaults to the current directory.",
+)
+@click.option(
+    "--theme",
+    default="default",
+    help="Pygments style to be applied to the presentation. Defaults to 'default'. See https://pygments.org/docs/styles/ for available styles.",
+)
+def process(notebook, output, theme):
+    """
+    A CLI tool to convert Jupyter Notebooks to slides.
+    """
+    namespaces = Namespaces(odf_namespaces)
+    theme = Theme(theme, namespaces)
+    presentation = Presentation(theme, namespaces)
+    output = Path(output) / f"{Path(notebook).stem}.odp" if Path(output).is_dir() else Path(output)
+
+    with open(notebook) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    for cell in nb.cells:
+        if cell.cell_type == "code":
+            slode = CodeSlideSource.from_code_cell(cell)
+            presentation.add_source_code(slode)
+
+    presentation.write(output)
---
+++ packages/presentpy/presentpy-1.1.0.tar.gz/presentpy-1.1.0/src/presentpy/templates/styles.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+from presentpy.constants import *
+from presentpy.namespaces import Namespaces
+from presentpy.templates.xml_file import XMLFile
+from presentpy.writer.theme import Theme
+
+
+class Styles(XMLFile):
+
+    def __init__(self, path: Path, namespaces: Namespaces, theme: Theme):
+        super().__init__(path, namespaces)
+        self.theme = theme
+
+        page_layout_properties = self.xpath(
+            "office:document-styles",
+            "office:styles",
+            "style:page-layout[@style:name='pageLayout1']",
+            "style:page-layout-properties",
+        )
+        page_layout_properties.set(namespaces("fo:page-width"), f"{self.theme.width:.2f}in")
+        page_layout_properties.set(namespaces("fo:page-height"), f"{self.theme.height:.2f}in")
+
+        drawing_page_properties = self.xpath(
+            "office:document-styles",
+            "office:styles",
+            "style:style[@style:name='masterStyle']",
+            "style:drawing-page-properties",
+        )
+        drawing_page_properties.set(namespaces("draw:fill-color"), self.theme.background_color)
+
+        master_title_style = self.xpath(
+            "office:document-styles",
+            "office:styles",
+            "style:style[@style:name='masterTitle']",
+        )
+        text_properties = master_title_style.xpath("style:text-properties", namespaces=namespaces.data)[0]
+        text_properties.set(namespaces("fo:color"), self.theme.title_color)
+        text_properties.set(namespaces("fo:font-size"), self.theme.font_size(48))
+        text_properties.set(namespaces("style:font-size-asian"), self.theme.font_size(48))
+        text_properties.set(namespaces("style:font-size-complex"), self.theme.font_size(48))
+
+        master_content_style = self.xpath(
+            "office:document-styles",
+            "office:styles",
+            "style:style[@style:name='masterContent']",
+        )
+        text_properties = master_content_style.xpath("style:text-properties", namespaces=namespaces.data)[0]
+        text_properties.set(namespaces("fo:color"), self.theme.content_color)
+        text_properties.set(namespaces("fo:font-size"), self.theme.font_size(18))
+        text_properties.set(namespaces("style:font-size-asian"), self.theme.font_size(18))
+        text_properties.set(namespaces("style:font-size-complex"), self.theme.font_size(18))
+
+        default_slide_properties = self.xpath(
+            "office:document-styles",
+            "office:styles",
+            f"style:style[@style:name='{DRAWING_PAGE_STYLE_NAME}']",
+            "style:drawing-page-properties",
+        )
+        default_slide_properties.set(namespaces("draw:fill-color"), self.theme.background_color)
+
+        highlighted_code_paragraph_style = self.xpath(
+            "office:document-styles",
+            "office:styles",
+            f"style:style[@style:name='{CODE_HIGHLIGHT_PARAGRAPH_STYLE_NAME}']",
+            "style:text-properties",
+        )
+        highlighted_code_paragraph_style.set(namespaces("fo:background-color"), self.theme.highlight_color)
+        highlighted_code_paragraph_style.set(namespaces("fo:font-weight"), "bold")
