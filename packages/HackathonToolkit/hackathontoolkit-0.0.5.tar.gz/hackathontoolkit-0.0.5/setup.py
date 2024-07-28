b46c2be1fd3a5601629fbab860233864472e741a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:48 +0000

    Add package HackathonToolkit/0.0.5/hackathontoolkit-0.0.5.tar.gz

---
+++ packages/HackathonToolkit/hackathontoolkit-0.0.5.tar.gz/hackathontoolkit-0.0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+]
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('CHANGELOG.txt', 'r', encoding='utf-8') as f:
+    long_description += '\n\n' + f.read()
+
+setup(
+  name='HackathonToolkit',
+  version='0.0.5',
+  description='The Hackathon Toolkit simplifies hackathon projects with utilities for setup, API interaction, data handling, ML, visualization, collaboration, and deployment, automating tasks for innovation focus.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',  
+  url='', 
+  author='Moutasim Qazi',
+  author_email='moutasimqazi@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='hackathon , toolkit , python , library', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
