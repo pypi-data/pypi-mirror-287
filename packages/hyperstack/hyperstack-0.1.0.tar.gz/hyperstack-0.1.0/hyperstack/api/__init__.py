d2b6910c6344725091c0b3c00b4024175b3941db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:44 +0000

    Add package hyperstack/0.1.0/hyperstack-0.1.0.tar.gz

---
+++ packages/hyperstack/hyperstack-0.1.0.tar.gz/hyperstack-0.1.0/hyperstack/api/__init__.py
@@ -0,0 +1,7 @@
+from . import virtual_machines
+from . import volumes
+from . import profiles
+from . import regions
+from . import environments
+
+__all__ = ['virtual_machines', 'network', 'volumes', 'profiles', 'regions', 'environments', 'stock', 'flavors', 'regions']
---
+++ packages/hyperstack/hyperstack-0.1.0.tar.gz/hyperstack-0.1.0/hyperstack/instance.py
@@ -0,0 +1,3 @@
+from .client import Hyperstack
+
+hyperstack = Hyperstack()
