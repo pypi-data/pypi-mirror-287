b41b9efd85f71bb6fa1b4d93c3dcc4bf482ca1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:50 +0000

    Add package the-datagarden-models/1.1.3/the_datagarden_models-1.1.3-py3-none-any.whl

---
+++ packages/the-datagarden-models/the_datagarden_models-1.1.3-py3-none-any.whl/datagarden_models/models/economics/base_economics.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+
+
+class EconomicBaseKeys:
+	VALUE = "value"
+	UNIT = "unit"
+	CURRENCY = "currency"
+
+
+class EconomicBaseLegends:
+	VALUE = "value in units of given currency"
+	UNIT = "units for value"
+	CURRENCY = "currency for value"
+
+
+L = EconomicBaseLegends
+
+
+class EconomicsValue(BaseModel):
+	value: float | None = Field(default=None, description=L.VALUE)
+	unit: int = Field(default=1, description=L.UNIT)
+	currency: str = Field(default="EUR", description=L.CURRENCY)
