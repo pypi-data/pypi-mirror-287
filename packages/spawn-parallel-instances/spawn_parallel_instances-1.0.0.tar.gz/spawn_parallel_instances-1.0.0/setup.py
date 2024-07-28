eaf2e4cf491a657cfeb39a9e36f89c3d1ed85578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:37 +0000

    Add package spawn-parallel-instances/1.0.0/spawn_parallel_instances-1.0.0.tar.gz

---
+++ packages/spawn-parallel-instances/spawn_parallel_instances-1.0.0.tar.gz/spawn_parallel_instances-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+setup(
+    name="spawn_parallel_instances",
+    version="1.0.0",
+    description="A package to run multiple instance of the same script.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/stenzr/concurrent_executor",
+    author="Rohit Kumar",
+    author_email="rkrohitkr01@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'concurrent_executor=concurrent_executor.__main__:main',
+        ],
+    },
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    extras_require={
+        'test': ['unittest', 'coverage'],
+    },
+)
