d4b7f9d724e2cb6e8d51a49d53c770949a53b42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:40 +0000

    Add package DataCentricKMeans/0.6/DataCentricKMeans-0.6.tar.gz

---
+++ packages/DataCentricKMeans/DataCentricKMeans-0.6.tar.gz/DataCentricKMeans-0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DataCentricKMeans',
+    version='0.6', 
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # Gereken diğer paketler
+    ],
+    package_data={
+        'DataCentricKMeans': ['DataCentricKMeans_universal.out','DataCentricKMeans_linux.out','DataCentricKMeans_windows.exe'],
+    },
+    entry_points={
+        'console_scripts': [
+            
+        ],
+    },
+    author="Vasfi Tataroglu, Parichit Sharma, Hasan Kurban, and Mehmet M. Dalkilic",
+    author_email="vtatarog@iu.edu",
+    description="A package for running DataCentricKMeans.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/brainvs/DataCentricKMeansPython",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
