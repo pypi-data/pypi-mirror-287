0afdc10bdee0452dc2c80b78326cfdea82cbf614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:03:45 +0000

    Add package slidge-whatsapp/0.2.0a0/slidge_whatsapp-0.2.0a0.tar.gz

---
+++ packages/slidge-whatsapp/slidge_whatsapp-0.2.0a0.tar.gz/slidge_whatsapp-0.2.0a0/slidge_whatsapp/__init__.py
@@ -0,0 +1,11 @@
+"""
+WhatsApp gateway using the multi-device API.
+"""
+
+from slidge.util.util import get_version  # noqa: F401
+
+from . import command, config, contact, group, session
+from .gateway import Gateway
+
+__version__ = "0.2.0alpha0"
+__all__ = "Gateway", "session", "command", "contact", "config", "group"
