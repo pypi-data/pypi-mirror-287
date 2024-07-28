7705c3ec24ee594f783644c1d7919b3ab39e92e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:37 +0000

    Add package lupin2/0.0.34/lupin2-0.0.34-py3-none-any.whl

---
+++ packages/lupin2/lupin2-0.0.34-py3-none-any.whl/lupin2/__init__.py
@@ -0,0 +1,13 @@
+from .tools import *
+from .plt import *
+from .ml import *
+
+#
+# import sys
+# import subprocess
+#
+# def install_package(package_name):
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+#
+# # Example usage:
+# install_package('pandas')
