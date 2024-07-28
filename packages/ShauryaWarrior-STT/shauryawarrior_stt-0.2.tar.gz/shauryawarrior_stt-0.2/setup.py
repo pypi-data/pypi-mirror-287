1340735d8e816ba2c2b5ccb7abe00be6332c5edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:46 +0000

    Add package ShauryaWarrior-STT/0.2/shauryawarrior_stt-0.2.tar.gz

---
+++ packages/ShauryaWarrior-STT/shauryawarrior_stt-0.2.tar.gz/shauryawarrior_stt-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='ShauryaWarrior-STT',
+    version='0.2',
+    author='ShauryaWarrior',
+    author_email='minecraft1212121212121213@gmail.com',
+    description='A Python package for Speech-to-Text conversion',
+)
+packages = find_packages(),
+install_requirements = [
+    'selenium',
+    'webdriver_manager',
+]
+
