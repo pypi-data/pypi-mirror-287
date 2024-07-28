d02a7d22a817780d0b3a0f9f6c211a55e7102d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:47 +0000

    Add package cupy-xarray/0.1.4/cupy_xarray-0.1.4-py3-none-any.whl

---
+++ packages/cupy-xarray/cupy_xarray-0.1.4-py3-none-any.whl/cupy_xarray/tests/test_accessors.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pytest
+import xarray as xr
+
+import cupy_xarray  # noqa: F401
+
+try:
+    import dask.array
+
+    dask_array_type = dask.array.Array
+except ImportError:
+    dask_array_type = None
+
+
+@pytest.fixture
+def tutorial_ds_air():
+    return xr.tutorial.load_dataset("air_temperature")
+
+
+@pytest.fixture
+def tutorial_da_air(tutorial_ds_air):
+    return tutorial_ds_air.air
+
+
+@pytest.fixture
+def tutorial_ds_air_dask():
+    return xr.tutorial.open_dataset("air_temperature", chunks={"lat": 25, "lon": 25, "time": -1})
+
+
+@pytest.fixture
+def tutorial_da_air_dask(tutorial_ds_air_dask):
+    return tutorial_ds_air_dask.air
+
+
+def test_data_set_accessor(tutorial_ds_air):
+    ds = tutorial_ds_air
+    assert hasattr(ds, "cupy")
+    assert not ds.cupy.is_cupy
+
+    ds = ds.as_cupy()
+    assert ds.cupy.is_cupy
+
+    ds = ds.cupy.as_numpy()
+    assert not ds.cupy.is_cupy
+
+
+def test_data_array_accessor(tutorial_da_air):
+    da = tutorial_da_air
+    assert hasattr(da, "cupy")
+    assert not da.cupy.is_cupy
+
+    da = da.as_cupy()
+    assert da.cupy.is_cupy
+
+    garr = da.cupy.get()
+    assert isinstance(garr, np.ndarray)
+
+    da = da.cupy.as_numpy()
+    assert not da.cupy.is_cupy
+
+
+def test_data_array_accessor_dask(tutorial_da_air_dask):
+    da = tutorial_da_air_dask
+    assert hasattr(da, "cupy")
+    assert not da.cupy.is_cupy
+
+    da = da.as_cupy()
+    assert da.cupy.is_cupy
+    assert isinstance(da.data, dask_array_type)
+
+    da = da.cupy.as_numpy()
+    assert not da.cupy.is_cupy
