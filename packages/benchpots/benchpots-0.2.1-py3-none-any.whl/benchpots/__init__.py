0e3b0e9d74363a5e9950120072b6ebd3e9a69db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:04:10 +0000

    Add package benchpots/0.2.1/benchpots-0.2.1-py3-none-any.whl

---
+++ packages/benchpots/benchpots-0.2.1-py3-none-any.whl/benchpots/__init__.py
@@ -0,0 +1,15 @@
+"""
+BenchPOTS package
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: BSD-3-Clause
+
+from . import utils, datasets
+from .version import __version__
+
+__all__ = [
+    "__version__",
+    "utils",
+    "datasets",
+]
