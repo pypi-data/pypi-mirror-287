6c78c536a9c262fc20fb109e2e0c379aca184b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:48 +0000

    Add package ckanext-federated-index/0.1.0/ckanext_federated_index-0.1.0.tar.gz

---
+++ packages/ckanext-federated-index/ckanext_federated_index-0.1.0.tar.gz/ckanext_federated_index-0.1.0/ckanext/federated_index/tests/logic/test_action.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+from ckan.tests.helpers import call_action
+
+from ckanext.federated_index import shared
+
+
+@pytest.mark.usefixtures("with_plugins", "clean_db", "clean_index")
+class TestRefresh:
+    def test_verification(self, profile: shared.Profile, package_factory: Any):
+        """Removed packages dropped from storage."""
+
+        pkgs = package_factory.create_batch(3)
+
+        result = call_action("federated_index_profile_refresh", profile=profile)
+        assert result == {"count": 3, "profile": profile.id}
+
+        call_action("package_delete", id=pkgs[-1]["id"])
+
+        result = call_action(
+            "federated_index_profile_refresh", profile=profile, verify=False
+        )
+        assert result["count"] == 3
+
+        result = call_action("federated_index_profile_refresh", profile=profile)
+        assert result["count"] == 2
