41b04e99cd444035a9d18d12dcb68f69d84239aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:04:27 +0000

    Add package datakart/1.8.4/datakart-1.8.4.tar.gz

---
+++ packages/datakart/datakart-1.8.4.tar.gz/datakart-1.8.4/src/datakart/__init__.py
@@ -0,0 +1,19 @@
+from .core.datagokr import Datagokr
+from .core.ecos import Ecos
+from .core.jusogokr import Jusogokr
+from .core.kakao import Kakao
+from .core.naver import Naver
+from .core.naver_ad import NaverAd
+from .core.sgis import Sgis
+
+__all__ = [
+    "Datagokr",
+    "Ecos",
+    "Jusogokr",
+    "Kakao",
+    "Naver",
+    "NaverAd",
+    "Sgis",
+]
+
+__version__ = "1.8.4"
