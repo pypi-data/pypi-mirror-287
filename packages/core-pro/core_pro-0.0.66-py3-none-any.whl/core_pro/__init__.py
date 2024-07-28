03482d317f6f6220e435a715247d94f0c165b4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:44 +0000

    Add package core-pro/0.0.66/core_pro-0.0.66-py3-none-any.whl

---
+++ packages/core-pro/core_pro-0.0.66-py3-none-any.whl/core_pro/__init__.py
@@ -0,0 +1,9 @@
+from .config import GoogleAuthentication
+from .GSheet import Sheet
+from .GMail import Gmail
+from .GDrive import Drive
+from .GSlide import Slide
+from .oop_sql import DataPipeLine
+from .cloud import AWS, Gcloud
+from .pipeline import ExtractTime, PipelineClassification, EDA, PipelineRegression
+from .chat_bot import OpenAIChat
