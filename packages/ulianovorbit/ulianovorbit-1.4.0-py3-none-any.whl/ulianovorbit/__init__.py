cca988513663b442977744f9170b538c7340603d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:56 +0000

    Add package ulianovorbit/1.4.0/ulianovorbit-1.4.0-py3-none-any.whl

---
+++ packages/ulianovorbit/ulianovorbit-1.4.0-py3-none-any.whl/ulianovorbit/__init__.py
@@ -0,0 +1,11 @@
+# ulianovellipse/__init__.py
+
+# Import the main UlianovOrbit object as 'ou' for convenient access to its methods.
+from .ulianovorbit import ou
+
+# Import class definitions for handling specific orbital parameters and vectors.
+from .ulianovorbit import uom_params, orbit_vect
+
+# Define the version of the ulianovellipse package.
+__version__ = '1.4.0'
+
