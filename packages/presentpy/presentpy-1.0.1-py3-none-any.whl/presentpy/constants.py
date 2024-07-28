6169c5bc2679e2ba5ecdd45bc9911c3155015919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:35 +0000

    Add package presentpy/1.0.1/presentpy-1.0.1-py3-none-any.whl

---
+++ packages/presentpy/presentpy-1.0.1-py3-none-any.whl/presentpy/constants.py
@@ -0,0 +1,5 @@
+CODE_FRAME_STYLE_NAME = "codeFrame"
+CODE_HIGHLIGHT_PARAGRAPH_STYLE_NAME = "highlightedCodeParagraph"
+CODE_PARAGRAPH_STYLE_NAME = "codeParagraph"
+DRAWING_PAGE_STYLE_NAME = "defaultSlide"
+MASTER_TITLE_STYLE_NAME = "masterTitle"
