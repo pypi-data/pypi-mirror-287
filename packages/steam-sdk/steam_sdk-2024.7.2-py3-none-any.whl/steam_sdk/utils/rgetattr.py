4a8912f2f257e6dcd4ec91a66a6101ebd36d6252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:47 +0000

    Add package steam-sdk/2024.7.2/steam_sdk-2024.7.2-py3-none-any.whl

---
+++ packages/steam-sdk/steam_sdk-2024.7.2-py3-none-any.whl/steam_sdk/utils/rgetattr.py
@@ -0,0 +1,12 @@
+import functools
+
+
+def rgetattr(obj, attr, *args):
+    '''
+    from https://stackoverflow.com/questions/31174295/getattr-and-setattr-on-nested-subobjects-chained-properties
+
+    Get attribute of an object, accepting dotted attr string
+    '''
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+    return functools.reduce(_getattr, [obj] + attr.split('.'))
