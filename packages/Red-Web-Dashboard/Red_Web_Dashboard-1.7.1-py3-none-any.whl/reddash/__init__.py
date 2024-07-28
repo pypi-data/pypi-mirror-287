599b181ad7886330a25fc12af5c8f6981f24b15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:59 +0000

    Add package Red-Web-Dashboard/1.7.1/Red_Web_Dashboard-1.7.1-py3-none-any.whl

---
+++ packages/Red-Web-Dashboard/Red_Web_Dashboard-1.7.1-py3-none-any.whl/reddash/__init__.py
@@ -0,0 +1,10 @@
+"""
+License: Commercial
+Copyright (c) 2019 - present AppSeed.us
+Copyright (c) 2020 - present Neuro Assassin (https://github.com/Cog-Creators/Red-Dashboard)
+"""
+
+__version__ = "1.7.1"
+__authors__ = ["AAA3A", "Neuro Assassin"]
+
+from .app import FlaskApp
