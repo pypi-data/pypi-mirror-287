5a6da7919e603e70e92823a9579f1264b427cdde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:48 +0000

    Add package trackus/0.0.2/trackus-0.0.2.tar.gz

---
+++ packages/trackus/trackus-0.0.2.tar.gz/trackus-0.0.2/trackus/core.py
@@ -0,0 +1,15 @@
+class Trackus:
+  def __init__(self, apiKey):
+    self.apiKey = apiKey
+
+  def getKey(self):
+    if self.apiKey != "":
+      return "Api key: " + self.apiKey
+    else:
+      return "Api key not set"
+    
+  def resetKey(self):
+    self.apiKey = ""
+
+trackus = Trackus("")
+    
