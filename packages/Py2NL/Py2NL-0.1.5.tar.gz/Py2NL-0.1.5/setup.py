3cb1d03fc8293a14b6a6e29ad2af6622d9f6f155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:03:39 +0000

    Add package Py2NL/0.1.5/Py2NL-0.1.5.tar.gz

---
+++ packages/Py2NL/Py2NL-0.1.5.tar.gz/Py2NL-0.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='Py2NL',
+    version='0.1.5',
+    description='A natural language interface for Python',
+    url='https://github.com/TsXTatzumi/PyNLI',
+    author='Simeon Quant',
+    author_email='K51831785@students.jku.at',
+    license='BSD 2-clause',
+    packages=['PyNLI'],
+    install_requires=['langchain~=0.2.10',
+                      'langchain-openai~=0.1.8',
+                      'langchain-community~=0.2.9',
+                      'langchain-experimental~=0.0.49',
+                      'solara~=1.35.1',
+                      'pandas~=2.2.2',
+                      'opencv-python~=4.10.0.84',
+                      'Pillow~=10.4.0',
+                      'pydantic~=2.7.0',
+                      'libcst~=1.3.1',
+                      'markdown~=3.6',
+                      'colorama==0.4.6',
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Jupyter',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
