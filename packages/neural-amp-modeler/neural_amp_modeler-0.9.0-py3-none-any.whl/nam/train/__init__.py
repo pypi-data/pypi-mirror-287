5e0dd3c60f34998126c06ae8ad4bb411479823d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:46 +0000

    Add package neural-amp-modeler/0.9.0/neural_amp_modeler-0.9.0-py3-none-any.whl

---
+++ packages/neural-amp-modeler/neural_amp_modeler-0.9.0-py3-none-any.whl/nam/train/__init__.py
@@ -0,0 +1,14 @@
+# File: __init__.py
+# Created Date: Sunday December 4th 2022
+# Author: Steven Atkinson (steven@atkinson.mn)
+
+"""
+Code for standardized training with NAM
+"""
+
+__all__ = ["colab", "core", "gui", "metadata"]
+
+from . import colab
+from . import core
+from . import gui
+from . import metadata
