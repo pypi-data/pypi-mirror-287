7ff406cbd9c9bbffff8e65ea0d810cfd7720519a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:04:15 +0000

    Add package trackus/0.0.16/trackus-0.0.16-py3-none-any.whl

---
+++ packages/trackus/trackus-0.0.16-py3-none-any.whl/lib/__init__.py
@@ -0,0 +1,9 @@
+class Trackus:
+  def __init__(self, apiKey):
+    self.apiKey = apiKey
+
+  def getKey(self):
+    if self.apiKey != "":
+      return "Api key: " + self.apiKey
+    else:
+      return "Api key not set"
