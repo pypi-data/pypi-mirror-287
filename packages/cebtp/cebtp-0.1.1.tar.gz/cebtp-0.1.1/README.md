ec2f2e5e4ba498e9f5fe50ef0fd9f4d451ed4aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:51 +0000

    Add package cebtp/0.1.1/cebtp-0.1.1.tar.gz

---
+++ packages/cebtp/cebtp-0.1.1.tar.gz/cebtp-0.1.1/README.md
@@ -0,0 +1,10 @@
+# CEBTP
+
+This package provides functions to preprocess EEG and feature data for cebtp.
+
+## Installation
+
+You can install the package using pip:
+
+```bash
+pip install git+https://github.com/yourusername/cebtp.git
---
+++ packages/cebtp/cebtp-0.1.1.tar.gz/cebtp-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='cebtp',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'mne',
+        'pywt',  # Include pywt
+        'nolds',
+        'antropy',
+        'tensorflow',
+        'google-cloud-storage',
+        'cryptography',
+        'keras'
+    ],
+    python_requires='>=3.9',  # Specify compatible Python versions
+    author='ZENIHTH',
+    author_email='chainloop3@mail.com',
+    description='Package for cebtp EEG and feature data preprocessing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ZENITH-Intelligence',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
