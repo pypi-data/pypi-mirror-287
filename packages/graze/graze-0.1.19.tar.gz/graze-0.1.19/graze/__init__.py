37fe63883c358962bb09b54fbd574081a6af213f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:03:49 +0000

    Add package graze/0.1.19/graze-0.1.19.tar.gz

---
+++ packages/graze/graze-0.1.19.tar.gz/graze-0.1.19/graze/__init__.py
@@ -0,0 +1,16 @@
+"""Getting stuff from the internet (and caching locally, automatically)"""
+
+from graze.base import (
+    DFLT_GRAZE_DIR,
+    Internet,
+    Graze,
+    GrazeWithDataRefresh,
+    GrazeReturningFilepaths,
+    graze,
+    url_to_localpath,
+    localpath_to_url,
+    url_to_filepath,
+    url_to_contents,
+    key_egress_print_downloading_message,
+)
+from graze.util import handle_missing_dir
