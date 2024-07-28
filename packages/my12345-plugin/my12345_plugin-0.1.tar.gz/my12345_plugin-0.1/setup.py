a42aab07255b8f3c97b761c79753ac7387f4772a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:53 +0000

    Add package my12345-plugin/0.1/my12345_plugin-0.1.tar.gz

---
+++ packages/my12345-plugin/my12345_plugin-0.1.tar.gz/my12345_plugin-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='my12345_plugin',
+    version='0.1',
+    author='linshenghua',
+    author_email='2592530336@qq.com',
+    description='A simple plugin example',
+    py_modules=['my_plugin'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
