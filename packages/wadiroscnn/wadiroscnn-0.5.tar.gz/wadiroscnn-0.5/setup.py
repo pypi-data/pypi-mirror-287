bb94a22ac7c216993a0e0f1ec1a5b9b251bd2064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 04:03:59 +0000

    Add package wadiroscnn/0.5/wadiroscnn-0.5.tar.gz

---
+++ packages/wadiroscnn/wadiroscnn-0.5.tar.gz/wadiroscnn-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wadiroscnn',
+    version='0.5',
+    package_dir = {"": "src"},
+    packages = find_packages(where="src"),
+    install_requires=[
+        'numpy>=1.26',
+        'cvxpy>=1.3',
+        'pandas>=2.1',
+        'scikit-learn>=1.3',
+        'scipy>=1.11'],
+    python_requires=">=3.8",
+    url = "https://github.com/jupall/WaDiRo-SCNN",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+        )
+
