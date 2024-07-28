9387761927b79c8c1356ea1670fe85f08cc78b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 03:03:45 +0000

    Add package umar/0.1.1/umar-0.1.1.tar.gz

---
+++ packages/umar/umar-0.1.1.tar.gz/umar-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="umar",
+    version="0.1.1",
+    packages=find_packages(),
+    description="A simple example package",
+    author="Umar Asghar",
+    author_email="mrumarasghar@gmail.com",
+    url="https://github.com/umarmughal824/umar",
+)
