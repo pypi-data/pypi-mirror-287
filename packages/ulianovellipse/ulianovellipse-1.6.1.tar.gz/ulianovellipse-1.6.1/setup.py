df4720fc357a68c2f1c900ee0130ce9013796c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:27 +0000

    Add package ulianovellipse/1.6.1/ulianovellipse-1.6.1.tar.gz

---
+++ packages/ulianovellipse/ulianovellipse-1.6.1.tar.gz/ulianovellipse-1.6.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# Ler o conteúdo do README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ulianovellipse',
+    version='1.6.1',
+    description='Library for handling Ulianov elliptical functions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Policarpo Yoshin Ulianov',
+    author_email='poliyu77@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    setup_requires=[
+        'numpy',
+    ],
+    package_data={
+        # Inclua todos os arquivos .png na pasta images e na pasta examples
+        '': ['images/*.png', 'examples/*.py'],
+    },
+    include_package_data=True,
+)
