18feed01e2426249a72c9845f00b35c4ab8218ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 15:03:48 +0000

    Add package walledeval/0.2.0/walledeval-0.2.0-py3-none-any.whl

---
+++ packages/walledeval/walledeval-0.2.0-py3-none-any.whl/walledeval/judge/__init__.py
@@ -0,0 +1,41 @@
+# walledeval/judge/__init__.py
+
+import warnings
+
+from walledeval.judge.core import Judge
+from walledeval.judge.lionguard import LionGuardJudge
+from walledeval.judge.mcq import MCQJudge
+from walledeval.judge.llm import (
+    LLMasaJudge,
+    QuestionLLMasaJudge,
+    MultiClassToxicityJudge,
+    LLMGuardJudge, LLMGuardOutput,
+    LlamaGuardJudge
+)
+from walledeval.judge.string import StringMatchingJudge
+from walledeval.judge.toxicity import (
+    ToxicityModelJudge
+)
+
+# Windows does not support CodeShield so we use this as a bypass
+try:
+    from walledeval.judge.code import CodeShieldJudge
+except ImportError:
+    warnings.warn("CodeShieldJudge could not be imported, not supported on Windows OS", ImportWarning, stacklevel=2)
+except OSError:
+    warnings.warn("CodeShieldJudge could not be imported, not supported on Windows OS", ImportWarning, stacklevel=2)
+
+__all__ = [
+    "Judge",
+    "MCQJudge",
+    "LLMasaJudge",
+    "QuestionLLMasaJudge",
+    "MultiClassToxicityJudge",
+    "LLMGuardJudge", "LLMGuardOutput",
+    "LlamaGuardJudge",
+    "ToxicityModelJudge", "LionGuardJudge",
+    "StringMatchingJudge"
+]
+
+if "CodeShieldJudge" in globals():
+    __all__.append("CodeShieldJudge")
