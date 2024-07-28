1da7062475322b67213ab0e0b0de3bcef800ffe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-28 14:03:44 +0000

    Add package lightning-thunder/0.2.0.dev20240728/lightning_thunder-0.2.0.dev20240728.tar.gz

---
+++ packages/lightning-thunder/lightning_thunder-0.2.0.dev20240728.tar.gz/lightning_thunder-0.2.0.dev20240728/thunder/executors/nvfuserex.py
@@ -0,0 +1,45 @@
+import torch
+from looseversion import LooseVersion
+
+from thunder.extend import FusionExecutor
+
+__all__ = ["nvfuser_version", "required_nvfuser_version", "nvfuser_available", "nvfuserex"]
+
+
+#
+# Functions for detecting nvFuser and its version
+#
+def nvfuser_version() -> LooseVersion | None:
+    # Short-circuits if CUDA isn't available
+    if not torch.cuda.is_available():
+        return None
+
+    try:
+        import nvfuser
+
+        if hasattr(nvfuser, "version"):
+            return LooseVersion(nvfuser.version())
+
+        # NOTE: This import of nvFuser may or may not have version info
+        return LooseVersion("0.0.0")
+    except ImportError:
+        pass
+
+    # NOTE This occurs when nvFuser couldn't be imported
+    return None
+
+
+def required_nvfuser_version() -> LooseVersion:
+    return LooseVersion("0.2.8")
+
+
+def nvfuser_available() -> bool:
+    v = nvfuser_version()
+    return v is not None and v >= required_nvfuser_version()
+
+
+nvfuserex: None | FusionExecutor = None
+if nvfuser_available():
+    import thunder.executors.nvfuserex_impl as impl
+
+    nvfuserex = impl.ex
