5b63ffb04813afa92a74fe59bf6711ec6bb486bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:45 +0000

    Add package discalla/0.1.0/discalla-0.1.0.tar.gz

---
+++ packages/discalla/discalla-0.1.0.tar.gz/discalla-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+import numpy
+
+extensions = [
+    Extension(
+        'discalla.core',
+        sources=['discalla/core.pyx'],
+        include_dirs=[numpy.get_include()]
+    )
+]
+
+setup(
+    name='discalla',
+    version='0.1.0',
+    author='YY',
+    author_email='',
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    ext_modules=cythonize(extensions),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'cython'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
