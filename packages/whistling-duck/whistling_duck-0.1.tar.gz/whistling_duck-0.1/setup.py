c67d951ab4fe8f3902d8b13f2caf2790c4c4a34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:05:17 +0000

    Add package whistling-duck/0.1/whistling_duck-0.1.tar.gz

---
+++ packages/whistling-duck/whistling_duck-0.1.tar.gz/whistling_duck-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whistling_duck', 
+    version='0.1',
+    description='Whistling Duck is a Python library for data quality exploration and validation, designed for small to medium-sized datasets. It is written using DuckDB.',
+    author='Subha Saha',
+    author_email='whistlingducklib@gmail.com',
+    install_requires=[
+        #'duckdb>=1.0.0'
+    ],
+)
