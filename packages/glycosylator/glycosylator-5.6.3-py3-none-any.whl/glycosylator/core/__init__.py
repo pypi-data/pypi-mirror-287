723b99ce14aca53506df78a24fc131348df02989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:52 +0000

    Add package glycosylator/5.6.3/glycosylator-5.6.3-py3-none-any.whl

---
+++ packages/glycosylator/glycosylator-5.6.3-py3-none-any.whl/glycosylator/core/__init__.py
@@ -0,0 +1,9 @@
+"""
+The core classes and functions of Glycosylator
+"""
+
+from glycosylator.core.Glycan import *
+from glycosylator.core.scaffold import *
+from glycosylator.core.protein_scaffold import *
+from glycosylator.core.membrane_scaffold import *
+from glycosylator.core.glyco_shield import *
