d4ed5503352080674a953c102390f36a57835d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:04:21 +0000

    Add package opea-comps/0.8/opea_comps-0.8-py3-none-any.whl

---
+++ packages/opea-comps/opea_comps-0.8-py3-none-any.whl/comps/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+# Document
+from comps.cores.proto.docarray import (
+    Audio2TextDoc,
+    Base64ByteStrDoc,
+    DocPath,
+    EmbedDoc768,
+    EmbedDoc1024,
+    GeneratedDoc,
+    LLMParamsDoc,
+    SearchedDoc,
+    RerankedDoc,
+    TextDoc,
+    RAGASParams,
+    RAGASScores,
+    GraphDoc,
+    LVMDoc,
+)
+
+# Constants
+from comps.cores.mega.constants import MegaServiceEndpoint, ServiceRoleType, ServiceType
+
+# Microservice
+from comps.cores.mega.orchestrator import ServiceOrchestrator
+from comps.cores.mega.orchestrator_with_yaml import ServiceOrchestratorWithYaml
+from comps.cores.mega.micro_service import MicroService, register_microservice, opea_microservices
+from comps.cores.mega.gateway import (
+    Gateway,
+    ChatQnAGateway,
+    CodeGenGateway,
+    CodeTransGateway,
+    DocSumGateway,
+    TranslationGateway,
+    SearchQnAGateway,
+    AudioQnAGateway,
+    FaqGenGateway,
+)
+
+# Telemetry
+from comps.cores.telemetry.opea_telemetry import opea_telemetry
+
+# Statistics
+from comps.cores.mega.base_statistics import statistics_dict, register_statistics
---
+++ packages/opea-comps/opea_comps-0.8-py3-none-any.whl/comps/cores/proto/docarray.py
@@ -0,0 +1,137 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Optional
+
+import numpy as np
+from docarray import BaseDoc, DocList
+from docarray.documents import AudioDoc
+from docarray.typing import AudioUrl
+from pydantic import Field, conint, conlist
+
+
+class TopologyInfo:
+    # will not keep forwarding to the downstream nodes in the black list
+    # should be a pattern string
+    downstream_black_list: Optional[list] = []
+
+
+class TextDoc(BaseDoc, TopologyInfo):
+    text: str
+
+
+class Base64ByteStrDoc(BaseDoc):
+    byte_str: str
+
+
+class DocPath(BaseDoc):
+    path: str
+    chunk_size: int = 1500
+    chunk_overlap: int = 100
+    process_table: bool = False
+    table_strategy: str = "fast"
+
+
+class EmbedDoc768(BaseDoc):
+    text: str
+    embedding: conlist(float, min_length=768, max_length=768)
+    search_type: str = "similarity"
+    k: int = 4
+    distance_threshold: Optional[float] = None
+    fetch_k: int = 20
+    lambda_mult: float = 0.5
+    score_threshold: float = 0.2
+
+
+class Audio2TextDoc(AudioDoc):
+    url: Optional[AudioUrl] = Field(
+        description="The path to the audio.",
+        default=None,
+    )
+    model_name_or_path: Optional[str] = Field(
+        description="The Whisper model name or path.",
+        default="openai/whisper-small",
+    )
+    language: Optional[str] = Field(
+        description="The language that Whisper prefer to detect.",
+        default="auto",
+    )
+
+
+class EmbedDoc1024(BaseDoc):
+    text: str
+    embedding: conlist(float, min_length=1024, max_length=1024)
+
+
+class SearchedDoc(BaseDoc):
+    retrieved_docs: DocList[TextDoc]
+    initial_query: str
+    top_n: int = 1
+
+    class Config:
+        json_encoders = {np.ndarray: lambda x: x.tolist()}
+
+
+class GeneratedDoc(BaseDoc):
+    text: str
+    prompt: str
+
+
+class RerankedDoc(BaseDoc):
+    reranked_docs: DocList[TextDoc]
+    initial_query: str
+
+
+class LLMParamsDoc(BaseDoc):
+    model: Optional[str] = None  # for openai and ollama
+    query: str
+    max_new_tokens: int = 1024
+    top_k: int = 10
+    top_p: float = 0.95
+    typical_p: float = 0.95
+    temperature: float = 0.01
+    repetition_penalty: float = 1.03
+    streaming: bool = True
+
+
+class LLMParams(BaseDoc):
+    max_new_tokens: int = 1024
+    top_k: int = 10
+    top_p: float = 0.95
+    typical_p: float = 0.95
+    temperature: float = 0.01
+    repetition_penalty: float = 1.03
+    streaming: bool = True
+
+
+class RAGASParams(BaseDoc):
+    questions: DocList[TextDoc]
+    answers: DocList[TextDoc]
+    docs: DocList[TextDoc]
+    ground_truths: DocList[TextDoc]
+
+
+class RAGASScores(BaseDoc):
+    answer_relevancy: float
+    faithfulness: float
+    context_recallL: float
+    context_precision: float
+
+
+class GraphDoc(BaseDoc):
+    text: str
+    strtype: Optional[str] = Field(
+        description="type of input query, can be 'query', 'cypher', 'rag'",
+        default="query",
+    )
+    max_new_tokens: Optional[int] = Field(default=1024)
+    rag_index_name: Optional[str] = Field(default="rag")
+    rag_node_label: Optional[str] = Field(default="Task")
+    rag_text_node_properties: Optional[list] = Field(default=["name", "description", "status"])
+    rag_embedding_node_property: Optional[str] = Field(default="embedding")
+
+
+class LVMDoc(BaseDoc):
+    image: str
+    prompt: str
+    max_new_tokens: conint(ge=0, le=1024) = 512
