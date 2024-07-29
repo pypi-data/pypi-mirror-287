d4b9df5eb880ecd56cc6b9d61fab0a4af749a80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:07 +0000

    Add package msgram-parser/1.1.4/msgram_parser-1.1.4-py3-none-any.whl

---
+++ packages/msgram-parser/msgram_parser-1.1.4-py3-none-any.whl/genericparser/plugins/statics/sonarqube.py
@@ -0,0 +1,23 @@
+from genericparser.plugins.domain.generic_class import GenericStaticABC
+
+
+class ParserSonarQube(GenericStaticABC):
+    def extract(self, input_file):
+        metrics = []
+        keys = []
+        values = []
+        for entry in input_file:
+            key = entry.get("key", {})
+            measures = entry.get("measures", [])
+            if entry.get("qualifier") in ["FIL", "UTS", "TRK"]:
+                for measure in measures:
+                    metric = measure.get("metric", None)
+                    value = measure.get("value", None)
+                    metrics.append(metric)
+                    keys.append(key)
+                    values.append(value)
+        return {"file_paths": keys, "metrics": metrics, "values": values}
+
+
+def main():
+    return ParserSonarQube()
