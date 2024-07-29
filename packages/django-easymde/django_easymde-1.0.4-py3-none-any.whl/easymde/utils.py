8c2227a7fcf07fd3f6c88be73cbbf9ae3f46f7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:04:46 +0000

    Add package django-easymde/1.0.4/django_easymde-1.0.4-py3-none-any.whl

---
+++ packages/django-easymde/django_easymde-1.0.4-py3-none-any.whl/easymde/utils.py
@@ -0,0 +1,22 @@
+try:
+    # django > 3 
+    from django.utils.encoding import force_str
+except ImportError:
+    # django 2 
+    from django.utils.encoding import force_unicode as force_str
+
+from django.utils.functional import Promise
+
+import json
+
+
+class LazyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, Promise):
+            return force_str(obj)
+        return super(LazyEncoder, self).default(obj)
+
+
+def json_dumps(data):
+    return json.dumps(data, cls=LazyEncoder)
