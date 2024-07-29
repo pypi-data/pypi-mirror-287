539495be64cfb054cfd4d34354a827294a6f17dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:56 +0000

    Add package lino-noi/24.7.0/lino_noi-24.7.0.tar.gz

---
+++ packages/lino-noi/lino_noi-24.7.0.tar.gz/lino_noi-24.7.0/lino_noi/lib/noi/fixtures/minimal_ledger.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2023 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+
+from lino.api import rt, dd, _
+
+
+def objects():
+    return []
