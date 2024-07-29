007222f75a7ccb335010c7d8d5f6a39f6772f4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:13 +0000

    Add package datakart/1.9.1/datakart-1.9.1-py3-none-any.whl

---
+++ packages/datakart/datakart-1.9.1-py3-none-any.whl/datakart/__init__.py
@@ -0,0 +1,21 @@
+from .core.datagokr import Datagokr
+from .core.ecos import Ecos
+from .core.fss import Fss
+from .core.jusogokr import Jusogokr
+from .core.kakao import Kakao
+from .core.naver import Naver
+from .core.naver_ad import NaverAd
+from .core.sgis import Sgis
+
+__all__ = [
+    "Datagokr",
+    "Ecos",
+    "Fss",
+    "Jusogokr",
+    "Kakao",
+    "Naver",
+    "NaverAd",
+    "Sgis",
+]
+
+__version__ = "1.9.1"
