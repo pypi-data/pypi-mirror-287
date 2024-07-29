8d981fb6b32f6836d68266c3fdfb9a2cd7c77ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:47 +0000

    Add package radonmc/1.1.5/radonmc-1.1.5-py3-none-any.whl

---
+++ packages/radonmc/radonmc-1.1.5-py3-none-any.whl/radon/builtin/time.py
@@ -0,0 +1,39 @@
+from ..cpl.score import CplScore
+from ..transpiler import FunctionDeclaration, TranspilerContext, add_lib
+from ..utils import FLOAT_PREC
+
+_ = 0
+
+
+def lib_time(ctx: TranspilerContext, _):
+    tr = ctx.transpiler
+    tr.files["__load__"].append("scoreboard players set __time__time__ 0")
+    tr.tickFile.append("scoreboard players add __time__time__ 1")
+
+
+def lib_ftime(ctx: TranspilerContext, _):
+    lib_time(ctx, _)
+    tr = ctx.transpiler
+    tr.files["__load__"].append(
+        f"scoreboard players set FTIME_FLOAT_PREC --temp-- {int(FLOAT_PREC / 20)}"
+    )
+    ctx.file.append(
+        f"scoreboard players operation __ftime__time__ *= FTIME_FLOAT_PREC --temp--"
+    )
+
+
+add_lib(FunctionDeclaration(
+    type="python",
+    name="time",
+    returns=CplScore(None, "__time__time__ --temp-", "int"),
+    arguments=[],
+    function=lib_time,
+))
+
+add_lib(FunctionDeclaration(
+    type="python",
+    name="ftime",
+    returns=CplScore(None, "__ftime__time__ --temp-", "float"),
+    arguments=[],
+    function=lib_ftime,
+))
