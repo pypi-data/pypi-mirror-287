90409dc759d6d01fdab469a728ccfa55dc73b7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:08 +0000

    Add package evo-downloader/1.0.7/evo_downloader-1.0.7-py3-none-any.whl

---
+++ packages/evo-downloader/evo_downloader-1.0.7-py3-none-any.whl/evo_downloader/__main__.py
@@ -0,0 +1,11 @@
+"""Entry point for evo_downloader."""
+
+from .cli import cli  # pragma: no cover
+
+
+def main():
+    cli()
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()  # pragma: no cover
