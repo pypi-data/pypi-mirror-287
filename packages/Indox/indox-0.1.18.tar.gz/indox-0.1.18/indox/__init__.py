9922e42736420cc1c2078913cfda41beea4f3772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:04:27 +0000

    Add package Indox/0.1.18/indox-0.1.18.tar.gz

---
+++ packages/Indox/indox-0.1.18.tar.gz/indox-0.1.18/indox/__init__.py
@@ -0,0 +1,4 @@
+# Indox/__init__.py
+from .indox import IndoxRetrievalAugmentation
+
+__version__ = "0.1.18"
---
+++ packages/Indox/indox-0.1.18.tar.gz/indox-0.1.18/indox/vector_stores/__init__.py
@@ -0,0 +1,4 @@
+# from .PGVector import PGVectorStore
+from .chroma import Chroma
+from .faiss import FAISS
+from .pgvector import PGVector
