5b21c8475caf7a790ba6e21f6aa3c2e40d9cb1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:03:55 +0000

    Add package unigen/1.2/unigen-1.2.tar.gz

---
+++ packages/unigen/unigen-1.2.tar.gz/unigen-1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="unigen",
+    version="1.2",
+    packages=find_packages(),
+    install_requires=["mutagen"],
+    tests_require=[
+        "pytest",
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
---
+++ packages/unigen/unigen-1.2.tar.gz/unigen-1.2/unigen/__init__.py
@@ -0,0 +1,3 @@
+from .audio_factory import AudioFactory, UnsupportedFileFormatError, isFileFormatSupported
+from .audio_manager import IAudioManager
+from .utils import pictureNameToNumber, pictureTypes
