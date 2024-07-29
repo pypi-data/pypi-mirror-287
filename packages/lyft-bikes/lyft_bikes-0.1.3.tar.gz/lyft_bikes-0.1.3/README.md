c7a50e080f343b88d2c880f681d93fcc2a608f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:37 +0000

    Add package lyft-bikes/0.1.3/lyft_bikes-0.1.3.tar.gz

---
+++ packages/lyft-bikes/lyft_bikes-0.1.3.tar.gz/lyft_bikes-0.1.3/README.md
@@ -0,0 +1,41 @@
+# Lyft Bike Share Data
+
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+[![Tests](https://github.com/wd60622/lyft-bikes/actions/workflows/tests.yml/badge.svg)](https://github.com/wd60622/lyft-bikes/actions/workflows/tests.yml)
+[![PyPI version](https://badge.fury.io/py/lyft-bikes.svg)](https://badge.fury.io/py/lyft-bikes)
+[![docs](https://github.com/wd60622/lyft-bikes/actions/workflows/docs.yml/badge.svg)](https://wd60622.github.io/lyft-bikes/)
+
+Python client for Lyft bike share data.
+
+## Features
+
+- Support for [cities](https://www.lyft.com/bikes#cities) with Lyft bike share
+- [Historical trips](https://wd60622.github.io/lyft-bikes/examples/historical-trips/)
+- Live station and bike / scooter availability
+- [Applying pricing to trips](https://wd60622.github.io/lyft-bikes/examples/new-pricing/)
+    - Unlock Fees
+    - Minute Rates
+
+## Installation
+
+Install from `pip`
+
+```shell
+$ pip install lyft-bikes
+```
+
+## Documentation
+
+The documentation is hosted on [GitHub Pages](https://wd60622.github.io/lyft-bikes/).
+
+## Development
+
+The development environment was created with [`poetry`](https://python-poetry.org/docs/). The `pyproject.toml` file is the main configuration file for the project.
+
+```bash
+poetry install .
+```
+
+## Contributing
+
+If you would like to contribute or find some issue in the code, please [open an Issue](https://github.com/wd60622/divvy/issues/new) or a PR on GitHub. Thanks!
---
+++ packages/lyft-bikes/lyft_bikes-0.1.3.tar.gz/lyft_bikes-0.1.3/lyft_bikes/historical/dates.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+from typing import List
+
+import datetime
+from calendar import monthrange
+from dateutil.relativedelta import relativedelta
+
+
+class DateRangeError(Exception):
+    """This date is before the Divvy start."""
+
+
+class DefaultDates:
+    def check_valid(self, date: datetime.date) -> None:
+        if date > self.last_date:
+            raise DateRangeError(f"{date} data hasn't been released yet.")
+
+    @property
+    def last_date(self) -> datetime.date:
+        """Latest available day in the historical dataset.
+
+        Based on the releases of the historical trips. Seems to be safe to include
+        the previous month.
+
+        """
+        previous_month = datetime.date.today().replace(day=1) - relativedelta(months=1)
+        last_day_previous_month = monthrange(previous_month.year, previous_month.month)[
+            1
+        ]
+
+        return previous_month.replace(day=last_day_previous_month)
+
+    def create_date_range(self, start_date: str, end_date: str) -> List[datetime.date]:
+        """Return list of dates from start to end"""
+        dates = pd.date_range(
+            str(self.first_of_month(start_date)),
+            str(self.first_of_month(end_date)),
+            freq="MS",
+        )
+
+        return [date.to_pydatetime().date() for date in dates]
+
+    @staticmethod
+    def first_of_month(date: str) -> datetime.date:
+        return datetime.datetime.strptime(date, "%Y-%m-%d").date().replace(day=1)
+
+    @staticmethod
+    def to_date(year: int, month: int) -> datetime.date:
+        return datetime.date(year, month, 1)
+
+
+class DivvyDates(DefaultDates):
+    """Class that stores and calculates different dates associated with the Divvy Program."""
+
+    first_date = datetime.date(2020, 4, 1)
+    electric_trials = datetime.date(2020, 7, 13)
+    first_electric_date = datetime.date(2020, 7, 28)
+    # Last day of the west of Western pricing
+    end_of_waiver = datetime.date(2022, 5, 10)
+
+    def check_valid(self, date: datetime.date) -> None:
+        super().check_valid(date)
+
+        if date < self.first_date:
+            raise DateRangeError(f"{date} is before the historical trips.")
