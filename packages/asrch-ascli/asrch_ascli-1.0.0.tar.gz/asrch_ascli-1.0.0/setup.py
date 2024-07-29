dbbdc7115dc02797f018c5df2915858d2bd9aa64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:05:10 +0000

    Add package asrch-ascli/1.0.0/asrch_ascli-1.0.0.tar.gz

---
+++ packages/asrch-ascli/asrch_ascli-1.0.0.tar.gz/asrch_ascli-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='asrch_ascli',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+
+    author='j',
+    author_email='none@none.com',
+    description='Formatter / dependency for asrch',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://bitbucket.org/asrch/ascli/src/main/',
+    license='MIT',  
+)
