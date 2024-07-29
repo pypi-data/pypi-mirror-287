00f13f9e480d165a48cb2a2ce3210d5347c247de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:36 +0000

    Add package autonomy-dev/0.2.55/autonomy_dev-0.2.55-py3-none-any.whl

---
+++ packages/autonomy-dev/autonomy_dev-0.2.55-py3-none-any.whl/auto_dev/test.py
@@ -0,0 +1,25 @@
+"""
+Module for testing the project.
+"""
+from .cli_executor import CommandExecutor
+
+
+def test_path(path: str, 
+              verbose: bool = False,
+              watch: bool = False,
+              ) -> bool:
+    """
+    Check the path for linting errors.
+    :param path: The path to check
+    """
+    command = CommandExecutor(
+        [
+            "poetry",
+            "run",
+            "pytest",
+            str(path),
+            "-vv",
+        ] + (["-ff"] if watch else [])
+    )
+    result = command.execute(verbose=verbose, stream=True)
+    return result
