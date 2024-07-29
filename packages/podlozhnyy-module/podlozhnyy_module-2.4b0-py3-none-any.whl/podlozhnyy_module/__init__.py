4a86892293387d7db9d885e0e2e9cd24b3084283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:57 +0000

    Add package podlozhnyy-module/2.4b0/podlozhnyy_module-2.4b0-py3-none-any.whl

---
+++ packages/podlozhnyy-module/podlozhnyy_module-2.4b0-py3-none-any.whl/podlozhnyy_module/__init__.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from podlozhnyy_module import (
+    abtesting,
+    bootstrap,
+    charts,
+    collocation,
+    correlation,
+    pareto,
+    permutation,
+    regression,
+    timeseries,
+    timetest,
+)
+
+print("Привет! Ты импортировал модуль созданный https://github.com/NPodlozhniy")
+print("В нем собраны часто востребованные в работе аналитика методы")
+print("Посмотреть полный cписок пакетов можно с помощью dir(<название библиотеки>)")
+print("Приятного использования!")
