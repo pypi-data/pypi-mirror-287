debbdd26513a0cf3992c7438deff99a06f3b4822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:22 +0000

    Add package PackageBuilding/1.0.0/PackageBuilding-1.0.0.tar.gz

---
+++ packages/PackageBuilding/PackageBuilding-1.0.0.tar.gz/PackageBuilding-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="PackageBuilding",
+    version="1.0.0",
+    description="This is the initial version on exploring How to build the custom package in Python",
+    author="Krishna Belamkonda",
+    author_email="Krishna@gmail.com",
+    python_requires='>=3.8',  # Required Python version
+    license='MIT',  # License type
+    platforms='Any',  # Supported platforms
+    license_files='LICENSE'  # Path to the license file
+)
