4fd021280da20edbfaaf778b7385aee4a20a26bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:05:26 +0000

    Add package distributions-nf/0.1/distributions_nf-0.1.tar.gz

---
+++ packages/distributions-nf/distributions_nf-0.1.tar.gz/distributions_nf-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_nf',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_nf'],
+      zip_safe=False)
