05282c923cc6511868a08b3222db2158fcd9c7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:08 +0000

    Add package msteamsapi/0.9.3/msteamsapi-0.9.3-py2.py3-none-any.whl

---
+++ packages/msteamsapi/msteamsapi-0.9.3-py2.py3-none-any.whl/msteamsapi/__init__.py
@@ -0,0 +1,5 @@
+from .adaptive_card import AdaptiveCard
+from .container import Container, FactSet
+from .enums import (TextSize, TextWeight, ContainerStyle, BackgroundFillMode,
+                    BackgroundVerticalAlignment, BackgroundHorizontalAlignment)
+from .webhook import TeamsWebhook
