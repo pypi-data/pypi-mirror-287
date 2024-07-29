1c4d0efc081c708c1ffb42586b2160b3d54250c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:09 +0000

    Add package sec-downloaders/0.1/sec_downloaders-0.1.tar.gz

---
+++ packages/sec-downloaders/sec_downloaders-0.1.tar.gz/sec_downloaders-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+long_description = Path("../readme.md").read_text()
+
+setup(
+    name="sec_downloaders",
+    author="John Friedman",
+    version="0.001",
+    description = "A package to download SEC filings",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    url="https://github.com/john-friedman/SEC-Downloaders",
+    package_data={
+        "sec_downloaders": ["data/basic_company_data.json"],
+    },
+    include_package_data=True,
+)
