1b8da47c4f7527c5206ec84e8700fb88eb288831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:09 +0000

    Add package django-admin-shortcuts/3.0.0/django-admin-shortcuts-3.0.0.tar.gz

---
+++ packages/django-admin-shortcuts/django-admin-shortcuts-3.0.0.tar.gz/django-admin-shortcuts-3.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from codecs import open
+import os
+
+from admin_shortcuts import version
+
+
+setup(
+    name='django-admin-shortcuts',
+    author='Ales Kocjancic',
+    author_email='alesdotio@gmail.com',
+    version=version,
+    description='Add simple and pretty shortcuts to the django admin homepage.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/alesdotio/django-admin-shortcuts',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=5.0',
+    ],
+    download_url='https://github.com/alesdotio/django-admin-shortcuts/tarball/' + version,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+)
