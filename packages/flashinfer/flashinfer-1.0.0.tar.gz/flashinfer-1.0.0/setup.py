84029a4edb073ceb7a38120cf52e6130985f1f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:32 +0000

    Add package flashinfer/1.0.0/flashinfer-1.0.0.tar.gz

---
+++ packages/flashinfer/flashinfer-1.0.0.tar.gz/flashinfer-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+import socket
+import getpass
+import os
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+
+setup(name='flashinfer', #package name
+      version='1.0.0',
+      description='White Hat Researcher',
+      author='Pastaga',
+      license='MIT',
+      zip_safe=False,
+      cmdclass={'install': CustomInstall})
