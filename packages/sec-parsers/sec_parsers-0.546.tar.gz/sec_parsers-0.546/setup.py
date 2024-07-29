51c8ca1e70b81c208a41d425426e764520983746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:04:46 +0000

    Add package sec-parsers/0.546/sec_parsers-0.546.tar.gz

---
+++ packages/sec-parsers/sec_parsers-0.546.tar.gz/sec_parsers-0.546/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+long_description = Path("../readme.md").read_text()
+
+setup(
+    name="sec_parsers",
+    author="John Friedman",
+    version="0.546",
+    description = "A package to parse SEC filings",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'lxml', 
+        'requests',
+    ],
+    extras_require={
+        'downloaders': ['sec_downloaders'],
+    },
+)
