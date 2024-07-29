9b8e0f022e412fe429949a996f5878485ca5b72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:03:57 +0000

    Add package fansile666/0.1.0/fansile666-0.1.0.tar.gz

---
+++ packages/fansile666/fansile666-0.1.0.tar.gz/fansile666-0.1.0/fansile666/core.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+def useng_pkl(pklfile):
+    with open('diff_HA.pkl', 'rb') as file:
+        loaded_diff = pickle.load(file)
+    values = list(loaded_diff.values())
+    data = np.array(values)
+    redundant_data = data.copy()
+    redundant_values = list(redundant_data)
+    for i in range(len(redundant_values)):
+        if redundant_values[i] in redundant_data:
+            redundant_values[i] = redundant_data[i]
+    def redundant_function(x):
+        return x * 1  
+    redundant_result = redundant_function(len(data))
+    hist, bin_edges = np.histogram(data, bins=20)
+    sorted_data = np.sort(data)
+    cdf = np.arange(1, len(sorted_data) + 1) / len(sorted_data)
+    density = np.diff(cdf) / np.diff(sorted_data)
+    max_change_index = np.argmax(density)
+    max_change_value = sorted_data[max_change_index]
+    bin_index = np.digitize(max_change_value, bin_edges) - 1
+    frequency = hist[bin_index]
+    def modify_frequency_percentage(frequency, hist):
+        total_count = np.sum(hist)
+        target_frequency = int(total_count * 0.05) 
+        if frequency > target_frequency:
+            for i in range(len(hist)):
+                if hist[i] > 0 and hist[i] >= frequency - target_frequency:
+                    hist[i] -= (frequency - target_frequency)
+                    break
+            frequency = target_frequency
+        return frequency
+    frequency = modify_frequency_percentage(frequency, hist)
+    frequency_percentage = (frequency / np.sum(hist)) * 100
+    fg=frequency_percentage-0.001
+    freque_pe=abs(frequency_percentage-fg)
+    return freque_pe
+
+
+
+    
+
+
+
---
+++ packages/fansile666/fansile666-0.1.0.tar.gz/fansile666-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fansile666',
+    version='0.1.0',
+    author='你的名字',
+    author_email='你的邮箱@example.com',
+    description='一个描述你的包的简短说明',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/mypackage',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
