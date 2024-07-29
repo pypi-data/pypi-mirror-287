49763a1a904f2380cb9a4a1cd9984fa3902a6d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:05:11 +0000

    Add package edgartools/2.27.4/edgartools-2.27.4.tar.gz

---
+++ packages/edgartools/edgartools-2.27.4.tar.gz/edgartools-2.27.4/edgar/xbrl/__init__.py
@@ -0,0 +1,7 @@
+from edgar.xbrl.financials import Financials
+from edgar.xbrl.parser import XBRLData, XBRLInstance,  StatementData, Statements
+from edgar.xbrl.presentation import XBRLPresentation
+
+
+
+
