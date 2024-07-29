40ca2765b177b1db6ba7243ea823fa118d610745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:04:39 +0000

    Add package slark/0.1.6/slark-0.1.6-py3-none-any.whl

---
+++ packages/slark/slark-0.1.6-py3-none-any.whl/slark/types/__init__.py
@@ -0,0 +1,9 @@
+from .client_types import CredentailTypes
+from .spreadsheets.data.common import CellTypes
+from .spreadsheets.data.range import Range
+
+__all__ = [
+    "CredentailTypes",
+    "Range",
+    "CellTypes",
+]
