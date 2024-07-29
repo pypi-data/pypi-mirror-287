45fff0e0da8f386682e59a66101f98776a7a0357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:54 +0000

    Add package kbrainsdk/0.12.0a4/kbrainsdk-0.12.0a4.tar.gz

---
+++ packages/kbrainsdk/kbrainsdk-0.12.0a4.tar.gz/kbrainsdk-0.12.0a4/kbrainsdk/events.py
@@ -0,0 +1,20 @@
+from typing import Any
+from kbrainsdk.validation.messages import validate_create_subscription, validate_servicebus_message, validate_servicebus_queue, validate_servicebus_topic, validate_websocket_group_request, validate_websocket_subscription_request
+from kbrainsdk.apibase import APIBase
+
+class Events(APIBase):
+
+    def __init__(self, *args: Any, **kwds: Any) -> Any:
+        return super().__init__(*args, **kwds)
+    
+    def publish_message(self, message: str, topic_name: str, application_properties: dict | None = None) -> None:
+        payload = {
+            "message": message,
+            "topic_name": topic_name,
+            "application_properties": application_properties
+        }
+        
+        validate_servicebus_message(payload)
+        path = f"/service_bus/send/v1"
+        response = self.apiobject.call_endpoint(path, payload, "post")
+        return response
