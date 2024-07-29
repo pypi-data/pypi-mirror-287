41270e305cedfd969baf9948023f4fe9d6189183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:08 +0000

    Add package cara-api/1.6.2/cara_api-1.6.2.tar.gz

---
+++ packages/cara-api/cara_api-1.6.2.tar.gz/cara_api-1.6.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("readme_pachage.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cara_api',
+    version='1.6.2',
+    packages=["cara_api"],
+    install_requires=["requests", "reds-simple-logger"],
+    author="Red_Wolf2467",
+    author_email="support@pyropixle.com",
+    description="Allows you to interact with the Cara API.",
+    long_description=long_description,  # Setze die lange Beschreibung
+    long_description_content_type="text/markdown",
+    python_requires='>=3.8'
+)
