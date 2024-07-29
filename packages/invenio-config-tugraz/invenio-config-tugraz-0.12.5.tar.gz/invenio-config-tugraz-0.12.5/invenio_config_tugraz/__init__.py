742841e819fe81ace07cf9cb479689a54320b030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:53 +0000

    Add package invenio-config-tugraz/0.12.5/invenio-config-tugraz-0.12.5.tar.gz

---
+++ packages/invenio-config-tugraz/invenio-config-tugraz-0.12.5.tar.gz/invenio-config-tugraz-0.12.5/invenio_config_tugraz/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2024 Graz University of Technology.
+#
+# invenio-config-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""invenio module that adds tugraz configs."""
+
+from .ext import InvenioConfigTugraz
+from .utils import get_identity_from_user_by_email
+
+__version__ = "0.12.5"
+
+__all__ = (
+    "__version__",
+    "InvenioConfigTugraz",
+    "get_identity_from_user_by_email",
+)
