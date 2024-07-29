c34584880f0ef210c1f50a461834651219bac712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:04:31 +0000

    Add package tach/0.8.3/tach-0.8.3-cp312-none-win32.whl

---
+++ packages/tach/tach-0.8.3-cp312-none-win32.whl/tach/core/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from tach.core.config import (
+    ModuleConfig,
+    ProjectConfig,
+    UnusedDependencies,
+)
+from tach.core.modules import ModuleNode, ModuleTree
+
+__all__ = [
+    "ProjectConfig",
+    "ModuleConfig",
+    "ModuleNode",
+    "ModuleTree",
+    "UnusedDependencies",
+]
