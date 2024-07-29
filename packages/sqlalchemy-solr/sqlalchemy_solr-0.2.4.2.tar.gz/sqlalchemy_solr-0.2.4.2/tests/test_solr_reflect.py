96e068569c9c97d8e61f974305c894574a850165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:03:53 +0000

    Add package sqlalchemy-solr/0.2.4.2/sqlalchemy_solr-0.2.4.2.tar.gz

---
+++ packages/sqlalchemy-solr/sqlalchemy_solr-0.2.4.2.tar.gz/sqlalchemy_solr-0.2.4.2/tests/test_solr_reflect.py
@@ -0,0 +1,65 @@
+from numpy import nan
+from pandas import DataFrame
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from sqlalchemy import types
+from sqlalchemy_solr.solrdbapi import Connection
+from sqlalchemy_solr.solrdbapi import Cursor
+from sqlalchemy_solr.solrdbapi import SolrTableReflection
+
+
+class TestSolrTableReflection:
+    # pylint: disable=too-few-public-methods
+    def test_solr_columns_reflection(self, settings):
+        sql = """
+        SELECT CITY_s, PHONE_ss FROM sales_test_
+        """
+
+        if settings["SOLR_USER"]:
+            auth = HTTPBasicAuth(settings["SOLR_USER"], settings["SOLR_PASS"])
+        else:
+            auth = None
+
+        session = Session()
+        session.auth = auth
+
+        SolrTableReflection.connection = Connection(
+            settings["HOST"],
+            "test",
+            settings["SOLR_USER"],
+            settings["SOLR_PASS"],
+            settings["SERVER_PATH"],
+            settings["SOLR_WORKER_COLLECTION_NAME"],
+            settings["PORT"],
+            settings["PROTO"],
+            session,
+        )
+        result = Cursor.submit_query(
+            Cursor.substitute_in_query(sql, []),
+            settings["HOST"],
+            settings["PORT"],
+            settings["PROTO"],
+            settings["SERVER_PATH"],
+            settings["SOLR_WORKER_COLLECTION_NAME"],
+            session,
+        )
+
+        rows = result.json()["result-set"]["docs"]
+
+        assert len(rows) > 0
+
+        columns = list(rows[0].keys())
+
+        result_set = DataFrame(data=rows, columns=columns).fillna(value=nan)
+
+        reflected_data_types = SolrTableReflection.reflect_column_types(
+            result_set, columns, sql
+        )
+
+        columns = dict(
+            zip(reflected_data_types[0], reflected_data_types[1])  # noqa: B905
+        )
+
+        assert isinstance(columns["CITY_s"], types.VARCHAR)
+        assert isinstance(columns["PHONE_ss"], types.ARRAY)
+        assert isinstance(columns["PHONE_ss"].item_type, types.VARCHAR)
