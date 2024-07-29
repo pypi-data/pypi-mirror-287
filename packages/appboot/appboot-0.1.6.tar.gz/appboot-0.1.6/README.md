2dd94d45fbb9bfe1e815820f1fdc345994656007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:12 +0000

    Add package appboot/0.1.6/appboot-0.1.6.tar.gz

---
+++ packages/appboot/appboot-0.1.6.tar.gz/appboot-0.1.6/README.md
@@ -0,0 +1,35 @@
+# App Boot
+App Boot a FastAPI project template, Designed to provide a Django-like structure and development experience.
+
+# Installation
+Require Python 3.9+
+```shell
+pip3 install appboot
+```
+
+# Quick start
+Startup up a new project
+```shell
+# Create the project directory
+mkdir mysite
+cd mysite
+
+# Create a virtual environment to isolate our package dependencies locally
+python3 -m venv env
+source env/bin/activate  # On Windows use `env\Scripts\activate`
+
+# Install appboot and aiosqlite into the virtual environment
+pip3 install appboot aiosqlite
+
+# Set up a new project with a single application
+appboot startproject mysite .  # Note the trailing '.' character
+
+# start runserver Application running on http://127.0.0.1:8000
+python3 manage.py runserver
+```
+Startup up a new app
+```shell
+python3 manage.py startapp polls
+```
+# Try out examples
+Go to [Examples](./examples)
---
+++ packages/appboot/appboot-0.1.6.tar.gz/appboot-0.1.6/appboot/interfaces.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Optional,
+    Protocol,
+    Sequence,
+    TypeVar,
+    runtime_checkable,
+)
+
+if TYPE_CHECKING:
+    from appboot.schema import ModelSchema
+
+T = TypeVar('T')
+
+
+@runtime_checkable
+class BaseRepository(Protocol[T]):
+    def disable_option(self):
+        ...
+
+    def filter(self, *condition):
+        ...
+
+    def filter_by(self, **kwargs):
+        ...
+
+    def limit(self, num: int):
+        ...
+
+    def offset(self, num: int):
+        ...
+
+    def slice(self, start: int, end: int):
+        ...
+
+    def options(self, *args):
+        ...
+
+    def order_by(self, *args):
+        ...
+
+    def get_query(self):
+        ...
+
+    def clone(self):
+        ...
+
+    async def count(self) -> int:
+        ...
+
+    async def all(self) -> Sequence[T]:
+        ...
+
+    async def first(self) -> Optional[T]:
+        ...
+
+    async def get(self, pk: int) -> Optional[T]:
+        ...
+
+    async def bulk_create(self, objs: list[ModelSchema], flush=False) -> list[T]:
+        ...
+
+    async def create(self, obj: ModelSchema, flush=False) -> T:
+        ...
+
+    async def update(self, pk: int, obj: ModelSchema, flush=False) -> T:
+        ...
+
+    async def delete(self, pk: int, flush=False) -> T:
+        ...
