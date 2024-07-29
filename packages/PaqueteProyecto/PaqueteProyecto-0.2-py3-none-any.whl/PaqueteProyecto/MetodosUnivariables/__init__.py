4aadd806fa3f92034d5b51f20b7b5d823ce52f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:04:28 +0000

    Add package PaqueteProyecto/0.2/PaqueteProyecto-0.2-py3-none-any.whl

---
+++ packages/PaqueteProyecto/PaqueteProyecto-0.2-py3-none-any.whl/PaqueteProyecto/MetodosUnivariables/__init__.py
@@ -0,0 +1,6 @@
+from .biseccion import biseccion
+from .BusquedaDorada import busquedaDorada
+from .Fibonacci import fibonacci_search
+from .IntervalHalving import interval_halving_method
+from .NewtonRaph import newton_raphson
+from .Secante import secante
