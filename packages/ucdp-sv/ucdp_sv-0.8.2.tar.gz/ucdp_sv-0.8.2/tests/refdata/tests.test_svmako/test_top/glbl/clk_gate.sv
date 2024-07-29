04f3fce985b7c552f599c9ab86b560b3b14f0e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:11 +0000

    Add package ucdp-sv/0.8.2/ucdp_sv-0.8.2.tar.gz

---
+++ packages/ucdp-sv/ucdp_sv-0.8.2.tar.gz/ucdp_sv-0.8.2/tests/refdata/tests.test_svmako/test_top/glbl/clk_gate.sv
@@ -0,0 +1,31 @@
+// GENERATE INPLACE BEGIN fileheader() =========================================
+//
+// Module:     glbl.clk_gate
+// Data Model: glbl.clk_gate.ClkGateMod
+//
+// GENERATE INPLACE END fileheader =============================================
+
+// GENERATE INPLACE BEGIN header() =============================================
+`begin_keywords "1800-2009"
+`default_nettype none  // implicit wires are forbidden
+// GENERATE INPLACE END header =================================================
+
+// GENERATE INPLACE BEGIN beginmod() ===========================================
+module clk_gate ( // glbl.clk_gate.ClkGateMod
+  input  logic clk_i,
+  output logic clk_o,
+  input  logic ena_i
+);
+// GENERATE INPLACE END beginmod ===============================================
+
+    // GENERATE INPLACE BEGIN logic() ==========================================
+    // GENERATE INPLACE END logic ==============================================
+
+// GENERATE INPLACE BEGIN endmod() =============================================
+endmodule // clk_gate
+// GENERATE INPLACE END endmod =================================================
+
+// GENERATE INPLACE BEGIN footer() =============================================
+`default_nettype wire
+`end_keywords
+// GENERATE INPLACE END footer =================================================
