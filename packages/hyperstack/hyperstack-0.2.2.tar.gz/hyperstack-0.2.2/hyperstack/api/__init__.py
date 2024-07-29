04bbca31244ebcd347b28b16e38ebe2efebe0fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:03:52 +0000

    Add package hyperstack/0.2.2/hyperstack-0.2.2.tar.gz

---
+++ packages/hyperstack/hyperstack-0.2.2.tar.gz/hyperstack-0.2.2/hyperstack/api/__init__.py
@@ -0,0 +1,21 @@
+from . import environments
+from . import flavors
+from . import images
+from . import network
+from . import profiles
+from . import regions
+from . import stock
+from . import virtual_machines
+from . import volumes
+
+__all__ = [
+    'environments',
+    'flavors',
+    'images',
+    'network',
+    'profiles',
+    'regions',
+    'stock',
+    'virtual_machines',
+    'volumes'
+]
---
+++ packages/hyperstack/hyperstack-0.2.2.tar.gz/hyperstack-0.2.2/hyperstack/api/virtual_machines.py
@@ -0,0 +1,62 @@
+def create_vm(self, name, image_name, flavor_name, key_name="development-key", user_data="", create_bootable_volume=False, assign_floating_ip=False, count=1):
+    self._check_environment_set()
+    
+    payload = {
+        "name": name,
+        "environment_name": self.environment,
+        "image_name": image_name,
+        "create_bootable_volume": create_bootable_volume,
+        "flavor_name": flavor_name,
+        "key_name": key_name,
+        "user_data": user_data,
+        "assign_floating_ip": assign_floating_ip,
+        "count": count
+    }
+    
+    return self.post("core/virtual-machines", data=payload)
+
+def list_virtual_machines(self):
+    self._check_environment_set()
+    return self.get("core/virtual-machines")
+
+def retrieve_vm_details(self, vm_id):
+    self._check_environment_set()
+    return self.get(f"core/virtual-machines/{vm_id}")
+
+def start_virtual_machine(self, vm_id):
+    self._check_environment_set()
+    return self.get(f"core/virtual-machines/{vm_id}/start")
+
+def stop_virtual_machine(self, vm_id):
+    self._check_environment_set()
+    return self.get(f"core/virtual-machines/{vm_id}/stop")
+
+def hard_reboot_virtual_machine(self, vm_id):
+    self._check_environment_set()
+    return self.get(f"core/virtual-machines/{vm_id}/hard-reboot")
+
+def hibernate_virtual_machine(self, vm_id):
+    self._check_environment_set()
+    return self.get(f"core/virtual-machines/{vm_id}/hibernate")
+
+def restore_hibernated_virtual_machine(self, vm_id):
+    self._check_environment_set()
+    return self.get(f"core/virtual-machines/{vm_id}/hibernate-restore")
+
+def delete_virtual_machine(self, vm_id):
+    self._check_environment_set()
+    return self.delete(f"core/virtual-machines/{vm_id}")
+
+def resize_virtual_machine(self, vm_id, flavor):
+    self._check_environment_set()
+    payload = {'flavor_name': flavor}
+    return self.post(f"core/virtual-machines/{vm_id}/resize", data=payload)
+
+def update_virtual_machine_labels(self, vm_id, labels: list):
+    self._check_environment_set()
+    payload = {'labels': labels}
+    return self.put(f"core/virtual-machines/{vm_id}/label", data=payload)
+
+def get_floating_ip(self, vm_id):
+    response = self.retrieve_vm_details(vm_id)
+    return response['instance']['floating_ip']
