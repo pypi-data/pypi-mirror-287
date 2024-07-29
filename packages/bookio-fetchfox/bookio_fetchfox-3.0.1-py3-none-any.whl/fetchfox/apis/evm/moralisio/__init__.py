49b1f85e6d38a0c2f2066742dccc2de667f7a827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 14:04:27 +0000

    Add package bookio-fetchfox/3.0.1/bookio_fetchfox-3.0.1-py3-none-any.whl

---
+++ packages/bookio-fetchfox/bookio_fetchfox-3.0.1-py3-none-any.whl/fetchfox/apis/evm/moralisio/__init__.py
@@ -0,0 +1,14 @@
+# addresses
+from .addresses import get_assets as get_address_assets
+from .addresses import get_balance as get_address_balance
+
+# assets
+from .assets import get_data as get_asset_data
+from .assets import get_holders as get_asset_holders
+
+# contracts
+from .contracts import get_assets as get_contract_assets
+from .contracts import get_holders as get_contract_holders
+
+# wallets
+from .wallets import get_transactions as get_wallet_transactions
