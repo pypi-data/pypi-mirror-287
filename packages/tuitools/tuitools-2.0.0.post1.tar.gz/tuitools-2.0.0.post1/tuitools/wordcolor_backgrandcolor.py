7e27849242c0bcf6633bf9f22ac055f53b0b9618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 15:04:30 +0000

    Add package tuitools/2.0.0.post1/tuitools-2.0.0.post1.tar.gz

---
+++ packages/tuitools/tuitools-2.0.0.post1.tar.gz/tuitools-2.0.0.post1/tuitools/wordcolor_backgrandcolor.py
@@ -0,0 +1,14 @@
+def normal_red(word):
+    print('\033[41m',word)
+def normal_black(word):
+    print('\033[40m',word)
+def normal_green(word):
+    print('\033[42m',word)
+def normal_blue(word):
+    print('\033[44m',word)
+def black_white(word):
+    print('\033[7m',word)
+def normal_yellow(word):
+    print('\033[102m',word)
+def normal_white(word):
+    print('\033[107m',word)
