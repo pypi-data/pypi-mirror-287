e7d400a55cd8dc8432426166365f9baca8001b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 03:05:16 +0000

    Add package irust-kernel/0.6.0/irust_kernel-0.6.0.tar.gz

---
+++ packages/irust-kernel/irust_kernel-0.6.0.tar.gz/irust_kernel-0.6.0/irust_kernel/install.py
@@ -0,0 +1,124 @@
+import argparse
+import json
+import os
+import pathlib
+import subprocess
+import shutil
+import sys
+
+from jupyter_client.kernelspec import KernelSpecManager
+from IPython.utils.tempdir import TemporaryDirectory
+
+from .resources import LOGO_PATH
+
+
+kernel_json = {
+  "argv": [sys.executable, "-m", "irust_kernel", "-f", "{connection_file}"],
+  "display_name": "IRust",
+  "language":"rust",
+}
+
+def install_my_kernel_spec(user=True, prefix=None, local_build=False):
+    def get_cargo_target_dir():
+        target = "target"
+        if 'CARGO_TARGET_DIR' in os.environ:
+            target = os.environ['CARGO_TARGET_DIR']
+        return target
+
+
+    with TemporaryDirectory() as td:
+        cargo_target_dir = get_cargo_target_dir()
+        os.chmod(td, 0o755) # Starts off as 700, not user readable
+        with open(os.path.join(td, 'kernel.json'), 'w') as f:
+            json.dump(kernel_json, f, sort_keys=True)
+        shutil.copyfile(LOGO_PATH, pathlib.Path(td) / LOGO_PATH.name)
+
+        if local_build:
+            print('Building `Re` executable')
+            try:
+                subprocess.run(["cargo", "b", "--example", "re", "--target-dir", cargo_target_dir],check=True)
+            except:
+                print('--local-build needs to be used inside irust repo')
+                exit(1)
+
+            src = os.path.join(cargo_target_dir, "debug", "examples", "re")
+            dst = os.path.join(td, "re")
+            os.symlink(src, dst)
+
+        else:
+            print('Fetching `irust` repo and compiling `Re` executable')
+            subprocess.run(["git", "clone","--depth","1", "https://github.com/sigmasd/irust"],cwd=td)
+            irust_repl_dir = os.path.join(td,"irust", "crates", "irust_repl")
+            subprocess.run(
+                [
+                    "cargo",
+                    "b",
+                    "--release",
+                    "--example",
+                    "re",
+                    "--target-dir",
+                    cargo_target_dir,
+                    "--manifest-path",
+                    os.path.join(irust_repl_dir, "Cargo.toml"),
+                ]
+            )
+          
+            src = os.path.join(cargo_target_dir, "release", "examples", "re")
+            dst = os.path.join(td, "re")
+            shutil.copy2(src, dst)
+            shutil.rmtree(os.path.join(td,"irust"))
+
+        print('Installing IRust kernel spec')
+        KernelSpecManager().install_kernel_spec(td, 'irust', user=user, prefix=prefix)
+        print('done')
+
+def _is_root():
+    try:
+        return os.geteuid() == 0
+    except AttributeError:
+        return False # assume not an admin on non-Unix platforms
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description='Install KernelSpec for IRust Kernel'
+    )
+    prefix_locations = parser.add_mutually_exclusive_group()
+
+    prefix_locations.add_argument(
+        '--user',
+        help='Install KernelSpec in user\'s home directory',
+        action='store_true'
+    )
+    prefix_locations.add_argument(
+        '--sys-prefix',
+        help='Install KernelSpec in sys.prefix. Useful in conda / virtualenv',
+        action='store_true',
+        dest='sys_prefix'
+    )
+    prefix_locations.add_argument(
+        '--prefix',
+        help='Install KernelSpec in this prefix',
+        default=None
+    )
+
+    parser.add_argument('--local-build',
+        help = "Build `Re` locally and copy it to the kernel location",
+        default = False,
+        action='store_true'
+    )
+
+    args = parser.parse_args(argv)
+
+    user = False
+    prefix = None
+    if args.sys_prefix:
+        prefix = sys.prefix
+    elif args.prefix:
+        prefix = args.prefix
+    elif args.user or not _is_root():
+        user = True
+
+    install_my_kernel_spec(user=user, prefix=prefix, local_build=args.local_build)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/irust-kernel/irust_kernel-0.6.0.tar.gz/irust_kernel-0.6.0/irust_kernel/kernel.py
@@ -0,0 +1,89 @@
+"""IRust repl jupyter kernel """
+
+__version__ = '0.6.0'
+
+from ipykernel.kernelbase import Kernel
+from jupyter_client.kernelspec import KernelSpecManager
+import os
+import json
+import subprocess
+
+def log(msg):
+    # with open("/tmp/irust-kernel-py.log", 'a') as f:
+    #     f.write(msg)
+    pass
+
+class IRustKernel(Kernel):
+    implementation = 'IRust'
+    implementation_version = '1.0'
+    language = 'rust'
+    language_version = '1'
+    language_info = {
+        'name': 'IRust',
+        'mimetype': 'text/x-rust',
+        'file_extension': '.rs',
+    }
+    banner = "IRust"
+    kernel_name = "irust"
+
+    def __init__(self, **kwargs):
+        cmd = ["cmd","/c", os.path.join(self.get_kernel_location(),"re")] if os.name == 'nt' else [os.path.join(self._get_kernel_location(),"re"),]
+        self.re = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        super().__init__(**kwargs)
+
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
+        # Send the first JSON object to the process's standard input
+        code_object = {"Execute": {"code": code}}
+        json_code = json.dumps(code_object)
+        self.re.stdin.write(json_code.encode("utf-8"))
+        self.re.stdin.write(b"\n")
+        self.re.stdin.flush()
+
+        # Read the first JSON object from the process's standard output
+        json_result = self.re.stdout.readline().decode("utf-8")
+        action_type = json.loads(json_result)
+
+        # Handle Stream response
+        if "AddDependencyStream" in action_type:
+            log("AddDependencyStream")
+            while True:
+                action = action_type["AddDependencyStream"]
+                self.send_response(self.iopub_socket, 'stream', {
+                    'name': 'stderr',
+                    'text': action["output_chunk"]
+                })
+                log(action["output_chunk"])
+                json_result = self.re.stdout.readline().decode("utf-8")
+                action_type = json.loads(json_result)
+                if "AddDependencyEnd" in action_type:
+                    log("AddDependencyEnd")
+                    break
+
+        # Handle Eval
+        if "Eval" in action_type:
+            action = action_type["Eval"]
+            self.send_response(self.iopub_socket, 'display_data', {
+                'metadata': {},
+                'data': {
+                    action["mime_type"]: action["value"]
+                }
+            })
+
+        return {'status': 'ok',
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
+
+
+    def _get_kernel_location(self):
+        kernel_spec_manager = KernelSpecManager()
+        kernel_dir =  os.path.join(str(kernel_spec_manager.user_kernel_dir),self.kernel_name)
+        return kernel_dir
+
+
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=IRustKernel)
