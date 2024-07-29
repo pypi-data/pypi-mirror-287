6f2321ee891a51c492776e1c360389f9c2c3b510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:03:52 +0000

    Add package seraphim/0.1.0/seraphim-0.1.0-py3-none-any.whl

---
+++ packages/seraphim/seraphim-0.1.0-py3-none-any.whl/seraphim/__init__.py
@@ -0,0 +1,10 @@
+from .api import API as API
+from .middleware import Middleware as Middleware
+from .response import Response as Response
+from .orm import Database as Database
+from .orm import Table as Table
+from .orm import Column as Column
+from .orm import ForeignKey as ForeignKey
+
+
+__version__ = "0.1.0"
