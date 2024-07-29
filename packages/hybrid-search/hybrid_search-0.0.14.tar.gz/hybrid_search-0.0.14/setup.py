70a9e3722c239d5f779241a55d3271663c2c651c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:07 +0000

    Add package hybrid-search/0.0.14/hybrid_search-0.0.14.tar.gz

---
+++ packages/hybrid-search/hybrid_search-0.0.14.tar.gz/hybrid_search-0.0.14/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.14'
+DESCRIPTION = 'Hybrid search with OpenSearch and Langchain'
+LONG_DESCRIPTION = 'Package for the hybrid search with opensearch and langchain library'
+
+# Setting up
+setup(
+    name="hybrid_search",
+    version=VERSION,
+    author="Alex Karmazin",
+    author_email="<karmazinalex@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pycomfort>=0.0.15', 'click','transformers>=4.35.2', "sentence_transformers", "opensearch-py>=2.6.0", "langchain>=0.2.11", "langchain-community>=0.2.10", "langchain-huggingface>=0.0.3"],
+    keywords=['python', 'llm', 'science', 'review', 'hybrid search', 'semantic search'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
