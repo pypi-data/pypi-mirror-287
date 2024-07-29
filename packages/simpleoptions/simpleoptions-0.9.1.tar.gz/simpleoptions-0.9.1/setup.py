017e545ac4955c7b483601c7dca6a90f04340928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 04:04:10 +0000

    Add package simpleoptions/0.9.1/simpleoptions-0.9.1.tar.gz

---
+++ packages/simpleoptions/simpleoptions-0.9.1.tar.gz/simpleoptions-0.9.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="simpleoptions",
+    version="0.9.1",
+    author="Joshua Evans",
+    author_email="jbe25@bath.ac.uk",
+    description="A simple and flexible framework for working with Options in Reinforcement Learning.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ueva/BaRL-SimpleOptions",
+    packages=setuptools.find_packages(exclude=("example", "test")),
+    # packages=["simpleoptions"],
+    install_requires=["numpy", "networkx", "gymnasium"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Natural Language :: English",
+    ],
+    python_requires=">=3.8",
+)
