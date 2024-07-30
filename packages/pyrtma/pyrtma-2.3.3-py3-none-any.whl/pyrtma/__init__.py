a7e1433c536d84202fd7ff5452c8197a7daad885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:04:42 +0000

    Add package pyrtma/2.3.3/pyrtma-2.3.3-py3-none-any.whl

---
+++ packages/pyrtma/pyrtma-2.3.3-py3-none-any.whl/pyrtma/__init__.py
@@ -0,0 +1,13 @@
+from .message import *
+from .client import *
+
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
