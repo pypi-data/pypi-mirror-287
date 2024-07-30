dd93d7dac93a4129c4c2ac63388d04b858d5fa64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 05:04:09 +0000

    Add package MobileInventoryCLI/0.4.376/MobileInventoryCLI-0.4.376.tar.gz

---
+++ packages/MobileInventoryCLI/MobileInventoryCLI-0.4.376.tar.gz/MobileInventoryCLI-0.4.376/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+from datetime import datetime
+version='0.4.376'
+
+setup(name='MobileInventoryCLI',
+      version=version,
+      author="Carl Joseph Hirner III",
+      author_email="k.j.hirner.wisdom@gmail.com",
+      description="modify/update/use MobileInventoryPro *.bck files",
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Operating System :: Android',
+        'Environment :: Console',
+        'Programming Language :: SQL',
+          ],
+      packages=find_packages(),
+      python_requires='>=3.6',
+      install_requires=['readline','nanoid','random-password-generator','cython','pint','pyupc-ean','openpyxl','plyer','colored','numpy','pandas','Pillow','python-barcode','qrcode','requests','sqlalchemy','argparse','geocoder','beautifulsoup4','pycryptodome','forecast_weather'],
+      package_data={
+        '':["*.config","*.txt","*.README","*.TTF"],
+        }
+      )
+
