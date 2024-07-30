549e75ca5820695292c41f21781e3efbe698ffa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:17 +0000

    Add package burla/0.1.0a0/burla-0.1.0a0-py3-none-any.whl

---
+++ packages/burla/burla-0.1.0a0-py3-none-any.whl/burla/__init__.py
@@ -0,0 +1,22 @@
+import os
+from fire import Fire
+
+# Imported here to avoid cyclic imports
+if os.getenv("IN_DEV") == "True":
+    _BURLA_SERVICE_URL = "http://127.0.0.1:5001"
+    # _BURLA_SERVICE_URL = "https://cluster.test.burla.dev"
+    # _BURLA_BACKEND_URL = "http://127.0.0.1:5002"
+    _BURLA_BACKEND_URL = "https://backend.test.burla.dev"
+else:
+    _BURLA_SERVICE_URL = "https://cluster.burla.dev"
+    _BURLA_BACKEND_URL = "https://backend.burla.dev"
+
+__version__ = "0.1.0-alpha"
+
+from burla._auth import login, login_cmd as _login_cmd
+from burla._nas import cd, ls, upload, download
+from burla._remote_parallel_map import remote_parallel_map
+
+
+def init_cli():
+    Fire({"login": _login_cmd, "nas": {"cd": cd, "ls": ls, "upload": upload, "download": download}})
