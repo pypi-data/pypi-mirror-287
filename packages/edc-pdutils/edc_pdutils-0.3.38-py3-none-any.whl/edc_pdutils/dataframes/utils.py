15ec889d8c6f9074cbf782353de14484ca777862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 07:04:04 +0000

    Add package edc-pdutils/0.3.38/edc_pdutils-0.3.38-py3-none-any.whl

---
+++ packages/edc-pdutils/edc_pdutils-0.3.38-py3-none-any.whl/edc_pdutils/dataframes/utils.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+numeric_datatypes = [
+    "DecimalField",
+    "IntegerField",
+    "BigIntegerField",
+    "FloatField",
+    "PositiveBigIntegerField",
+    "PositiveIntegerField",
+    "PositiveSmallIntegerField",
+    "SmallIntegerField",
+]
+
+date_datatypes = ["DateTimeField", "DateField"]
+
+
+def convert_numerics_from_model(source_df: pd.DataFrame, model_cls) -> pd.DataFrame:
+    numeric_cols = []
+    for field_cls in model_cls._meta.get_fields():
+        if field_cls.get_internal_type() in numeric_datatypes:
+            numeric_cols.append(field_cls.name)
+    if numeric_cols:
+        source_df[numeric_cols] = source_df[numeric_cols].apply(pd.to_numeric, errors="coerce")
+    return source_df
+
+
+def convert_dates_from_model(source_df: pd.DataFrame, model_cls) -> pd.DataFrame:
+    date_cols = []
+    for field_cls in model_cls._meta.get_fields():
+        if field_cls.get_internal_type() in date_datatypes:
+            date_cols.append(field_cls.name)
+    if date_cols:
+        source_df[date_cols] = source_df[date_cols].apply(pd.to_datetime, errors="coerce")
+    return source_df
