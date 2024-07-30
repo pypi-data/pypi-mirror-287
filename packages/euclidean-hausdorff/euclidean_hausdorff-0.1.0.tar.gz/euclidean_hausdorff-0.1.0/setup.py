5607922213374868b919c54381bd64d9bed97db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:28 +0000

    Add package euclidean-hausdorff/0.1.0/euclidean_hausdorff-0.1.0.tar.gz

---
+++ packages/euclidean-hausdorff/euclidean_hausdorff-0.1.0.tar.gz/euclidean_hausdorff-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='euclidean_hausdorff',
+    version='0.1.0',
+    author='Vladyslav Oles',
+    author_email='vlad.oles@proton.me',
+    packages=['euclidean_hausdorff'],
+    url='http://pypi.python.org/pypi/euclidean-hausdorff/',
+    license='LICENSE',
+    description='Approximating the Euclidean Hausdorff Distance',
+    long_description=open('README.MD').read(),
+    install_requires=[
+        "scipy >= 1.12.0",
+        "sortedcontainers >= 2.4.0",
+        "tqdm >= 4.66.4",
+        "numpy >= 1.26.4",
+    ],
+)
