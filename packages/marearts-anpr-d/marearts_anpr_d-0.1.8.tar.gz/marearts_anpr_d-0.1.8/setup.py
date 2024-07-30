c0b5822389aa0701186a5e08123e8c778cb46716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:46 +0000

    Add package marearts-anpr-d/0.1.8/marearts_anpr_d-0.1.8.tar.gz

---
+++ packages/marearts-anpr-d/marearts_anpr_d-0.1.8.tar.gz/marearts_anpr_d-0.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+setup(
+    name="marearts-anpr-d",
+    version="0.1.8",
+    packages=find_packages(),
+    ext_modules = cythonize("source_code/crypto_mng.pyx"),
+    package_data={'': ['*.pyx']},
+    include_package_data=True,
+)
