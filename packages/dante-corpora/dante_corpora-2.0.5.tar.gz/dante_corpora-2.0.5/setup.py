b6f3f28913b1081a9c90ec65769254514035b48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 07:04:27 +0000

    Add package dante-corpora/2.0.5/dante_corpora-2.0.5.tar.gz

---
+++ packages/dante-corpora/dante_corpora-2.0.5.tar.gz/dante_corpora-2.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dante-corpora',
+    version='2.0.5',
+    description='Pacote Python contendo os corpora do projeto DANTE do POeTiSA',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='felmateos',
+    author_email='felmateos@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'dante': ['data/**/*']
+    },
+    install_requires=[
+        'pandas==2.2.2'
+    ],
+    license='MIT'
+)
