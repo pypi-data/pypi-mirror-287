4b165fa972a6a3014689c5809482532fe9eb7818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 05:03:57 +0000

    Add package sec-visualizers/0.2/sec_visualizers-0.2.tar.gz

---
+++ packages/sec-visualizers/sec_visualizers-0.2.tar.gz/sec_visualizers-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+long_description = Path("../readme.md").read_text()
+
+setup(
+    name="sec_visualizers",
+    author="John Friedman",
+    version="0.002",
+    description = "A package to visualize SEC filings",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url="https://github.com/john-friedman/SEC-Visualizers",
+    install_requires=[
+        'flask',
+    ],
+)
