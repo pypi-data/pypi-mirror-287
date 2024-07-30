2cafe275ed551c37f215e75f2145a9c76d677ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:45 +0000

    Add package mcptool-py/1.0.11/mcptool_py-1.0.11.tar.gz

---
+++ packages/mcptool-py/mcptool_py-1.0.11.tar.gz/mcptool_py-1.0.11/src/mcptool/commands/clearservers.py
@@ -0,0 +1,22 @@
+from loguru import logger
+from mccolors import mcwrite
+
+from ..nbt.servers_dat import ServersDAT
+from ..utilities.language.utilities import LanguageUtils as Lm
+
+
+class Command:
+    @logger.catch
+    def __init__(self):
+        self.name: str = 'clearservers'
+        self.command_arguments: list = [i for i in Lm.get(f'commands.{self.name}.arguments')]
+
+    @logger.catch
+    def execute(self, user_arguments: list) -> bool:
+        """
+        Method to execute the command
+        :param user_arguments: list: The arguments to execute the command
+        """
+        mcwrite(Lm.get(f'commands.{self.name}.serversCleared'))
+        ServersDAT().remove_servers_dat_file()
+        return True
