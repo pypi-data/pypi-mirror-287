84ced119af3aeadd4ac96287ae50633679f883bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:05:33 +0000

    Add package codefeedback/0.1.0/codefeedback-0.1.0.tar.gz

---
+++ packages/codefeedback/codefeedback-0.1.0.tar.gz/codefeedback-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codefeedback',  # Replace with your package name
+    version='0.1.0',  # Replace with your package version
+    description='na',  # Replace with a short description
+    packages=find_packages(),  # Automatically find and include packages
+    python_requires='>=3.6',  # Minimum Python version required
+    install_requires=[  # List of dependencies
+        'numpy',  # Example dependency
+    ],
+)
