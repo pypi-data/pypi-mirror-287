28ac2d8017e3625ec96383538c33b0db263dfcee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:04:26 +0000

    Add package griffe-modernized-annotations/1.0.4/griffe_modernized_annotations-1.0.4-py3-none-any.whl

---
+++ packages/griffe-modernized-annotations/griffe_modernized_annotations-1.0.4-py3-none-any.whl/griffe_modernized_annotations/extension.py
@@ -0,0 +1,37 @@
+from griffe import Attribute, Class, Extension, Function, Module
+
+from griffe_modernized_annotations.modernizer import AnnotationModernizer
+from griffe_modernized_annotations.proposals import PEP585, PEP604
+from griffe_modernized_annotations.traversal import walk_objects_of
+
+
+class ModernizedAnnotationsExtension(Extension):
+    def __init__(self) -> None:
+        self._modernizer = AnnotationModernizer(
+            proposals=[
+                PEP585(),
+                PEP604(),
+            ]
+        )
+
+    def on_package_loaded(self, *, pkg: Module) -> None:
+        for cls in walk_objects_of(pkg, type=Class):
+            self._handle_class(cls)
+
+        for function in walk_objects_of(pkg, type=Function):
+            self._handle_function(function)
+
+        for attribute in walk_objects_of(pkg, type=Attribute):
+            self._handle_attribute(attribute)
+
+    def _handle_class(self, cls: Class) -> None:
+        cls.bases = [self._modernizer(base) for base in cls.bases]  # type: ignore[misc]
+
+    def _handle_function(self, function: Function) -> None:
+        for parameter in function.parameters:
+            parameter.annotation = self._modernizer(parameter.annotation)
+
+        function.returns = self._modernizer(function.returns)
+
+    def _handle_attribute(self, attribute: Attribute) -> None:
+        attribute.annotation = self._modernizer(attribute.annotation)
