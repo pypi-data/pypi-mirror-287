d2c82f43f8b58005ff342737375c93cc0db48a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 05:04:33 +0000

    Add package posit-sdk/0.3.1/posit_sdk-0.3.1.tar.gz

---
+++ packages/posit-sdk/posit_sdk-0.3.1.tar.gz/posit_sdk-0.3.1/examples/connect/fastapi/app.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# mypy: ignore-errors
+import os
+from typing import Annotated
+
+from databricks import sql
+from fastapi import FastAPI, Header
+from fastapi.responses import JSONResponse
+from posit.connect.external.databricks import viewer_credentials_provider
+
+DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
+DATABRICKS_HOST_URL = f"https://{DATABRICKS_HOST}"
+SQL_HTTP_PATH = os.getenv("DATABRICKS_PATH")
+
+rows = None
+app = FastAPI()
+
+
+@app.get("/fares")
+async def get_fares(
+    posit_connect_user_session_token: Annotated[str | None, Header()] = None,
+) -> JSONResponse:
+    """
+    FastAPI example API that returns the first few rows from
+    a table hosted in Databricks.
+    """
+    global rows
+
+    credentials_provider = viewer_credentials_provider(
+        user_session_token=posit_connect_user_session_token
+    )
+
+    if rows is None:
+        query = "SELECT * FROM samples.nyctaxi.trips LIMIT 10;"
+
+        with sql.connect(
+            server_hostname=DATABRICKS_HOST,
+            http_path=SQL_HTTP_PATH,
+            auth_type="databricks-oauth",
+            credentials_provider=credentials_provider,
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                rows = cursor.fetchall()
+
+    return [row.asDict() for row in rows]
---
+++ packages/posit-sdk/posit_sdk-0.3.1.tar.gz/posit_sdk-0.3.1/examples/connect/streamlit/app.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# mypy: ignore-errors
+import os
+
+import pandas as pd
+import streamlit as st
+from databricks import sql
+from databricks.sdk.core import ApiClient, Config
+from databricks.sdk.service.iam import CurrentUserAPI
+from posit.connect.external.databricks import viewer_credentials_provider
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+
+DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
+DATABRICKS_HOST_URL = f"https://{DATABRICKS_HOST}"
+SQL_HTTP_PATH = os.getenv("DATABRICKS_PATH")
+
+session_token = _get_websocket_headers().get(
+    "Posit-Connect-User-Session-Token"
+)
+
+credentials_provider = viewer_credentials_provider(
+    user_session_token=session_token
+)
+
+cfg = Config(
+    host=DATABRICKS_HOST_URL, credentials_provider=credentials_provider
+)
+databricks_user = CurrentUserAPI(ApiClient(cfg)).me()
+st.write(f"Hello, {databricks_user.display_name}!")
+
+with sql.connect(
+    server_hostname=DATABRICKS_HOST,
+    http_path=SQL_HTTP_PATH,
+    auth_type="databricks-oauth",
+    credentials_provider=credentials_provider,
+) as connection:
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM samples.nyctaxi.trips LIMIT 10;")
+        result = cursor.fetchall()
+        st.table(pd.DataFrame(result))
