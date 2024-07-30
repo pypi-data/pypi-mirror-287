7f84bf736760ae82de2d132af97082ffd42a09f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:15 +0000

    Add package pivpy/0.0.20/pivpy-0.0.20-py3-none-any.whl

---
+++ packages/pivpy/pivpy-0.0.20-py3-none-any.whl/pivpy/graphics.py
@@ -0,0 +1,343 @@
+# -*- coding: utf-8 -*-
+"""
+Various plots, mostly wraping xarray.plot 
+
+"""
+import os
+import numpy as np
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+import xarray as xr
+from pivpy.io import POS_UNITS, VEL_UNITS
+from typing import List
+import warnings
+
+
+def quiver(
+    data: xr.DataArray,
+    arrScale: float = 25.0,
+    threshold: float = None,
+    nthArr: int = 1,
+    aspectratio: str = "equal",
+    colorbar: bool = False,
+    colorbar_orient: str = "vertical",
+    units: List = [],
+    streamlines: bool = False,
+    cmap: str = 'RdBu',
+    **kwargs,
+):
+    """creates quiver of xr.Dataset
+
+    Args:
+        data (xr.DataArray): _description_
+        arrScale (float, optional): _description_. Defaults to 25.0.
+        threshold (float, optional): _description_. Defaults to None.
+        nthArr (int, optional): _description_. Defaults to 1.
+        aspectratio (str, optional): _description_. Defaults to "equal".
+        colorbar (bool, optional): _description_. Defaults to False.
+        colorbar_orient (str, optional): _description_. Defaults to "vertical".
+        units (List, optional): _description_. Defaults to [].
+        streamlines (bool, optional): _description_. Defaults to False.
+        cmap (str, optional): matplotlib.colormap, e.g. 'jet', 'hot', 'RdBu', 'Reds'
+
+    Returns:
+        _type_: _description_
+    """
+    data = dataset_to_array(data)
+
+    pos_units = data.x.attrs["units"] if len(units) == 0 else units[0]
+    vel_units = data.u.attrs["units"] if len(units) == 0 else units[2]
+
+    # subsampling number of vectors
+    data = data.sel(x=data.x[::nthArr], y=data.y[::nthArr])  
+
+    # clip data to the threshold
+    if threshold is not None:
+        data["u"] = xr.where(data["u"] > threshold, threshold, data["u"])
+        data["v"] = xr.where(data["v"] > threshold, threshold, data["v"])
+
+    data["s"] = np.sqrt(data["u"] ** 2 + data["v"] ** 2)
+    
+    
+
+    if len(plt.get_fignums()) == 0:  # if no figure is open
+        fig, ax = plt.subplots()  # open a new figure
+    else:
+        fig = plt.gcf()
+        ax = fig.gca()
+
+    # quiver itself
+    Q = data.plot.quiver(
+        x="x",
+        y="y",
+        u="u",
+        v="v",
+        hue="s",
+        units="width",
+        scale=np.max(data["s"].values * arrScale),
+        headwidth=2,
+        cmap=cmap,
+        ax=ax,
+        **kwargs,
+    )
+
+    if colorbar is False:
+        # cbar = fig.colorbar(Q, shrink=0.9, orientation=colbar_orient)
+        cb = Q.colorbar
+        if cb:
+            cb.remove()
+        plt.draw()
+    else:
+        if colorbar_orient == "horizontal":
+            cb = Q.colorbar
+            cb.remove()
+            cb = fig.colorbar(Q, orientation=colorbar_orient, ax=ax)
+
+    if streamlines:  # contours or streamlines
+        strm = data.plot.streamplot(
+            x="x",
+            y="y",
+            u="u",
+            v="v",
+            hue="s",
+            cmap="hot",
+            linewidth=1,
+            ax=ax,
+        )
+        strm.colorbar.remove()
+
+        # if colorbar:
+        #     cbar = fig.colorbar(
+        #         strm,
+        #         orientation=colorbar_orient,
+        #         fraction=0.1,
+        #     )
+        #     cbar.set_label(r"$ V \, (" + vel_units + r")$")
+
+    ax.set_xlabel(f"x ({pos_units})")
+    ax.set_ylabel(f"y ({pos_units})")
+    ax.set_aspect(aspectratio)
+    # ax.invert_yaxis()
+
+    return fig, ax
+
+
+def histogram(data, normed=False):
+    """creates two histograms of two velocity components
+
+    Args:
+        data (_type_): _description_
+        normed (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
+
+    u = np.asarray(data.u).flatten()
+    v = np.asarray(data.v).flatten()
+
+    f, ax = plt.subplots(2)
+
+    ax[0].hist(u, bins=np.int32(np.sqrt(len(u)) * 0.5), density=normed)
+    ax[0].set_xlabel(f"u ({data.u.attrs['units']})")
+
+    ax[1] = plt.subplot2grid((2, 1), (1, 0))
+    ax[1].hist(v, bins=np.int32(np.sqrt(len(v) * 0.5)), density=normed)
+    ax[1].set_xlabel(f"v ({data.v.attrs['units']})")
+    plt.tight_layout()
+    return f, ax
+
+
+def contour_plot(
+    data: xr.DataArray,
+    threshold: float = None,
+    contourLevels: List[float] = None,
+    colorbar: bool = False,
+    logscale: bool = False,
+    aspectratio: str = "equal",
+    units: List[str] = [],
+):
+    """creates contour plot of xr.DataArray
+
+    Args:
+        data (xr.DataArray): _description_
+        threshold (float, optional): _description_. Defaults to None.
+        contourLevels (List[float], optional): _description_. Defaults to None.
+        colorbar (bool, optional): _description_. Defaults to False.
+        logscale (bool, optional): _description_. Defaults to False.
+        aspectratio (str, optional): _description_. Defaults to "equal".
+        units (List[str], optional): _description_. Defaults to [].
+
+    Returns:
+        _type_: _description_
+    """
+    data = dataset_to_array(data)
+
+    if "w" not in data.var():
+        data = data.piv.vec2scal("ke")
+
+    if threshold is not None:
+        data["w"] = xr.where(data["w"] > threshold, threshold, data["w"])
+
+    f, ax = plt.subplots()
+    # data.plot.contourf(x='x',y='y',row='y',col='x', ax=ax)
+
+    if contourLevels is None:
+        levels = np.linspace(
+            np.min(data["w"].values),
+            np.max(data["w"].values),
+            10,
+        )
+    else:
+        levels = contourLevels  # vector of levels to set
+
+    if logscale:
+        data["w"] = np.abs(data["w"])
+
+        c = data["w"].plot.contourf(
+            x="x",
+            y="y",
+            levels=levels,
+            cmap=plt.get_cmap("RdYlBu"),
+            norm=colors.LogNorm(),
+            ax=ax,
+        )
+    else:
+        c = data["w"].plot.contourf(
+            x="x",
+            y="y",
+            levels=levels,
+            cmap=plt.get_cmap("RdYlBu"),
+            ax=ax,
+        )
+
+    if not colorbar:
+        # cbar = c.colorbar(c, orientation=colbar)
+        c.colorbar.remove()
+        # cbar.set_label(propUnits)
+
+    ax.set_aspect(aspectratio)
+
+    return f, ax
+
+
+def showf(data, flow_property="ke", **kwargs):
+    """shows data as quiver over a scalar background
+
+    Args:
+        data (_type_): _description_
+        flow_property (str, optional): _description_. Defaults to "ke".
+    """
+    fig, ax = showscal(data, flow_property=flow_property, **kwargs)
+    fig, ax = quiver(data, **kwargs)
+
+
+def showscal(data, flow_property="ke", **kwargs):
+    """creates contour plot of some scalar field of a flow property
+
+    Args:
+        data (_type_): _description_
+        flow_property (str, optional): _description_. Defaults to "ke".
+
+    Returns:
+        _type_: _description_
+    """
+    data = data.piv.vec2scal(flow_property=flow_property)
+    fig, ax = contour_plot(data, **kwargs)
+    return fig, ax
+
+
+def animate(data: xr.Dataset,
+            arrowscale: int = 1,
+            savepath: str = None,
+            units: str = "pix/dt"):
+    """animates flow fields in the data and saves to MP4 format
+
+    Args:
+        data (xr.Dataset): _description_
+        arrowscale (int, optional): _description_. Defaults to 1.
+        savepath (str, optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
+    X, Y = np.meshgrid(data.x, data.y)
+    X = X.T
+    Y = Y.T
+    U, V = data.u[:, :, 0], data.v[:, :, 0]  # first frame
+    fig, ax = plt.subplots(1, 1)
+    M = np.sqrt(U**2 + V**2)
+
+    Q = ax.quiver(
+        X[::3, ::3],
+        Y[::3, ::3],
+        U[::3, ::3],
+        V[::3, ::3],
+        M[::3, ::3],
+        units="inches",
+        scale=arrowscale,
+    )
+
+    cb = plt.colorbar(Q)
+
+    # units = data.attrs["units"]
+
+    cb.ax.set_ylabel(f"velocity ({units})")
+
+    text = ax.text(
+        0.2,
+        1.05,
+        "1/" + str(len(data.t)),
+        ha="center",
+        va="center",
+        transform=ax.transAxes,
+    )
+
+    def update_quiver(num, Q, data, text):
+        """_summary_
+
+        Args:
+            num (_type_): _description_
+            Q (_type_): _description_
+            data (_type_): _description_
+            text (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        U, V = data.u[:, :, num], data.v[:, :, num]
+
+        M = np.sqrt(U[::3, ::3] ** 2 + V[::3, ::3] ** 2)
+        Q.set_UVC(U[::3, ::3], V[::3, ::3], M[::3, ::3])
+        text.set_text(str(num + 1) + "/" + str(len(data.t)))
+        return Q
+
+    anim = FuncAnimation(
+        fig,
+        update_quiver,
+        fargs=(Q, data, text),
+        frames=len(data.t),
+        blit=False,
+    )
+    mywriter = FFMpegWriter()
+    if savepath:
+        p = os.getcwd()
+        os.chdir(savepath)
+        anim.save("im.mp4", writer=mywriter)
+        os.chdir(p)
+    else:
+        anim.save("im.mp4", writer=mywriter)
+
+
+def dataset_to_array(data: xr.Dataset, t_index: int = 0):
+    """converts xarray Dataset to array"""
+    if "t" in data.dims:
+        warnings.warn(
+            "Warning: this function uses the first \
+               frame, otherwise use: data.isel(t=N)"
+        )
+        return data.isel(t=t_index)
+    
+    
+    return data
---
+++ packages/pivpy/pivpy-0.0.20-py3-none-any.whl/pivpy/pivpy.py
@@ -0,0 +1,477 @@
+# -*- coding: utf-8 -*-
+"""
+This script extends the functionality of xarray.Dataset by adding a new accessor called piv. The accessor adds several properties and methods that are useful for working with particle image velocimetry (PIV) data. The properties include average, which returns the mean flow field, and delta_t, which returns the time step used in the PIV measurement. The methods include crop, which allows the user to crop the data by a given number of rows and columns from the boundaries, vec2scal, which converts vector data to scalar data, pan, which pans the data by a given number of pixels, and rotate, which rotates the data by a given angle.
+
+
+@author: Ron, Alex
+"""
+try:
+    from typing_extensions import Literal
+except ImportError:
+    from typing import Literal
+from typing import List
+
+import numpy as np
+import xarray as xr
+from scipy.interpolate import griddata
+from scipy.ndimage import gaussian_filter
+
+from pivpy.graphics import quiver as gquiver
+from pivpy.graphics import showf as gshowf
+from pivpy.graphics import showscal as gshowscal
+
+# """ learn from this example
+# import xarray as xr
+# @xr.register_dataset_accessor('geo')
+# class GeoAccessor(object):
+#     def __init__(self, xarray_obj):
+#         self._obj = xarray_obj
+#         self._center = None
+
+#     @property
+#     def center(self):
+#         " Return the geographic center point of this dataset."
+#         if self._center is None:
+#             # we can use a cache on our accessor objects, because accessors
+#             # themselves are cached on instances that access them.
+#             lon = self._obj.latitude
+#             lat = self._obj.longitude
+#             self._center = (float(lon.mean()), float(lat.mean()))
+#         return self._center
+
+#     def plot(self):
+#         " Plot data on a map."
+#         return 'plotting!'
+
+
+#     In [1]: ds = xr.Dataset({'longitude': np.linspace(0, 10),
+#    ...:                  'latitude': np.linspace(0, 20)})
+#    ...:
+
+# In [2]: ds.geo.center
+# Out[2]: (10.0, 5.0)
+
+# In [3]: ds.geo.plot()
+# Out[3]: 'plotting!'
+
+# """
+
+
+@xr.register_dataset_accessor("piv")
+class PIVAccessor(object):
+    """extends xarray Dataset with PIVPy properties"""
+
+    def __init__(self, xarray_obj):
+        """
+        Arguments:
+            data : xarray Dataset:
+            x,y,t are coordinates
+            u,v,chs are the data arracc_ys
+
+        We add few shortcuts (properties):
+            data.piv.average is the time average (data.mean(dim='t'))
+            data.piv.delta_t is the shortcut to get $\\Delta t$
+            data.piv.vorticity
+            data.piv.tke
+            data.piv.shear
+
+        and a few methods:
+            data.piv.vec2scal()
+            data.piv.pan
+            data.piv.rotate
+
+        """
+        self._obj = xarray_obj
+        self._average = None
+        self._delta_t = None
+
+    @property
+    def average(self):
+        """Return the mean flow field ."""
+        if self._average is None:  # only first time
+            self._average = self._obj.mean(dim="t")
+            self._average.attrs = self._obj.attrs  # we need units in quiver
+            self._average.assign_coords({"t": 0})
+
+        return self._average
+
+    def crop(self, crop_vector=None):
+        """ crops xr.Dataset by some rows, cols from the boundaries
+
+        Args:
+            crop_vector (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        if crop_vector is None:
+            crop_vector = 4 * [None]
+
+        xmin, xmacc_x, ymin, ymacc_x = crop_vector
+
+        xmin = self._obj.x.min() if xmin is None else xmin
+        xmacc_x = self._obj.x.macc_x() if xmacc_x is None else xmacc_x
+        ymin = self._obj.y.min() if ymin is None else ymin
+        ymacc_x = self._obj.y.macc_x() if ymacc_x is None else ymacc_x
+
+        self._obj = self._obj.sel(x=slice(xmin, xmacc_x), y=slice(ymin, ymacc_x))
+
+        return self._obj
+
+    def pan(self, shift_x=0.0, shift_y=0.0):
+        """moves the field by shift_x,shift_y in the same units as x,y"""
+        self._obj = self._obj.assign_coords(
+            {"x": self._obj.x + shift_x, "y": self._obj.y + shift_y}
+        )
+        return self._obj
+
+    def filterf(self, sigma: List[float]=[1.,1.,0], **kwargs):
+        """Gaussian filtering of velocity"""
+
+        self._obj["u"] = xr.DataArray(
+            gaussian_filter(
+                self._obj["u"].values, sigma, **kwargs),
+                dims=("y", "x", "t"),
+                attrs = self._obj["u"].attrs,
+        )
+        self._obj["v"] = xr.DataArray(
+            gaussian_filter(
+                self._obj["v"].values, sigma, **kwargs),
+                dims=("y", "x", "t"),
+                attrs = self._obj["v"].attrs,
+        )
+
+        return self._obj
+
+    def fill_nans(self, method: Literal["linear", "nearest", "cubic"] = "nearest"):
+        """
+        This method uses scipy.interpolate.griddata to interpolate missing data.
+        Parameters
+        ----------
+        src_data: Any
+            Input data array.
+        method: {'linear', 'nearest', 'cubic'}, optional
+            The method to use for interpolation in `scipy.interpolate.griddata`.
+        Returns
+        -------
+        :class:`numpy.ndarray`:
+            An interpolated :class:`numpy.ndarray`.
+        """
+
+        def _griddata_nans(src_data, x_coords, y_coords, method=method):
+
+            src_data_flat = src_data.copy().flatten()
+            data_bool = ~np.isnan(src_data_flat)
+
+            if not data_bool.any():
+                return src_data
+
+            return griddata(
+                points=(x_coords.flatten()[data_bool], y_coords.flatten()[data_bool]),
+                values=src_data_flat[data_bool],
+                xi=(x_coords, y_coords),
+                method=method,
+                # fill_value=nodata,
+            )
+
+        x_coords, y_coords = np.meshgrid(
+            self._obj.coords["x"].values, self._obj.coords["y"].values
+        )
+
+        for var_name in self._obj.variables:
+            if var_name not in self._obj.coords:
+                for t_i in self._obj["t"]:
+                    new_data = _griddata_nans(
+                        self._obj.sel(t=t_i)[var_name].data,
+                        x_coords,
+                        y_coords,
+                        method=method,
+                    )
+                    self._obj.sel(t=t_i)[var_name].data[:] = new_data
+
+        return self._obj
+
+    def __add__(self, other):
+        """add two datasets means that we sum up the velocities, assume
+        that x,y,t,delta_t are all identical
+        """
+        self._obj["u"] += other._obj["u"]
+        self._obj["v"] += other._obj["v"]
+        return self._obj
+
+    def __sub__(self, other):
+        """add two datasets means that we sum up the velocities, assume
+        that x,y,t,delta_t are all identical
+        """
+        self._obj["u"] -= other._obj["u"]
+        self._obj["v"] -= other._obj["v"]
+        return self._obj
+
+    def vorticity(self):
+        """calculates vorticity of the data arracc_y (at one time instance) and
+        adds it to the attributes
+
+        Input:
+            xarray with the variables u,v and dimensions x,y
+
+        Output:
+            xarray with the estimated vorticity as a scalar field with
+            same dimensions
+
+        """
+
+        self._obj["w"] = self._obj["v"].differentiate("x") - self._obj[
+            "u"
+        ].differentiate("y")
+
+        self._obj["w"].attrs["units"] = "1/delta_t"
+        self._obj["w"].attrs["standard_name"] = "vorticity"
+
+        return self._obj
+
+    def strain(self):
+        """ calculates rate of strain of a two component field
+
+        Returns:
+            _type_: adds ["w"] = du_dx^2 + dv_dy^2 + 0.5*(du_dy+dv_dx)^2
+        """
+        du_dx = self._obj["u"].differentiate("x")
+        du_dy = self._obj["u"].differentiate("y")
+        dv_dx = self._obj["v"].differentiate("x")
+        dv_dy = self._obj["v"].differentiate("y")
+
+        self._obj["w"] = du_dx**2 + dv_dy**2 + 0.5 * (du_dy + dv_dx) ** 2
+        self._obj["w"].attrs["units"] = "1/delta_t"
+        self._obj["w"].attrs["standard_name"] = "strain"
+
+        return self._obj
+
+    def divergence(self):
+        """ calculates divergence field
+
+        Returns:
+            self._obj: xr.Dataset with the new property ["w"] = divergence
+        """
+        du_dx, _ = np.gradient(
+            self._obj["u"], self._obj["x"], self._obj["y"], acc_x_is=(0, 1)
+        )
+        _, dv_dy = np.gradient(
+            self._obj["v"], self._obj["x"], self._obj["y"], acc_x_is=(0, 1)
+        )
+
+        if "t" in self._obj.coords:
+            self._obj["w"] = (("x", "y", "t"), dv_dy + du_dx)
+        else:
+            self._obj["w"] = (("x", "y"), dv_dy + du_dx)
+
+        self._obj["w"].attrs["units"] = "1/delta_t"
+        self._obj["w"].attrs["standard_name"] = "divergence"
+
+        return self._obj
+
+    def acceleration(self):
+        """calculates material derivative or acceleration of the
+        data arracc_y (single frame)
+
+        Input:
+            xarray with the variables u,v and dimensions x,y
+
+        Output:
+            xarray with the estimated acceleration as a scalar field data['w']
+
+        """
+        du_dx = self._obj["u"].differentiate("x")
+        du_dy = self._obj["u"].differentiate("y")
+        dv_dx = self._obj["v"].differentiate("x")
+        dv_dy = self._obj["v"].differentiate("y")
+
+        acc_x = self._obj["u"] * du_dx + self._obj["v"] * du_dy
+        acc_y = self._obj["u"] * dv_dx + self._obj["v"] * dv_dy
+
+        self._obj["w"] = xr.DataArray(
+            np.sqrt(acc_x**2 + acc_y**2), dims=["x", "y", "t"]
+        )
+
+        self._obj["w"].attrs["units"] = "1/delta_t"
+        self._obj["w"].attrs["standard_name"] = "acceleration"
+
+        return self._obj
+
+    def kinetic_energy(self):
+        """estimates turbulent kinetic energy"""
+        self._obj["w"] = self._obj["u"] ** 2 + self._obj["v"] ** 2
+        self._obj["w"].attrs["units"] = "(m/s)^2"
+        self._obj["w"].attrs["standard_name"] = "kinetic_energy"
+        return self._obj
+
+    def tke(self):
+        """estimates turbulent kinetic energy"""
+        if len(self._obj.t) < 2:
+            raise ValueError(
+                "TKE is not defined for a single vector field, \
+                              use .piv.kinetic_energy()"
+            )
+
+        new_obj = self._obj.copy()
+        new_obj -= new_obj.mean(dim="t")
+        new_obj["w"] = new_obj["u"] ** 2 + new_obj["v"] ** 2
+        new_obj["w"].attrs["units"] = "(m/s)^2"
+        new_obj["w"].attrs["standard_name"] = "TKE"
+
+        return new_obj
+
+    def fluct(self):
+        """returns fluctuations as a new dataset"""
+
+        if len(self._obj.t) < 2:
+            raise ValueError(
+                "fluctuations cannot be defined for a \
+                              single vector field, use .piv.ke()"
+            )
+
+        new_obj = self._obj.copy()
+        new_obj -= new_obj.mean(dim="t")
+
+        new_obj["u"].attrs["standard_name"] = "fluctation"
+        new_obj["v"].attrs["standard_name"] = "fluctation"
+
+        return new_obj
+
+    def reynolds_stress(self):
+        """returns fluctuations as a new dataset"""
+
+        if len(self._obj.t) < 2:
+            raise ValueError(
+                "fluctuations cannot be defined for a \
+                              single vector field, use .piv.ke()"
+            )
+
+        new_obj = self._obj.copy()
+        new_obj -= new_obj.mean(dim="t")
+
+        new_obj["w"] = -1 * new_obj["u"] * new_obj["v"]  # new scalar
+        self._obj["w"] = new_obj["w"].mean(dim="t")  # reynolds stress is -\rho < u' v'>
+        self._obj["w"].attrs["standard_name"] = "Reynolds_stress"
+
+        return self._obj
+
+    def rms(self):
+        """Root mean square"""
+        self._obj = self.tke()
+        self._obj["w"] = np.sqrt(self._obj["w"])
+        self._obj["w"].attrs["standard_name"] = "rms"
+        self._obj["w"].attrs["units"] = "m/s"
+
+    def vec2scal(self, flow_property: str = "curl"):
+        """ creates a scalar flow property field
+
+        Args:
+            flow_property (str, optional): one of the flow properties. Defaults to "curl".
+
+        Returns:
+            _type_: _description_
+        """
+        # replace few common names
+        flow_property = "vorticity" if flow_property == "curl" else flow_property
+        flow_property = "kinetic_energy" if flow_property == "ken" else flow_property
+        flow_property = "kinetic_energy" if flow_property == "ke" else flow_property
+        flow_property = "vorticity" if flow_property == "vort" else flow_property
+
+        method = getattr(self, str(flow_property))
+
+        self._obj = method()
+
+        return self._obj
+
+    def __mul__(self, scalar):
+        """
+        multiplication of a velocity field by a scalar (simple scaling)
+        """
+        self._obj["u"] *= scalar
+        self._obj["v"] *= scalar
+        if "w" in self._obj.var():
+            self._obj["w"] += scalar
+
+        return self._obj
+
+    def __div__(self, scalar):
+        """
+        multiplication of a velocity field by a scalar (simple scaling)
+        """
+        self._obj["u"] /= scalar
+        self._obj["v"] /= scalar
+
+        return self._obj
+
+    def set_delta_t(self, delta_t: float = 0.0):
+        """sets delta_t attribute, float, default is 0.0"""
+        self._obj.attrs["delta_t"] = delta_t
+        return self._obj
+
+    def set_scale(self, scale: float = 1.0):
+        """scales all variables by a sclar"""
+        for var in ["x", "y", "u", "v"]:
+            self._obj[var] = self._obj[var] * scale
+
+        return self._obj
+
+    def rotate(self, theta: float = 0.0):
+        """rotates the data, but only for some x,y grids
+        Args:
+            theta (float): degrees in the clockwise direction
+            it can only work for the cases with equal size along
+            x and y
+        Returns:
+            rotated object
+        """
+
+        theta = theta / 360.0 * 2 * np.pi
+
+        x_i = self._obj.x * np.cos(theta) + self._obj.y * np.sin(theta)
+        eta = self._obj.y * np.cos(theta) - self._obj.x * np.sin(theta)
+        du_dx_i = self._obj.u * np.cos(theta) + self._obj.v * np.sin(theta)
+        u_eta = self._obj.v * np.cos(theta) - self._obj.u * np.sin(theta)
+
+        self._obj["x"] = x_i
+        self._obj["y"] = eta
+        self._obj["u"] = du_dx_i
+        self._obj["v"] = u_eta
+
+        if "theta" in self._obj:
+            self._obj["theta"] += theta
+        else:
+            self._obj["theta"] = theta
+
+        return self._obj
+
+    @property
+    def delta_t(self):
+        """receives the delta_t from the set"""
+        if self._delta_t is None:
+            self._delta_t = self._obj.attrs["delta_t"]
+        return self._delta_t
+
+    def quiver(self, **kwargs):
+        """graphics.quiver() as a flow_property"""
+        fig, acc_x = gquiver(self._obj, **kwargs)
+        return fig, acc_x
+
+    def streamplot(self, **kwargs):
+        """graphics.quiver(streamlines=True)"""
+        gquiver(self._obj, streamlines=True, **kwargs)
+
+    def showf(self, **kwargs):
+        """method for graphics.showf"""
+        gshowf(self._obj, **kwargs)
+
+    def showscal(self, **kwargs):
+        """method for graphics.showscal"""
+        gshowscal(self._obj, **kwargs)
+
+    # @property
+    # def vel_units(self):
+    #     " Return the geographic center point of this dataset."
+    #     if self._vel_units is None:
+    #         self._vel_units = self._obj.attrs.l_units + '/' + \
+    #                           self._obj.attrs.t_units
+    #     return self._vel_units
