3859e44035e4e3dba1d60285a5d7026d508876de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:04:36 +0000

    Add package secretfunctions/0.0.2/secretfunctions-0.0.2-py3-none-any.whl

---
+++ packages/secretfunctions/secretfunctions-0.0.2-py3-none-any.whl/secretfunctions/__init__.py
@@ -0,0 +1,24 @@
+# =============================================================================
+#
+#  Licensed Materials, Property of Ralph Vogl, Munich
+#
+#  Project : basefunctions
+#
+#  Copyright (c) by Ralph Vogl
+#
+#  All rights reserved.
+#
+#  Description:
+#
+#  simple library to have some commonly used functions for everyday purpose
+#
+# =============================================================================
+
+# -------------------------------------------------------------
+# IMPORTS
+# -------------------------------------------------------------
+from secretfunctions.secretfunctions import SecretFunctions
+
+__all__ = [
+    "SecretFunctions",
+]
