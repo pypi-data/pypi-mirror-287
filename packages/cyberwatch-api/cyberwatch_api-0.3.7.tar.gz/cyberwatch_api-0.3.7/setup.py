2fd75cc907675f66acac06f89fe13ecadbc4328d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:36 +0000

    Add package cyberwatch-api/0.3.7/cyberwatch_api-0.3.7.tar.gz

---
+++ packages/cyberwatch-api/cyberwatch_api-0.3.7.tar.gz/cyberwatch_api-0.3.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='cyberwatch_api',
+      version='0.3.7',
+      description='Python Api client for the Cyberwatch software',
+      long_description=open('README.md').read().strip(),
+      long_description_content_type="text/markdown",
+      author='CyberWatch SAS',
+      py_modules=['cyberwatch_api'],
+      packages=find_packages(include=['cli.*']),
+      license='MIT',
+      install_requires=['requests>=2.20.1'],
+      scripts=['cli/cyberwatch-cli']
+      )
