4b32c5b54841f9932c00be90a28f17e1eb37277d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:05:09 +0000

    Add package pySQLExport-gui/1.0/pySQLExport_gui-1.0-py3-none-any.whl

---
+++ packages/pySQLExport-gui/pySQLExport_gui-1.0-py3-none-any.whl/pySQLExport_gui/database.py
@@ -0,0 +1,35 @@
+"""
+================================================================================
+   Project: pySQLExport-gui
+   Description: A PyQt6 GUI application for managing and exporting SQL query results.
+   Author: Aaron
+   Email: aaron.mathis@gmail.com
+   License: GNU General Public License v3.0 (GPL-3.0)
+   License URL: https://www.gnu.org/licenses/gpl-3.0.en.html
+================================================================================
+
+   This file is part of pySQLExport-gui.
+
+   pySQLExport-gui is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   pySQLExport-gui is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with pySQLExport-gui. If not, see <https://www.gnu.org/licenses/>.
+
+================================================================================
+"""
+
+def get_database(db_type, host, user, pw, database, port):
+    if db_type == 'MySQL':
+        from pySQLExport_gui.database_mysql import MySQLDatabase
+        return MySQLDatabase(host, user, pw, database, port)
+    elif db_type == 'PostgreSQL':
+        from pySQLExport_gui.database_pgsql import PgSQLDatabase
+        return PgSQLDatabase(host, user, pw, database, port)
---
+++ packages/pySQLExport-gui/pySQLExport_gui-1.0-py3-none-any.whl/pySQLExport_gui/database_pgsql.py
@@ -0,0 +1,51 @@
+"""
+================================================================================
+   Project: pySQLExport-gui
+   Description: A PyQt6 GUI application for managing and exporting SQL query results.
+   Author: Aaron
+   Email: aaron.mathis@gmail.com
+   License: GNU General Public License v3.0 (GPL-3.0)
+   License URL: https://www.gnu.org/licenses/gpl-3.0.en.html
+================================================================================
+
+   This file is part of pySQLExport-gui.
+
+   pySQLExport-gui is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   pySQLExport-gui is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with pySQLExport-gui. If not, see <https://www.gnu.org/licenses/>.
+
+================================================================================
+"""
+# database_pgsql.py
+import psycopg2
+
+class PgSQLDatabase:
+    def __init__(self, host, user, password, database, port):
+        self.connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            dbname=database,
+            port=port
+        )
+    
+    def execute(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        cursor.close()
+        return results, columns
+    
+    def close(self):
+        if self.connection:
+            self.connection.close()
