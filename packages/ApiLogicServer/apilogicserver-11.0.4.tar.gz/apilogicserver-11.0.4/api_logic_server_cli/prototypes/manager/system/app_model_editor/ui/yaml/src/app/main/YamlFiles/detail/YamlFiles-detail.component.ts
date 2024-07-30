4229616426f3846a1c04f3e7bac8e59357a7280e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:38 +0000

    Add package ApiLogicServer/11.0.4/apilogicserver-11.0.4.tar.gz

---
+++ packages/ApiLogicServer/apilogicserver-11.0.4.tar.gz/apilogicserver-11.0.4/api_logic_server_cli/prototypes/manager/system/app_model_editor/ui/yaml/src/app/main/YamlFiles/detail/YamlFiles-detail.component.ts
@@ -0,0 +1,96 @@
+import { Injector, ViewChild, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { OFormComponent, OntimizeService, OListPickerComponent, OTableComponent, ORealPipe, ONIFInputComponent } from 'ontimize-web-ngx';
+import { DialogService } from 'ontimize-web-ngx';
+
+@Component({
+  selector: 'YamlFiles-detail',
+  templateUrl: './YamlFiles-detail.component.html',
+  styleUrls: ['./YamlFiles-detail.component.scss']
+})
+export class YamlFilesDetailComponent implements OnInit  {
+  protected service: OntimizeService;
+  public data: any;
+  public content: string;
+  public downloaded: string;
+
+  @ViewChild('oDetailForm') form: OFormComponent;
+  
+  constructor(protected injector: Injector,
+    protected dialogService: DialogService)  {
+    this.service = this.injector.get(OntimizeService);
+  
+  }
+
+  ngOnInit() {
+    this.configureService();
+  }
+
+  protected configureService() {
+    const conf = this.service.getDefaultServiceConfiguration();
+    conf['path'] = '/YamlFiles';
+    this.service.configureService(conf);
+  }
+  onDataLoaded(e: object) {
+
+    this.data = e;
+    //console.log("OnDataLoad: " + JSON.stringify(this.data));
+    this.content = this.data.content;
+    this.downloaded = this.data.downloaded;
+  }
+  getContent(): string {
+    //console.log("getValue " + this.content);
+    if (!this.content) {
+      return "foo bar";
+    }
+    return this.content;
+  }
+  getConverted(): string {
+    //console.log("getValue " + this.content);
+    if (!this.downloaded) {
+      return "";
+    }
+    return this.downloaded;
+  }
+  process_yaml() {
+    console.log("process_yaml");
+    this.service.query({ 'id': this.data.id }, ['content'],"importyaml").subscribe((resp) => {
+        console.log("res: " + JSON.stringify(resp));
+        if (resp.code === 0) {
+          this.updateProcessFlag()
+        }
+      });
+    }
+    updateProcessFlag() {
+      console.log("updateProcessFlag");
+      this.data.upload_flag = true;
+      this.service.update({'id':this.data.id}, {'upload_flag':this.data.upload_flag},"YamlFiles").subscribe((resp) => {
+        console.log("res: " + JSON.stringify(resp));
+        if (resp.code === 0) {
+          this.showInfo();
+        }
+      });
+    }
+    showInfo() {
+        if (this.dialogService) {
+        this.dialogService.info('Yaml Processing Complete',
+            'Entities, Attributes, and Relationships have been created from the Yaml "original content"',);
+        }
+    }
+    download_yaml() {
+      console.log("download_yaml");
+      this.service.update({'id':this.data.id}, {'download_flag':true},"YamlFiles").subscribe((resp) => {
+        console.log("res: " + JSON.stringify(resp));
+        if (resp.code === 0) {
+          this.downloaded = resp.data.content;
+          this.showDownloadInfo();
+          this.form.reload();
+        }
+      });
+    }
+    showDownloadInfo(){
+      if (this.dialogService) {
+        this.dialogService.info('Yaml File Downloaded',
+            'The Yaml "converted content" has been downloaded',);
+      } 
+    }
+}
---
+++ packages/ApiLogicServer/apilogicserver-11.0.4.tar.gz/apilogicserver-11.0.4/api_logic_server_cli/prototypes/manager/system/app_model_editor/ui/yaml/src/app/shared/app.menu.config.ts
@@ -0,0 +1,53 @@
+import { MenuRootItem } from 'ontimize-web-ngx';
+
+import { EntityCardComponent } from './Entity-card/Entity-card.component';
+
+import { EntityAttrCardComponent } from './EntityAttr-card/EntityAttr-card.component';
+
+import { GlobalSettingCardComponent } from './GlobalSetting-card/GlobalSetting-card.component';
+
+import { TabGroupCardComponent } from './TabGroup-card/TabGroup-card.component';
+
+import { TemplateCardComponent } from './Template-card/Template-card.component';
+
+import { YamlFilesCardComponent } from './YamlFiles-card/YamlFiles-card.component';
+
+
+export const MENU_CONFIG: MenuRootItem[] = [
+    { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
+    { id: 'New YamlFiles', name: '1. Import Yaml File', icon: 'upload_file', route: '/main//YamlFiles/new' },
+    { id: 'YamlFiles', name: '2. Process Yaml File', icon: 'upload_file', route: '/main/YamlFiles' },
+    { id: 'data', name: '3. Edit Yaml Data', icon: 'remove_red_eye', opened: true,
+    items: [
+        { id: 'Entity', name: 'ENTITY', icon: 'view_list', route: '/main/Entity' }
+        ,{ id: 'EntityAttr', name: 'ENTITYATTR', icon: 'view_list', route: '/main/EntityAttr' }
+        ,{ id: 'TabGroup', name: 'TABGROUP', icon: 'view_list', route: '/main/TabGroup' }
+    
+        ] 
+    }
+    ,{ id: 'other', name: 'Global', icon: 'remove_red_eye', opened: false,
+    items: [        
+        { id: 'GlobalSetting', name: 'Global Settings', icon: 'view_list', route: '/main/GlobalSetting' }
+        ,{ id: 'Template', name: 'TEMPLATE', icon: 'view_list', route: '/main/Template' }
+        ] 
+    }
+    ,{ id: 'settings', name: 'Settings', icon: 'settings', route: '/main/settings'}
+    ,{ id: 'about', name: 'About', icon: 'info', route: '/main/about'}
+    ,{ id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
+];
+
+export const MENU_COMPONENTS = [
+
+    EntityCardComponent
+
+    ,EntityAttrCardComponent
+
+    ,GlobalSettingCardComponent
+
+    ,TabGroupCardComponent
+
+    ,TemplateCardComponent
+
+    ,YamlFilesCardComponent
+
+];
