db2ed1af11881856441490e822a5d6ce8c3bc55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:04:16 +0000

    Add package edx-i18n-tools/1.6.1/edx_i18n_tools-1.6.1.tar.gz

---
+++ packages/edx-i18n-tools/edx_i18n_tools-1.6.1.tar.gz/edx_i18n_tools-1.6.1/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import os
+import re
+import pathlib
+
+from setuptools import setup
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+def get_version(*file_paths):
+     """
+     Extract the version string from the file at the given relative path fragments.
+     """
+     filename = os.path.join(os.path.dirname(__file__), *file_paths)
+     version_file = open(filename).read()
+     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+     if version_match:
+         return version_match.group(1)
+     raise RuntimeError("Unable to find version string.")
+
+
+VERSION = get_version("i18n", "__init__.py")
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.rst").read_text(encoding="utf-8")
+
+setup(
+    name='edx-i18n-tools',
+    version=VERSION,
+    description='edX Internationalization Tools',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='edX',
+    author_email='oscm@edx.org',
+    url='https://github.com/openedx/i18n-tools',
+    packages=[
+        'i18n',
+    ],
+    install_requires=load_requirements('requirements/base.in'),
+    entry_points={
+        'console_scripts': [
+            'i18n_tool = i18n.main:main',
+        ],
+    },
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+    ],
+)
