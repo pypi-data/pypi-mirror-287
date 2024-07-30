8e11667f12ebdd85a433fe94a1801612e3ea4a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:16 +0000

    Add package hitss-glpi/0.1.0/hitss_glpi-0.1.0-py3-none-any.whl

---
+++ packages/hitss-glpi/hitss_glpi-0.1.0-py3-none-any.whl/glpi_api/api.py
@@ -0,0 +1,124 @@
+# glpi_api/api.py
+
+import requests
+from bs4 import BeautifulSoup
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+
+class GLPIAPI:
+    def __init__(self, base_url, username, password):
+        self.base_url = base_url
+        self.username = username
+        self.password = password
+        self.session = requests.Session()
+        self.login()
+
+    def login(self):
+        login_url = f"{self.base_url}/front/login.php"
+        
+        # Configurar Selenium WebDriver
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')  # Ejecutar en modo headless
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        
+        # Navegar a la página de login
+        driver.get(login_url)
+        
+        # Encontrar y llenar los campos de login utilizando los XPaths proporcionados
+        user_input = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/form/div/div/div[2]/input")
+        password_input = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/form/div/div/div[3]/input")
+        login_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/form/div/div/div[6]/button")
+        
+        user_input.send_keys(self.username)
+        password_input.send_keys(self.password)
+        
+        # Desplazarse al botón de inicio de sesión y hacer clic
+        actions = ActionChains(driver)
+        actions.move_to_element(login_button).perform()
+        driver.execute_script("arguments[0].scrollIntoView();", login_button)
+        login_button.click()
+        
+        # Obtener cookies y CSRF token
+        cookies = driver.get_cookies()
+        csrf_token = driver.find_element(By.NAME, '_glpi_csrf_token').get_attribute('value')
+        
+        driver.quit()
+        
+        # Configurar sesión de requests con cookies y CSRF token
+        for cookie in cookies:
+            self.session.cookies.set(cookie['name'], cookie['value'])
+        self.session.headers.update({'X-CSRF-Token': csrf_token})
+
+    def create_ticket(self, title, content, urgency=3, requester_id=None, assigned_id=None):
+        create_ticket_url = f"{self.base_url}/front/ticket.form.php"
+        response = self.session.get(create_ticket_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Extraer el token del formulario
+        form_token = soup.find('input', {'name': '_glpi_csrf_token'})['value']
+        
+        payload = {
+            '_glpi_csrf_token': form_token,
+            'name': title,
+            'content': content,
+            'urgency': urgency,
+            'add': 'Agregar'
+        }
+
+        if requester_id:
+            payload['_users_id_requester'] = requester_id
+        if assigned_id:
+            payload['_users_id_assign'] = assigned_id
+        
+        # Depuración: Imprimir el payload
+        print("Payload:", payload)
+        
+        response = self.session.post(create_ticket_url, data=payload, allow_redirects=False)
+        
+        # Depuración: Imprimir el contenido de la respuesta
+        print("Response status code:", response.status_code)
+        print("Response headers:", response.headers)
+        
+        if response.status_code == 302:
+            redirect_url = response.headers['Location']
+            print("Redirect URL:", redirect_url)
+            response = self.session.get(f"{self.base_url}{redirect_url}")
+
+        response_text = response.text
+        print("Response text after redirect:", response_text)
+        
+        # Verificar si la respuesta contiene el mensaje de éxito y extraer el ID del ticket
+        if response.status_code == 200:
+            ticket_id = self.extract_ticket_id_from_message(response_text)
+            if ticket_id:
+                return {"message": "Ticket created successfully", "ticket_id": ticket_id}
+            else:
+                return {"message": "Ticket created successfully, but could not extract ticket ID"}
+        else:
+            return {"message": "Failed to create ticket", "details": response_text}
+
+    def extract_ticket_id_from_message(self, response_text):
+        # Buscar el ID del ticket en el mensaje de éxito
+        soup = BeautifulSoup(response_text, 'html.parser')
+        print("HTML parsed successfully")
+        toast_container = soup.find('div', {'id': 'messages_after_redirect'})
+        print("Toast container found:", toast_container is not None)
+        if toast_container:
+            toast_body = toast_container.find('div', {'class': 'toast-body'})
+            print("Toast body found:", toast_body is not None)
+            if toast_body:
+                ticket_link = toast_body.find('a', href=True)
+                print("Ticket link found:", ticket_link is not None)
+                if ticket_link:
+                    print("Ticket link href:", ticket_link['href'])
+                    ticket_id_match = re.search(r'id=(\d+)', ticket_link['href'])
+                    if ticket_id_match:
+                        return ticket_id_match.group(1)
+        return None
+
+    def close(self):
+        self.session.close()
