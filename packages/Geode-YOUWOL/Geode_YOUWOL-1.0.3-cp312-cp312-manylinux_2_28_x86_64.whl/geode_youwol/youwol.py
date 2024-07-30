15b265e331fa3cc2fbb60560ef0e925e6120a8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 07:04:02 +0000

    Add package Geode-YOUWOL/1.0.3/Geode_YOUWOL-1.0.3-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-YOUWOL/Geode_YOUWOL-1.0.3-cp312-cp312-manylinux_2_28_x86_64.whl/geode_youwol/youwol.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2024 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_inspector
+
+from .lib64.geode_youwol_py_youwol import *
+
+GeodeYouWolLibrary.initialize()
