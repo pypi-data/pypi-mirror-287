afff0b4ffda85f213599ce9f1811c47d0e0c312a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:04:18 +0000

    Add package py5/0.10.2a0/py5-0.10.2a0-py3-none-any.whl

---
+++ packages/py5/py5-0.10.2a0-py3-none-any.whl/py5_tools/__init__.py
@@ -0,0 +1,30 @@
+# *****************************************************************************
+#
+#   Part of the py5 library
+#   Copyright (C) 2020-2024 Jim Schmitz
+#
+#   This library is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 2.1 of the License, or (at
+#   your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this library. If not, see <https://www.gnu.org/licenses/>.
+#
+# *****************************************************************************
+"""
+Utilities and accessory tools for py5. 
+"""
+from . import translators  # noqa
+from .config import *  # noqa
+from .hooks import *  # noqa
+from .imported import _lock_imported_mode, get_imported_mode, set_imported_mode  # noqa
+from .jvm import *  # noqa
+from .libraries import *  # noqa
+
+__version__ = "0.10.2a0"
