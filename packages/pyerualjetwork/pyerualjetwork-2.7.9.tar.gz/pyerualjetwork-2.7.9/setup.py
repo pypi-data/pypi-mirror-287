94127395a263beb219a638255f62874d2317b26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:04:34 +0000

    Add package pyerualjetwork/2.7.9/pyerualjetwork-2.7.9.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-2.7.9.tar.gz/pyerualjetwork-2.7.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.7.9",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Decision Space included in evaluate with plot_decision_space function",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
