72eb0e1b8278083012264c77f279edc21c7b0acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:04:24 +0000

    Add package hugr/0.5.0/hugr-0.5.0-py3-none-any.whl

---
+++ packages/hugr/hugr-0.5.0-py3-none-any.whl/hugr/serialization/serial_hugr.py
@@ -0,0 +1,62 @@
+from typing import Any
+
+from pydantic import ConfigDict, Field
+
+import hugr
+from hugr import get_serialisation_version
+from hugr.node_port import NodeIdx, PortOffset
+
+from .ops import OpType
+from .ops import classes as ops_classes
+from .tys import ConfiguredBaseModel, model_rebuild
+
+Port = tuple[NodeIdx, PortOffset | None]
+Edge = tuple[Port, Port]
+
+VersionField = Field(
+    default_factory=get_serialisation_version,
+    title="Version",
+    description="Serialisation Schema Version",
+    frozen=True,
+)
+
+
+class SerialHugr(ConfiguredBaseModel):
+    """A serializable representation of a Hugr."""
+
+    version: str = VersionField
+    nodes: list[OpType]
+    edges: list[Edge]
+    metadata: list[dict[str, Any] | None] | None = None
+    encoder: str | None = Field(
+        default=None, description="The name of the encoder used to generate the Hugr."
+    )
+
+    def to_json(self) -> str:
+        """Return a JSON representation of the Hugr."""
+        self.encoder = f"hugr-py v{hugr.__version__}"
+        return self.model_dump_json()
+
+    @classmethod
+    def load_json(cls, json: dict[Any, Any]) -> "SerialHugr":
+        """Decode a JSON-encoded Hugr."""
+        return cls(**json)
+
+    @classmethod
+    def get_version(cls) -> str:
+        """Return the version of the schema."""
+        return cls(nodes=[], edges=[]).version
+
+    @classmethod
+    def _pydantic_rebuild(cls, config: ConfigDict | None = None, **kwargs):
+        config = config or ConfigDict()
+        my_classes = dict(ops_classes)
+        my_classes[cls.__name__] = cls
+        model_rebuild(my_classes, config=config, **kwargs)
+
+    model_config = ConfigDict(
+        title="Hugr",
+        json_schema_extra={
+            "required": ["version", "nodes", "edges"],
+        },
+    )
---
+++ packages/hugr/hugr-0.5.0-py3-none-any.whl/hugr/serialization/testing_hugr.py
@@ -0,0 +1,32 @@
+from pydantic import ConfigDict
+
+from .ops import OpDef, OpType, Value
+from .ops import classes as ops_classes
+from .serial_hugr import VersionField
+from .tys import ConfiguredBaseModel, PolyFuncType, SumType, Type, model_rebuild
+
+
+class TestingHugr(ConfiguredBaseModel):
+    """A serializable representation of a Hugr Type, SumType, PolyFuncType,
+    Value, OpType. Intended for testing only.
+    """
+
+    version: str = VersionField
+    typ: Type | None = None
+    sum_type: SumType | None = None
+    poly_func_type: PolyFuncType | None = None
+    value: Value | None = None
+    optype: OpType | None = None
+    op_def: OpDef | None = None
+
+    @classmethod
+    def get_version(cls) -> str:
+        """Return the version of the schema."""
+        return cls().version
+
+    @classmethod
+    def _pydantic_rebuild(cls, config: ConfigDict | None = None, **kwargs):
+        config = config or ConfigDict()
+        my_classes = dict(ops_classes)
+        my_classes[cls.__name__] = cls
+        model_rebuild(my_classes, config=config, **kwargs)
