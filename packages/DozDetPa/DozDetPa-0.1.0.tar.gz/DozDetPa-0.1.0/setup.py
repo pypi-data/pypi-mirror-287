49285a15eba51b6ce1d77b5deaf2a69d59b1a830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:16 +0000

    Add package DozDetPa/0.1.0/DozDetPa-0.1.0.tar.gz

---
+++ packages/DozDetPa/DozDetPa-0.1.0.tar.gz/DozDetPa-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DozDetPa',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'DozDetPa = DozDetPa:main',
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A package that waits for 5 minutes, returns a value, and uninstalls itself.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/self_deleting_package',  # 你的项目主页
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
