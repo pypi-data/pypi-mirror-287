cf571f13d3c1a0f4620d3e2d11233cf1d3d89547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:04:33 +0000

    Add package unstable-baselines3/0.0.10069/unstable_baselines3-0.0.10069.tar.gz

---
+++ packages/unstable-baselines3/unstable_baselines3-0.0.10069.tar.gz/unstable_baselines3-0.0.10069/tests/rps.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+from pettingzoo.classic import rps_v2
+
+from stable_baselines3.dqn.policies import MlpPolicy as DQNPolicy
+from stable_baselines3.dqn.dqn import DQN
+
+from stable_baselines3.ppo.policies import MlpPolicy as PPOPolicy
+
+from unstable_baselines3.ppo.PPO import WorkerPPO
+from unstable_baselines3.common.auto_multi_alg import AutoMultiAgentAlgorithm
+import os, sys
+
+Worker = WorkerPPO
+
+if issubclass(Worker, DQN):
+    MlpPolicy = DQNPolicy
+else:
+    MlpPolicy = PPOPolicy
+
+DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.join(os.getcwd(), sys.argv[0]))))
+
+env = rps_v2.parallel_env()  # render_mode="human")
+
+
+class always_0:
+    def get_action(self, *args, **kwargs):
+        return 0
+
+
+class easy_pred:
+    def __init__(self, p=.01):
+        self.choice = 0
+        self.p = p
+
+    def get_action(self, *args, **kwargs):
+        if np.random.random() < self.p:
+            self.choice = np.random.randint(3)
+
+        return self.choice
+
+
+thingy = AutoMultiAgentAlgorithm(policy=MlpPolicy,
+                                 env=env,
+                                 DefaultWorkerClass=Worker,
+                                 worker_infos={'player_1': {'train': False}},
+                                 workers={'player_1': easy_pred()},
+                                 gamma=0.,
+
+                                 # buffer_size=1000,
+                                 # learning_starts=10,
+
+                                 n_steps=200,
+                                 batch_size=100,
+                                 )
+
+thingy.learn(total_timesteps=400)
+
+print(thingy.workers)
+worker0 = thingy.workers['player_0']
+worker0: Worker
+
+env = rps_v2.parallel_env(render_mode="human")
+
+thingy = AutoMultiAgentAlgorithm(policy=MlpPolicy,
+                                 env=env,
+                                 DefaultWorkerClass=Worker,
+                                 worker_infos={'player_1': {'train': False}},
+                                 workers={'player_1': easy_pred()},
+                                 gamma=0.,
+
+                                 # buffer_size=1000,
+                                 # learning_starts=10,
+
+                                 n_steps=200,
+                                 batch_size=100,
+                                 )
+
+thingy.learn(total_timesteps=10)
