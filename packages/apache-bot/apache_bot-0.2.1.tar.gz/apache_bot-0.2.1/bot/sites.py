7f758a5fcce0447cfe1abd0595e32c26ebd39ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:25 +0000

    Add package apache-bot/0.2.1/apache_bot-0.2.1.tar.gz

---
+++ packages/apache-bot/apache_bot-0.2.1.tar.gz/apache_bot-0.2.1/bot/sites.py
@@ -0,0 +1,22 @@
+SITES = {
+    'dmitriev-o2k.ru': {
+        'hostname': '192.168.1.235',
+        'port': 22,
+        'username': 'root',
+        'key': '/root/.ssh/id_rsa.pub',
+    },
+    'Site 2': {
+        'hostname': 'example2.com',
+        'port': 22,
+        'username': 'user2',
+        'password': 'password2',
+        'command': 'systemctl restart httpd',
+    },
+     'Site 3': {
+        'hostname': 'example2.com',
+        'port': 22,
+        'username': 'user2',
+        'password': 'password2',
+        'command': 'systemctl restart httpd',
+    },
+}
