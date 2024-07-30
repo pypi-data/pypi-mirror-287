e9890906ff4b10be9a61a3610fe778c5f0e3387f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:03:59 +0000

    Add package tracer-extension/1.0.27/tracer-extension-1.0.27.tar.gz

---
+++ packages/tracer-extension/tracer-extension-1.0.27.tar.gz/tracer-extension-1.0.27/python_worker_extension/__init__.py
@@ -0,0 +1,46 @@
+from ddtrace import patch_all, tracer
+
+patch_all()
+
+import typing
+from azure.functions import AppExtensionBase, Context
+from logging import Logger
+
+class TracerExtension(AppExtensionBase):
+    """A Python worker extension to start Datadog tracer and insturment Azure Functions"""
+
+    @classmethod
+    def init(cls):
+        print("============= new verison 14 ===========")
+        pass
+
+    @classmethod
+    def pre_invocation_app_level(cls, logger: Logger, context: Context, *args, **kwargs) -> None:
+        print("here")
+        print("context name: ", context.function_name)
+        # logger.info("context: ", context)
+        print("here1")
+        # logger.info(f'Recording start time of {context.function_name}')
+        # logger.info("context: ", context.function_name)
+
+        func_name = context.function_name
+
+        t = tracer.trace("azure.function")
+        print("here3")
+
+        span = tracer.current_span()
+        print("span?: ", span)
+        span.set_tag('route_name', func_name)
+        print("span get tags: ==", span.get_tags())
+        print("span after: ", span)
+        print("trace ater: " , t)
+        print("here4")
+        cls.t = t
+        print("here5")
+
+
+    @classmethod
+    def post_invocation_app_level(cls, *args, **kwargs) -> None:
+        cls.t.finish()
+        print("end")
+        pass
