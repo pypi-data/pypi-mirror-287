ecf20a932f8bf34ae831df408741ac7eec6e589b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:04:26 +0000

    Add package core-models/0.5.4/core_models-0.5.4.tar.gz

---
+++ packages/core-models/core_models-0.5.4.tar.gz/core_models-0.5.4/core_models/app/models/company_configuration.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from . import Company
+from .sector import Sector
+from .country import Country
+from .base import BaseModelAbstract
+from .user import User
+
+
+class CompanyConfiguration(BaseModelAbstract, models.Model):
+    created_by = None
+    company = models.OneToOneField(Company, models.CASCADE,
+                                   related_name='config')
+    countries = models.ManyToManyField(Country, null=True, blank=True)
+    sectors = models.ManyToManyField(Sector, null=True, blank=True)
+    maturity = models.IntegerField(default=0)
+    discount_range = models.JSONField(default=list([1, 50]))
+
+    def __unicode__(self):
+        return f"{self.company}'s config"
