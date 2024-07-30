5dc3e471ca1db0cc663cbc46d588556e542bbfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 05:03:59 +0000

    Add package scanerr/0.0.9/scanerr-0.0.9.tar.gz

---
+++ packages/scanerr/scanerr-0.0.9.tar.gz/scanerr-0.0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='scanerr',
+    version='0.0.9',
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv',
+        'requests',
+        'rich',
+        'typer',
+    ],
+    entry_points={
+        'console_scripts': [
+            'scanerr=src.scanerr.root:root'
+        ]
+    }
+)
