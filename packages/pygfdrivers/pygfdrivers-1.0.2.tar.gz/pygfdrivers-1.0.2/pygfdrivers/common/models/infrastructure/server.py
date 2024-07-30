fd42dd23b43966e38b453308a73b49b60b48c326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:05:09 +0000

    Add package pygfdrivers/1.0.2/pygfdrivers-1.0.2.tar.gz

---
+++ packages/pygfdrivers/pygfdrivers-1.0.2.tar.gz/pygfdrivers-1.0.2/pygfdrivers/common/models/infrastructure/server.py
@@ -0,0 +1,44 @@
+from pydantic import Field, AliasChoices
+
+from pygfdrivers.common.models.infrastructure.config import BaseConfigModel
+
+class BaseServerInfo(BaseConfigModel):
+    server_host: str = Field(
+        default=None,
+        alias='host',
+        validation_alias=AliasChoices('host', 'host_ip'))
+    server_port: int = Field(
+        default=None,
+        alias='port',
+        validation_alias=AliasChoices('port', 'port_num'))
+    server_header: int = Field(
+        default=None,
+        alias='header'
+    )
+    server_format: str = Field(
+        default=None,
+        alias='format'
+    )
+    server_delimiter: str = Field(
+        default= None,
+        alias= 'delimeter',
+        validation_alias=AliasChoices('server_delimiter', 'delimiter', 'termination_string')
+    )
+    
+    server_disconnect_msg: str = Field(
+        default=None,
+        alias='disconnect_msg'
+    )
+
+    log_path: str = Field(
+        default=None,
+        alias='log_path',
+        validation_alias=AliasChoices('log_path', 'log_file_path', 'log_save_path')
+    )
+
+
+class BaseServerModel(BaseConfigModel):
+    server: BaseServerInfo = Field(
+        default_factory=lambda: BaseServerInfo(),
+        validation_alias=AliasChoices('server', 'server_settings')
+    )
---
+++ packages/pygfdrivers/pygfdrivers-1.0.2.tar.gz/pygfdrivers-1.0.2/pygfdrivers/keysight/models/scope_config.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from typing import List, Dict
+from pydantic import AliasChoices, Field
+
+# custom models
+from pygfdrivers.common.models.infrastructure.config import BaseConfigDeviceModel
+from pygfdrivers.common.models.device.visa import BaseVisaDeviceModel
+from pygfdrivers.keysight.models.capture import KeysightCaptureModel
+from pygfdrivers.keysight.models.trigger import KeysightTriggerModel
+from pygfdrivers.keysight.models.channel import KeysightChannelModel
+
+
+# This model is used for configuring the devices from a yaml file, hence the different aliases for the fields
+class KeysightScopeConfigModel(BaseConfigDeviceModel):
+    scope: BaseVisaDeviceModel = Field(
+        default_factory=lambda: BaseVisaDeviceModel(),
+        validation_alias=AliasChoices('scope', 'scope_settings', 'scope_info')
+    )
+    capture: KeysightCaptureModel = Field(
+        default_factory=lambda: KeysightCaptureModel(),
+        validation_alias=AliasChoices('capture', 'capture_settings')
+    )
+    trigger: KeysightTriggerModel = Field(
+        default_factory=lambda: KeysightTriggerModel(),
+        validation_alias=AliasChoices('trigger', 'trigger_settings')
+    )
+    active_channels: List[int] = Field(
+        default_factory=lambda: list(),
+        validation_alias=AliasChoices('active_channels', 'active_sources')
+    )
+    channels: Dict[int, KeysightChannelModel] = Field(
+        default_factory=lambda: defaultdict(KeysightChannelModel),
+        validation_alias=AliasChoices('channels', 'channel_settings')
+    )
