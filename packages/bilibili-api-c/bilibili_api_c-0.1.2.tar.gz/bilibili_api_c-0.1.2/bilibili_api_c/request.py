21a04c7ec67fffc0458ee7c6423d44323d3e3162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:45 +0000

    Add package bilibili-api-c/0.1.2/bilibili_api_c-0.1.2.tar.gz

---
+++ packages/bilibili-api-c/bilibili_api_c-0.1.2.tar.gz/bilibili_api_c-0.1.2/bilibili_api_c/request.py
@@ -0,0 +1,6 @@
+REQUEST_HEADERS = {
+    "Referer": "https://www.bilibili.com/",
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+REQUEST_TIMEOUT = 30
