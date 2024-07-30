c7df7bc6fdf0c760b1704a2f08a5c425e87ac3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 07:04:16 +0000

    Add package faimly-t-ddd-core/0.1.3/faimly_t_ddd_core-0.1.3-py3-none-any.whl

---
+++ packages/faimly-t-ddd-core/faimly_t_ddd_core-0.1.3-py3-none-any.whl/faimly_t_ddd/framework_domain/repository/ispecification.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Callable, TypeVar, Generic
+
+from faimly_t_ddd.framework_domain.base_objects import IAggregate
+
+
+TAggregate = TypeVar('TAggregate', bound='IAggregate')
+
+
+class ISpecification(ABC, Generic[TAggregate]):
+    @property
+    @abstractmethod
+    def where_criteria(self) -> Callable[[TAggregate], bool]:
+        pass
+
+    @property
+    @abstractmethod
+    def where_criteria_str(self) -> str:
+        pass
+
+
+class ISpecificationSingleOrDefault(ISpecification[TAggregate], ABC):
+    pass
+
+
+class ISpecificationToList(ISpecification[TAggregate], ABC):
+    pass
