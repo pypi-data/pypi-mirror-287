deaee1022f6f54789f3bfaef57cdb8e593a04d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:04:38 +0000

    Add package aioaseko/0.2.0/aioaseko-0.2.0-py3-none-any.whl

---
+++ packages/aioaseko/aioaseko-0.2.0-py3-none-any.whl/aioaseko/variable.py
@@ -0,0 +1,38 @@
+# Copyright 2021, 2022, 2024 Milan Meulemans.
+#
+# This file is part of aioaseko.
+#
+# aioaseko is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# aioaseko is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with aioaseko.  If not, see <https://www.gnu.org/licenses/>.
+
+"""aioAseko variable."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Variable:
+    """Aseko variable."""
+
+    type: str
+    name: str
+    unit: str
+    icon: str | None
+    color: str | None
+    has_error: bool
+    current_value: int | None
+    required_value: int | None
+    has_alarm: bool | None
+    min_value: int | None
+    max_value: int | None
