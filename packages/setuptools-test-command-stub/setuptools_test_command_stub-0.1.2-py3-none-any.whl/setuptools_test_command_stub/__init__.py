5fabc5378869f95982934d29c5227a52122083ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:04:46 +0000

    Add package setuptools-test-command-stub/0.1.2/setuptools_test_command_stub-0.1.2-py3-none-any.whl

---
+++ packages/setuptools-test-command-stub/setuptools_test_command_stub-0.1.2-py3-none-any.whl/setuptools_test_command_stub/__init__.py
@@ -0,0 +1,13 @@
+def patch():
+    import sys
+    from unittest.mock import MagicMock
+
+    try:
+        from build._builder import _DEFAULT_BACKEND
+    except ImportError:
+        ...
+    else:
+        # For bypassing ProjectBuilder.from_isolated_env
+        _DEFAULT_BACKEND["requires"].append("setuptools-test-command-stub")
+
+    sys.modules["setuptools.command.test"] = MagicMock()
