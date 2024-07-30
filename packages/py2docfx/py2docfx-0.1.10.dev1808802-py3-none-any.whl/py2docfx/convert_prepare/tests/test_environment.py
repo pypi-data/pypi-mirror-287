162e5a0205fbb7afc66e1d91735ffd952ab1c218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 05:04:22 +0000

    Add package py2docfx/0.1.10.dev1808802/py2docfx-0.1.10.dev1808802-py3-none-any.whl

---
+++ packages/py2docfx/py2docfx-0.1.10.dev1808802-py3-none-any.whl/py2docfx/convert_prepare/tests/test_environment.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import pytest
+import subprocess
+
+from py2docfx import PACKAGE_ROOT
+from py2docfx.convert_prepare.environment import VirtualEnvironmentManager
+
+def test_create_venv():
+
+    venvManager = VirtualEnvironmentManager([], [], None, None)
+
+    vitual_env_executable_path = venvManager.create_venv("test_venv")
+
+    # Check if the virtual environment executable path is correct
+    assert os.path.exists(os.path.join(PACKAGE_ROOT, "venv"))
+    assert os.path.exists(vitual_env_executable_path)
+
+    # Check if the venv executable is functional
+    result = subprocess.run([vitual_env_executable_path, "-m", "pip", "--version"], capture_output=True, check=True)
+    assert result.returncode == 0
+
+    # Clean up the venv folder
+    shutil.rmtree(os.path.join(PACKAGE_ROOT, "venv"))
