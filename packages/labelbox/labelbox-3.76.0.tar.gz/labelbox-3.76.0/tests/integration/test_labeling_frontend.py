ecf0e0fcba94ac7c8aed5d79651bfb2749ed2ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:17 +0000

    Add package labelbox/3.76.0/labelbox-3.76.0.tar.gz

---
+++ packages/labelbox/labelbox-3.76.0.tar.gz/labelbox-3.76.0/tests/integration/test_labeling_frontend.py
@@ -0,0 +1,22 @@
+from labelbox import LabelingFrontend
+import pytest
+
+
+def test_get_labeling_frontends(client):
+    filtered_frontends = list(
+        client.get_labeling_frontends(where=LabelingFrontend.name == 'Editor'))
+    assert len(filtered_frontends)
+
+
+def test_labeling_frontend_connecting_to_project(project):
+    client = project.client
+    default_labeling_frontend = next(
+        client.get_labeling_frontends(where=LabelingFrontend.name == "Editor"))
+
+    assert project.labeling_frontend() is None
+
+    project.labeling_frontend.connect(default_labeling_frontend)
+    assert project.labeling_frontend() == default_labeling_frontend
+
+    project.labeling_frontend.disconnect(default_labeling_frontend)
+    assert project.labeling_frontend() == None
