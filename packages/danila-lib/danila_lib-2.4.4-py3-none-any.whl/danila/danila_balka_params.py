63092f72888c479e720471216ac6dbcd0c4e0d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:04:43 +0000

    Add package danila-lib/2.4.4/danila_lib-2.4.4-py3-none-any.whl

---
+++ packages/danila-lib/danila_lib-2.4.4-py3-none-any.whl/danila/danila_balka_params.py
@@ -0,0 +1,4 @@
+class Danila_balka_params:
+    def __init__(self, danila_balka_classify_params, danila_balka_detect_params):
+        self.danila_balka_classify_params = danila_balka_classify_params
+        self.danila_balka_text_detect_params = danila_balka_detect_params
