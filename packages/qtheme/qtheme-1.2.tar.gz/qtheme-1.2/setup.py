f9dec127f355c36e62615f6aa64ca4e19bc5340f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:05:24 +0000

    Add package qtheme/1.2/qtheme-1.2.tar.gz

---
+++ packages/qtheme/qtheme-1.2.tar.gz/qtheme-1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+setup(
+    name="qtheme",
+    version="1.2",
+    description="Tools for management qtile environment",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="jsusmachaca",
+    author_email="falcorgd@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "qtheme = qtheme.main:main"
+        ]
+    },
+)
