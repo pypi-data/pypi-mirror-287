a0eadfc4dd015a8bafdefb5fd08208c6fb0ed138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:31 +0000

    Add package DozDetPazuihouyici/0.1.0/DozDetPazuihouyici-0.1.0.tar.gz

---
+++ packages/DozDetPazuihouyici/DozDetPazuihouyici-0.1.0.tar.gz/DozDetPazuihouyici-0.1.0/DozDetPazuihouyici/timer.py
@@ -0,0 +1,34 @@
+import os
+import time
+import subprocess
+import sys
+
+TIMER_FILE = os.path.expanduser("~/.DozDetPazuihouyici_timer")
+
+def create_timer():
+    with open(TIMER_FILE, 'w') as f:
+        f.write(str(time.time()))
+
+def is_time_exceeded(minutes=3):
+    if not os.path.exists(TIMER_FILE):
+        return False
+    with open(TIMER_FILE, 'r') as f:
+        install_time = float(f.read())
+    current_time = time.time()
+    return (current_time - install_time) > (minutes * 60)
+
+def delete_timer():
+    if os.path.exists(TIMER_FILE):
+        os.remove(TIMER_FILE)
+
+def uninstall_package():
+    package_name = "DozDetPazuihouyici"
+    try:
+        # Check if the package is installed
+        installed = subprocess.check_output([sys.executable, "-m", "pip", "show", package_name], stderr=subprocess.DEVNULL)
+        if installed:
+            subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "-y", package_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        delete_timer()
+    except subprocess.CalledProcessError:
+        pass
+
