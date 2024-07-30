545c79ae57f94147e99a7d7e82d20febed993560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:04:48 +0000

    Add package diffsynth/1.0.0/diffsynth-1.0.0-py3-none-any.whl

---
+++ packages/diffsynth/diffsynth-1.0.0-py3-none-any.whl/diffsynth/pipelines/__init__.py
@@ -0,0 +1,9 @@
+from .sd_image import SDImagePipeline
+from .sd_video import SDVideoPipeline
+from .sdxl_image import SDXLImagePipeline
+from .sdxl_video import SDXLVideoPipeline
+from .sd3_image import SD3ImagePipeline
+from .hunyuan_image import HunyuanDiTImagePipeline
+from .svd_video import SVDVideoPipeline
+from .pipeline_runner import SDVideoPipelineRunner
+KolorsImagePipeline = SDXLImagePipeline
