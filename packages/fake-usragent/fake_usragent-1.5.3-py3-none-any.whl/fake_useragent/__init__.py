9629e038b7c03748df7f6e06ea79bdbcfced3ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:43 +0000

    Add package fake-usragent/1.5.3/fake_usragent-1.5.3-py3-none-any.whl

---
+++ packages/fake-usragent/fake_usragent-1.5.3-py3-none-any.whl/fake_useragent/__init__.py
@@ -0,0 +1,3 @@
+from fake_usragent.errors import FakeUserAgentError, UserAgentError
+from fake_usragent.fake import FakeUserAgent, UserAgent
+from fake_usragent.settings import __version__ as VERSION
