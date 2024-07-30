d5207b60ef1d11dd97451e64243c05e0748b0043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:16 +0000

    Add package delceshi2/0.1.0/delceshi2-0.1.0-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/delceshi2/delceshi2-0.1.0-cp311-cp311-macosx_10_9_x86_64.whl/delceshi2/__init__.py
@@ -0,0 +1,9 @@
+# self_deleting_package/__init__.py
+
+__version__ = '0.1.0'
+__author__ = 'Your Name'
+__email__ = 'your.email@example.com'
+__description__ = 'A package that returns 0.01 and uninstalls itself after 3 minutes.'
+
+from .main import main
+
