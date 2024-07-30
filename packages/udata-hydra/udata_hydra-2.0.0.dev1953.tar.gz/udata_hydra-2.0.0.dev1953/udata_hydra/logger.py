e9131f1f493edef5513aa7e63898cc93ba58df9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:03:49 +0000

    Add package udata-hydra/2.0.0.dev1953/udata_hydra-2.0.0.dev1953.tar.gz

---
+++ packages/udata-hydra/udata_hydra-2.0.0.dev1953.tar.gz/udata_hydra-2.0.0.dev1953/udata_hydra/logger.py
@@ -0,0 +1,36 @@
+import logging
+
+import coloredlogs
+import sentry_sdk
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
+from sentry_sdk.integrations.rq import RqIntegration
+
+from udata_hydra import config
+
+log = logging.getLogger("udata-hydra")
+context = {"inited": False}
+
+
+def setup_logging() -> logging.Logger:
+    if context.get("inited"):
+        return log
+    if config.SENTRY_DSN:
+        sentry_sdk.init(
+            dsn=config.SENTRY_DSN,
+            integrations=[
+                AioHttpIntegration(),
+                RqIntegration(),
+            ],
+        )
+    coloredlogs.install(level=config.LOG_LEVEL)
+    # silence urllib3 a bit
+    logging.getLogger("urllib3").setLevel("INFO")
+    logging.getLogger("asyncio").setLevel("INFO")
+    context["inited"] = True
+    return log
+
+
+def stop_sentry() -> None:
+    """Stop sentry collection programatically"""
+    if sentry_sdk.Hub.current.client:
+        sentry_sdk.Hub.current.client.close()
