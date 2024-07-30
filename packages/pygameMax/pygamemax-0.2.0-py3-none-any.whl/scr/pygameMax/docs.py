e79d57a87a4092993a8d2530080b201ff95a260c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:04:26 +0000

    Add package pygameMax/0.2.0/pygamemax-0.2.0-py3-none-any.whl

---
+++ packages/pygameMax/pygamemax-0.2.0-py3-none-any.whl/scr/pygameMax/docs.py
@@ -0,0 +1,14 @@
+def overveiw():
+    print(data.overveiw())
+
+
+class data:
+    def overveiw(self):
+        data = """
+            Docs/overveiw:
+
+            no data yet avalible, please wait
+
+            time est. [July 31 2024]
+"""
+        return data
