6f297e778e67b465054330ef20f13b92a6cd6c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 07:04:19 +0000

    Add package torch-tensorrt/2.4.0/torch_tensorrt-2.4.0-cp39-cp39-win_amd64.whl

---
+++ packages/torch-tensorrt/torch_tensorrt-2.4.0-cp39-cp39-win_amd64.whl/torch_tensorrt/dynamo/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+from torch_tensorrt._utils import sanitized_torch_version
+
+from packaging import version
+
+logger = logging.getLogger(__name__)
+
+if version.parse(sanitized_torch_version()) >= version.parse("2.1.dev"):
+    from ._compiler import compile, convert_module_to_trt_engine
+    from ._exporter import export
+    from ._settings import CompilationSettings
+    from ._SourceIR import SourceIR
+    from ._tracer import trace
