2fcc899ba0567330e2b50e8b47428340df6ea7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 16:04:54 +0000

    Add package dash-leaflet-basemaps/0.0.0/dash_leaflet_basemaps-0.0.0.tar.gz

---
+++ packages/dash-leaflet-basemaps/dash_leaflet_basemaps-0.0.0.tar.gz/dash_leaflet_basemaps-0.0.0/tests/test_dash_leaflet_basemaps.py
@@ -0,0 +1,8 @@
+"""Test the dash_leaflet_basemaps package."""
+
+import dash_leaflet_basemaps
+
+
+def test_hello_world():
+    """Hello world test."""
+    assert dash_leaflet_basemaps.Hello().hello_world() == "hello world !"
