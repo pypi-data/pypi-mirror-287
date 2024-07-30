c4e6ebb1edbd2d8db6a814248bbafcfcc49dd8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:47 +0000

    Add package DualRegevEncryption/1.0.0/DualRegevEncryption-1.0.0-py3-none-any.whl

---
+++ packages/DualRegevEncryption/DualRegevEncryption-1.0.0-py3-none-any.whl/DualRegev/IO/__init__.py
@@ -0,0 +1,10 @@
+from .Converter import *
+
+
+__all__ = [
+    'bytes_to_binary',
+    'binary_to_bytes',
+    'bin_to_hex',
+    'hex_to_bin',
+]
+
---
+++ packages/DualRegevEncryption/DualRegevEncryption-1.0.0-py3-none-any.whl/DualRegev/Math/Prime.py
@@ -0,0 +1,12 @@
+def is_prime(n: int) -> bool:
+    if not isinstance(n, int):
+        error_message = 'Invalid data type input.'
+        raise TypeError(error_message)
+    
+    if n < 2:
+        return False
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
