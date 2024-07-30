edc2057e733250c2fdbd0018d25dba3484e82cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:44 +0000

    Add package Rythmix-dot-py/1.0.6/Rythmix_dot_py-1.0.6.tar.gz

---
+++ packages/Rythmix-dot-py/Rythmix_dot_py-1.0.6.tar.gz/Rythmix_dot_py-1.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Rythmix_dot_py",
+    version="1.0.6",
+    packages=find_packages(),
+    description="A simple CLI tool for playing music. (VLC is required!)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "youtube-search-python==1.6.6",
+        "yt-dlp==2024.7.25",
+        "python-vlc==3.0.20123",
+        "distro==1.9.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "rythmix = Rythmix_dot_py:main",
+            "rythmix_setup_vlc = Rythmix_dot_py:install_vlc"
+        ]
+    }
+)
