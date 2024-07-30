cbb0e8ae18d65e7a2522f77efc66ed3c5bb9b65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 16:04:42 +0000

    Add package aisee/0.1.1/aisee-0.1.1-py3-none-any.whl

---
+++ packages/aisee/aisee-0.1.1-py3-none-any.whl/aisee/__init__.py
@@ -0,0 +1,10 @@
+from .custom_datasets import (
+    DatasetFromDataFrame,
+    DatasetFromFolder,
+    DatasetFromSingleImage,
+)
+from .trainer import Trainer
+from .vision_classifier import VisionClassifier
+
+
+__version__ = "0.1.1"
