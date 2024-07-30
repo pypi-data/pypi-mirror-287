47b69ec45042542c05131ad5024bb0e197e89574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 16:04:43 +0000

    Add package digiflow/4.2.1/digiflow-4.2.1.tar.gz

---
+++ packages/digiflow/digiflow-4.2.1.tar.gz/digiflow-4.2.1/src/digiflow/__init__.py
@@ -0,0 +1,49 @@
+
+from .digflow_identifier import *
+from .digiflow_io import *
+from .digiflow_metadata import *
+from .digiflow_generate import *
+from .digiflow_export import *
+from .validate.metadata_xslt import (
+    DDB_IGNORE_RULES_BASIC,
+    DDB_IGNORE_RULES_MVW,
+    DDB_IGNORE_RULES_NEWSPAPERS,
+    FAILED_ASSERT_ERROR,
+    FAILED_ASSERT_OTHER,
+    DIGIS_MULTIVOLUME,
+    DIGIS_NEWSPAPER,
+    REPORT_FILE_XSLT,
+    DigiflowDDBException,
+    DigiflowTransformException,
+    apply,
+    ddb_validation,
+    gather_failed_asserts,
+)
+from .validate import (
+    LABEL_SCAN_VALIDATOR_CHANNEL,
+    LABEL_SCAN_VALIDATOR_COMPRESSION,
+    LABEL_SCAN_VALIDATOR_RESOLUTION,
+    LABEL_SCAN_VALIDATOR_FILEDATA,
+    UNSET_NUMBR,
+    INVALID_LABEL_UNSET,
+    FSReadException,
+    FSWriteException,
+    Image,
+    ImageMetadata,
+    Invalid,
+    InvalidImageDataException,
+	ScanValidatorCombined,
+	ScanValidatorChannel,
+	ScanValidatorCompression,
+	ScanValidatorFile,
+	ScanValidatorPhotometric,
+	ScanValidatorResolution,
+    Validator,
+	ValidatorFactory,
+    resource_can_be,
+    group_can_read,
+    group_can_write,
+    validate_tiff,
+)
+
+from .common import UNSET_LABEL, XMLNS
