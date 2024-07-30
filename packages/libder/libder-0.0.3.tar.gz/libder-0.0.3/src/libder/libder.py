092b74ef50a683f3f0622506d74cdfc722b62021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 14:04:34 +0000

    Add package libder/0.0.3/libder-0.0.3.tar.gz

---
+++ packages/libder/libder-0.0.3.tar.gz/libder-0.0.3/src/libder/libder.py
@@ -0,0 +1,8 @@
+import cv2
+
+
+def printDer(height=512, width=512):
+    black_image = cv2.UMat(height, width, cv2.CV_8UC3)
+    cv2.imshow('Der', black_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
