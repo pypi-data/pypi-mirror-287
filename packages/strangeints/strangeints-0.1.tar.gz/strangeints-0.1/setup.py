d7fa840c176c5ddd0f76db94227ec3c00d5b6062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 07:03:59 +0000

    Add package strangeints/0.1/strangeints-0.1.tar.gz

---
+++ packages/strangeints/strangeints-0.1.tar.gz/strangeints-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='strangeints',
+        version='0.1',
+        packages=find_packages(),
+        install_requires=[],
+        description='Converts a string of ranges to a list of ints',
+        author='Scott Smith',
+        author_email='trinitite@protonmail.com',
+        url='https://github.com/siriusarcturus/strangeints',
+        license='MIT'
+        )
+
