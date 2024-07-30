5c70cc71474a7cda992e8ae7d35b0ae6f499bcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:42 +0000

    Add package random-dict/1.2.0/random_dict-1.2.0.tar.gz

---
+++ packages/random-dict/random_dict-1.2.0.tar.gz/random_dict-1.2.0/random_dict/__init__.py
@@ -0,0 +1,36 @@
+"""Random-Dict module."""
+
+from random_dict.random_dict import (
+    random_dict,
+    random_float_dict,
+    random_bool_dict,
+    random_string_dict,
+    random_int_dict,
+)
+
+from random_dict.random_dict import (
+    random_bool,
+    random_float,
+    random_int,
+    random_bytes,
+    random_string,
+    random_pandas_dataframe,
+    random_pandas_series,
+    random_numpy_array,
+)
+
+__all__ = [
+    "random_dict",
+    "random_float_dict",
+    "random_bool_dict",
+    "random_string_dict",
+    "random_int_dict",
+    "random_bool",
+    "random_float",
+    "random_int",
+    "random_bytes",
+    "random_string",
+    "random_pandas_dataframe",
+    "random_pandas_series",
+    "random_numpy_array",
+]
---
+++ packages/random-dict/random_dict-1.2.0.tar.gz/random_dict-1.2.0/random_dict/__version__.py
@@ -0,0 +1,3 @@
+"""Current version of package random_dict."""
+
+__version__ = "1.2.0"
