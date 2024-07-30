e1b4baa7b13b9177da14a8f336dc15a048c6073c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:04:46 +0000

    Add package langchain-openai-api-bridge/0.10.0/langchain_openai_api_bridge-0.10.0-py3-none-any.whl

---
+++ packages/langchain-openai-api-bridge/langchain_openai_api_bridge-0.10.0-py3-none-any.whl/langchain_openai_api_bridge/assistant/adapter/langgraph_event_to_openai_assistant_event_stream.py
@@ -0,0 +1,80 @@
+from typing import AsyncIterator
+from langchain_core.runnables.schema import StreamEvent
+from openai.types.beta import AssistantStreamEvent
+from langchain_openai_api_bridge.assistant.adapter.on_chat_model_end_handler import (
+    OnChatModelEndHandler,
+)
+from langchain_openai_api_bridge.assistant.adapter.on_chat_model_stream_handler import (
+    OnChatModelStreamHandler,
+)
+from langchain_openai_api_bridge.assistant.adapter.on_tool_end_handler import (
+    OnToolEndHandler,
+)
+from langchain_openai_api_bridge.assistant.adapter.on_tool_start_handler import (
+    OnToolStartHandler,
+)
+from langchain_openai_api_bridge.assistant.adapter.thread_run_event_handler import (
+    ThreadRunEventHandler,
+)
+from langchain_openai_api_bridge.assistant.create_thread_runs_api_dto import (
+    ThreadRunsDto,
+)
+
+
+class LanggraphEventToOpenAIAssistantEventStream:
+    def __init__(
+        self,
+        thread_run_event_factory: ThreadRunEventHandler,
+        on_chat_model_stream_handler: OnChatModelStreamHandler,
+        on_chat_model_end_handler: OnChatModelEndHandler,
+        on_tool_start_handler: OnToolStartHandler,
+        on_tool_end_handler: OnToolEndHandler,
+    ) -> None:
+        self.thread_run_event_factory = thread_run_event_factory
+        self.on_chat_model_stream_handler = on_chat_model_stream_handler
+        self.on_chat_model_end_handler = on_chat_model_end_handler
+        self.on_tool_start_handler = on_tool_start_handler
+        self.on_tool_end_handler = on_tool_end_handler
+
+    async def to_openai_assistant_event_stream(
+        self,
+        astream_events: AsyncIterator[StreamEvent],
+        dto: ThreadRunsDto,
+    ) -> AsyncIterator[AssistantStreamEvent]:
+
+        thread_run = self.thread_run_event_factory.on_thread_run_start(
+            assistant_id=dto.assistant_id,
+            thread_id=dto.thread_id,
+            model=dto.model,
+        )
+
+        yield thread_run
+
+        async for event in astream_events:
+            kind = event["event"]
+            adapted_events: list[AssistantStreamEvent] = []
+            match kind:
+                case "on_chat_model_stream":
+                    adapted_events += self.on_chat_model_stream_handler.handle(
+                        event=event,
+                        dto=dto,
+                        run=thread_run.data,
+                    )
+                case "on_chat_model_end":
+                    adapted_events += self.on_chat_model_end_handler.handle(
+                        event=event,
+                        dto=dto,
+                        run=thread_run.data,
+                    )
+                case "on_tool_start":
+                    adapted_events += self.on_tool_start_handler.handle(
+                        event=event, dto=dto
+                    )
+                case "on_tool_end":
+                    adapted_events += self.on_tool_end_handler.handle(
+                        event=event, dto=dto
+                    )
+            for event in adapted_events:
+                yield event
+
+        yield self.thread_run_event_factory.on_thread_run_completed(run=thread_run.data)
