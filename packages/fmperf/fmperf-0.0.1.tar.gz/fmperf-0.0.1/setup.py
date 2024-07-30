378f3d637fc5f502cff559925a83de2f4d163629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:04:50 +0000

    Add package fmperf/0.0.1/fmperf-0.0.1.tar.gz

---
+++ packages/fmperf/fmperf-0.0.1.tar.gz/fmperf-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="fmperf",
+    version="0.0.1",
+    package_data={"fmperf.data": ["ai.txt", "all_nbins_64.pkl"]},
+    packages=["fmperf", "fmperf.utils", "fmperf.loadgen", "fmperf.data"],
+    url = 'https://github.com/fmperf-project/fmperf.git',
+    author= 'IBM Research',
+    license='Apache 2.0',
+)
