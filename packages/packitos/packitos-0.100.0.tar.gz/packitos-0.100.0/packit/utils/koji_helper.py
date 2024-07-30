66a8ac6d8d8f132d8a2682745809327235c03a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:05:34 +0000

    Add package packitos/0.100.0/packitos-0.100.0.tar.gz

---
+++ packages/packitos/packitos-0.100.0.tar.gz/packitos-0.100.0/packit/utils/koji_helper.py
@@ -0,0 +1,355 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+import logging
+from datetime import date, datetime
+from typing import Optional, Union
+
+import koji
+from specfile.changelog import ChangelogEntry
+
+from packit.constants import KOJI_BASEURL
+
+logger = logging.getLogger(__name__)
+
+
+class KojiHelper:
+    """
+    Class for querying Koji.
+
+    Attributes:
+        session: Koji client session.
+    """
+
+    def __init__(self, session: Optional[koji.ClientSession] = None) -> None:
+        self.session = (
+            session if session is not None else koji.ClientSession(baseurl=KOJI_BASEURL)
+        )
+
+    def get_builds(self, package: str, since: datetime) -> list[dict]:
+        """
+        Gets list of builds of a package since the specified datetime.
+
+        Args:
+            package: Package name.
+            since: Only builds newer than this datetime will be considered.
+
+        Returns:
+            List of builds.
+        """
+        try:
+            package_id = self.session.getPackageID(package, strict=True)
+        except Exception as e:
+            logger.debug(f"Failed to get ID of package {package} from Koji: {e}")
+            return []
+        try:
+            builds = self.session.listBuilds(
+                packageID=package_id,
+                state=koji.BUILD_STATES["COMPLETE"],
+                completeAfter=since.timestamp(),
+            )
+        except Exception as e:
+            logger.debug(f"Failed to get builds of package {package} from Koji: {e}")
+            return []
+        if not builds:
+            logger.debug(f"No builds found for package {package} since {since}")
+            return []
+        return builds
+
+    def get_nvrs(self, package: str, since: datetime) -> list[str]:
+        """
+        Gets list of nvr for builds of a package since the specified datetime.
+
+        Args:
+            package: Package name.
+            since: Only builds newer than this datetime will be considered.
+
+        Returns:
+            List of NVRs.
+        """
+        return [b["nvr"] for b in self.get_builds(package, since)]
+
+    def get_latest_build_in_tag(self, package: str, tag: str) -> Optional[dict]:
+        """
+        Gets the latest build of a package tagged into the specified tag.
+
+        Args:
+            package: Package name.
+            tag: Koji tag.
+
+        Returns:
+            Latest build or None if there is no such build.
+        """
+        try:
+            builds = self.session.listTagged(
+                package=package,
+                tag=tag,
+                inherit=True,
+                latest=True,
+                strict=True,
+            )
+        except Exception as e:
+            logger.debug(
+                f"Failed to get latest build of package {package} in tag {tag} from Koji: {e}",
+            )
+            return None
+        if not builds:
+            return None
+        return builds[0]
+
+    def get_latest_nvr_in_tag(self, package: str, tag: str) -> Optional[str]:
+        """
+        Gets the latest build of a package tagged into the specified tag.
+
+        Args:
+            package: Package name.
+            tag: Koji tag.
+
+        Returns:
+            NVR of the latest build or None if there is no such build.
+        """
+        build = self.get_latest_build_in_tag(package, tag)
+        if not build:
+            return None
+        return build["nvr"]
+
+    def get_build_tags(self, nvr: str) -> list[str]:
+        """
+        Gets tags the specified build is tagged into.
+
+        Args:
+            nvr: NVR of the build.
+
+        Returns:
+            List of tag names.
+        """
+        try:
+            tags = self.session.listTags(build=nvr)
+        except Exception as e:
+            logger.debug(f"Failed to get tags of build {nvr} from Koji: {e}")
+            return []
+        return [t["name"] for t in tags]
+
+    def get_build_changelog(self, nvr: str) -> list[tuple[int, str, str]]:
+        """
+        Gets changelog associated with SRPM of the specified build.
+
+        Args:
+            nvr: NVR of the build.
+
+        Returns:
+            List of changelog entries in form of (timestamp, author, content) tuples.
+        """
+        requested_headers = ["changelogtime", "changelogname", "changelogtext"]
+        try:
+            headers = self.session.getRPMHeaders(
+                rpmID=f"{nvr}.src",
+                headers=requested_headers,
+                strict=True,
+            )
+        except Exception as e:
+            logger.debug(f"Failed to get changelog of build {nvr} from Koji: {e}")
+            return []
+        for k, v in headers.items():
+            if not isinstance(v, list):
+                headers[k] = [v]
+        return list(zip(*[headers[h] for h in requested_headers]))
+
+    def get_builds_in_tag(self, tag: str) -> list[dict]:
+        """
+        Gets list of builds tagged into the specified tag.
+
+        Args:
+            tag: Tag name.
+
+        Returns:
+            List of builds.
+        """
+        try:
+            builds = self.session.listTagged(
+                tag=tag,
+                inherit=False,
+                latest=False,
+                strict=True,
+            )
+        except Exception as e:
+            logger.debug(f"Failed to get builds tagged into {tag} from Koji: {e}")
+            return []
+        return builds
+
+    def get_build_info(self, build: Union[int, str]) -> Optional[dict]:
+        """
+        Gets build information.
+
+        Args:
+            build: Koji build ID or NVR.
+
+        Returns:
+            Build information or None if there is no such build.
+        """
+        try:
+            info = self.session.getBuild(build)
+        except Exception as e:
+            logger.debug(f"Failed to get build info of {build} from Koji: {e}")
+            return None
+        return info
+
+    def get_tag_info(self, tag: str) -> Optional[dict]:
+        """
+        Gets tag information.
+
+        Args:
+            tag: Koji tag.
+
+        Returns:
+            Tag information or None if there is no such tag.
+        """
+        try:
+            info = self.session.getBuildConfig(tag)
+        except Exception as e:
+            logger.debug(f"Failed to get tag info of {tag} from Koji: {e}")
+            return None
+        return info
+
+    def create_sidetag(self, dist_git_branch: str) -> Optional[dict]:
+        """
+        Creates a new sidetag for the specified dist-git branch.
+
+        Attempts to log in if the underlying session is not authenticated.
+
+        Args:
+            dist_git_branch: dist-git branch name.
+
+        Returns:
+            New tag information or None if creation failed.
+        """
+        target_name = self.get_build_target(dist_git_branch)
+        try:
+            target = self.session.getBuildTarget(target_name)
+        except Exception as e:
+            logger.debug(f"Failed to get build target {target_name} from Koji: {e}")
+            return None
+        if not target:
+            logger.debug(f"Failed to get build target {target_name} from Koji")
+            return None
+        if not (build_tag := target.get("build_tag_name")):
+            logger.debug(f"Failed to get build tag from build target {target_name}")
+            return None
+        if not self.session.logged_in:
+            try:
+                self.session.gssapi_login()
+            except Exception as e:
+                logger.debug(f"Authentication failed: {e}")
+                return None
+        try:
+            info = self.session.createSideTag(build_tag)
+        except Exception as e:
+            logger.debug(f"Failed to create sidetag for {build_tag} in Koji: {e}")
+            return None
+        return info
+
+    def remove_sidetag(self, sidetag: str) -> None:
+        """
+        Removes the specified sidetag.
+
+        Attempts to log in if the underlying session is not authenticated.
+
+        Args:
+            sidetag: Sidetag name.
+        """
+        if not self.session.logged_in:
+            try:
+                self.session.gssapi_login()
+            except Exception as e:
+                logger.debug(f"Authentication failed: {e}")
+                return
+        try:
+            self.session.removeSideTag(sidetag)
+        except Exception as e:
+            logger.debug(f"Failed to remove sidetag {sidetag} in Koji: {e}")
+
+    @staticmethod
+    def get_build_target(dist_git_branch: str) -> str:
+        """
+        Gets a build target from a dist-git branch name.
+
+        Args:
+            dist_git_branch: dist-git branch name.
+
+        Returns:
+            Name of matching build target.
+        """
+        if dist_git_branch in ["rawhide", "main"]:
+            return "rawhide"
+        return f"{dist_git_branch}-candidate"
+
+    @staticmethod
+    def format_changelog(changelog: list[tuple[int, str, str]], since: int = 0) -> str:
+        """
+        Formats changelog entries since the specified timestamp.
+
+        Args:
+            changelog: Changelog as a list of entries in form of
+              (timestamp, author, content) tuples.
+            since: Only entries newer than this timestamp will be included.
+
+        Returns:
+            Formatted changelog as a string.
+        """
+        lines = []
+        for time, name, text in changelog:
+            if time <= since:
+                break
+            timestamp = date.fromtimestamp(time)
+            lines.append(
+                str(ChangelogEntry.assemble(timestamp, name, text.splitlines())),
+            )
+        return "\n".join(lines)
+
+    # | Overview of tag names
+    # |------------------------------|----------------------|-------------------------|
+    # |                              | Fedora               | EPEL                    |
+    # |------------------------------|----------------------|-------------------------|
+    # | fresh build                  | fN-updates-candidate | epelN-testing-candidate |
+    # | associated update in testing | fN-updates-testing   | epelN-testing           |
+    # | associated update in stable  | fN-updates/fN        | epelN                   |
+    # |------------------------------|----------------------|-------------------------|
+
+    @staticmethod
+    def get_candidate_tag(dist_git_branch: str) -> str:
+        """
+        Gets a candidate tag from a dist-git branch name.
+
+        E.g. for branch f37 the result would be f37-updates-candidate,
+        for epel8 branch it would be epel8-testing-candidate.
+
+        Args:
+            dist_git_branch: dist-git branch name.
+
+        Returns:
+            Name of matching candidate tag.
+        """
+        if dist_git_branch.startswith("epel"):
+            return f"{dist_git_branch}-testing-candidate"
+        return f"{dist_git_branch}-updates-candidate"
+
+    @staticmethod
+    def get_stable_tags(tag: str) -> list[str]:
+        """
+        Gets a list of stable tags from the specified tag name.
+
+        E.g. for tag f37-updates-testing the result would be [f37-updates, f37],
+        for epel8-testing-candidate it would be [epel8].
+
+        Args:
+            tag: Tag name.
+
+        Returns:
+            List of stable tags deduced. Can be empty.
+        """
+        if not tag.endswith("-candidate") and not tag.endswith("-testing"):
+            return []
+        stable_tag = tag.removesuffix("-candidate").removesuffix("-testing")
+        if stable_tag.endswith("-updates"):
+            return [stable_tag, stable_tag.removesuffix("-updates")]
+        return [stable_tag]
---
+++ packages/packitos/packitos-0.100.0.tar.gz/packitos-0.100.0/tests/unit/utils/test_koji_helper.py
@@ -0,0 +1,284 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+import datetime
+
+import pytest
+from flexmock import flexmock
+
+from packit.utils.koji_helper import KojiHelper
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_get_builds(error):
+    nvrs = [f"test-1.{n}-1.fc37" for n in range(3)]
+
+    def getPackageID(*_, **__):
+        return 12345
+
+    def listBuilds(*_, **__):
+        if error:
+            raise Exception
+        return [{"nvr": nvr} for nvr in nvrs]
+
+    session = flexmock(getPackageID=getPackageID, listBuilds=listBuilds)
+    result = KojiHelper(session).get_nvrs("test", datetime.datetime(2022, 6, 1))
+    if error:
+        assert result == []
+    else:
+        assert result == nvrs
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_get_latest_nvr_in_tag(error):
+    nvr = "test-1.0-1.fc37"
+
+    def listTagged(*_, **__):
+        if error:
+            raise Exception
+        return [{"nvr": nvr}]
+
+    session = flexmock(listTagged=listTagged)
+    result = KojiHelper(session).get_latest_nvr_in_tag("test", "f37-updates-candidate")
+    if error:
+        assert result is None
+    else:
+        assert result == nvr
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_get_build_tags(error):
+    tags = ["f37-updates-testing"]
+
+    def listTags(*_, **__):
+        if error:
+            raise Exception
+        return [{"name": t} for t in tags]
+
+    session = flexmock(listTags=listTags)
+    result = KojiHelper(session).get_build_tags("test-1.0-1.fc37")
+    if error:
+        assert result == []
+    else:
+        assert result == tags
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_get_build_changelog(error):
+    changelog = [
+        (1655726400, "Nikola Forró <nforro@redhat.com> - 0.2-1.fc37", "- third entry"),
+        (1652702400, "Nikola Forró <nforro@redhat.com> - 0.1-2.fc37", "- second entry"),
+        (1648728000, "Nikola Forró <nforro@redhat.com> - 0.1-1.fc37", "- first entry"),
+    ]
+
+    def getRPMHeaders(*_, **__):
+        if error:
+            raise Exception
+        result = list(zip(*changelog))
+        return {
+            "changelogtime": list(result[0]),
+            "changelogname": list(result[1]),
+            "changelogtext": list(result[2]),
+        }
+
+    session = flexmock(getRPMHeaders=getRPMHeaders)
+    result = KojiHelper(session).get_build_changelog("test-0.2-1.fc37")
+    if error:
+        assert result == []
+    else:
+        assert result == changelog
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_get_builds_in_tag(error):
+    builds = [
+        {
+            "package_name": "python-specfile",
+            "name": "python3-specfile",
+            "version": "0.28.0",
+            "release": "1.fc39",
+        },
+    ]
+
+    def listTagged(*_, **__):
+        if error:
+            raise Exception
+        return builds
+
+    session = flexmock(listTagged=listTagged)
+    result = KojiHelper(session).get_builds_in_tag("f39-build-side-12345")
+    if error:
+        assert result == []
+    else:
+        assert result == builds
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_get_build_info(error):
+    info = {"id": 123456, "name": "test", "nvr": "test-1.0-1.fc39"}
+
+    def getBuild(*_, **__):
+        if error:
+            raise Exception
+        return info
+
+    session = flexmock(getBuild=getBuild)
+    for build in [123456, "test-1.0-1.fc39"]:
+        result = KojiHelper(session).get_build_info(build)
+        if error:
+            assert result is None
+        else:
+            assert result == info
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_get_tag_info(error):
+    info = {"name": "f39-build-side-12345", "id": 12345}
+
+    def getBuildConfig(*_, **__):
+        if error:
+            raise Exception
+        return info
+
+    session = flexmock(getBuildConfig=getBuildConfig)
+    result = KojiHelper(session).get_tag_info("f39-build-side-12345")
+    if error:
+        assert result is None
+    else:
+        assert result == info
+
+
+@pytest.mark.parametrize(
+    "error",
+    [False, True],
+)
+def test_create_sidetag(error):
+    info = {"name": "f39-build-side-12345", "id": 12345}
+
+    def createSideTag(*_, **__):
+        if error:
+            raise Exception
+        return info
+
+    session = flexmock(
+        logged_in=True,
+        getBuildTarget=lambda _: {"build_tag_name": "f39-build"},
+        createSideTag=createSideTag,
+    )
+    result = KojiHelper(session).create_sidetag("f39")
+    if error:
+        assert result is None
+    else:
+        assert result == info
+
+
+@pytest.mark.parametrize(
+    "logged_in",
+    [False, True],
+)
+def test_remove_sidetag(logged_in):
+    session = flexmock(logged_in=logged_in)
+    session.should_receive("gssapi_login").and_return().times(
+        0 if logged_in else 1,
+    ).mock()
+    session.should_receive("removeSideTag").once()
+    KojiHelper(session).remove_sidetag("f39-build-side-12345")
+
+
+@pytest.mark.parametrize(
+    "branch, target",
+    [
+        ("f39", "f39-candidate"),
+        ("epel9", "epel9-candidate"),
+        ("rawhide", "rawhide"),
+        ("main", "rawhide"),
+    ],
+)
+def test_get_build_target(branch, target):
+    assert KojiHelper.get_build_target(branch) == target
+
+
+@pytest.mark.parametrize(
+    "since, formatted_changelog",
+    [
+        (
+            1652702400,
+            "* Mon Jun 20 2022 Nikola Forró <nforro@redhat.com> - 0.2-1\n"
+            "- third entry\n",
+        ),
+        (
+            1648728000,
+            "* Mon Jun 20 2022 Nikola Forró <nforro@redhat.com> - 0.2-1\n"
+            "- third entry\n"
+            "\n"
+            "* Mon May 16 2022 Nikola Forró <nforro@redhat.com> - 0.1-2\n"
+            "- second entry\n",
+        ),
+        (
+            0,
+            "* Mon Jun 20 2022 Nikola Forró <nforro@redhat.com> - 0.2-1\n"
+            "- third entry\n"
+            "\n"
+            "* Mon May 16 2022 Nikola Forró <nforro@redhat.com> - 0.1-2\n"
+            "- second entry\n"
+            "\n"
+            "* Thu Mar 31 2022 Nikola Forró <nforro@redhat.com> - 0.1-1\n"
+            "- first entry\n",
+        ),
+    ],
+)
+def test_format_changelog(since, formatted_changelog):
+    changelog = [
+        (1655726400, "Nikola Forró <nforro@redhat.com> - 0.2-1", "- third entry"),
+        (1652702400, "Nikola Forró <nforro@redhat.com> - 0.1-2", "- second entry"),
+        (1648728000, "Nikola Forró <nforro@redhat.com> - 0.1-1", "- first entry"),
+    ]
+    assert KojiHelper.format_changelog(changelog, since) == formatted_changelog
+
+
+@pytest.mark.parametrize(
+    "branch, tag",
+    [
+        ("f37", "f37-updates-candidate"),
+        ("epel8", "epel8-testing-candidate"),
+    ],
+)
+def test_get_candidate_tag(branch, tag):
+    assert KojiHelper.get_candidate_tag(branch) == tag
+
+
+@pytest.mark.parametrize(
+    "tag, stable_tags",
+    [
+        ("f37-updates-candidate", ["f37-updates", "f37"]),
+        ("f37-updates-testing", ["f37-updates", "f37"]),
+        ("epel8-testing-candidate", ["epel8"]),
+        ("epel8-testing", ["epel8"]),
+        ("f37-updates", []),
+        ("epel8", []),
+        ("eln", []),
+    ],
+)
+def test_get_stable_tags(tag, stable_tags):
+    assert KojiHelper.get_stable_tags(tag) == stable_tags
