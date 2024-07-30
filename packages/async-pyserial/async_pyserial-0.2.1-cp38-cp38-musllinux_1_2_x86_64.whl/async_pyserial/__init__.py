833ebba7a6606506af491c6e1263d016bbcd7382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 03:04:43 +0000

    Add package async-pyserial/0.2.1/async_pyserial-0.2.1-cp38-cp38-musllinux_1_2_x86_64.whl

---
+++ packages/async-pyserial/async_pyserial-0.2.1-cp38-cp38-musllinux_1_2_x86_64.whl/async_pyserial/__init__.py
@@ -0,0 +1,23 @@
+import sys
+
+from async_pyserial.common import *
+
+from async_pyserial.backend import set_async_worker
+
+__version__ = '0.2.1'
+
+VERSION = __version__
+
+__all__ = ["SerialPort", "SerialPortOptions", "SerialPortEvent", "SerialPortParity", "set_async_worker"]
+
+sys_platform = sys.platform
+    
+if sys_platform  == 'win32' or \
+    sys_platform == 'linux' or \
+    sys_platform == 'darwin' or \
+    'bsd' in sys_platform.lower():
+
+    from async_pyserial.native_serialport import SerialPort
+
+else:
+    raise PlatformNotSupported()
