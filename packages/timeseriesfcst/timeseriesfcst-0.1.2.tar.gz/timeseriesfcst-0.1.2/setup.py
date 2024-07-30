c2fb295499d6bbf65b3cfa7fd36cd216c09196bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:05:05 +0000

    Add package timeseriesfcst/0.1.2/timeseriesfcst-0.1.2.tar.gz

---
+++ packages/timeseriesfcst/timeseriesfcst-0.1.2.tar.gz/timeseriesfcst-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="timeseriesfcst",
+    packages=["timeseriesfcst"],
+    version="0.1.2",
+    author="Anna-Valentina Hirsch",
+    author_email="a-valentina.hirsch@hotmail.com",
+    description="A python toolkit for time series preprocessing, feature engineering, and forecasting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnnaValentinaHirsch/time-series-toolkit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "statsmodels",
+        "scikit-learn",
+        "tensorflow"
+    ],
+)
