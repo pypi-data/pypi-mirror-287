4e22ceb32d6a5c6b407f545888947e9455913396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 04:05:24 +0000

    Add package ensurepython/0.0.0/ensurepython-0.0.0.tar.gz

---
+++ packages/ensurepython/ensurepython-0.0.0.tar.gz/ensurepython-0.0.0/src/ensurepython/__init__.py
@@ -0,0 +1,7 @@
+"""ensurepython - A CLI utility to enauto install python on a machine."""
+from __future__ import annotations
+
+
+def greet(name: str) -> str:
+    """Returns a greeting."""
+    return f"Hello {name}!"
