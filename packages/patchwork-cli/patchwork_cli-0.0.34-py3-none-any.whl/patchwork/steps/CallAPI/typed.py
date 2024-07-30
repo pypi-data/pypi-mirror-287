c998e00779ce457f6f1dbff459bcad096cc18c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:09 +0000

    Add package patchwork-cli/0.0.34/patchwork_cli-0.0.34-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.34-py3-none-any.whl/patchwork/steps/CallAPI/typed.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing_extensions import Annotated, Any, Literal, TypedDict
+
+from patchwork.common.utils.typing import IS_CONFIG
+
+
+class __CallAPIRequiredInputs(TypedDict):
+    url: str
+    method: Annotated[Literal["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"], IS_CONFIG]
+
+
+class CallAPIInputs(__CallAPIRequiredInputs, total=False):
+    headers: Annotated[dict[str, str], IS_CONFIG]
+    body: dict[str, Any]
+
+
+class CallAPIOutputs(TypedDict):
+    status_code: int
+    headers: dict[str, str]
+    body: str
---
+++ packages/patchwork-cli/patchwork_cli-0.0.34-py3-none-any.whl/patchwork/steps/PreparePR/typed.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing_extensions import Annotated, TypedDict
+
+from patchwork.common.utils.typing import IS_CONFIG
+
+
+class __PreparePRRequiredInputs(TypedDict):
+    modified_code_files: list["ModifiedCodeFile"]
+
+
+class PreparePRInputs(__PreparePRRequiredInputs, total=False):
+    pr_header: Annotated[str, IS_CONFIG]
+
+
+class PreparePROutputs(TypedDict):
+    pr_body: str
+
+
+class ModifiedCodeFile(TypedDict, total=False):
+    path: str
+    start_line: int
+    end_line: int
