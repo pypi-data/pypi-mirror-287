b7631e39983cbee004d68dcebd890a124b1d4c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:03:44 +0000

    Add package openinference-instrumentation/0.1.9/openinference_instrumentation-0.1.9-py3-none-any.whl

---
+++ packages/openinference-instrumentation/openinference_instrumentation-0.1.9-py3-none-any.whl/openinference/instrumentation/__init__.py
@@ -0,0 +1,30 @@
+from .config import REDACTED_VALUE, TraceConfig, suppress_tracing
+from .context_attributes import (
+    get_attributes_from_context,
+    using_attributes,
+    using_metadata,
+    using_prompt_template,
+    using_session,
+    using_tags,
+    using_user,
+)
+from .helpers import safe_json_dumps
+
+# The following line is needed to ensure that other modules using the
+# `openinference.instrumentation` path can be discovered by Bazel. For details,
+# see: https://github.com/Arize-ai/openinference/issues/398
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+__all__ = [
+    "get_attributes_from_context",
+    "using_attributes",
+    "using_metadata",
+    "using_prompt_template",
+    "using_session",
+    "using_tags",
+    "using_user",
+    "safe_json_dumps",
+    "suppress_tracing",
+    "TraceConfig",
+    "REDACTED_VALUE",
+]
