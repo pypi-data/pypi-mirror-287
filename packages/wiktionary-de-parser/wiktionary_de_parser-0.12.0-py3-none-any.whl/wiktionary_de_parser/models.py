ef2ba5570e45bc5519a4984ff194e52b80731630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 06:03:44 +0000

    Add package wiktionary-de-parser/0.12.0/wiktionary_de_parser-0.12.0-py3-none-any.whl

---
+++ packages/wiktionary-de-parser/wiktionary_de_parser-0.12.0-py3-none-any.whl/wiktionary_de_parser/models.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+
+
+class WiktionaryPage(BaseModel):
+    page_id: int
+    name: str
+    wikitext: str | None
+    redirect_to: str | None = None
+
+
+class WiktionaryPageEntry(BaseModel):
+    page: WiktionaryPage
+    index: int
+    wikitext: str
+
+
+class Language(BaseModel):
+    lang: str | None
+    lang_code: str | None
+
+
+class Lemma(BaseModel):
+    lemma: str
+    inflected: bool
+
+
+ParseFlexionResult = dict | None
+ParseIpaResult = list[str] | None
+ParseLanuageResult = Language
+ParseLemmaResult = Lemma
+ParsePosResult = dict[str, list[str]] | None
+ParseRhymesResult = list[str] | None
+ParseHyphenationResult = list[str] | None
+
+
+class ParsedWiktionaryPageEntry(BaseModel):
+    name: str
+    hyphenation: ParseHyphenationResult
+    flexion: ParseFlexionResult
+    ipa: ParseIpaResult
+    language: ParseLanuageResult
+    lemma: ParseLemmaResult
+    pos: ParsePosResult
+    rhymes: ParseRhymesResult
