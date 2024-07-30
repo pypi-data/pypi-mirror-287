f5d1cebdd61fc4b5d0b9f293226dec38cfc75caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:28 +0000

    Add package Rythmix-dot-py/1.0.1/Rythmix_dot_py-1.0.1.tar.gz

---
+++ packages/Rythmix-dot-py/Rythmix_dot_py-1.0.1.tar.gz/Rythmix_dot_py-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Rythmix_dot_py",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "youtube-search-python",
+        "yt-dlp==2024.7.25",
+        "python-vlc==3.0.20123"
+    ],
+    entry_points={
+        "console_scripts": [
+            "rythmix = Rythmix_dot_py:main"
+        ]
+    }
+)
