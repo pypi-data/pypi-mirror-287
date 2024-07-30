afc79f1a5cf44e23b9f28c8d0362f7f2a8736872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 07:04:08 +0000

    Add package pagarme-python/5.9.0/pagarme_python-5.9.0-py3-none-any.whl

---
+++ packages/pagarme-python/pagarme_python-5.9.0-py3-none-any.whl/pagarme/resources/routes/kyc_link_routes.py
@@ -0,0 +1,3 @@
+BASE_URL = 'https://api.pagar.me/1/recipients'
+
+CREATE_KYC_LINK = BASE_URL + '/{0}/kyc_link'
---
+++ packages/pagarme-python/pagarme_python-5.9.0-py3-none-any.whl/tests/resources/dictionaries/bulk_anticipation_dictionary.py
@@ -0,0 +1,12 @@
+from tests.resources.pagarme_test import generate_timestamp
+
+BULK_ANTICIPATION = {
+    'payment_date': generate_timestamp(),
+    'timeframe': 'start',
+    'requested_amount': '500000'
+}
+
+LIMITS = {
+    'payment_date': generate_timestamp(),
+    'timeframe': 'start'
+}
