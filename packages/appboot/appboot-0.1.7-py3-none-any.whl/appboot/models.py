8efd8464646f9c3d68f8a321e410112c72e7309f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 05:04:07 +0000

    Add package appboot/0.1.7/appboot-0.1.7-py3-none-any.whl

---
+++ packages/appboot/appboot-0.1.7-py3-none-any.whl/appboot/models.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import typing
+from datetime import datetime
+from typing import Optional, TypeVar
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, declared_attr, mapped_column, with_loader_criteria
+from sqlalchemy.sql.base import ExecutableOption
+from typing_extensions import Self
+
+from appboot.db import Base
+from appboot.interfaces import BaseRepository
+from appboot.repository import Repository
+from appboot.utils import camel_to_snake
+
+ModelT = TypeVar('ModelT', bound='Model')
+
+
+class TableNameMixin:
+    @declared_attr.directive  # noqa
+    @classmethod
+    def __tablename__(cls) -> Optional[str]:
+        return camel_to_snake(cls.__name__)
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=func.now(), onupdate=func.now()
+    )
+
+
+class OperatorMixin:
+    create_by: Mapped[int] = mapped_column(default=0)
+    update_by: Mapped[int] = mapped_column(default=0)
+
+
+class DeletedAtMixin:
+    deleted_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), default=None
+    )
+
+    @declared_attr.directive  # noqa
+    @classmethod
+    def __deleted_at_option__(cls) -> typing.Optional[ExecutableOption]:
+        return with_loader_criteria(cls, cls.deleted_at.is_(None))
+
+
+class Model(TableNameMixin, Base):
+    __abstract__ = True
+    id: Mapped[int] = mapped_column(primary_key=True)
+    objects: typing.ClassVar[BaseRepository[Self]] = Repository()
+
+    @declared_attr.directive  # noqa
+    @classmethod
+    def __deleted_at_option__(cls) -> typing.Optional[ExecutableOption]:
+        return None
