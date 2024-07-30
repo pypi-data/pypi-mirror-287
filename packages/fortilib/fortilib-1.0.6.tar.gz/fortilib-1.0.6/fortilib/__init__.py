85f1bf171e727cb0289fc192642d4b26badc0a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 05:04:02 +0000

    Add package fortilib/1.0.6/fortilib-1.0.6.tar.gz

---
+++ packages/fortilib/fortilib-1.0.6.tar.gz/fortilib-1.0.6/fortilib/__init__.py
@@ -0,0 +1,35 @@
+__version__ = "1.0.6"
+
+from typing import (
+    Dict,
+    List,
+)
+
+
+class FortilibSettings:
+    strict_address_group_member_matching: bool = True
+
+
+def get_by(attrname, attrvalue, haystack):
+    for o in haystack:
+        if attrvalue == getattr(o, attrname):
+            return o
+    return None
+
+
+def get_fortigate_member_array(source: List, attrname="name") -> List[Dict]:
+    ret = []
+    for member in sorted(
+        source, key=lambda _source: getattr(_source, attrname)
+    ):
+        ret.append(
+            {
+                "name": getattr(member, attrname),
+            }
+        )
+
+    return ret
+
+
+def remove_empty_dict_values(source: dict):
+    return {k: v for k, v in source.items() if v}
