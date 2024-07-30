d91f0730e7b4ec72b23b98ca45c9fb9456e4093d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-30 15:04:48 +0000

    Add package ally-ai-llamaindex/0.1.0/ally_ai_llamaindex-0.1.0-py3-none-any.whl

---
+++ packages/ally-ai-llamaindex/ally_ai_llamaindex-0.1.0-py3-none-any.whl/ally_ai_llamaindex/__init__.py
@@ -0,0 +1,10 @@
+from ally_ai_llamaindex.LLM import LLM
+from ally_ai_llamaindex.EmbeddingModel import EmbeddingModel
+
+from ally_ai_core import Settings
+
+__all__ = [
+    'LLM',
+    'EmbeddingModel',
+    'Settings'
+]
