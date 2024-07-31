347816bbcf0fe042d61663ead08431634e69872f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 04:05:14 +0000

    Add package i18ndude/6.2.1/i18ndude-6.2.1.tar.gz

---
+++ packages/i18ndude/i18ndude-6.2.1.tar.gz/i18ndude-6.2.1/setup.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "6.2.1"
+
+setup(
+    name="i18ndude",
+    version=version,
+    description="i18ndude performs various tasks related to ZPT's, Python "
+    "Scripts and i18n.",
+    long_description=(
+        Path("README.rst").read_text()
+        + "\n\n"
+        + (Path("docs") / "command.rst").read_text()
+        + "\n\n"
+        + Path("CHANGES.rst").read_text()
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Utilities",
+    ],
+    keywords="Plone i18n zpt",
+    author="Daniel Nouri",
+    author_email="plone-i18n@lists.sourceforge.net",
+    maintainer="Maurits van Rees",
+    maintainer_email="maurits@vanrees.org",
+    url="https://github.com/collective/i18ndude",
+    license="GPL",
+    package_dir={"": "src"},
+    packages=find_packages(
+        "src",
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "lxml",
+        "zope.i18nmessageid >= 3.3",
+        "zope.interface >= 3.3",
+        "zope.tal >= 4.3.0",
+    ],
+    extras_require={
+        "plone": ["plone.i18n"],
+    },
+    entry_points={
+        "console_scripts": [
+            "i18ndude=i18ndude.script:main",
+        ],
+        # Documentation generation
+        "zest.releaser.prereleaser.before": [
+            "i18ndude_cli = i18ndude.utils:prepare_cli_documentation",
+        ],
+    },
+)
---
+++ packages/i18ndude/i18ndude-6.2.1.tar.gz/i18ndude-6.2.1/src/i18ndude/tests/utils.py
@@ -0,0 +1,35 @@
+from contextlib import contextmanager
+
+import os
+import sys
+
+
+def package_home(globals_dict):
+    __name__ = globals_dict["__name__"]
+    m = sys.modules[__name__]
+    if hasattr(m, "__path__"):
+        r = m.__path__[0]
+    elif "." in __name__:
+        r = sys.modules[__name__[: __name__.rfind(".")]].__path__[0]
+    else:
+        r = __name__
+    return os.path.abspath(r)
+
+
+GLOBALS = globals()
+PACKAGE_HOME = os.path.dirname(package_home(GLOBALS))
+
+TESTDATA_DIR = os.path.join(PACKAGE_HOME, "testdata")
+
+
+@contextmanager
+def suppress_stdout():
+    # Taken with thanks from Dave Smith's site:
+    # https://thesmithfam.org/blog/2012/10/25/
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
