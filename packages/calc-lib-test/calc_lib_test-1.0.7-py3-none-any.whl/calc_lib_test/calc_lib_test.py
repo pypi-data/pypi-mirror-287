f51a51d0acb4192dfae41914aa1e5cc635c1c75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:27 +0000

    Add package calc-lib-test/1.0.7/calc_lib_test-1.0.7-py3-none-any.whl

---
+++ packages/calc-lib-test/calc_lib_test-1.0.7-py3-none-any.whl/calc_lib_test/calc_lib_test.py
@@ -0,0 +1,32 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+    
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Деление на ноль невозможно")
+    return a / b
+
+# class CalcTest:
+#     @staticmethod
+#     def add(a, b):
+#         return a + b
+
+#     @staticmethod
+#     def subtract(a, b):
+#         return a - b
+
+#     @staticmethod
+#     def multiply(a, b):
+#         return a * b
+
+#     @staticmethod
+#     def divide(a, b):
+#         if b == 0:
+#             raise ValueError("Деление на ноль невозможно")
+#         return a / b
