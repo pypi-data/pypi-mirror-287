bf07b14ae93921ab066ad6695e043f51d366effa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:43 +0000

    Add package neuralviz/0.0.2/neuralviz-0.0.2.tar.gz

---
+++ packages/neuralviz/neuralviz-0.0.2.tar.gz/neuralviz-0.0.2/README.md
@@ -0,0 +1,14 @@
+# neuralviz : A unique visualization tool
+A Python module for visualizing neural network training in real-time.
+
+# Installation
+You can install the unique_nn_visualizer package from PyPI using pip:
+pip install neuralviz
+
+# Features
+Real-time Visualization: See how the decision boundary evolves during training.
+Customizable: Easily integrate with your existing training loops.
+Interactive Plots: Updates plots in real-time for a simulation-like experience.
+
+# Contributing
+Feel free to contribute to the project! To contribute, please fork the repository and submit a pull request.
---
+++ packages/neuralviz/neuralviz-0.0.2.tar.gz/neuralviz-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neuralviz',
+    version='0.0.2',
+    description='A package for visualizing neural network training and decision boundaries.',
+    author='Jit Roy',
+    author_email='jitroy0506@gmial.com',
+    packages=find_packages(include=['neuralviz', 'neuralviz.*']),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'torch',
+        'seaborn',
+        'scikit-learn'
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
