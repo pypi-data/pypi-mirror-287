47b1abeb9dac35ae4a4be4eb2a3bad17595092ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 04:04:48 +0000

    Add package Netaxstimrie/0.1.1/Netaxstimrie-0.1.1.tar.gz

---
+++ packages/Netaxstimrie/Netaxstimrie-0.1.1.tar.gz/Netaxstimrie-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension("Netaxstimrie.netnetwork", ["Netaxstimrie/netnetwork.pyx"]),
+]
+
+setup(
+    name="Netaxstimrie",
+    version="0.1.1",
+    description="A package for prepare network files",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="YY",
+    packages=["Netaxstimrie"],
+    ext_modules=cythonize(extensions),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
