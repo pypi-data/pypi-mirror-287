7561612098c1f8b3c1ecccf3cbad51464af4a726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 06:04:47 +0000

    Add package WedgieIntegrator/0.1.2/WedgieIntegrator-0.1.2-py3-none-any.whl

---
+++ packages/WedgieIntegrator/WedgieIntegrator-0.1.2-py3-none-any.whl/WedgieIntegrator/__init__.py
@@ -0,0 +1,5 @@
+from . import auth
+from .client import BaseAPIClient, APIClient
+from .config import APIConfig
+from . import custom_exceptions as exceptions
+from .response import APIResponse, BaseAPIResponse
