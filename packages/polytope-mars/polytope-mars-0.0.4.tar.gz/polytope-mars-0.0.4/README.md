87c2ba71e496fdea3aa8d76087347a6540484e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:56 +0000

    Add package polytope-mars/0.0.4/polytope-mars-0.0.4.tar.gz

---
+++ packages/polytope-mars/polytope-mars-0.0.4.tar.gz/polytope-mars-0.0.4/README.md
@@ -0,0 +1,80 @@
+| :warning: This project is BETA and will be experimental for the foreseeable future. Interfaces and functionality are likely to change. DO NOT use this software in any project/software that is operational. |
+| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ |
+
+<h3 align="center">
+<img src="./docs/images/ECMWF_logo.svg.png" width=60%>
+</br>
+
+# Polytope-mars
+
+<p align="center">
+  <a href="https://github.com/ecmwf/polytope-mars/actions/workflows/ci.yaml">
+  <img src="https://github.com/ecmwf/polytope-mars/actions/workflows/ci.yaml/badge.svg" alt="ci">
+</a>
+  <a href="https://codecov.io/gh/ecmwf/polytope-mars"><img src="https://codecov.io/gh/ecmwf/polytope-mars/branch/develop/graph/badge.svg"></a>
+  <a href="https://opensource.org/licenses/Apache-2.0"><img src="https://img.shields.io/badge/License-Apache%202.0-blue.svg"></a>
+  <a href="https://github.com/ecmwf/polytope-mars/releases"><img src="https://img.shields.io/github/v/release/ecmwf/polytope-mars?color=blue&label=Release&style=flat-square"></a>
+</p>
+<p align="center">
+  <a href="#concept">Concept</a> •
+  <a href="#installation">Installation</a> •
+  <a href="#example">Example</a> •
+  <a href="#testing">Testing</a>
+</p>
+
+## Concept
+
+Repository for high level API of polytope feature extraction such as vertical profile and time series.
+
+Current features include:
+
+* Time Series
+* Vertical Profile
+* Bounding Box
+* Path
+* WKT POLYGON shape
+* Shape File
+* Frame
+
+## Installation
+
+```bash
+pip install polytope-mars
+```
+
+Or clone the repo and install locally
+
+```bash
+git clone git@github.com:ecmwf/polytope-mars.git
+cd polytope-mars
+
+pip install -e .
+```
+
+
+## Example
+
+**Create time series request**: Create a request for a time series request using the time series feature, set options and config for use by polytope feature extraction. NB: Assumes data is in a local FDB.
+
+```python
+
+request = {
+    "class": "od",
+    "stream" : "enfo",
+    "type" : "pf",
+    "date" : "20231205",
+    "time" : "00:00:00",
+    "levtype" : "sfc",
+    "expver" : "0001", 
+    "domain" : "g",
+    "param" : "228/49/164/165/166/167",
+    "number" : "1/to/5",
+    "feature" : {
+        "type" : "timeseries",
+        "points": [[51.5073219, 2.1]],
+        "start": 0,
+        "end" : 9
+    },
+}
+
+```
---
+++ packages/polytope-mars/polytope-mars-0.0.4.tar.gz/polytope-mars-0.0.4/polytope_mars/api.py
@@ -0,0 +1,149 @@
+import json
+from typing import List
+
+import pygribjump as gj
+from covjsonkit.api import Covjsonkit
+from polytope import shapes
+from polytope.engine.hullslicer import HullSlicer
+from polytope.polytope import Polytope, Request
+
+from .features.boundingbox import BoundingBox
+from .features.frame import Frame
+from .features.path import Path
+from .features.shpfile import Shapefile
+from .features.timeseries import TimeSeries
+from .features.verticalprofile import VerticalProfile
+from .features.wkt import Wkt
+
+features = {
+    "timeseries": TimeSeries,
+    "verticalprofile": VerticalProfile,
+    "boundingbox": BoundingBox,
+    "frame": Frame,
+    "path": Path,
+    "shapefile": Shapefile,
+    "wkt": Wkt,
+}
+
+
+class PolytopeMars:
+    def __init__(self, datacube_type, datacube_options):
+        # Initialise polytope
+        # fdbdatacube = FDBDatacube(
+        #    datacube_config, axis_options=datacube_options
+        # )  # noqa: E501
+        # slicer = HullSlicer()
+        # self.api = Polytope(datacube=fdbdatacube, engine=slicer)
+
+        self.datacube_type = datacube_type
+        self.datacube_options = datacube_options
+
+        self.coverage = {}
+
+    def extract(self, request):
+        # request expected in JSON or dict
+        if not isinstance(request, dict):
+            try:
+                request = json.loads(request)
+            except ValueError:
+                raise ValueError(
+                    "Request not in JSON format or python dictionary"
+                )  # noqa: E501
+
+        # expect a "feature" key in the request
+        try:
+            feature_config = request.pop("feature")
+        except KeyError:
+            raise KeyError("Request does not contain a 'feature' keyword")
+
+        # get feature type
+        try:
+            feature_type = feature_config["type"]
+        except KeyError:
+            raise KeyError("The 'feature' does not contain a 'type' keyword")
+
+        feature = self._feature_factory(feature_type, feature_config)
+
+        feature.validate(request)
+
+        shapes = self._create_base_shapes(request)
+
+        shapes.extend(feature.get_shapes())
+
+        preq = Request(*shapes)
+
+        # TODO: make polytope request to get data
+
+        if self.datacube_type == "grib":
+            fdbdatacube = gj.GribJump()
+        else:
+            raise NotImplementedError(
+                f"Datacube type '{self.datacube_type}' not found"
+            )  # noqa: E501
+        slicer = HullSlicer()
+        self.api = Polytope(
+            # request=preq,
+            datacube=fdbdatacube,
+            engine=slicer,
+            options=self.datacube_options,
+        )
+        # result = API.retrieve(request)
+
+        result = self.api.retrieve(preq)
+        # result.pprint()
+
+        encoder = Covjsonkit().encode("CoverageCollection", feature_type)
+
+        self.coverage = encoder.from_polytope(result)
+
+        return self.coverage
+
+    def _create_base_shapes(self, request: dict) -> List[shapes.Shape]:
+        base_shapes = []
+
+        # TODO: not handling type conversion
+        #   * strings to integers for step, number, etc.
+        #   * date/times to datetime (and merging of date + time)
+        #   * enforcing strings are actually strings (e.g. type=fc)
+
+        time = request.pop("time").replace(":", "")
+        request["date"] = request["date"] + "T" + time
+
+        # TODO: not restricting certain keywords:
+        #   * AREA, GRID
+        # do we need to filter this... it will fail later anyway
+
+        for k, v in request.items():
+            split = str(v).split("/")
+
+            # ALL -> All
+            if len(split) == 1 and split[0] == "ALL":
+                base_shapes.append(shapes.All(k))
+
+            # Single value -> Select
+            elif len(split) == 1:
+                base_shapes.append(shapes.Select(k, [split[0]]))
+
+            # Range a/to/b, "by" not supported -> Span
+            elif len(split) == 3 and split[1] == "to":
+                base_shapes.append(
+                    shapes.Span(k, lower=split[0], upper=split[2])
+                )  # noqa: E501
+
+            elif "by" in split:
+                raise ValueError(
+                    "Ranges with step-size specified with 'by' keyword is not supported"  # noqa: E501
+                )
+
+            # List of individual values -> Union of Selects
+            else:
+                base_shapes.append(shapes.Select(k, split))
+
+        return base_shapes
+
+    def _feature_factory(self, feature_name, feature_config):
+        feature_class = features.get(feature_name)
+        if feature_class:
+            return feature_class(feature_config)
+        else:
+            raise NotImplementedError(f"Feature '{feature_name}' not found")
