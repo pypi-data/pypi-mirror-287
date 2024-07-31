7fa3f0265265a1e86f197d646b21bad02e68304f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 04:04:51 +0000

    Add package databricks-labs-remorph/0.4.0/databricks_labs_remorph-0.4.0.tar.gz

---
+++ packages/databricks-labs-remorph/databricks_labs_remorph-0.4.0.tar.gz/databricks_labs_remorph-0.4.0/core/src/test/scala/com/databricks/labs/remorph/parsers/tsql/TSqlParserTestCommon.scala
@@ -0,0 +1,19 @@
+package com.databricks.labs.remorph.parsers.tsql
+
+import com.databricks.labs.remorph.parsers.{ErrorCollector, ParserTestCommon, ProductionErrorCollector}
+import org.antlr.v4.runtime.{CharStream, TokenSource, TokenStream}
+import org.scalatest.Assertions
+
+trait TSqlParserTestCommon extends ParserTestCommon[TSqlParser] { self: Assertions =>
+
+  override final protected def makeLexer(chars: CharStream): TokenSource = new TSqlLexer(chars)
+
+  override protected def makeErrHandler(chars: String): ErrorCollector =
+    new ProductionErrorCollector(chars, "-- test string --")
+
+  override final protected def makeParser(tokenStream: TokenStream): TSqlParser = {
+    val parser = new TSqlParser(tokenStream)
+    parser.setErrorHandler(new TSqlErrorStrategy)
+    parser
+  }
+}
