1ab5de46f37fe3069da37c47763e7ed80b883b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 05:05:01 +0000

    Add package dean-utils/0.0.22/dean_utils-0.0.22-py3-none-any.whl

---
+++ packages/dean-utils/dean_utils-0.0.22-py3-none-any.whl/dean_utils/__init__.py
@@ -0,0 +1,23 @@
+__all__ = [
+    "async_abfs",
+    "cos_query_all",
+    "send_message",
+    "update_queue",
+    "delete_message",
+    "send_email",
+    "az_send",
+    "def_cos",
+    "pl_scan_hive",
+    "pl_scan_pq",
+    "pl_write_pq",
+]
+from dean_utils.utils.az_utils import (
+    async_abfs,
+    cos_query_all,
+    send_message,
+    update_queue,
+    delete_message,
+    def_cos,
+)
+from dean_utils.utils.email_utility import send_email, az_send
+from dean_utils.polars_extras import pl_scan_hive, pl_scan_pq, pl_write_pq
