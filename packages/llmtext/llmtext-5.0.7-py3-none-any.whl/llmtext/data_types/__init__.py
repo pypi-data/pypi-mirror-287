786119fb5d0be09354eaf1c7f3c3b719e66d70b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:15 +0000

    Add package llmtext/5.0.7/llmtext-5.0.7-py3-none-any.whl

---
+++ packages/llmtext/llmtext-5.0.7-py3-none-any.whl/llmtext/data_types/__init__.py
@@ -0,0 +1,61 @@
+from abc import abstractmethod
+from typing import Annotated, Any
+from typing import Literal, TypedDict
+from pydantic import BaseModel, Field
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class Event(TypedDict):
+    step: int
+    type: Literal["tool_call", "tool_output", "message_stream", "message", "feedback"]
+    id: str
+    content: str
+
+
+class RunnableTool(BaseModel):
+    @abstractmethod
+    async def arun(self) -> str:
+        pass
+
+    def get_tool_call(self) -> dict[str, Any]:
+        tool_name = self.__class__.__name__
+        return {
+            "type": "tool_call",
+            "name": tool_name,
+            "description": self.__doc__ or "",
+            "params": self.model_dump(),
+        }
+
+    async def aget_tool_output(self) -> dict[str, Any]:
+        tool_name = self.__class__.__name__
+        return {
+            "type": "tool_output",
+            "name": tool_name,
+            "description": self.__doc__ or "",
+            "params": self.model_dump(),
+            "output": await self.arun(),
+        }
+
+
+class ToolSelector(BaseModel):
+    """Selected tools"""
+
+    choices: Annotated[
+        list[RunnableTool], Field(description="Selected tool to call")
+    ] = []
+
+
+class IsFinalResponse(BaseModel):
+    """Is the response final or not"""
+
+    is_final_response: Annotated[
+        bool,
+        Field(
+            description="True if the response is final, False if the response is not final",
+            default=True,
+        ),
+    ]
