58ffe5f8b3c44b55f3c926a964bc931f7acb4420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:45 +0000

    Add package calc-lib-test/1.0.4/calc_lib_test-1.0.4.tar.gz

---
+++ packages/calc-lib-test/calc_lib_test-1.0.4.tar.gz/calc_lib_test-1.0.4/calc_lib_test/calc_lib_test.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+# class CalcTest:
+#     @staticmethod
+#     def add(a, b):
+#         return a + b
+
+#     @staticmethod
+#     def subtract(a, b):
+#         return a - b
+
+#     @staticmethod
+#     def multiply(a, b):
+#         return a * b
+
+#     @staticmethod
+#     def divide(a, b):
+#         if b == 0:
+#             raise ValueError("Деление на ноль невозможно")
+#         return a / b
