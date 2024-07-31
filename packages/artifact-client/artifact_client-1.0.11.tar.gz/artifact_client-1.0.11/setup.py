7edc68dd86f6c5b22bbc06b0484150d9ca8fcffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:55 +0000

    Add package artifact-client/1.0.11/artifact_client-1.0.11.tar.gz

---
+++ packages/artifact-client/artifact_client-1.0.11.tar.gz/artifact_client-1.0.11/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='artifact_client',
+    version='1.0.11',
+    description='Python client for the Artifact API',
+    author='Artifact Dev Team',
+    author_email='contact@useartifact.ai',
+    packages=find_packages(include=['artifact', 'artifact.*']),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    package_data={
+        '': ['*.txt', '*.rst', '*.md'],
+    },
+)
