3db9c35792ccc2ed438af7f96adc25f517259fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:10 +0000

    Add package electrogram/2.184.1/electrogram-2.184.1-py3-none-any.whl

---
+++ packages/electrogram/electrogram-2.184.1-py3-none-any.whl/pyrogram/raw/types/bots/__init__.py
@@ -0,0 +1,27 @@
+from .bot_info import BotInfo
+
+
+__all__ = [
+    "BotInfo",
+    "help",
+    "storage",
+    "auth",
+    "contacts",
+    "messages",
+    "updates",
+    "photos",
+    "upload",
+    "account",
+    "channels",
+    "payments",
+    "phone",
+    "stats",
+    "stickers",
+    "users",
+    "chatlists",
+    "bots",
+    "stories",
+    "premium",
+    "smsjobs",
+    "fragment",
+]
