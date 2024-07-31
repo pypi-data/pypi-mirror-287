2cd5d2549a730a47ac870b2d40c92e1f0b4b15a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 16:04:52 +0000

    Add package pywss/0.1.28/pywss-0.1.28.tar.gz

---
+++ packages/pywss/pywss-0.1.28.tar.gz/pywss-0.1.28/pywss/handler/cors.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import pywss
+
+from pywss.constant import *
+
+
+def NewCORSHandler(
+        allow_origins: tuple = ("*",),
+        allow_methods: tuple = ("*",),
+        allow_headers: tuple = ("*",),
+        allow_credentials: bool = True,
+):
+    allowOrigins = ",".join(allow_origins)
+    allowMethods = ",".join(allow_methods)
+    allowHeaders = ",".join(allow_headers)
+    allowCredentials = "true" if allow_credentials else "false"
+
+    def corsHandler(ctx: pywss.Context):
+        ctx.set_header(HeaderAccessControlAllowOrigin, allowOrigins)
+        ctx.set_header(HeaderAccessControlAllowMethods, allowMethods)
+        ctx.set_header(HeaderAccessControlAllowHeaders, allowHeaders)
+        ctx.set_header(HeaderAccessControlAllowCredentials, allowCredentials)
+        if ctx.method == MethodOptions:
+            return
+        ctx.next()
+
+    return corsHandler
