3850737cea935a9b13a3b5bef24a13cf6b53e6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:21 +0000

    Add package HorridAPI/1.0.22/horridapi-1.0.22.tar.gz

---
+++ packages/HorridAPI/horridapi-1.0.22.tar.gz/horridapi-1.0.22/HorridAPI/core.py
@@ -0,0 +1,9 @@
+from telegraph import upload_file
+
+class Core:
+    """HorridAPI Core""" 
+        
+    def upload(url: str):                
+        out = upload_file(url)
+        return out
+          
