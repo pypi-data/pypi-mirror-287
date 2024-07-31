3cd8d5f67deed5015a8e84666441f747c41f90cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:10 +0000

    Add package pin-pink/3.0.0/pin_pink-3.0.0-py3-none-any.whl

---
+++ packages/pin-pink/pin_pink-3.0.0-py3-none-any.whl/pink/barriers/exceptions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 Ivan Domrachev, Simeon Nedelchev
+
+from ..exceptions import PinkError
+
+
+class NoPositionLimitProvided(PinkError):
+    """If neither minimum nor maximum position limits are provided."""
+
+
+class NegativeMinimumDistance(PinkError):
+    """If the minimum distance in body spherical barrier is negative."""
+
+
+class InvalidCollisionPairs(PinkError):
+    """IF the number of collision pairs is invalid."""
