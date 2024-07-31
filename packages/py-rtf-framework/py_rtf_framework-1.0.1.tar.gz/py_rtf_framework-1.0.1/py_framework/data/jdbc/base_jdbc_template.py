3b9795d373e1d18005056adbee33bffa6fdca6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:29 +0000

    Add package py-rtf-framework/1.0.1/py_rtf_framework-1.0.1.tar.gz

---
+++ packages/py-rtf-framework/py_rtf_framework-1.0.1.tar.gz/py_rtf_framework-1.0.1/py_framework/data/jdbc/base_jdbc_template.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Any
+from pydantic import BaseModel, Field
+import pandas as pd
+
+
+class DbConfig(BaseModel):
+    host: str = Field(title="数据库IP地址", default=None)
+    port: int = Field(title="数据库端口", default=None)
+    user: str = Field(title="数据库连接账号", default=None)
+    password: str = Field(title="数据库连接密码", default=None)
+    database: str = Field(title="数据库", default=None)
+
+
+class BaseJdbcTemplate(ABC):
+    """基础jdbc模板类"""
+
+    db_config: DbConfig
+
+    def __init__(self, db_config: DbConfig):
+        self.db_config = db_config
+
+    @abstractmethod
+    def query(self, sql: str, param: dict[str, Any] = None) -> list[dict[str, Any]]:
+        """执行SQL查询"""
+
+    @abstractmethod
+    def query_for_df(self, sql: str, param: dict[str, Any] = None) -> pd.DataFrame:
+        """查询返回pandas的DataFrame"""
+
+    @abstractmethod
+    def insert_df(self, table_name: str, record_df:pd.DataFrame):
+        """执行数据插入"""
+
+    @abstractmethod
+    def execute(self, sql: str, param: dict[str, Any] = None) -> Any:
+        """执行SQL"""
