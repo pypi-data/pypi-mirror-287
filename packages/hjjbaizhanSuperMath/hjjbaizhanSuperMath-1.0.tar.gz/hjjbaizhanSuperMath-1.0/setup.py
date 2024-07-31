2d9f1b1a4fba0f94cacdf4ebe449bcf2984ff0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 16:05:06 +0000

    Add package hjjbaizhanSuperMath/1.0/hjjbaizhanSuperMath-1.0.tar.gz

---
+++ packages/hjjbaizhanSuperMath/hjjbaizhanSuperMath-1.0.tar.gz/hjjbaizhanSuperMath-1.0/setup.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+from distutils.core import setup
+setup(
+name='hjjbaizhanSuperMath', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='SanSanZi', # 作者
+author_email='1363331756@qq.com', py_modules=['hjjbaizhanSuperMath.demo1','hjjbaizhanSuperMath.demo2'] # 要发布的模块
+)
