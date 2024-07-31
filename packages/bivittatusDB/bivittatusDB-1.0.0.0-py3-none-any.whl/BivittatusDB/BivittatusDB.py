9a73c35f1a0cfc51df1b06d161e746c47911b45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 05:04:31 +0000

    Add package bivittatusDB/1.0.0.0/bivittatusDB-1.0.0.0-py3-none-any.whl

---
+++ packages/bivittatusDB/bivittatusDB-1.0.0.0-py3-none-any.whl/BivittatusDB/BivittatusDB.py
@@ -0,0 +1,40 @@
+import metaclass
+try: 
+    from bdb_aggregate import *
+    from BDB_tb import *
+    from BDB_io import Handler
+except:
+    raise metaclass.BDBException.ImportError(f"Could not import needed files in {__file__}")
+
+class database:
+    def __init__(self, database_name:str, Encrpyt:bool=False):
+        self.database_name=database_name
+        self.db=Handler(database_name, Encrpyt)
+
+    def init(self):
+        self.db.init().use()
+        return self
+    
+    def use(self):
+        self.db.use()
+        return self
+
+    def load_table(self, table_name:str):
+        '''load preexisting tables from the database.'''
+        try:
+            return table(self.db, self.database_name, table_name)
+        except Exception as e:
+            raise metaclass.BDBException.IOError(f"Error finding table {table_name}")
+    
+    def New_table(self, name:str, columns:tuple, data_types:tuple, primary:str=None, foreign:str=None):
+        '''Make a new table and specify the name, columns and data types. Optionally assign primary key. Returns the table'''
+        if primary not in columns:
+            while primary != None:
+                raise NameError(f"Can't make unknown column {primary} into a primary key")
+        metadata=[("Data", "Type")]
+        for column, value in zip(columns, data_types):
+            metadata.append((column, value))
+        metadata.append(("Primary Key", f"{primary}"))
+        metadata.append(("Foreign Key", f"{foreign}"))
+        self.db.CreateTable(name, [columns], metadata)
+        return self.load_table(name)
