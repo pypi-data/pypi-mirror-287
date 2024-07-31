67951b5b6e53b58d2fdd1c78e5c025e359dc0145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:58 +0000

    Add package vecx-llamaindex/0.1.0/vecx_llamaindex-0.1.0.tar.gz

---
+++ packages/vecx-llamaindex/vecx_llamaindex-0.1.0.tar.gz/vecx_llamaindex-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# myproject/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="vecx-llamaindex",
+    version="0.1.0",
+    install_requires=[
+        # List your dependencies here
+        "llama-index",
+        "vecx",
+    ],
+    author="LaunchX Labs",
+    author_email="vineet@launchxlabs.ai",
+    description="Encrypted Vector Database for Secure and Fast ANN Searches",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://vectorxdb.ai",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
