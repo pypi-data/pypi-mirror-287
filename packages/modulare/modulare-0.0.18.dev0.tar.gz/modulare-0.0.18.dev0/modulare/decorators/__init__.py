c08217cd871bcb290b58ccffcdd689ac5f356636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:05:25 +0000

    Add package modulare/0.0.18.dev0/modulare-0.0.18.dev0.tar.gz

---
+++ packages/modulare/modulare-0.0.18.dev0.tar.gz/modulare-0.0.18.dev0/modulare/decorators/__init__.py
@@ -0,0 +1,4 @@
+from .http.route_decorator import route, register_routes
+from .http.status_code_decorator import response_status_code
+from .module.module_decorator import Module
+from .http.enums.status_code_decorator_enum import StatusCode
---
+++ packages/modulare/modulare-0.0.18.dev0.tar.gz/modulare-0.0.18.dev0/sample/01-basic-crud/src/modules/partner/dto/update_partner_dto.py
@@ -0,0 +1,6 @@
+from modules.partner.dto.create_partner_dto import CreatePartnerDTO
+from shared.class_helper.partial_class import PartialClass
+
+
+class UpdatePartnerDTO(PartialClass(CreatePartnerDTO)):
+    pass
