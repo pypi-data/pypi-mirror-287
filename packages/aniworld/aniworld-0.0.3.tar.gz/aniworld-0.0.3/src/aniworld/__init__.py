186612ae1c37ed250ab3c73bfd2765d2ea77526d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:21 +0000

    Add package aniworld/0.0.3/aniworld-0.0.3.tar.gz

---
+++ packages/aniworld/aniworld-0.0.3.tar.gz/aniworld-0.0.3/src/aniworld/__init__.py
@@ -0,0 +1,4 @@
+from .extractors.doodstream import doodstream_get_direct_link
+from .extractors.streamtape import streamtape_get_direct_link
+from .extractors.vidoza import vidoza_get_direct_link
+from .extractors.voe import voe_get_direct_link
