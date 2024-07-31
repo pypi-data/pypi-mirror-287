bcf4e4c4c2857f864ead517f226e4b38faf78961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 07:04:22 +0000

    Add package xyzcad/0.4.4.post1/xyzcad-0.4.4.post1.tar.gz

---
+++ packages/xyzcad/xyzcad-0.4.4.post1.tar.gz/xyzcad-0.4.4.post1/xyzcad/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#######################################################################
+#
+#    xyzCad - functional cad software for 3d printing
+#    Copyright (c) 2021 - 2024 Stefan Helmert <stefan.helmert@t-online.de>
+#
+#######################################################################
+
+
+import os
+
+# VERSION
+__version__ = "0.4.4-1"
+
+
+if __name__ == '__main__':
+    import git
+    repo = git.Repo(search_parent_directories=False)
+    version = '-'.join(repo.git.describe('--tags').split('-')[:2])
+    if __version__ != version:
+        print("Version in git repo changed!")
+        print("  -> Change version in {} from {} to {}".format(__file__,
+                __version__, version))
+        __version__ = version
+        with open(os.path.realpath(__file__), 'rt') as v:
+            initfile = v.read()
+            lines = initfile.splitlines()
+            bg = [i for i, e in enumerate(lines) if 'VERSION' in e][0]
+            lines[bg+1] = "__version__ = \"{}\"".format(__version__)
+            initfile = '\n'.join(lines)
+        with open(os.path.realpath(__file__), 'wt') as v:
+            v.write(initfile)
