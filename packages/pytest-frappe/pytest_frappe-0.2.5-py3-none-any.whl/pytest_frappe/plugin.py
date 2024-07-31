8712c2565a4ca4c69fbfee3e804ef8ef5b3e8f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:39 +0000

    Add package pytest-frappe/0.2.5/pytest_frappe-0.2.5-py3-none-any.whl

---
+++ packages/pytest-frappe/pytest_frappe-0.2.5-py3-none-any.whl/pytest_frappe/plugin.py
@@ -0,0 +1,18 @@
+from pytest_frappe.fixtures import db_instance, db_transaction
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--sites-path",
+        action="store",
+        dest="sites_path",
+        default="config",
+        help="Frappe config file",
+    )
+    parser.addoption(
+        "--test-site",
+        action="store",
+        dest="test_site",
+        default=None,
+        help="Specify the test site for running tests",
+    )
