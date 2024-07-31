4868e2a4d722bfe38058d96a3c952e550033cc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:55 +0000

    Add package dalianpai-my-library/1.0.0/dalianpai_my_library-1.0.0.tar.gz

---
+++ packages/dalianpai-my-library/dalianpai_my_library-1.0.0.tar.gz/dalianpai_my_library-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dalianpai_my_library',
+    version='1.0.0',
+    packages=find_packages(),
+    description='A simple example library',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/my_library',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
