6e08bdc2a0924346d75810cbdbb1db347a366257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:26 +0000

    Add package ropt/0.5.1/ropt-0.5.1.tar.gz

---
+++ packages/ropt/ropt-0.5.1.tar.gz/ropt-0.5.1/docs/usage/running.md
@@ -0,0 +1,67 @@
+# Running a basic optimization script
+
+To demonstrate basic optimization with `ropt`, consider the Rosenbrock function,
+a standard test problem:
+
+$$ f(x,y) = (a - x)^2 + b (y = x^2)^2, $$
+
+which has a global minimum of $f(x, y) = 0$ at $(x, y) = (a, a^2)$ .
+
+Here's an example optimizing the Rosenbrock function for $a = 1$ and $b = 100$:
+
+```python
+import numpy as np
+from numpy.typing import NDArray
+
+from ropt.evaluator import EvaluatorContext, EvaluatorResult
+from ropt.plan import OptimizationPlanRunner
+
+
+def rosenbrock(
+    variables: NDArray[np.float64],                                   # (1)!
+    context: EvaluatorContext                                         # (2)!
+) -> EvaluatorResult:
+    objectives = np.zeros((variables.shape[0], 1), dtype=np.float64)
+    for idx in range(variables.shape[0]):
+        x, y = variables[idx, :]
+        objectives[idx, 0] = (1.0 - x) ** 2 + 100 * (y - x * x) ** 2
+    return EvaluatorResult(objectives=objectives)                     # (3)!
+
+CONFIG = {                                                            # (4)!
+    "variables": {"initial_values": [0.5, 2.0]},
+    "gradient": {"perturbation_magnitudes": 1e-6}                     # (5)!
+}
+
+plan = OptimizationPlanRunner(CONFIG, rosenbrock)                      # (6)!
+plan.run()                                                            # (7)!
+optimum = plan.results                                                # (8)
+
+print(
+    optimum.evaluations.variables,
+    optimum.functions.weighted_objective,
+)
+```
+
+1. The variables to optimize ($x, y$) are passes as a single `numpy` array. It
+   may receive multiple variable vectors to evaluate, hence the input is a
+   matrix where the variable vectors are the rows of the matrix.
+2. Additional information is passes via an
+   [`EvaluatorContext`][ropt.evaluator.EvaluatorContext] object. It is not
+   needed in this case.
+3. Results are returned via an
+   [`EvaluatorResult`][ropt.evaluator.EvaluatorResult] object. The objectives
+   result is a matrix since multiple input vectors and multiple objectives may
+   be evaluated.
+4. Create an optimizer configuration with default values except for initial
+   values and perturbation magnitudes.
+5. Set perturbation magnitudes to a small value for accurate gradient estimation.
+6. Make a basic plan that runs a single optimization.
+7. Run the optimization.
+8. Get the results.
+
+Running this will print the estimated optimal variables and the corresponding
+function value:
+
+```python
+[0.99959301 0.99917614] 1.7574715576837572e-07
+```
---
+++ packages/ropt/ropt-0.5.1.tar.gz/ropt-0.5.1/examples/rosenbrock_deterministic.py
@@ -0,0 +1,97 @@
+"""Example of optimization of the Rosenbrock test function.
+
+This example demonstrates optimization of the unmodified (deterministic)
+Rosenbrock function. It shows how to write a minimal configuration and how to
+run and monitor the optimization.
+"""
+
+from typing import Any, Dict
+
+import numpy as np
+from numpy.typing import NDArray
+
+from ropt.enums import EventType
+from ropt.evaluator import EvaluatorContext, EvaluatorResult
+from ropt.events import Event
+from ropt.plan import OptimizationPlanRunner
+from ropt.results import FunctionResults
+
+CONFIG: Dict[str, Any] = {
+    "variables": {
+        "initial_values": [0.5, 2.0],
+    },
+    "gradient": {
+        "perturbation_magnitudes": 1e-6,
+    },
+}
+
+
+def rosenbrock(variables: NDArray[np.float64], _: EvaluatorContext) -> EvaluatorResult:
+    """Function evaluator for the rosenbrock function.
+
+    This function returns a tuple containing the calculated objectives and
+    `None`, the latter because no constraints are calculated.
+
+    Args:
+        variables: The variables to evaluate
+
+    Returns:
+        The calculated objective, and `None`
+    """
+    objectives = np.zeros((variables.shape[0], 1), dtype=np.float64)
+    for idx in range(variables.shape[0]):
+        x, y = variables[idx, :]
+        objectives[idx, 0] = (1.0 - x) ** 2 + 100 * (y - x * x) ** 2
+    return EvaluatorResult(objectives=objectives)
+
+
+def report(event: Event) -> None:
+    """Report results of an evaluation.
+
+    Args:
+        event: event data
+    """
+    assert event.results is not None
+    for item in event.results:
+        if isinstance(item, FunctionResults) and item.functions is not None:
+            print(f"result: {item.result_id}")
+            print(f"  variables: {item.evaluations.variables}")
+            print(f"  objective: {item.functions.weighted_objective}\n")
+
+
+def run_optimization(config: Dict[str, Any]) -> FunctionResults:
+    """Run the optimization.
+
+    Args:
+        config: The configuration of the optimizer
+
+    Returns:
+        The optimal results.
+    """
+    optimal_result = (
+        OptimizationPlanRunner(config, rosenbrock)
+        .add_observer(EventType.FINISHED_EVALUATION, report)
+        .run()
+        .results
+    )
+    assert optimal_result is not None
+    assert optimal_result.functions is not None
+
+    print(f"BEST RESULT: {optimal_result.result_id}")
+    print(f"  variables: {optimal_result.evaluations.variables}")
+    print(f"  objective: {optimal_result.functions.weighted_objective}\n")
+
+    return optimal_result
+
+
+def main() -> None:
+    """Run the example and check the result."""
+    optimal_result = run_optimization(CONFIG)
+    assert optimal_result is not None
+    assert optimal_result.functions is not None
+    assert np.allclose(optimal_result.functions.weighted_objective, 0, atol=1e-5)
+    assert np.allclose(optimal_result.evaluations.variables, 1, atol=1e-3)
+
+
+if __name__ == "__main__":
+    main()
