301f91e8aebd0bf3e327e3b0b4989e497682202f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 07:04:11 +0000

    Add package verysimplemodule987654321/0.0.1/verysimplemodule987654321-0.0.1.tar.gz

---
+++ packages/verysimplemodule987654321/verysimplemodule987654321-0.0.1.tar.gz/verysimplemodule987654321-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A very simple module'
+LONG_DESCRIPTION = 'A very simple module that does nothing'
+
+setup(
+    name="verysimplemodule987654321",
+    version=VERSION,
+    author="Rob McInerney",
+    author_email="rob@robmcinerney.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ]
+)
