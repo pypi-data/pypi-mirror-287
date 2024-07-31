e384dbee6988a116cd0fbc5e5777593c045318c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 04:04:55 +0000

    Add package Linshenghua-utils/0.1.3/linshenghua_utils-0.1.3.tar.gz

---
+++ packages/Linshenghua-utils/linshenghua_utils-0.1.3.tar.gz/linshenghua_utils-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Linshenghua_utils',
+    version='0.1.3',
+    author='linshenghua',
+    author_email='2592530336@qq.com',
+    description='A simple plugin example',
+    py_modules=['db_utils', 'file_utils','log_utils','requests_utils', 'sql_utils'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
