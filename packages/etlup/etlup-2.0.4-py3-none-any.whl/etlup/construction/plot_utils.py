098badb6c865567412a3c5459a5a247c886909ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 16:04:52 +0000

    Add package etlup/2.0.4/etlup-2.0.4-py3-none-any.whl

---
+++ packages/etlup/etlup-2.0.4-py3-none-any.whl/etlup/construction/plot_utils.py
@@ -0,0 +1,11 @@
+
+from io import BytesIO
+import base64
+
+def convert_fig_to_html_img(fig) -> str:
+    buf = BytesIO()
+    fig.savefig(buf, format="png", bbox_inches='tight')
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+    
