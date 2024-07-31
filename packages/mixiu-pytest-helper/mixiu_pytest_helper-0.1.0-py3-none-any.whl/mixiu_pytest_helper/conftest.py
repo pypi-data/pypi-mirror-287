0971bccecaefb65436e097b95c6a8cb403a39fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 16:05:01 +0000

    Add package mixiu-pytest-helper/0.1.0/mixiu_pytest_helper-0.1.0-py3-none-any.whl

---
+++ packages/mixiu-pytest-helper/mixiu_pytest_helper-0.1.0-py3-none-any.whl/mixiu_pytest_helper/conftest.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  mixiu-pytest-helper
+# FileName:     conftest.py
+# Description:  TODO
+# Author:       mfkifhss2023
+# CreateDate:   2024/07/31
+# Copyright ©2011-2024. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
+import sys
+import pytest
+from airtest_helper.core import DeviceProxy
+from mixiu_pytest_helper.annotation import logger
+from airtest_helper.dir import join_path, create_directory
+from mixiu_pytest_helper.repository import MiddlewareRepository
+from mixiu_pytest_helper.dir import get_project_path, copy_file, get_package_path
+from mixiu_pytest_helper.infrastructure import RedisCacheClientManager, RedisLockClientManager, RedisClientManager
+
+
+def get_phone_device_lock_key(device_ip: str, port: int = None) -> str:
+    string = "phone:{}".format(device_ip)
+    if port and port > 0:
+        string += ":{}".format(port)
+    return string
+
+
+def get_redis_lock_api() -> RedisClientManager:
+    redis_client = RedisLockClientManager()
+    redis_api = RedisClientManager(redis=redis_client)
+    return redis_api
+
+
+def get_idle_device(redis_api: RedisClientManager) -> DeviceProxy or None:
+    devices = MiddlewareRepository.get_devices()
+    for device_info in devices:
+        port = device_info.get("port")
+        device_ip = device_info.get("device")
+        lock_key = get_phone_device_lock_key(device_ip=device_ip, port=port)
+        lock_status = redis_api.get_redis_data(key=lock_key)
+        if not lock_status or lock_status != "running":
+            try:
+                # 重置 sys.argv 为新的参数列表，避免与airtest包冲突，同时保留当前运行的文件名，作为sys.argv的第一个参数
+                sys.argv = sys.argv[:1]
+                device = DeviceProxy(**device_info)
+                redis_api.set_redis_data(key=lock_key, value="running", ex=7200)
+                return device
+            except Exception as e:
+                logger.error(e)
+    return None
+
+
+@pytest.fixture(scope="session")
+def device_context() -> tuple:
+    redis_api = get_redis_lock_api()
+    device = get_idle_device(redis_api=redis_api)
+    yield device
+    if device:
+        lock_key = get_phone_device_lock_key(device_ip=device.device_id)
+        redis_api.set_redis_data(key=lock_key, value="idle", ex=86400)
+    redis_api.redis.close()
+
+
+@pytest.fixture(scope="session")
+def redis_context() -> RedisClientManager:
+    redis_client = RedisCacheClientManager()
+    redis_api = RedisClientManager(redis=redis_client)
+    yield redis_api
+    redis_api.redis.close()
+
+
+def run_tests(script_path: str = None):
+    project_path = get_project_path()
+    package_path = get_package_path()
+    config_path = str(join_path([project_path, "configuration"]))
+    config_template = str(join_path([package_path, "pytest.ini"]))
+    logging_template = str(join_path([package_path, "logging.yaml"]))
+    copy_file(src_file_path=config_template, dst_path=project_path)
+    create_directory(dir_path=config_path)
+    copy_file(src_file_path=logging_template, dst_path=config_path)
+    allure_dir = join_path([project_path, "allure-results"])
+    pytest_args = ['--strict-markers', '--tb=short', '-v', '-ra', '-q', '-s', '--alluredir={}'.format(allure_dir)]
+    if script_path is not None:
+        if script_path == "__main__":
+            script_path = sys.argv[0]
+        pytest_args.append(script_path)
+    pytest.main(pytest_args)
