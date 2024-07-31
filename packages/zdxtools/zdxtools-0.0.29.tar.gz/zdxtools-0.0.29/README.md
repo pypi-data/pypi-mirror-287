c6e7a679b0b4101cb9999b230c3525a8ee07cdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:17 +0000

    Add package zdxtools/0.0.29/zdxtools-0.0.29.tar.gz

---
+++ packages/zdxtools/zdxtools-0.0.29.tar.gz/zdxtools-0.0.29/README.md
@@ -0,0 +1,8 @@
+
+图像处理库
+
+    zdxtools.dx_img
+png_jpg
+
+    把png图片转换成jpg图片
+    
---
+++ packages/zdxtools/zdxtools-0.0.29.tar.gz/zdxtools-0.0.29/tests/test1.py
@@ -0,0 +1,23 @@
+from pypi_update.src.zdxtools.dx_tools import decorator
+from pypi_update.src.zdxtools.dx_tools import spider_tools
+import time
+from pypi_update.src.zdxtools.process.run_as_admin import test
+
+# class a :
+#     @decorator.ExceptionD
+#     def test(self):
+#         data = {
+#             'a':1,
+#             '222':'2',
+#             'getsig':'213123',
+#         }
+#         int(data)
+#     def test1(self):
+#
+#         print(spider_tools.return_header(choice='Chrome'))
+
+
+if __name__ == '__main__':
+    # b = a()
+    # b.test1()
+    test()
