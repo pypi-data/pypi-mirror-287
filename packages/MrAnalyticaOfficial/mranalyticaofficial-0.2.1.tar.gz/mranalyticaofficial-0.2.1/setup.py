27dbd456eb8d126b394f0d5cc41c9bc570f7f0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:29 +0000

    Add package MrAnalyticaOfficial/0.2.1/mranalyticaofficial-0.2.1.tar.gz

---
+++ packages/MrAnalyticaOfficial/mranalyticaofficial-0.2.1.tar.gz/mranalyticaofficial-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MrAnalyticaOfficial',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-contrib-python',
+        'numpy'
+    ],
+    author='Matheus Rodrigo',
+    author_email='mranalytica@mranalytica.com',
+    description='The MR Analytica technologies',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://mranalytica.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
