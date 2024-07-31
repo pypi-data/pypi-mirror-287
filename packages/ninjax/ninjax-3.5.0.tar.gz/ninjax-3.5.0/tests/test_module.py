88f11de2db489ae65b2100b312761f7d0f36d2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:35 +0000

    Add package ninjax/3.5.0/ninjax-3.5.0.tar.gz

---
+++ packages/ninjax/ninjax-3.5.0.tar.gz/ninjax-3.5.0/tests/test_module.py
@@ -0,0 +1,81 @@
+import jax.numpy as jnp
+import ninjax as nj
+
+
+class TestModule:
+
+  def test_basic(self):
+    class Foo(nj.Module):
+      def __init__(self):
+        self.bar = nj.Variable(jnp.float32, 5, name='bar')
+      def method(self):
+        baz = self.value('baz', jnp.float32, 3)
+        result = self.bar.read() + baz
+        self.write('baz', result)
+        return result
+    params = {}
+    foo = Foo(name='foo')
+    assert foo.path == 'foo'
+    params, result = nj.pure(foo.method)(params, create=True)
+    assert result == 8
+    assert set(params.keys()) == {'foo/bar/value', 'foo/baz'}
+    params, result = nj.pure(foo.method)(params)
+    assert result == 13
+    assert set(params.keys()) == {'foo/bar/value', 'foo/baz'}
+    params, result = nj.pure(foo.method)(params)
+    assert result == 18
+    assert set(params.keys()) == {'foo/bar/value', 'foo/baz'}
+
+  def test_cloudpickle(self):
+    import cloudpickle
+    class Foo(nj.Module):
+      def __init__(self):
+        self.bar = nj.Variable(jnp.float32, 5, name='bar')
+      def method(self):
+        baz = self.value('baz', jnp.float32, 3)
+        result = self.bar.read() + baz
+        self.write('baz', result)
+        return result
+    params = {}
+    foo = Foo(name='foo')
+    assert foo.path == 'foo'
+    params, result = nj.pure(foo.method)(params, create=True)
+    assert result == 8
+    assert set(params.keys()) == {'foo/bar/value', 'foo/baz'}
+    params, result = nj.pure(foo.method)(params)
+    assert result == 13
+    assert set(params.keys()) == {'foo/bar/value', 'foo/baz'}
+    foo2 = cloudpickle.loads(cloudpickle.dumps(foo))
+    assert foo2.path == 'foo'
+    params, result = nj.pure(foo2.method)(params)
+    assert result == 18
+    assert set(params.keys()) == {'foo/bar/value', 'foo/baz'}
+
+  def test_scopes(self):
+    with nj.scope('foo'):
+      with nj.scope('bar'):
+        v = nj.Variable(jnp.float32, 5, name='baz')
+    assert v.path == 'foo/bar/baz'
+
+    class Foo(nj.Module):
+      def method(self):
+        from ninjax import ninjax as nj
+        with nj.scope('scope'):
+          sub = self.sub('sub1', nj.Variable, jnp.float32, 5)
+          sub.read()
+          assert sub.path == 'foo/scope/sub1'
+        self.value('value', jnp.float32, 5)
+      @nj.scope('method2')
+      def method2(self):
+        sub = self.sub('sub2', nj.Variable, jnp.float32, 2)
+        sub.read()
+
+    foo = Foo(name='foo')
+    state = {}
+    state = nj.init(foo.method)(state)
+    state = nj.init(foo.method2)(state)
+    assert set(state.keys()) == {
+        'foo/scope/sub1/value',
+        'foo/value',
+        'foo/method2/sub2/value',
+    }
