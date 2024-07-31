495de5904d20808258d64621b829c610939642ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 05:04:32 +0000

    Add package tab_manager/0.1.0/tab_manager-0.1.0.tar.gz

---
+++ packages/tab_manager/tab_manager-0.1.0.tar.gz/tab_manager-0.1.0/tab_manager/main.py
@@ -0,0 +1,14 @@
+from .ws import app
+import uvicorn
+
+import typer
+
+
+fast_api = typer.Typer(name="TabManagerWebServer")
+
+
+def run(
+    port: int = 8001,
+    host: str = "0.0.0.0",
+):
+    uvicorn.run(app, port=port, host=host)
