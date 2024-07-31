6cc9098975d3d64a49a6bef639552e1f0392e913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:59 +0000

    Add package vtexpy/0.0.0a13/vtexpy-0.0.0a13.tar.gz

---
+++ packages/vtexpy/vtexpy-0.0.0a13.tar.gz/vtexpy-0.0.0a13/vtex/_api/transactions.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from .._constants import LIST_TRANSACTION_INTERACTIONS_RETRIES
+from .._dto import VTEXListResponse
+from .base import BaseAPI
+
+
+class TransactionsAPI(BaseAPI):
+    """
+    Client for the Transactions API.
+    This is not in the VTEX API documentation.
+    """
+
+    ENVIRONMENT = "vtexpayments"
+
+    def list_transaction_interactions(
+        self,
+        transaction_id: str,
+        **kwargs: Any,
+    ) -> VTEXListResponse:
+        return VTEXListResponse.factory(
+            vtex_response=self._request(
+                method="GET",
+                environment=self.ENVIRONMENT,
+                endpoint=f"/api/pvt/transactions/{transaction_id}/interactions/",
+                config=self._config.with_overrides(
+                    retries=LIST_TRANSACTION_INTERACTIONS_RETRIES,
+                    **kwargs,
+                ),
+            ),
+        )
