29de05b887e9c4712c5933f42d2d8225a463cf91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:06:30 +0000

    Add package pytimeline/0.3.0/pytimeline-0.3.0.tar.gz

---
+++ packages/pytimeline/pytimeline-0.3.0.tar.gz/pytimeline-0.3.0/pytimeline/__init__.py
@@ -0,0 +1,23 @@
+"""
+timeline library
+~~~~~~~~~~~~~~~~
+
+timeline is a library for creating clean SVG timelines from a JSON
+representation of their events and eras. It is a port and rewrite of the
+very old Python 2 script by Jason Reisman.
+
+:Copyright: (c) 2021 by Felix Soubelet.
+:license: MIT, see LICENSE file for more details.
+"""
+
+from .structures import Timeline  # noqa: TID252
+
+__all__ = ["Timeline"]
+
+__title__ = "pytimeline"
+__description__ = "A library for creating and cleaning SVG timelines."
+__url__ = "https://github.com/fsoubelet/Timeline"
+__version__ = "0.3.0"
+__author__ = "Felix Soubelet"
+__author_email__ = "felix.soubelet@cern.ch"
+__license__ = "MIT"
