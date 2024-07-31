e2d8340fe692a6589c38721429eb6f9a2155824a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 05:06:23 +0000

    Add package glider-ingest/1.0.5/glider_ingest-1.0.5.tar.gz

---
+++ packages/glider-ingest/glider_ingest-1.0.5.tar.gz/glider_ingest-1.0.5/src/glider_ingest/__init__.py
@@ -0,0 +1,8 @@
+'''
+Module to ingest and process raw glider data into NetCDF files
+'''
+
+from .processor import Processor
+from .utils import utils
+from .gridder import Gridder
+from .ingest import ingest
