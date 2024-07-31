e9cba97ce4736cf50ae57b54eab59934fe6f387e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:29 +0000

    Add package async-httpd-data-collector/2.0.2/async_httpd_data_collector-2.0.2.tar.gz

---
+++ packages/async-httpd-data-collector/async_httpd_data_collector-2.0.2.tar.gz/async_httpd_data_collector-2.0.2/ahttpdc/read/store/collector.py
@@ -0,0 +1,59 @@
+"""Asynchronous data storage within InfluxDB
+
+Author: Piotr Krzysztof Lis - github.com/straightchlorine
+"""
+
+from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+from influxdb_client.client.write.point import Point
+
+from ahttpdc.read.store.parse.parser import JSONInfluxParser
+
+
+class AsyncCollector:
+    """Store the data asyncronously within InfluxDB.
+
+    Args:
+        sensors (dict[str, list[str]]): readings to store from each sensor.
+        db_url (str): url link to the InfluxDB.
+        db_token (str): token to the InfluxDB.
+        db_org (str): organization in the InfluxDB
+        db_bucket (str): bucket to store data within in InfluxDB
+    """
+
+    def __init__(
+        self,
+        sensors: dict[str, list[str]],
+        db_url: str,
+        db_token: str,
+        db_org: str,
+        db_bucket: str,
+    ) -> None:
+        self._sensors = sensors
+        self._parser = JSONInfluxParser(self._sensors)
+
+        self._url = db_url
+        self._token = db_token
+        self._org = db_org
+        self._bucket = db_bucket
+
+    async def store_readings(self, json_response):
+        """Store sensor readings within InfluxDB.
+
+        JSON reponse is parsed, decorated and stored in InfluxDB.
+
+        Args:
+            records (dict): The sensor readings as InfluxDB record.
+        """
+        # crating the time-series point out of the record
+        record = self._parser.parse(json_response)
+        point = Point.from_dict(record, write_precision='ms')
+
+        # writing created point into influx
+        async with InfluxDBClientAsync(
+            url=self._url,
+            token=self._token,
+            org=self._org,
+        ) as client:
+            await client.write_api().write(
+                bucket=self._bucket, org=self._org, record=point
+            )
---
+++ packages/async-httpd-data-collector/async_httpd_data_collector-2.0.2.tar.gz/async_httpd_data_collector-2.0.2/examples/query_example.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+"""
+Example script, which queries data from the last 30 days and exports it to a
+csv file.
+
+Author: Piotr Krzysztof Lis - github.com/straightchlorine
+"""
+
+import json
+import pandas as pd
+from ahttpdc.read.database_interface import DatabaseInterface
+
+# load the secrets
+with open('secrets/secrets.json', 'r') as f:
+    secrets = json.load(f)
+
+# define the sensors and parameters to fetch, according to the JSON response:
+# {
+#   "nodemcu": {
+#     "mq135": {
+#       "co": "2.56",
+#       "co2": "402.08",
+#       "alcohol": "0.94",
+#       "nh4": "3.30",
+#       "aceton": "0.32",
+#       "toulen": "0.38"
+#     },
+#     "bmp180": {
+#       "temperature": "28.60",
+#       "pressure": "1006.13",
+#       "seaLevelPressure": "1024.18",
+#       "altitude": "149.75"
+#     },
+#     "ds18b20": {
+#       "temperature": "27.00"
+#     },
+#     "dht22": {
+#       "temperature": "27.90",
+#       "humidity": "47.30"
+#     }
+#   }
+# }
+# response above is an example from my own setup.
+sensors = {
+    'bmp180': ['altitude', 'pressure', 'seaLevelPressure'],
+    'mq135': ['aceton', 'alcohol', 'co', 'co2', 'nh4', 'toulen'],
+    'ds18b20': ['temperature'],
+    'dht22': ['humidity'],
+}
+
+# create the DatabaseInterface object
+interface = DatabaseInterface(
+    sensors,
+    secrets['host'],
+    secrets['port'],
+    secrets['token'],
+    secrets['organization'],
+    secrets['bucket'],
+    secrets['srv_ip'],
+    secrets['srv_port'],
+    secrets['handle'],
+    1,
+)
+
+
+# run the query via asyncio
+if __name__ == '__main__':
+    dataframe: pd.DataFrame = interface.query_historical('-1d')
+    dataframe.to_csv('sensor-data.csv')
