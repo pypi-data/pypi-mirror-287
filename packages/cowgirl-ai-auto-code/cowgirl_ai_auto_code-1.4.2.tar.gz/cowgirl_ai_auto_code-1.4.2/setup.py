c3c6ab2348ffa0b58f55714ca4f51cfd6a07ba29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 07:04:20 +0000

    Add package cowgirl-ai-auto-code/1.4.2/cowgirl_ai_auto_code-1.4.2.tar.gz

---
+++ packages/cowgirl-ai-auto-code/cowgirl_ai_auto_code-1.4.2.tar.gz/cowgirl_ai_auto_code-1.4.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+def read_requirements():
+    """Utility function to read the requirements.txt file."""
+    requirements_path = "requirements.txt"
+    if os.path.isfile(requirements_path):
+        with open(requirements_path) as req:
+            return req.read().splitlines()
+    return []
+
+setup(
+    name="cowgirl-ai-auto-code",
+    version="1.4.2",
+    description="Cowgirl AI - Auto Code",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cowgirl-AI/auto-code",
+    author="Tera Earlywine",
+    author_email="dev@teraearlywine.com",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=read_requirements(),
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "auto-code=cli.main:main", 
+            "front_end=cli.front_end:main",    # front_end refine --file='test.js'
+            "back_end=cli.back_end:main",      # back_end refine --file='test.py'
+            "qa=cli.qa:main",                  # qa refine --file='test.py'
+            "script=cli.scripting_bot:main",   # script refine --file='test.sh'
+            "html_css=cli.html_css:main",      # html_css refine --file='test.html'
+            "network=cli.network:main"         # network refine --file='test.py'
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
