2dc6bbf688e88b90609e7ae747cde55adba6c2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 16:05:09 +0000

    Add package amisha-star/0.1/amisha_star-0.1.tar.gz

---
+++ packages/amisha-star/amisha_star-0.1.tar.gz/amisha_star-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name="amisha_star",
+      version="0.1",
+      desc="print pattern ",
+      long_desc="printing a star ",
+      author="amisha",
+      packages=['star'])
