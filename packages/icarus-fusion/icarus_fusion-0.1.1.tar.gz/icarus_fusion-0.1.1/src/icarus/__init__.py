2cca64f4e98e642d0f7885469f64d1b68085b8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:05:02 +0000

    Add package icarus-fusion/0.1.1/icarus_fusion-0.1.1.tar.gz

---
+++ packages/icarus-fusion/icarus_fusion-0.1.1.tar.gz/icarus_fusion-0.1.1/src/icarus/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+icarus
+"""
+
+from icarus.sample_generator import SampleGenerator
+from icarus.dataloader import ThermalModelDataset
+from icarus.model import IcarusModel
+from icarus.datasets import load_2d_thermal_dataset, load_monoblock_thermal_dataset
+
+
+__all__ = ["thermalmodeldataset",
+            "samplegenerator",
+            "icarusmodel"]
