17bee1deaf4035e0a722717bcafc3f307eabc5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:46 +0000

    Add package ragstack-ai-langchain/1.4.0/ragstack_ai_langchain-1.4.0.tar.gz

---
+++ packages/ragstack-ai-langchain/ragstack_ai_langchain-1.4.0.tar.gz/ragstack_ai_langchain-1.4.0/ragstack_langchain/colbert/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from ragstack_colbert.base_retriever import BaseRetriever  # noqa: F401
+except (ImportError, ModuleNotFoundError) as e:
+    raise ImportError(
+        "Could not import ragstack-ai-colbert. "
+        "Please install it with `pip install ragstack-ai-langchain[colbert]`."
+    ) from e
+
+from .colbert_retriever import ColbertRetriever
+from .colbert_vector_store import ColbertVectorStore
+
+__all__ = [
+    "ColbertRetriever",
+    "ColbertVectorStore",
+]
---
+++ packages/ragstack-ai-langchain/ragstack_ai_langchain-1.4.0.tar.gz/ragstack_ai_langchain-1.4.0/ragstack_langchain/colbert/embedding.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Optional
+
+from langchain_core.embeddings import Embeddings
+from ragstack_colbert import DEFAULT_COLBERT_MODEL, ColbertEmbeddingModel
+from typing_extensions import Self, override
+
+if TYPE_CHECKING:
+    from ragstack_colbert.base_embedding_model import BaseEmbeddingModel
+
+
+class TokensEmbeddings(Embeddings):
+    """Adapter for token-based embedding models and the LangChain Embeddings."""
+
+    def __init__(self, embedding: Optional[BaseEmbeddingModel] = None):
+        self.embedding = embedding or ColbertEmbeddingModel()
+
+    @override
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        raise NotImplementedError
+
+    @override
+    def embed_query(self, text: str) -> List[float]:
+        raise NotImplementedError
+
+    @override
+    async def aembed_documents(self, texts: List[str]) -> List[List[float]]:
+        raise NotImplementedError
+
+    @override
+    async def aembed_query(self, text: str) -> List[float]:
+        raise NotImplementedError
+
+    def get_embedding_model(self) -> BaseEmbeddingModel:
+        """Get the embedding model."""
+        return self.embedding
+
+    @classmethod
+    def colbert(
+        cls,
+        checkpoint: str = DEFAULT_COLBERT_MODEL,
+        doc_maxlen: int = 256,
+        nbits: int = 2,
+        kmeans_niters: int = 4,
+        nranks: int = -1,
+        query_maxlen: Optional[int] = None,
+        verbose: int = 3,
+        chunk_batch_size: int = 640,
+    ) -> Self:
+        """Create a new ColBERT embedding model."""
+        return cls(
+            ColbertEmbeddingModel(
+                checkpoint,
+                doc_maxlen,
+                nbits,
+                kmeans_niters,
+                nranks,
+                query_maxlen,
+                verbose,
+                chunk_batch_size,
+            )
+        )
