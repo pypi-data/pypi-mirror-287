dddef963236f430000804e44bcf326a2fae74a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:05:22 +0000

    Add package method-versioning-decorator/0.0.1/method-versioning-decorator-0.0.1.tar.gz

---
+++ packages/method-versioning-decorator/method-versioning-decorator-0.0.1.tar.gz/method-versioning-decorator-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="method-versioning-decorator",
+    version="0.0.1",
+    author="Sanghun Lee",
+    author_email="nrhys2005@gmail.com",
+    description="Method versioning package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nrhys2005/method-versioning",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
