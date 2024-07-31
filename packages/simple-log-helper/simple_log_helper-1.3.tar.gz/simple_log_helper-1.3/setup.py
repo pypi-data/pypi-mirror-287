6fed99e6332d34137a31444010d4c1d8fcd98f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 04:05:35 +0000

    Add package simple-log-helper/1.3/simple_log_helper-1.3.tar.gz

---
+++ packages/simple-log-helper/simple_log_helper-1.3.tar.gz/simple_log_helper-1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_log_helper',
+    version='1.3',
+    packages=find_packages(),
+    install_requires=[
+        # list your project dependencies here
+    ],
+)
