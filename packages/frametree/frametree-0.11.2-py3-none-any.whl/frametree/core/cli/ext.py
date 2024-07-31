e096315d3985c11815bc2313f19183e1220356e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:04:55 +0000

    Add package frametree/0.11.2/frametree-0.11.2-py3-none-any.whl

---
+++ packages/frametree/frametree-0.11.2-py3-none-any.whl/frametree/core/cli/ext.py
@@ -0,0 +1,13 @@
+import frametree
+from frametree.core.packaging import submodules
+from .base import cli
+
+
+@cli.group()
+def ext():
+    """Command-line group for extension hooks"""
+
+
+# Ensure that all sub-packages under CLI are loaded so they are added to the
+# base command
+extensions = list(submodules(frametree, subpkg="cli"))
