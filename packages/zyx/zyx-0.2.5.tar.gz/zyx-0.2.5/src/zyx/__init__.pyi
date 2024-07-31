d644d22d4cfc45d8513922671e94e62f95f2c959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:06:43 +0000

    Add package zyx/0.2.5/zyx-0.2.5.tar.gz

---
+++ packages/zyx/zyx-0.2.5.tar.gz/zyx-0.2.5/src/zyx/__init__.pyi
@@ -0,0 +1,44 @@
+# zyx ==============================================================================
+
+__all__ = [
+    "audio",
+    "image",
+    "BaseModel",
+    "Field",
+    "logger",
+    "rich_console",
+    "batch",
+    "lightning",
+    "completion",
+    "embeddings",
+    "cast",
+    "extract",
+    "classify",
+    "function",
+    "generate",
+    "inference",
+    "paint",
+    "speak",
+    "transcribe",
+]
+
+from .core import BaseModel as BaseModel
+from .core import Field as Field
+from .core import _logger as logger
+from .core import _rich_console as rich_console
+from .core import _batch as batch
+from .core import _lightning as lightning
+from .client import completion as completion
+from .client import embeddings as embeddings
+from marvin.ai.text import cast as cast
+from marvin.ai.text import extract as extract
+from marvin.ai.text import classify as classify
+from marvin.ai.text import fn as function
+from marvin.ai.text import generate as generate
+from huggingface_hub.inference._client import InferenceClient as inference
+from .image import paint as paint
+from .audio import speak as speak
+from .audio import transcribe as transcribe
+
+from . import audio as audio
+from . import image as image
