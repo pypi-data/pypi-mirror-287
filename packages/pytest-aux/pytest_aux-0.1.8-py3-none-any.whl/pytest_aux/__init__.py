1e34643d7ef234c8a7dbdcd8d1d492d704b9975c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 04:05:36 +0000

    Add package pytest-aux/0.1.8/pytest_aux-0.1.8-py3-none-any.whl

---
+++ packages/pytest-aux/pytest_aux-0.1.8-py3-none-any.whl/pytest_aux/__init__.py
@@ -0,0 +1,94 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#     # AUX
+#     # TYPES
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .pytest_tester import (
+    # BASE
+    pytest_func_tester,
+    pytest_func_tester__no_kwargs,
+    pytest_func_tester__no_args,
+    pytest_func_tester__no_args_kwargs,
+    # AUX
+    # TYPES
+    # EXX
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .primitives import (
+    # BASE
+    GEN_COMPR,
+
+    FUNC,
+    FUNC_NONE,
+    FUNC_TRUE,
+    FUNC_FALSE,
+    FUNC_ALL,
+    FUNC_ANY,
+    FUNC_LIST_DIRECT,
+    FUNC_LIST_VALUES,
+    FUNC_DICT,
+    FUNC_EXX,
+    FUNC_GEN,
+
+    LAMBDA,
+    LAMBDA_NONE,
+    LAMBDA_TRUE,
+    LAMBDA_FALSE,
+    LAMBDA_ALL,
+    LAMBDA_ANY,
+    LAMBDA_LIST_DIRECT,
+    LAMBDA_LIST_VALUES,
+    LAMBDA_DICT,
+    LAMBDA_EXX,
+    LAMBDA_GEN,
+
+    Exx,
+
+    ClsInt,
+    ClsFloat,
+    ClsStr,
+    ClsList,
+    ClsSet,
+    ClsDict,
+
+    Cls,
+    ClsEmpty,
+
+    ClsInitArgsKwargs,
+    ClsInitExx,
+
+    ClsCall,
+    ClsCallNone,
+    ClsCallTrue,
+    ClsCallFalse,
+    ClsCallExx,
+
+    ClsBoolTrue,
+    ClsBoolFalse,
+    ClsBoolExx,
+
+    ClsIterYield,
+    ClsGen,
+
+    ClsEq,
+    ClsEqExx,
+
+    ClsFullTypes,
+    # AUX
+    # TYPES
+    # EXX
+)
+
+
+# =====================================================================================================================
