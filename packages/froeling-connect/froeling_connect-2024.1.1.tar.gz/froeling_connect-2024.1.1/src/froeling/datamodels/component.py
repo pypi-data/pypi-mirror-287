97c4d5cda24d3d524ab921bf88afad34b6bd3bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 06:04:31 +0000

    Add package froeling-connect/2024.1.1/froeling_connect-2024.1.1.tar.gz

---
+++ packages/froeling-connect/froeling_connect-2024.1.1.tar.gz/froeling_connect-2024.1.1/src/froeling/datamodels/component.py
@@ -0,0 +1,113 @@
+from dataclasses import dataclass
+
+from .. import endpoints
+from ..session import Session
+from ..exceptions import NetworkError
+from .generics import TimeWindowDay
+
+class Component:
+    """Represents a component. Contains its parameters. Remember to call Component.update to populate values."""
+    component_id: str
+    display_name: str
+    display_category: str
+    standard_name: str
+    type: str
+    sub_type: str
+    time_windows_view: list[TimeWindowDay]
+    picture_url: str
+
+    parameters: list['Parameter']
+
+    def __init__(self, facility_id: int, component_id: str, session: Session):
+        self.facility_id = facility_id
+        self.component_id = component_id
+        self.session = session
+
+    def __str__(self):
+        return f'Component([Facility {self.facility_id}] -> {self.component_id})'
+
+    async def update(self) -> list['Parameter']:
+        res = await self.session.request("get", endpoints.COMPONENT.format(self.session.user_id, self.facility_id, self.component_id))
+        self.component_id = res['componentId']
+        self.display_name = res['displayName']
+        self.display_category = res['displayCategory']
+        self.standard_name = res['standardName']
+        self.type = res['type']
+        self.sub_type = res['subType']
+        if res['timeWindowsView']:
+            self.time_windows_view = TimeWindowDay.from_list(res['timeWindowsView'])
+
+        #  TODO: Find endpoint that gives all parameters
+        topview = res.get('topView')
+
+        parameters = dict()
+        if topview:
+            self.picture_url = topview.get('pictureUrl')
+            if 'pictureParams' in topview:
+                parameters |= topview.get('pictureParams')
+            if 'infoParams' in topview:
+                parameters |= topview.get('infoParams')
+            if 'configParams' in topview:
+                parameters |= topview.get('configParams')
+        if 'stateView' in res:
+            parameters |= {i['name']: i for i in res.get('stateView')}
+        if 'setupView' in res:
+            parameters |= {i['name']: i for i in res.get('setupView')}
+
+        self.parameters = Parameter.from_list(parameters.values(), self.session, self.facility_id)
+        return self.parameters
+
+
+@dataclass
+class Parameter:
+    session: Session
+    facility_id: int
+
+    id: str
+    display_name: str
+    name: str
+    editable: bool
+    parameter_type: str
+    unit: str
+    value: str
+    min_val: str
+    max_val: str
+    string_list_key_values: dict[str, str]
+
+    @classmethod
+    def from_dict(cls, obj, session: Session, facility_id: int):
+        _id = obj["id"]
+        display_name = obj.get("displayName")
+        name = obj.get("name")
+        editable = obj.get("editaböe")
+        parameter_type =  obj.get("parameterType")
+        unit = obj.get("unit")
+        value =  obj.get("value")
+        min_val = obj.get("minVal")
+        max_val = obj.get("maxVal")
+        string_list_key_values = obj.get("stringListKeyValues")
+
+        return cls(session, facility_id, _id, display_name, name, editable, parameter_type, unit, value, min_val, max_val, string_list_key_values)
+
+    @classmethod
+    def from_list(cls, obj, session: Session, facility_id: int):
+        return [cls.from_dict(i, session, facility_id) for i in obj]
+
+    @property
+    def display_value(self) -> str:
+        if self.string_list_key_values:
+            return self.string_list_key_values[str(self.value)]
+        if self.unit:
+            return f'{self.value} {self.unit}'
+        return str(self.value)
+
+    async def set_value(self, value):
+        """Returns None if value is the same"""
+        try:
+            return await self.session.request('put',
+                                          endpoints.SET_PARAMETER.format(self.session.user_id, self.facility_id, self.id),
+                                          json={"value": str(value)}
+                                          )
+        except NetworkError as e:
+            if e.status == 304:
+                return None
---
+++ packages/froeling-connect/froeling_connect-2024.1.1.tar.gz/froeling_connect-2024.1.1/src/froeling/session.py
@@ -0,0 +1,97 @@
+"""Manages authentication and error handling"""
+
+from aiohttp import ClientSession
+import json
+import base64
+import logging
+
+from . import endpoints, exceptions
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36', 'Referer': 'https://connect-web.froeling.com/'}
+
+
+class Session:
+    user_id: int = None
+    token: str = None
+    def __init__(self,
+                 username: str=None, password: str=None, token: str=None,
+                 auto_reauth: bool=True,
+                 token_callback=None,
+                 lang: str = 'en',
+                 logger: logging.Logger=None
+                 ):
+        assert token or (username and password), "Set either token or username and password."
+        assert not (auto_reauth and not (username and password)), "Set username and password to use auto_reauth."
+
+        self.session = ClientSession(headers=headers|{'Accept-Language': lang})
+        self.username = username
+        self.password = password
+        self.auto_reauth = auto_reauth
+        self.token_callback = token_callback
+
+
+        if token:
+            self.set_token(token)
+
+        self._logger = logger or logging.getLogger(__name__)
+
+    async def close(self):
+        await self.session.close()
+
+    def set_token(self, token: str):
+        """Sets the token used in Authorization and updates/sets user-id
+        :param token The bearer token"""
+        self.session.headers['Authorization'] = token
+        try:
+            self.user_id = json.loads(base64.b64decode(token.split('.')[1] + "==").decode("utf-8"))['userId']
+        except:
+            raise ValueError("Token is in an invalid format.")
+        if self.token_callback and self.token:  # Only run when overwriting existing token
+            self.token = token
+            self.token_callback(token)
+        else:
+            self.token = token
+
+    async def login(self) -> dict:
+        """Gets a token using username and password
+        :return: Json sent by server (includes userdata)"""
+        data = {'osType': 'web', 'username': self.username, 'password': self.password}
+        async with await self.session.post(endpoints.LOGIN, json=data) as res:
+            if res.status != 200:
+                raise exceptions.AuthenticationError(f'Server returned {res.status}: "{await res.text()}"')
+            self.set_token(res.headers['Authorization'])
+            userdata = await res.json()
+        self._logger.info("Logged in with username and password.")
+        return userdata
+
+    async def request(self, method, url, **kwargs):
+        """
+
+        :param method:
+        :param url:
+        :param kwargs:
+        """
+        self._logger.info(f'Sent %s: %s', method.upper(), url)
+        try:
+            async with await self.session.request(method, url, **kwargs) as res:
+                if 299 >= res.status >= 200:
+                    r = await res.text()
+                    self._logger.info('Got %s', r)
+                    return await res.json()
+
+                if res.status == 401:
+                    if self.auto_reauth:
+                        self._logger.info('Error %s, renewing token...', await res.text())
+                        await self.login()
+                        self._logger.info('Reauthorized.')
+                        return await self.request(method, url, **kwargs)
+                    else:
+                        self._logger.error("Request unauthorized")
+                        raise exceptions.AuthenticationError("Request not authorized: ", await res.text())
+                else:
+                    error_data = await res.text()
+                    raise exceptions.NetworkError("Unexpected return code", status=res.status, url=res.url, res=error_data)
+
+        except json.decoder.JSONDecodeError as e:
+            raise exceptions.ParsingError(e.msg, e.doc, e.pos, url)
