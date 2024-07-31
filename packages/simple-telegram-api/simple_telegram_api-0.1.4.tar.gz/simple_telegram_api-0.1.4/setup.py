3c0ae979e027135cfb58acfd6d48a2360af06903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 06:04:48 +0000

    Add package simple-telegram-api/0.1.4/simple_telegram_api-0.1.4.tar.gz

---
+++ packages/simple-telegram-api/simple_telegram_api-0.1.4.tar.gz/simple_telegram_api-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    description = file.read()
+
+setup(
+    name='simple-telegram-api',        # Name of your package
+    version='0.1.4',            # Version of your package
+    packages=find_packages(), # Automatically find packages
+    install_requires=[],      # List of dependencies
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
