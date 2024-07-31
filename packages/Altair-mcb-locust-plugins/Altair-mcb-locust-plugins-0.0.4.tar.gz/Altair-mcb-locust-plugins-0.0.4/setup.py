a01918e711a0ec6c21dff215711e6b6138fe086d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:06:38 +0000

    Add package Altair-mcb-locust-plugins/0.0.4/Altair-mcb-locust-plugins-0.0.4.tar.gz

---
+++ packages/Altair-mcb-locust-plugins/Altair-mcb-locust-plugins-0.0.4.tar.gz/Altair-mcb-locust-plugins-0.0.4/setup.py
@@ -0,0 +1,91 @@
+import setuptools
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="Altair-mcb-locust-plugins",
+    version="0.0.4",
+    author="Bhagyaraj",
+    author_email="mc_raj4u@yahoo.co.in",
+    #packages=["locust_plugins/", "locust_plugins/listeners/", "locust_plugins/users/"],
+    packages=find_packages(exclude=["examples"]),
+    include_package_data=True,
+    package_data={'':['locust_plugins/dashboards/*']},
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcraj4u/altair-coe-locust-plugins",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=["locust>=2.20.0",
+                      "typing-extensions"]
+)
+
+'''
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+
+from setuptools import find_packages, setup
+
+extras = {
+    "websocket": ["websocket-client"],
+    "playwright": ["playwright>=1.40.0"],
+    "dashboards": ["psycogreen", "psycopg2-binary"],
+    "kafka": ["confluent-kafka"],
+    "mongo": ["pymongo"],
+    "mqtt": ["paho-mqtt>=2.1.0"],
+    "appinsights": ["opencensus-ext-azure"],
+    "resource": ["lxml"],
+    "boto3": ["boto3"],
+}
+
+setup(
+    name="Altair-mcb-locust-plugins",
+    description="Useful plugins/extensions for Locust",
+    long_description="""https://github.com/mcraj4u/altair-coe-locust-plugins""",
+    classifiers=[
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+    ],
+    python_requires=">=3.8, <4",
+    keywords="",
+    author="Lars Holmberg",
+    url="https://github.com/SvenskaSpel/locust-plugins",
+    license="Apache-2.0",
+    packages=find_packages(exclude=["examples"]),
+    include_package_data=True,
+    package_data={"locust_plugins": ["py.typed"]},
+    zip_safe=False,
+    install_requires=[
+        "locust>=2.20.0",
+        "typing-extensions",
+    ],
+    extras_require={
+        **extras,
+        "all": list(extras.values()),
+        "webdriver": ["selenium>=4.0.0"],  # warning: this installs trio which is incompatible with playwright
+    },
+    scripts=["bin/locust-compose"],
+    use_scm_version={
+        "write_to": "locust_plugins/_version.py",
+        "local_scheme": "no-local-version",
+    },
+    setup_requires=["setuptools_scm"],
+)
+'''
