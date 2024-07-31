851ecca8b0537740b631f4f4b80c0301fad599b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 16:05:12 +0000

    Add package py3-tts-wrapper/0.9.6/py3_tts_wrapper-0.9.6-py3-none-any.whl

---
+++ packages/py3-tts-wrapper/py3_tts_wrapper-0.9.6-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,32 @@
+import os
+from typing import Callable
+
+import filetype  # type: ignore
+import pytest
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_DATA_DIR = os.path.join(SCRIPT_DIR, "data")
+
+
+def load_resp_wav():
+    with open(os.path.join(TEST_DATA_DIR, "test.wav"), "rb") as f:
+        return f.read()
+
+class Helpers:
+    @staticmethod
+    def check_audio_file(path, format="wav"):
+        assert os.path.exists(path), f"{path} does not exists"
+        assert os.path.getsize(path) > 1024
+        assert filetype.guess_extension(path) == format
+
+    @staticmethod
+    def create_tmp_filename(tmp_dir, filename):
+        if not os.path.exists(tmp_dir):
+            os.makedirs(tmp_dir)
+        return os.path.join(tmp_dir, filename)
+
+
+@pytest.fixture(scope="session")
+def helpers():
+    return Helpers
+
---
+++ packages/py3-tts-wrapper/py3_tts_wrapper-0.9.6-py3-none-any.whl/tests/test_ssml.py
@@ -0,0 +1,35 @@
+from tts_wrapper import SSMLNode
+
+
+def test_simple_tag():
+    assert str(SSMLNode("speak")) == "<speak></speak>"
+
+
+def test_tag_with_attrs():
+    assert str(SSMLNode("speak", {"foo": "bar"})) == '<speak foo="bar"></speak>'
+
+
+def test_tag_with_children():
+    assert (
+        str(SSMLNode("speak", children=[SSMLNode("a"), SSMLNode("b")]))
+        == "<speak><a></a><b></b></speak>"
+    )
+
+
+def test_add_node():
+    assert str(SSMLNode("speak").add(SSMLNode("a"))) == "<speak><a></a></speak>"
+
+
+def test_add_text():
+    assert (
+        str(SSMLNode("speak").add(SSMLNode("a").add("hello")))
+        == "<speak><a>hello</a></speak>"
+    )
+
+
+def test_render_multiple_children():
+    children = ["Hello, ", SSMLNode("break", attrs={"time": "3s"}), " World!"]
+    assert (
+        str(SSMLNode("speak", children=children))
+        == '<speak>Hello, <break time="3s"></break> World!</speak>'
+    )
