a1e4d3a4cd4683d46f7e063e48cf1dc0a3159644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 16:04:51 +0000

    Add package justlogit/1.0.1/justlogit-1.0.1.tar.gz

---
+++ packages/justlogit/justlogit-1.0.1.tar.gz/justlogit-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='justlogit',
+      version='1.0.1',
+      description='Simple logger for your python program',
+      packages=['justlogit'],
+      author_email='holinim@duck.com',
+      zip_safe=False)
