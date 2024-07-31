ff29412a094dc7584417a2720c99a06246b7949e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:05:19 +0000

    Add package python-lwext4ex/1.1.2/python-lwext4ex-1.1.2.tar.gz

---
+++ packages/python-lwext4ex/python-lwext4ex-1.1.2.tar.gz/python-lwext4ex-1.1.2/setup.py
@@ -0,0 +1,9 @@
+import platform
+from glob import glob
+from setuptools import setup, Extension
+
+setup(
+    name='python-lwext4ex',
+    version="1.1.2",
+    description='Some android tools',
+)
