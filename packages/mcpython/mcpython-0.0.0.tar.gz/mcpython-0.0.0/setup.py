75cf4199b4d1f8ab8251366944885532593773d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 03:04:36 +0000

    Add package mcpython/0.0.0/mcpython-0.0.0.tar.gz

---
+++ packages/mcpython/mcpython-0.0.0.tar.gz/mcpython-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="mcpython",
+    version="0.0.0",
+    description="Reserved by xavvvv",
+    long_description="Reserved by xavvvv for a future project. https://xavvvv.xyz",
+    url="https://xavvvv.xyz",
+    author="xavvvv",
+    author_email="xav@xavvvv.xyz",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"]
+)
