ec3e7e4edd638e718f4929b0053ce98df5341d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 14:04:34 +0000

    Add package endex-factset-api/0.1.0a2/endex_factset_api-0.1.0a2.tar.gz

---
+++ packages/endex-factset-api/endex_factset_api-0.1.0a2.tar.gz/endex_factset_api-0.1.0a2/src/endex_factset_api/resources/batch_processing/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .batch_result import (
+    BatchResultResource,
+    AsyncBatchResultResource,
+    BatchResultResourceWithRawResponse,
+    AsyncBatchResultResourceWithRawResponse,
+    BatchResultResourceWithStreamingResponse,
+    AsyncBatchResultResourceWithStreamingResponse,
+)
+from .batch_status import (
+    BatchStatusResource,
+    AsyncBatchStatusResource,
+    BatchStatusResourceWithRawResponse,
+    AsyncBatchStatusResourceWithRawResponse,
+    BatchStatusResourceWithStreamingResponse,
+    AsyncBatchStatusResourceWithStreamingResponse,
+)
+from .batch_processing import (
+    BatchProcessingResource,
+    AsyncBatchProcessingResource,
+    BatchProcessingResourceWithRawResponse,
+    AsyncBatchProcessingResourceWithRawResponse,
+    BatchProcessingResourceWithStreamingResponse,
+    AsyncBatchProcessingResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "BatchStatusResource",
+    "AsyncBatchStatusResource",
+    "BatchStatusResourceWithRawResponse",
+    "AsyncBatchStatusResourceWithRawResponse",
+    "BatchStatusResourceWithStreamingResponse",
+    "AsyncBatchStatusResourceWithStreamingResponse",
+    "BatchResultResource",
+    "AsyncBatchResultResource",
+    "BatchResultResourceWithRawResponse",
+    "AsyncBatchResultResourceWithRawResponse",
+    "BatchResultResourceWithStreamingResponse",
+    "AsyncBatchResultResourceWithStreamingResponse",
+    "BatchProcessingResource",
+    "AsyncBatchProcessingResource",
+    "BatchProcessingResourceWithRawResponse",
+    "AsyncBatchProcessingResourceWithRawResponse",
+    "BatchProcessingResourceWithStreamingResponse",
+    "AsyncBatchProcessingResourceWithStreamingResponse",
+]
---
+++ packages/endex-factset-api/endex_factset_api-0.1.0a2.tar.gz/endex_factset_api-0.1.0a2/src/endex_factset_api/types/__init__.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .metrics_response import MetricsResponse as MetricsResponse
+from .segments_response import SegmentsResponse as SegmentsResponse
+from .metric_list_params import MetricListParams as MetricListParams
+from .segment_list_params import SegmentListParams as SegmentListParams
+from .fundamentals_response import FundamentalsResponse as FundamentalsResponse
+from .segment_create_params import SegmentCreateParams as SegmentCreateParams
+from .fact_set_fundamental_list_params import FactSetFundamentalListParams as FactSetFundamentalListParams
+from .fact_set_fundamental_create_params import FactSetFundamentalCreateParams as FactSetFundamentalCreateParams
