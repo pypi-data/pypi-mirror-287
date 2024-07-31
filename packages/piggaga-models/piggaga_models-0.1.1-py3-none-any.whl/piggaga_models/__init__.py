2b7acbc25e5c0955841e1093804cdf7f4c0be3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 04:04:52 +0000

    Add package piggaga-models/0.1.1/piggaga_models-0.1.1-py3-none-any.whl

---
+++ packages/piggaga-models/piggaga_models-0.1.1-py3-none-any.whl/piggaga_models/__init__.py
@@ -0,0 +1,4 @@
+from .piggaga_models import download_google_images
+from .piggaga_models import print_all_models
+from .piggaga_models import download_website_images
+from .piggaga_models import print_all_models
