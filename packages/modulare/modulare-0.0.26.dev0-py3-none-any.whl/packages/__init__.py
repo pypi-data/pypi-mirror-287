367e501e4e2848184b79f0b0801d8cf3e6037771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:05:26 +0000

    Add package modulare/0.0.26.dev0/modulare-0.0.26.dev0-py3-none-any.whl

---
+++ packages/modulare/modulare-0.0.26.dev0-py3-none-any.whl/packages/__init__.py
@@ -0,0 +1,2 @@
+from .decorators import route, register_routes, response_status_code, StatusCode
+from .decorators import Module
---
+++ packages/modulare/modulare-0.0.26.dev0-py3-none-any.whl/packages/decorators/__init__.py
@@ -0,0 +1,4 @@
+from .http.status_code_decorator import response_status_code
+from .http.route_decorator import route, register_routes
+from .http.enums.status_code_decorator_enum import StatusCode
+from .module.module_decorator import Module
