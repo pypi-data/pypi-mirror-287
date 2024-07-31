2c3d5ece8b14fed43dcb5e02989bbb94dfec6b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:05:09 +0000

    Add package fanbookbotapi/1.1.5/fanbookbotapi-1.1.5.tar.gz

---
+++ packages/fanbookbotapi/fanbookbotapi-1.1.5.tar.gz/fanbookbotapi-1.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fanbookbotapi',
+    version='1.1.5',
+    packages=find_packages(),
+    install_requires=[
+        # 这里列出你的依赖包，例如：
+        # 'numpy',
+    ],
+    author='wangdage',
+    author_email='fanbookwdg1122@outlook.com',
+    description='fb bot api',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
