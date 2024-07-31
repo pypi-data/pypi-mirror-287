fab5bdaf508ac5ce98a49332970bfa7e9e0a003c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:04:59 +0000

    Add package ppputils/0.1/ppputils-0.1.tar.gz

---
+++ packages/ppputils/ppputils-0.1.tar.gz/ppputils-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ppputils',
+    version='0.1',
+    packages=find_packages(),
+    description='init code',
+    # python3，readme文件中文报错
+    long_description_content_type='text/markdown',
+    url='https://github.com/puffhub/ppputils',
+    author='Peter Puff',
+    author_email='',
+    license='MIT',
+    install_requires=[
+        # 依赖列表
+    ],
+    classifiers=[
+        # 分类信息
+    ]
+)
