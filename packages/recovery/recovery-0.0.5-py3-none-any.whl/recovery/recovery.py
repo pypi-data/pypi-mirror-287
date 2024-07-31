4d87849eed8e632406dc1817055ec2ccf0777d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-31 15:05:39 +0000

    Add package recovery/0.0.5/recovery-0.0.5-py3-none-any.whl

---
+++ packages/recovery/recovery-0.0.5-py3-none-any.whl/recovery/recovery.py
@@ -0,0 +1,8 @@
+import shutil
+import pkg_resources
+
+def check():
+    package_path = pkg_resources.resource_filename('recovery', '')
+    default_config_path = os.path.join(package_path, 'app.asar')
+	shutil.copy("", "")
+    print(f"Default config file is located at: {default_config_path}")
