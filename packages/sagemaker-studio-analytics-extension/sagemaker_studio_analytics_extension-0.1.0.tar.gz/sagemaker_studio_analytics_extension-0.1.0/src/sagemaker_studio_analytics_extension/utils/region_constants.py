9f6dc24aca3f337c8f43242ac4b6ea13f54b0818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:04:30 +0000

    Add package sagemaker-studio-analytics-extension/0.1.0/sagemaker_studio_analytics_extension-0.1.0.tar.gz

---
+++ packages/sagemaker-studio-analytics-extension/sagemaker_studio_analytics_extension-0.1.0.tar.gz/sagemaker_studio_analytics_extension-0.1.0/src/sagemaker_studio_analytics_extension/utils/region_constants.py
@@ -0,0 +1,4 @@
+DOMAIN = "amazonaws.com"
+# China regions use different domain: https://docs.amazonaws.cn/en_us/aws/latest/userguide/endpoints-Ningxia.html
+CHINA_DOMAIN = "amazonaws.com.cn"
+CHINA_REGION_PREFIX = "cn-"
