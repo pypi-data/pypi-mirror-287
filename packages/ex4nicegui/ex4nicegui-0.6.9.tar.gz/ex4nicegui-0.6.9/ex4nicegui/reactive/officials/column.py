77c77c98fcd0fbf57db6178ef2383ccb55321004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:49 +0000

    Add package ex4nicegui/0.6.9/ex4nicegui-0.6.9.tar.gz

---
+++ packages/ex4nicegui/ex4nicegui-0.6.9.tar.gz/ex4nicegui-0.6.9/ex4nicegui/reactive/officials/column.py
@@ -0,0 +1,37 @@
+from typing import (
+    Any,
+)
+from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
+from ex4nicegui.utils.signals import (
+    TGetterOrReadonlyRef,
+    _TMaybeRef as TMaybeRef,
+)
+from nicegui import ui
+from .base import BindableUi
+
+
+class ColumnBindableUi(BindableUi[ui.column]):
+    def __init__(self, *, wrap: TMaybeRef[bool] = True) -> None:
+        pc = ParameterClassifier(locals(), maybeRefs=["wrap"], events=[])
+        element = ui.column(**pc.get_values_kws())
+
+        super().__init__(element)
+
+        for key, value in pc.get_bindings().items():
+            self.bind_prop(key, value)  # type: ignore
+
+    def bind_prop(self, prop: str, ref_ui: TGetterOrReadonlyRef):
+        if prop == "wrap":
+            return self.bind_wrap(ref_ui)
+
+        return super().bind_prop(prop, ref_ui)
+
+    def bind_wrap(self, ref_ui: TGetterOrReadonlyRef):
+        self.bind_classes({"wrap": ref_ui})
+
+    def __enter__(self):
+        self.element.__enter__()
+        return self
+
+    def __exit__(self, *_: Any):
+        self.element.__exit__(*_)
---
+++ packages/ex4nicegui/ex4nicegui-0.6.9.tar.gz/ex4nicegui-0.6.9/ex4nicegui/reactive/officials/label.py
@@ -0,0 +1,45 @@
+from typing import Any
+from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
+from ex4nicegui.utils.signals import (
+    TGetterOrReadonlyRef,
+    to_value,
+    _TMaybeRef as TMaybeRef,
+)
+from nicegui import ui
+from .base import BindableUi
+from ex4nicegui.reactive.mixins.textColor import HtmlTextColorableMixin
+
+
+class LabelBindableUi(BindableUi[ui.label], HtmlTextColorableMixin):
+    def __init__(
+        self,
+        text: TMaybeRef[Any] = "",
+    ) -> None:
+        pc = ParameterClassifier(locals(), maybeRefs=["text"], events=[])
+
+        element = ui.label(**pc.get_values_kws())
+        super().__init__(element)
+
+        for key, value in pc.get_bindings().items():
+            self.bind_prop(key, value)  # type: ignore
+
+    @property
+    def text(self):
+        return self.element.text
+
+    def bind_prop(self, prop: str, ref_ui: TGetterOrReadonlyRef):
+        if prop == "text":
+            return self.bind_text(ref_ui)
+
+        if prop == "color":
+            return self.bind_color(ref_ui)
+
+        return super().bind_prop(prop, ref_ui)
+
+    def bind_text(self, ref_ui: TGetterOrReadonlyRef):
+        @self._ui_effect
+        def _():
+            self.element.set_text(str(to_value(ref_ui)))
+            self.element.update()
+
+        return self
