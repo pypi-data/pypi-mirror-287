1ed2f3ba16a728ec46ee7f30219d7acab3f58c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:56 +0000

    Add package calamari-ocr/1.0.7/calamari_ocr-1.0.7.tar.gz

---
+++ packages/calamari-ocr/calamari_ocr-1.0.7.tar.gz/calamari_ocr-1.0.7/calamari_ocr/ocr/text_processing/default_text_preprocessor.py
@@ -0,0 +1,19 @@
+from calamari_ocr.ocr.text_processing import (
+    MultiTextProcessor,
+    StripTextProcessor,
+    BidiTextProcessor,
+    TextNormalizer,
+    TextRegularizer,
+)
+
+
+class DefaultTextPreprocessor(MultiTextProcessor):
+    def __init__(self):
+        super().__init__(
+            [
+                BidiTextProcessor(),
+                StripTextProcessor(),
+                TextNormalizer(),
+                TextRegularizer(),
+            ]
+        )
