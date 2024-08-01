b2c406aa567a40f2127620a86a83304f45ed49f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:04:45 +0000

    Add package stompman/1.0.0/stompman-1.0.0.tar.gz

---
+++ packages/stompman/stompman-1.0.0.tar.gz/stompman-1.0.0/testing/consumer.py
@@ -0,0 +1,17 @@
+import asyncio
+
+import stompman
+from tests.integration import CONNECTION_PARAMETERS
+
+
+async def main() -> None:
+    async with stompman.Client(servers=[CONNECTION_PARAMETERS]) as client:
+
+        async def handle_message(frame: stompman.MessageFrame) -> None:  # noqa: RUF029
+            print(frame)  # noqa: T201
+
+        await client.subscribe("DLQ", handler=handle_message, on_suppressed_exception=print)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
