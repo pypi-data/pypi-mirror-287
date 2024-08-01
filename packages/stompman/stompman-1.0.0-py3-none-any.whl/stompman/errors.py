7973a7e8566645037a81c3cf605dd149c5f50894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:04:45 +0000

    Add package stompman/1.0.0/stompman-1.0.0-py3-none-any.whl

---
+++ packages/stompman/stompman-1.0.0-py3-none-any.whl/stompman/errors.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from stompman.frames import ErrorFrame, HeartbeatFrame, MessageFrame, ReceiptFrame
+
+if TYPE_CHECKING:
+    from stompman.config import ConnectionParameters
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class Error(Exception):
+    def __str__(self) -> str:
+        return self.__repr__()
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class ConnectionConfirmationTimeoutError(Error):
+    timeout: int
+    frames: list[MessageFrame | ReceiptFrame | ErrorFrame | HeartbeatFrame]
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class UnsupportedProtocolVersionError(Error):
+    given_version: str
+    supported_version: str
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class FailedAllConnectAttemptsError(Error):
+    servers: list["ConnectionParameters"]
+    retry_attempts: int
+    retry_interval: int
+    timeout: int
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class RepeatedConnectionLostError(Error):
+    retry_attempts: int
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class ConnectionLostError(Error): ...
