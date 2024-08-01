fb79260d611d526b3200ec3f9866dab7eb781d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:04:52 +0000

    Add package ghactions/0.0.0/ghactions-0.0.0-py3-none-any.whl

---
+++ packages/ghactions/ghactions-0.0.0-py3-none-any.whl/ghactions/__init__.py
@@ -0,0 +1,15 @@
+"""A toolkit for developing GitHub Actions."""
+
+__version__: str = "0.0.0"
+"""Version of the Python package presented as a :class:`string`.
+
+Dynamically set upon release by [poetry-dynamic-versioning](https://github.com/mtkennerly/poetry-dynamic-versioning).
+
+"""
+
+__version_tuple__: tuple[int, int, int] | tuple[int, int, int, str] = (0, 0, 0)
+"""Version of the Python package presented as a :class:`tuple`.
+
+Dynamically set upon release by [poetry-dynamic-versioning](https://github.com/mtkennerly/poetry-dynamic-versioning).
+
+"""
