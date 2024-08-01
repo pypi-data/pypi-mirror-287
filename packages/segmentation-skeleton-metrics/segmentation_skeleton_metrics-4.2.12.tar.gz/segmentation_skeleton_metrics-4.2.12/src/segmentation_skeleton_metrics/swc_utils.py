9928934cb9245aad407740bc7a528a5dd1b51864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:05:19 +0000

    Add package segmentation-skeleton-metrics/4.2.12/segmentation_skeleton_metrics-4.2.12.tar.gz

---
+++ packages/segmentation-skeleton-metrics/segmentation_skeleton_metrics-4.2.12.tar.gz/segmentation_skeleton_metrics-4.2.12/src/segmentation_skeleton_metrics/swc_utils.py
@@ -0,0 +1,374 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed June 5 16:00:00 2023
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+"""
+
+from concurrent.futures import (
+    ProcessPoolExecutor,
+    ThreadPoolExecutor,
+    as_completed,
+)
+from io import BytesIO
+from zipfile import ZipFile
+
+import networkx as nx
+import numpy as np
+from google.cloud import storage
+
+from segmentation_skeleton_metrics import graph_utils as gutils
+from segmentation_skeleton_metrics import utils
+
+
+def init_valid_labels(swc_paths, min_size):
+    """
+    Reads swc files and extracts the xyz coordinates.
+
+    Paramters
+    ---------
+    swc_paths : list or dict
+        If swc files are on local machine, list of paths to swc files where
+        each file corresponds to a neuron in the prediction. If swc files are
+        on cloud, then dict with keys "bucket_name" and "path".
+    min_size : int
+        Threshold on the number of nodes contained in an swc file. Only swc
+        files with more than "min_size" nodes are stored in "valid_labels".
+
+    Returns
+    -------
+    dict
+        Dictionary where each item is an swc_id and an array of the xyz
+        coordinates read from cooresponding swc file.
+
+    """
+    if type(swc_paths) == list:
+        return parse_local_paths(swc_paths, min_size)
+    elif type(swc_paths) == dict:
+        return parse_cloud_paths(swc_paths, min_size)
+    else:
+        return None
+
+
+def parse_local_paths(swc_paths, min_size):
+    """
+    Reads swc files from local machine and extracts the xyz coordinates.
+
+    Paramters
+    ---------
+    swc_paths : list or dict
+        If swc files are on local machine, list of paths to swc files where
+        each file corresponds to a neuron in the prediction. If swc files are
+        on cloud, then dict with keys "bucket_name" and "path".
+    min_size : int
+        Threshold on the number of nodes contained in an swc file. Only swc
+        files with more than "min_size" nodes are stored in "valid_labels".
+
+    Returns
+    -------
+    valid_labels : dict
+        Dictionary where each item is an swc_id and an array of the xyz
+        coordinates read from cooresponding swc file.
+
+    """
+    valid_labels = dict()
+    for path in swc_paths:
+        contents = read_from_local(path)
+        if len(contents) > min_size:
+            id = int(utils.get_id(path))
+            valid_labels[id] = get_coords(contents)
+    return valid_labels
+
+
+def parse_cloud_paths(cloud_dict, min_size):
+    """
+    Reads swc files from a GCS bucket and extracts the xyz coordinates.
+
+    Parameters
+    ----------
+    cloud_dict : dict
+        Dictionary where keys are "bucket_name" and "path".
+    min_size : int
+        Threshold on the number of nodes contained in an swc file. Only swc
+        files with more than "min_size" nodes are stored in "valid_labels".
+
+    Returns
+    -------
+    valid_labels : dict
+        Dictionary where each item is an swc_id and an array of the xyz
+        coordinates read from cooresponding swc file.
+
+    """
+    # Initializations
+    bucket = storage.Client().bucket(cloud_dict["bucket_name"])
+    zip_paths = utils.list_gcs_filenames(bucket, cloud_dict["path"], ".zip")
+    print("Downloading predicted swc files from cloud...")
+    print("# zip files:", len(zip_paths))
+
+    # Assign processes
+    cnt = 1
+    chunk_size = len(zip_paths) * 0.02
+    t0, t1 = utils.init_timers()
+    with ProcessPoolExecutor() as executor:
+        processes = []
+        for i, path in enumerate(zip_paths):
+            zip_content = bucket.blob(path).download_as_bytes()
+            processes.append(executor.submit(download, zip_content, min_size))
+            if i >= cnt * chunk_size:
+                cnt, t1 = utils.report_progress(
+                    i, len(zip_paths), chunk_size, cnt, t0, t1
+                )
+
+    # Store results
+    valid_labels = set()
+    for i, process in enumerate(as_completed(processes)):
+        valid_labels = valid_labels.union(process.result())
+    print("\n# Valid Labels:", len(valid_labels))
+    return valid_labels
+
+
+def download(zip_content, min_size):
+    """
+    Downloads the contents from each swc file contained in the zip file at
+    "zip_path".
+
+    Parameters
+    ----------
+    zip_content : ...
+        Contents of a zip file.
+    min_size : int
+        Threshold on the number of nodes contained in an swc file. Only swc
+        files with more than "min_size" nodes are stored in "valid_labels".
+
+    Returns
+    -------
+    valid_labels : dict
+        Dictionary where each item is an swc_id and an array of the xyz
+        coordinates read from cooresponding swc file.
+
+    """
+    with ZipFile(BytesIO(zip_content)) as zip_file:
+        with ThreadPoolExecutor() as executor:
+            # Assign threads
+            paths = utils.list_files_in_gcs_zip(zip_content)
+            threads = [
+                executor.submit(parse_gcs_zip, zip_file, path, min_size)
+                for path in paths
+            ]
+
+    # Process results
+    valid_labels = set()
+    for thread in as_completed(threads):
+        label = thread.result()
+        if label > 0:
+            valid_labels.add(label)
+    return valid_labels
+
+
+def parse_gcs_zip(zip_file, path, min_size):
+    """
+    Reads swc file stored at "path" which points to a file in a GCS bucket.
+
+    Parameters
+    ----------
+    zip_file : ZipFile
+        Zip file containing swc file to be read.
+    path : str
+        Path to swc file to be read.
+    min_size : int
+        Threshold on the number of nodes contained in an swc file. Only swc
+        files with more than "min_size" nodes are stored in "valid_labels".
+
+    Returns
+    -------
+    int
+        ...
+
+    """
+    contents = read_from_cloud(zip_file, path)
+    return int(utils.get_id(path)) if len(contents) >= min_size else -1
+
+
+def read_from_local(path):
+    """
+    Reads swc file stored at "path" on local machine.
+
+    Parameters
+    ----------
+    path : str
+        Path to swc file to be read.
+
+    Returns
+    -------
+    list
+        List such that each entry is a line from the swc file.
+
+    """
+    with open(path, "r") as file:
+        return file.readlines()
+
+
+def read_from_cloud(zip_file, path):
+    """
+    Reads the content of an swc file from a zip file in a GCS bucket.
+
+    """
+    with zip_file.open(path) as f:
+        return f.read().decode("utf-8").splitlines()
+
+
+def get_coords(contents, anisotropy):
+    """
+    Gets the xyz coords from the swc file at "path".
+
+    Parameters
+    ----------
+    path : str
+        Path to swc file to be parsed.
+    anisotropy : list[float]
+        Image to World scaling factors applied to xyz coordinates to account
+        for anisotropy of the microscope.
+
+    Returns
+    -------
+    numpy.ndarray
+        xyz coords from an swc file.
+
+    """
+    coords_list = []
+    offset = [0, 0, 0]
+    for line in contents:
+        if line.startswith("# OFFSET"):
+            parts = line.split()
+            offset = read_xyz(parts[2:5])
+        if not line.startswith("#"):
+            parts = line.split()
+            coord = read_xyz(parts[2:5], anisotropy=anisotropy, offset=offset)
+            coords_list.append(coord)
+    return np.array(coords_list)
+
+
+def read_xyz(xyz, anisotropy=[1.0, 1.0, 1.0], offset=[0, 0, 0]):
+    """
+    Reads the (x,y,z) coordinates from an swc file, then reverses and scales
+    them.
+
+    Parameters
+    ----------
+    coord : str
+        xyz coordinate.
+    anisotropy : list[float], optional
+        Image to real-world coordinates scaling factors applied to "xyz". The
+        default is [1.0, 1.0, 1.0].
+    offset : list[int], optional
+        Offset of (x, y, z) coordinates in swc file. The default is [0, 0, 0].
+
+    Returns
+    -------
+    tuple
+        The (x,y,z) coordinates of an entry from an swc file in real-world
+        coordinates.
+
+    """
+    xyz = [float(xyz[i]) + offset[i] for i in range(3)]
+    return np.array([xyz[i] / anisotropy[i] for i in range(3)], dtype=int)
+
+
+def save(path, xyz_1, xyz_2, color=None):
+    """
+    Writes an swc file.
+
+    Parameters
+    ----------
+    path : str
+        Path on local machine that swc file will be written to.
+    entry_list : list[list]
+        List of entries that will be written to an swc file.
+    color : str, optional
+        Color of nodes. The default is None.
+
+    Returns
+    -------
+    None.
+
+    """
+    with open(path, "w") as f:
+        # Preamble
+        if color is not None:
+            f.write("# COLOR " + color)
+        else:
+            f.write("# id, type, z, y, x, r, pid")
+        f.write("\n")
+
+        # Entries
+        f.write(make_entry(1, -1, xyz_1))
+        f.write("\n")
+        f.write(make_entry(2, 1, xyz_2))
+
+
+def make_entry(node_id, parent_id, xyz):
+    """
+    Makes an entry to be written in an swc file.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph that "node_id" and "parent_id" belong to.
+    node_id : int
+        Node that entry corresponds to.
+    parent_id : int
+         Parent of node "node_id".
+    xyz : ...
+        xyz coordinate of "node_id".
+
+    Returns
+    -------
+    entry : str
+        Entry to be written in an swc file.
+
+    """
+    x, y, z = tuple(xyz)
+    entry = f"{node_id} 2 {x} {y} {z} 8 {parent_id}"
+    return entry
+
+
+def to_graph(path, anisotropy=[1.0, 1.0, 1.0]):
+    """
+    Reads an swc file and builds an undirected graph from it.
+
+    Parameters
+    ----------
+    path : str
+        Path to swc file to be read.
+    anisotropy : list[float], optional
+        Image to real-world coordinates scaling factors for (x, y, z) that is
+        applied to swc files. The default is [1.0, 1.0, 1.0].
+
+    Returns
+    -------
+    networkx.Graph
+        Graph built from an swc file.
+
+    """
+    # Build Gaph
+    graph = nx.Graph(swc_id=utils.get_id(path))
+    offset = [0, 0, 0]
+    for line in read_from_local(path):
+        if line.startswith("# OFFSET"):
+            parts = line.split()
+            offset = read_xyz(parts[2:5])
+        if not line.startswith("#"):
+            parts = line.split()
+            child = int(parts[0])
+            parent = int(parts[-1])
+            xyz = read_xyz(parts[2:5], anisotropy=anisotropy, offset=offset)
+            graph.add_node(child, xyz=xyz)
+            if parent != -1:
+                graph.add_edge(parent, child)
+
+    # Set graph-level attributes
+    graph.graph["initial_number_of_edges"] = graph.number_of_edges()
+    graph.graph["initial_run_length"] = gutils.compute_run_length(graph)
+    return graph
---
+++ packages/segmentation-skeleton-metrics/segmentation_skeleton_metrics-4.2.12.tar.gz/segmentation_skeleton_metrics-4.2.12/src/segmentation_skeleton_metrics/utils.py
@@ -0,0 +1,492 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 19:00:00 2022
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+"""
+
+import os
+from io import BytesIO
+from time import time
+from zipfile import ZipFile
+
+import networkx as nx
+import numpy as np
+import tensorstore as ts
+from scipy.spatial import distance
+
+SUPPORTED_DRIVERS = ["neuroglancer_precomputed", "n5", "zarr"]
+
+
+# -- os utils ---
+def listdir(directory, ext=None):
+    """
+    Lists all files in "directory". If an extension "ext" is
+    provided, then only files containing "ext" are returned.
+
+    Parameters
+    ----------
+    directory : str
+        Path to directory to be searched.
+
+    ext : str, optional
+       Extension of file type of interest. The default is None.
+
+    Returns
+    -------
+    list
+        List of all files in directory at "path" with extension "ext" if
+        provided. Otherwise, list of all files in directory.
+
+    """
+    if ext is None:
+        return [f for f in os.listdir(directory)]
+    else:
+        return [f for f in os.listdir(directory) if ext in f]
+
+
+def list_paths(directory, ext=None):
+    """
+    Lists all paths within "directory" with extension "ext" if provided.
+
+    Parameters
+    ----------
+    directory : str
+        Directory to be searched.
+    ext : str, optional
+        If provided, only paths of files with the extension "ext" are
+        returned. The default is None.
+
+    Returns
+    -------
+    list[str]
+        List of all paths within "directory" with extension "ext" if provided.
+
+    """
+    paths = []
+    for f in listdir(directory, ext=ext):
+        paths.append(os.path.join(directory, f))
+    return paths
+
+
+# --- io utils ---
+def open_tensorstore(path, driver):
+    """
+    Uploads segmentation mask stored as a directory of shard files.
+
+    Parameters
+    ----------
+    path : str
+        Path to directory containing shard files.
+    driver : str
+        Storage driver needed to read data at "path".
+
+    Returns
+    -------
+    sparse_volume : dict
+        Sparse image volume.
+
+    """
+    assert driver in SUPPORTED_DRIVERS, "Driver is not supported!"
+    arr = ts.open(
+        {
+            "driver": driver,
+            "kvstore": {
+                "driver": "gcs",
+                "bucket": "allen-nd-goog",
+                "path": path,
+            },
+            "context": {
+                "cache_pool": {"total_bytes_limit": 1000000000},
+                "cache_pool#remote": {"total_bytes_limit": 1000000000},
+                "data_copy_concurrency": {"limit": 8},
+            },
+            "recheck_cached_data": "open",
+        }
+    ).result()
+    if driver == "neuroglancer_precomputed":
+        return arr[ts.d["channel"][0]]
+    return arr
+
+
+def read_tensorstore(path):
+    """
+    Reads neuroglancer_precomputed file at "path".
+
+    Parameters
+    ----------
+    path : str
+        Path to directory containing shardsS.
+
+    Returns
+    -------
+    ts.TensorStore
+        Image volume.
+
+    """
+    dataset_ts = ts.open(
+        {
+            "driver": "neuroglancer_precomputed",
+            "kvstore": {"driver": "file", "path": path},
+        }
+    ).result()
+    return dataset_ts[ts.d["channel"][0]]
+
+
+def read_from_gcs_zip(zip_file, path):
+    """
+    Reads the content of an swc file from a zip file in a GCS bucket.
+
+    """
+    with zip_file.open(path) as text_file:
+        return text_file.read().decode("utf-8").splitlines()
+
+
+def read_txt(path):
+    """
+    Reads txt file stored at "path".
+
+    Parameters
+    ----------
+    path : str
+        Path where txt file is stored.
+
+    Returns
+    -------
+    str
+        Contents of txt file.
+
+    """
+    with open(path, "r") as f:
+        return f.readlines()
+
+
+def list_gcs_filenames(bucket, cloud_path, extension):
+    """
+    Lists all files in a GCS bucket with the given extension.
+
+    """
+    blobs = bucket.list_blobs(prefix=cloud_path)
+    return [blob.name for blob in blobs if extension in blob.name]
+
+
+def list_files_in_gcs_zip(zip_content):
+    """
+    Lists all files in a zip file stored in a GCS bucket.
+
+    """
+    with ZipFile(BytesIO(zip_content), "r") as zip_file:
+        return zip_file.namelist()
+
+
+# -- miscellaneous --
+def check_edge(edge_list, edge):
+    """
+    Checks if "edge" is in "edge_list".
+
+    Parameters
+    ----------
+    edge_list : list or set
+        List or set of edges.
+    edge : tuple
+        Edge.
+
+    Returns
+    -------
+    bool : bool
+        Indication of whether "edge" is contained in "edge_list".
+
+    """
+    if edge in edge_list or (edge[1], edge[0]) in edge_list:
+        return True
+    else:
+        return False
+
+
+def dist(v_1, v_2):
+    """
+    Computes distance between "v_1" and "v_2".
+
+    Parameters
+    ----------
+    v_1 : np.ndarray
+        Vector.
+    v_2 : np.ndarray
+        Vector.
+
+    Returns
+    -------
+    float
+        Distance between "v_1" and "v_2".
+
+    """
+    return distance.euclidean(v_1, v_2)
+
+
+def get_midpoint(xyz_1, xyz_2):
+    """
+    Computes the midpoint between "xyz_1" and "xyz_2".
+
+    Parameters
+    ----------
+    xyz_1 : numpy.ndarray
+        xyz coordinate.
+    xyz_2 : numpy.ndarray
+        xyz coordinate.
+
+    Returns
+    -------
+    numpy.ndarray
+        Midpoint between "xyz_1" and "xyz_2".
+
+    """
+    return np.array([np.mean([xyz_1[i], xyz_2[i]]) for i in range(3)])
+
+
+def to_world(xyz, anisotropy):
+    """
+    Converts "xyz" from image coordinates to real-world coordinates.
+
+    Parameters
+    ----------
+    xyz : tuple or list
+        Coordinates to be transformed.
+    anisotropy : list[float]
+        Image to real-world coordinates scaling factors for (x, y, z) that is
+        applied to swc files.
+
+    Returns
+    -------
+    list[float]
+        Transformed coordinates.
+
+    """
+    return [xyz[i] * anisotropy[i] for i in range(3)]
+
+
+def resolve(multi_hits, dists, xyz_to_id_node):
+    """
+    Resolves discrepancy when xyz coordinates project onto ground truth graphs
+    that contain nodes with same xyz coordinates.
+
+    Parameters
+    ----------
+    multi_hits : set
+        xyz coordinates that are common across multiple graphs.
+    dists : dict
+        Dictionary containing graph ids that predicted swc file has
+        intersected.
+    xyz_to_id_node : dict
+        Dictionary where the keys are xyz coordinates that correspond to nodes
+        in ground truth graphs and values are the graph ids and nodes.
+
+    Return
+    ------
+    dists : dict
+        Updated dictionary with discrepancies resolved.
+
+    """
+    for hat_xyz in multi_hits:
+        keys = list(xyz_to_id_node[hat_xyz].keys())
+        key = find_best(dists, keys)
+        if key:
+            node = xyz_to_id_node[hat_xyz][key]
+            dists = append_dict_value(dists, key, node)
+    return dists
+
+
+def append_dict_value(my_dict, key, value):
+    """
+    Appends "value" to the list stored at "key".
+
+    Parameters
+    ----------
+    my_dict : dict
+        Dictionary to be queried.
+    key : hashable data type
+        Key to be query.
+    value : list item type
+        Value to append to list stored at "key".
+
+    Returns
+    -------
+    my_dict : dict
+        Updated dictionary.
+
+    """
+    if key in my_dict.keys():
+        my_dict[key].append(value)
+    else:
+        my_dict[key] = [value]
+    return my_dict
+
+
+def find_best(my_dict, keys):
+    best_key = None
+    best_vote_cnt = 0
+    if len(my_dict) > 0:
+        for key in keys:
+            vote_cnt = len(my_dict[key]) if key in my_dict.keys() else 0
+            if vote_cnt > best_vote_cnt:
+                best_key = key
+                best_vote_cnt = vote_cnt
+    return best_key
+
+
+def get_id(path):
+    """
+    Gets segment id of the swc file at "path".
+
+    Parameters
+    ----------
+    path : str
+        Path to an swc file
+
+    Return
+    ------
+    Segment id of swc file.
+
+    """
+    filename = os.path.basename(path)
+    return filename.split(".")[0]
+
+
+def init_label_map(connections_path, labels):
+    label_to_class = {0: 0}
+    labels_graph = build_labels_graph(connections_path, labels)
+    for i, component in enumerate(nx.connected_components(labels_graph)):
+        i += 1
+        for label in component:
+            label_to_class[label] = i
+    return label_to_class
+
+
+def build_labels_graph(connections_path, labels):
+    # Initializations
+    labels_graph = nx.Graph()
+    labels_graph.add_nodes_from(labels)
+    n_components = nx.number_connected_components(labels_graph)
+
+    # Main
+    print("Building Label Graph...")
+    print("# connected components - before adding edges:", n_components)
+    for line in read_txt(connections_path):
+        ids = line.split(",")
+        id_1 = int(ids[0])
+        id_2 = int(ids[1])
+        assert id_1 in labels_graph.nodes
+        assert id_2 in labels_graph.nodes
+        labels_graph.add_edge(id_1, id_2)
+    n_components = nx.number_connected_components(labels_graph)
+    print("# connected components - after adding edges:", n_components)
+    print("")
+    return labels_graph
+
+
+# -- runtime --
+def time_writer(t, unit="seconds"):
+    """
+    Converts runtime "t" to its proper unit.
+
+    Parameters
+    ----------
+    t : float
+        Runtime to be converted.
+    unit : str, optional
+        Unit of "t".
+
+    Returns
+    -------
+    t : float
+        Converted runtime.
+    unit : str
+        Unit of "t"
+
+    """
+    assert unit in ["seconds", "minutes", "hours"]
+    upd_unit = {"seconds": "minutes", "minutes": "hours"}
+    if t < 60 or unit == "hours":
+        return t, unit
+    else:
+        t /= 60
+        unit = upd_unit[unit]
+        t, unit = time_writer(t, unit=unit)
+    return t, unit
+
+
+def init_timers():
+    """
+    Initializes two timers.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    time.time
+        Timer.
+    time.time
+        Timer.
+
+    """
+    return time(), time()
+
+
+# -- Utils --
+def report_progress(current, total, chunk_size, cnt, t0, t1):
+    eta = get_eta(current, total, chunk_size, t1)
+    runtime = get_runtime(current, total, chunk_size, t0, t1)
+    progress_bar2(current, total, eta=eta, runtime=runtime)
+    return cnt + 1, time()
+
+
+def get_eta(current, total, chunk_size, t0, return_str=True):
+    chunk_runtime = time() - t0
+    remaining = total - current
+    eta = remaining * (chunk_runtime / chunk_size)
+    t, unit = time_writer(eta)
+    return f"{round(t, 4)} {unit}" if return_str else eta
+
+
+def get_runtime(current, total, chunk_size, t0, t1):
+    eta = get_eta(current, total, chunk_size, t1, return_str=False)
+    total_runtime = time() - t0 + eta
+    t, unit = time_writer(total_runtime)
+    return f"{round(t, 4)} {unit}"
+
+
+def progress_bar(current, total, bar_length=50):
+    """
+    Reports the progress of completing some process.
+
+    Parameters
+    ----------
+    current : int
+        Current iteration of process.
+    total : int
+        Total number of iterations to be completed
+    bar_length : int, optional
+        Length of progress bar
+
+    Returns
+    -------
+    None
+
+    """
+    progress = int(current / total * bar_length)
+    bar = (
+        f"[{'=' * progress}{' ' * (bar_length - progress)}] {current}/{total}"
+    )
+    print(f"\r{bar}", end="", flush=True)
+
+
+def progress_bar2(current, total, bar_length=50, eta=None, runtime=None):
+    progress = int(current / total * bar_length)
+    n_completed = f"Completed: {current}/{total}"
+    bar = f"[{'=' * progress}{' ' * (bar_length - progress)}]"
+    eta = f"Time Remaining: {eta}" if eta else ""
+    runtime = f"Estimated Total Runtime: {runtime}" if runtime else ""
+    print(f"\r{bar} {n_completed} | {eta} | {runtime}    ", end="", flush=True)
