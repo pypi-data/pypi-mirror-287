b72656f089a71d0eca0b149a44ff39ac21838122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:05:38 +0000

    Add package here-debugger/0.0.5/here_debugger-0.0.5-py3-none-any.whl

---
+++ packages/here-debugger/here_debugger-0.0.5-py3-none-any.whl/here_debugger/debug.py
@@ -0,0 +1,28 @@
+import sys
+import inspect
+def here_debugger(*args, custom_arguments=None, include_types=False):
+    """
+    Prints any number of variables, their names, also types (Optional)
+    :param args: variables passed as a keyword argument
+    :param custom_arguments: Search String, append at the beginning
+    :param include_types: False (By default) if True, it will print the types of the variable.
+    :return: None, Raises exception for constant values.
+    """
+    try:
+        current_frame = inspect.currentframe()
+        frame_locals = current_frame.f_back.f_locals
+        if custom_arguments is not None:
+            print(custom_arguments,end=":")
+        print(f"Line-{sys._getframe().f_back.f_lineno}:",end=' ')
+        if not include_types:
+            for arg in args:
+                variable_name = [name for name, value in frame_locals.items() if value is arg][0]
+                print(f"{variable_name} = {arg}",end=' | ')
+        else:
+            for arg in args:
+                variable_name = [name for name, value in frame_locals.items() if value is arg][0]
+                print(f"{variable_name} = {arg}, type={type(arg)}", end=' | ')
+        del current_frame
+        print(end="\n")
+    except Exception as e:
+        raise Exception("HereDebuggerException: Expected variable(s), got value(s).")
