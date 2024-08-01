13d758cab32c493d5f6f918322107378fd82be4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:05:12 +0000

    Add package osagent/0.1.0/osagent-0.1.0-py3-none-any.whl

---
+++ packages/osagent/osagent-0.1.0-py3-none-any.whl/osagent/env/providers/local/provider.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+import signal
+import logging
+import threading
+
+from desktop_env.providers.base import Provider
+
+logger = logging.getLogger("desktopenv.providers.local.LocalProvider")
+logger.setLevel(logging.INFO)
+
+
+class LocalProvider(Provider):
+    def __init__(self, region: str = None, conda_env: str = "base"):
+        super().__init__(region)
+        self.process = None
+        self.conda_env = conda_env
+
+    def start_emulator(self, path_to_vm: str, headless: bool):
+        def run():
+            logger.info("Starting OSWorld service locally...")
+
+            # Prepare the command to run the emulator
+            command = ['python', 'desktop_env/server/main.py']
+
+            # Run the command directly
+            subprocess.run(command, shell=False)
+
+            logger.info("OSWorld service started")
+
+        # Create a new thread to run the emulator
+        self.process_thread = threading.Thread(target=run)
+        self.process_thread.start()
+        logger.info("OSWorld service started in a new thread")
+
+    def get_ip_address(self, path_to_vm: str) -> str:
+        return "localhost"
+
+    def save_state(self, path_to_vm: str, snapshot_name: str):
+        pass
+
+    def revert_to_snapshot(self, path_to_vm: str, snapshot_name: str):
+        pass
+
+    def stop_emulator(self, path_to_vm, region=None):
+        logger.info("Stopping OSWorld service locally...")
+
+        # If the process was started, terminate it
+        if self.process:
+            os.kill(self.process.pid, signal.SIGTERM)
+            self.process.wait()
+
+            logger.info("OSWorld service with PID %s has been stopped", self.process.pid)
+            self.process = None
+        else:
+            logger.warning("OSWorld service is not running")
