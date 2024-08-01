6579ae221dca040a8ab65bc4612cb717759f46c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:05:50 +0000

    Add package mvn/0.1.1/mvn-0.1.1-py2.py3-none-any.whl

---
+++ packages/mvn/mvn-0.1.1-py2.py3-none-any.whl/mvn/aux_pandas/__init__.py
@@ -0,0 +1,5 @@
+from .aux_pandas import wine_df  # noqa: F401
+from .aux_pandas import disp_df  # noqa: F401
+from .aux_pandas import repr_df  # noqa: F401
+from .aux_pandas import fix_column_names  # noqa: F401
+from .aux_pandas import split_dataframe  # noqa: F401
