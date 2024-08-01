46530393ac7b30b2442c176def28126363eb8599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 06:08:01 +0000

    Add package SigProfilerMatrixGenerator/1.2.27/SigProfilerMatrixGenerator-1.2.27-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.27-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.27'
+version = '1.2.27'
+Update = 'v1.2.27: Add environment variable, fix binomtest, improve CLI boolean handling, update dependencies'
+
+	
