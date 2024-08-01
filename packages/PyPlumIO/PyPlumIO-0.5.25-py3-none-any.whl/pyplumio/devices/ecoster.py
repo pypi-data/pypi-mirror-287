bfff0b13715c6168ea30341cc6fe2577fad86336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:05:23 +0000

    Add package PyPlumIO/0.5.25/PyPlumIO-0.5.25-py3-none-any.whl

---
+++ packages/PyPlumIO/PyPlumIO-0.5.25-py3-none-any.whl/pyplumio/devices/ecoster.py
@@ -0,0 +1,14 @@
+"""Contains an ecoSTER class."""
+
+from __future__ import annotations
+
+from typing import ClassVar
+
+from pyplumio.const import DeviceType
+from pyplumio.devices import AddressableDevice
+
+
+class EcoSTER(AddressableDevice):
+    """Represents an ecoSTER thermostat."""
+
+    address: ClassVar[int] = DeviceType.ECOSTER
