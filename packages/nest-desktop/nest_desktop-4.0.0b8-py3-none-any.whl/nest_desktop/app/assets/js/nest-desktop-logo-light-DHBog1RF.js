769d626637de37961b1d7da5c26a2a94ab9b0752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:50 +0000

    Add package nest-desktop/4.0.0b8/nest_desktop-4.0.0b8-py3-none-any.whl

---
+++ packages/nest-desktop/nest_desktop-4.0.0b8-py3-none-any.whl/nest_desktop/app/assets/js/nest-desktop-logo-light-DHBog1RF.js
@@ -0,0 +1,1 @@
+const t=""+new URL("../img/nest-desktop-logo-light-CCuQsPxC.svg",import.meta.url).href;export{t as _};
---
+++ packages/nest-desktop/nest_desktop-4.0.0b8-py3-none-any.whl/nest_desktop/app/assets/simulators/nest/js/ModelDoc-qZprjUy2.js
@@ -0,0 +1,1 @@
+import{_ as m}from"../../../js/ModelDoc-BEeBhzQM.js";import"../../../js/index-DCXcuAJz.js";export{m as default};
