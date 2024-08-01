4b0a76a9df310f5f39020ac94b6b6979f72e572a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:05:01 +0000

    Add package policy-calculator/0.1.0/policy_calculator-0.1.0.tar.gz

---
+++ packages/policy-calculator/policy_calculator-0.1.0.tar.gz/policy_calculator-0.1.0/README.md
@@ -0,0 +1,9 @@
+# Policy Calculator
+
+A Python library for calculating policy prices based on various factors such as age, height, weight, smoking habits, and more.
+
+## Installation
+
+You can install the library using pip:
+
+pip install .
---
+++ packages/policy-calculator/policy_calculator-0.1.0.tar.gz/policy_calculator-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='policy_calculator',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A library for calculating policy prices based on various factors.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/policy_calculator',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
+)
