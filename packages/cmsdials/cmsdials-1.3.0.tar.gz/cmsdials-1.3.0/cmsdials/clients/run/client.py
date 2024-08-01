ceb411c558691468687477d08a25d15272761ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:05:26 +0000

    Add package cmsdials/1.3.0/cmsdials-1.3.0.tar.gz

---
+++ packages/cmsdials/cmsdials-1.3.0.tar.gz/cmsdials-1.3.0/cmsdials/clients/run/client.py
@@ -0,0 +1,27 @@
+import requests
+from requests.exceptions import HTTPError
+
+from ...utils.api_client import BaseAuthorizedAPIClient
+from ...utils.logger import logger
+from .models import PaginatedRunList, Run, RunFilters
+
+
+class RunClient(BaseAuthorizedAPIClient):
+    data_model = Run
+    pagination_model = PaginatedRunList
+    filter_class = RunFilters
+    lookup_url = "run/"
+
+    def get(self, dataset_id: int, run_number: int):
+        endpoint_url = f"{self.api_url}{self.lookup_url}{dataset_id}/{run_number}/"
+        headers = self._build_headers()
+        response = requests.get(endpoint_url, headers=headers, timeout=self.default_timeout)
+
+        try:
+            response.raise_for_status()
+        except HTTPError as err:
+            logger.info(f"Api raw response: {response.text}")
+            raise err
+
+        response = response.json()
+        return self.data_model(**response)
