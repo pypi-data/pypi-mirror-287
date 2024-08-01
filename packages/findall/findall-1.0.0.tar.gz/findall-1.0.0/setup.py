b4e98fe601d9be9912eecf0b1d5587833e4e44a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:06:32 +0000

    Add package findall/1.0.0/findall-1.0.0.tar.gz

---
+++ packages/findall/findall-1.0.0.tar.gz/findall-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="findall",
+      version="1.0.0",
+      description="Python script to find anything from your system.",
+      author="Deepanshu antil",
+      packages=['findall'],
+      author_email="deepanshuantil4113@gmail.com",
+      url="https://github.com/deepanshu414",
+      license="MIT", 
+      install_requires=['psutil'])
