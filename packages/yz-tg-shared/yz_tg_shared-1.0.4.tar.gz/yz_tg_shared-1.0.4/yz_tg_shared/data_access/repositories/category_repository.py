b2054f1994c761ec6b80ba2d157bcbedb9304e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:56 +0000

    Add package yz-tg-shared/1.0.4/yz_tg_shared-1.0.4.tar.gz

---
+++ packages/yz-tg-shared/yz_tg_shared-1.0.4.tar.gz/yz_tg_shared-1.0.4/yz_tg_shared/data_access/repositories/category_repository.py
@@ -0,0 +1,9 @@
+from entities.category import Category
+
+
+class CategoryRepository:
+    def __init__(self, session):
+        self.session = session
+
+    def get_all(self):
+        return self.session.query(Category).all()
