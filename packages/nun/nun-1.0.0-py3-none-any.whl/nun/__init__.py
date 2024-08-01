35ebde610b7bc270ea1bf21bc93ceddc7f7eabd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:05:10 +0000

    Add package nun/1.0.0/nun-1.0.0-py3-none-any.whl

---
+++ packages/nun/nun-1.0.0-py3-none-any.whl/nun/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "1.0.0"
+# __title__ = "mypackage"
+__author__ = "Mitchell Medeiros"
+__license__ = "MIT"
+__description__ = ""
+__docformat__ = "restructuredtext"
+# __docs_url__ = "https://mypackage.readthedocs.io/en/latest/api_reference"
+# __github_url__ = "https://github.com/MitchMedeiros/mypackage"
+# __github_issues_url__ = "https://github.com/MitchMedeiros/mypackage/issues"
+# __doc__ = f"""
+# ... See the docs for more info: {__docs_url__}."
+# """
+__keywords__ = ["python"]
+__all__ = [""]
