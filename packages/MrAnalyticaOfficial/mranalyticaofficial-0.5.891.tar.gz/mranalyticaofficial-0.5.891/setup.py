d56d3954a02c6eb703cc25048eb669d083a65bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 06:08:13 +0000

    Add package MrAnalyticaOfficial/0.5.891/mranalyticaofficial-0.5.891.tar.gz

---
+++ packages/MrAnalyticaOfficial/mranalyticaofficial-0.5.891.tar.gz/mranalyticaofficial-0.5.891/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MrAnalyticaOfficial',
+    version='0.5.891',  # Atualize a versão para refletir as novas mudanças
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'opencv-contrib-python',  # Inclua contrib para suportar o módulo cv2.face
+        'numpy'
+    ],
+    author='Seu Nome',
+    author_email='seu.email@example.com',
+    description='Uma biblioteca poderosa para análise de dados, incluindo o módulo PhoenixVision para reconhecimento facial.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/seuusuario/MrAnalyticaOfficial',  # Atualize com a URL correta do seu projeto
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
