50b483b07dbad72501b36332801d95cd59a17f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 06:07:56 +0000

    Add package propulsionai/0.12.0/propulsionai-0.12.0-py3-none-any.whl

---
+++ packages/propulsionai/propulsionai-0.12.0-py3-none-any.whl/propulsionai/types/knowledgebase/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .file import File as File
+from .knowledgebase import Knowledgebase as Knowledgebase
+from .file_upload_params import FileUploadParams as FileUploadParams
+from .file_delete_response import FileDeleteResponse as FileDeleteResponse
