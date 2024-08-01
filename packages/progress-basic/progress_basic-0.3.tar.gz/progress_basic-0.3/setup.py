6fdeb6e5d67c0f294f813aacbfcf53ea82ce02d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:05:12 +0000

    Add package progress-basic/0.3/progress_basic-0.3.tar.gz

---
+++ packages/progress-basic/progress_basic-0.3.tar.gz/progress_basic-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import setuptools_scm
+
+setup(
+    name="progress_basic",
+    use_scm_version=True,
+    setup_requires=["setuptools>=42", "setuptools_scm"],
+    packages=find_packages(include=['progress_basic', 'progress_basic.*']),
+    include_package_data=True,
+    install_requires=['library_externals'],
+    author="leonardogonzalolaura",
+    author_email="glleonardodaniel@gmail.com",
+    description="A Python library for displaying progress indicators and progress bars in the console with customizable messages and colors.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/leonardogonzalolaura/progress_basic",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
