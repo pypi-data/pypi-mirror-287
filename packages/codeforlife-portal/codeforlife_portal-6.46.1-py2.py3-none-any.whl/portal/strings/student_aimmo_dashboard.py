fd00f9aea48ec8f6b45338c1a6a0a187d5b98eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:41 +0000

    Add package codeforlife-portal/6.46.1/codeforlife_portal-6.46.1-py2.py3-none-any.whl

---
+++ packages/codeforlife-portal/codeforlife_portal-6.46.1-py2.py3-none-any.whl/portal/strings/student_aimmo_dashboard.py
@@ -0,0 +1,6 @@
+AIMMO_DASHBOARD_BANNER = {
+    "title": "Kurono dashboard",
+    "subtitle": "Let the adventures begin! A collection of challenges await your "
+    "exploration. See how far you can get.",
+    "image_class": "banner--picture--aimmo",
+}
