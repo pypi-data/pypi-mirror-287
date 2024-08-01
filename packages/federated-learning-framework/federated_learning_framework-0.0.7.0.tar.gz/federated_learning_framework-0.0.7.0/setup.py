abe9d89eff9a51695598c469c046c9c249ff49ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:05:14 +0000

    Add package federated-learning-framework/0.0.7.0/federated_learning_framework-0.0.7.0.tar.gz

---
+++ packages/federated-learning-framework/federated_learning_framework-0.0.7.0.tar.gz/federated_learning_framework-0.0.7.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='federated-learning-framework',
+    version='0.0.7.0',
+    description='A modular and extensible framework for federated learning applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Mehrdad Javadi',
+    author_email='mehrdaddjavadi@gmail.com',
+    url='https://github.com/mehrdaddjavadi/federated_learning_framework',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='federated learning, machine learning, deep learning, active learning, encryption, homomorphic encryption',
+    install_requires=[
+        'numpy',
+        'tensorflow',
+        'websockets',
+        'pytest',
+        'tenseal'
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=False,
+)
