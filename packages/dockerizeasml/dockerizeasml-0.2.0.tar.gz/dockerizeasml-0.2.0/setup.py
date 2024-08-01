cae23f1a1a85d7f0af4bc10eb5ec2957f3f69d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:47 +0000

    Add package dockerizeasml/0.2.0/dockerizeasml-0.2.0.tar.gz

---
+++ packages/dockerizeasml/dockerizeasml-0.2.0.tar.gz/dockerizeasml-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dockerizeasml",
+    version="0.2.0",
+    author="palaviroop",
+    author_email="avirooppal42@gmail.com",
+    description="A tool to dockerize machine learning projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/yourusername/ml-dockerizer",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            'dockerizeasml=dockerizeasml.dockerizer:main',
+        ],
+    },
+)
