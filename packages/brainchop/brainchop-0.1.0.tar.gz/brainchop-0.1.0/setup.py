e2a7c49bd22b45d795a7ebaec7db36c2db19c950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:04:35 +0000

    Add package brainchop/0.1.0/brainchop-0.1.0.tar.gz

---
+++ packages/brainchop/brainchop-0.1.0.tar.gz/brainchop-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="brainchop",
+    version="0.1.0",
+    author="Mike Doan",
+    author_email="spikedoanz@gmail.com",
+    description="A tool for brain segmentation using tinygrad",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neuroneural/brainchop-cli",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "tinygrad",
+        "nibabel",
+    ],
+    entry_points={
+        "console_scripts": [
+            "brainchop=brainchop.main:main",
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        "brainchop": ["model.json", "model.bin"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
