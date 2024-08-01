ca5a5ddb77b369e8b8235203dc0191b262695f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:05:00 +0000

    Add package robert/1.1.0/robert-1.1.0.tar.gz

---
+++ packages/robert/robert-1.1.0.tar.gz/robert-1.1.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+version="1.1.0"
+setup(
+    name="robert",
+    packages=find_packages(exclude=["tests"]),
+    package_data={"robert": ["model_params/*","report/*"]},
+    version=version,
+    license="MIT",
+    description="Refiner and Optimizer of a Bunch of Existing Regression Tools",
+    long_description="Documentation in Read The Docs: https://robert.readthedocs.io",
+    long_description_content_type="text/markdown",
+    author="Juan V. Alegre Requena, David Dalmau",
+    author_email="jv.alegre@csic.es",
+    keywords=[
+        "workflows",
+        "machine learning",
+        "cheminformatics",
+        "data curation",
+        "prediction",
+        "automated",
+    ],
+    url="https://github.com/jvalegre/robert",
+    download_url=f"https://github.com/jvalegre/robert/archive/refs/tags/{version}.tar.gz",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",  # Specify which python versions you want to support
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    install_requires=[
+        "PyYAML",
+        "matplotlib>=3.7,<3.8",
+        "pandas==2.2.2",
+        "numpy==1.26.4",
+        "progress",
+        "seaborn",
+        "scipy",
+        "scikit-learn==1.5.1", # fixed to make ROBERT reproducible when using the same version
+        "hyperopt",
+        "numba==0.60.0",
+        "shap==0.46.0",
+        "mapie==0.8.6"
+        # requires also "conda install -c conda-forge gtk3 glib weasyprint pango" for report.py
+        # optionally, the intelex accelerator might be installed "pip install scikit-learn-intelex"
+    ],
+    python_requires=">=3.0",
+    include_package_data=True,
+)
