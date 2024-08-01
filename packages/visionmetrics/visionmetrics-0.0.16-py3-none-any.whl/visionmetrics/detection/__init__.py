f79422227c4dbb42cfa2c3404d83b2f6a17eaef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 06:08:05 +0000

    Add package visionmetrics/0.0.16/visionmetrics-0.0.16-py3-none-any.whl

---
+++ packages/visionmetrics/visionmetrics-0.0.16-py3-none-any.whl/visionmetrics/detection/__init__.py
@@ -0,0 +1,7 @@
+# Import metrics directly from torchmetrics
+
+
+# Import custom metrics from visionmetrics
+from visionmetrics.detection.confusion_matrix import DetectionConfusionMatrix
+from visionmetrics.detection.mean_ap import (ClassAgnosticAveragePrecision,
+                                             MeanAveragePrecision)
