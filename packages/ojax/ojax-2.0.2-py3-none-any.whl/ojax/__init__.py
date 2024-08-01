5787d701ae55bec8714876d24118058b9795bc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:05:46 +0000

    Add package ojax/2.0.2/ojax-2.0.2-py3-none-any.whl

---
+++ packages/ojax/ojax-2.0.2-py3-none-any.whl/ojax/__init__.py
@@ -0,0 +1,19 @@
+from . import otree, pureclass
+from .otree import (
+    OTree,
+    ignore,
+    aux,
+    child,
+    FieldType,
+    fields,
+    get_field_type,
+)
+from .pureclass import (
+    new,
+    PureClass,
+    NoAnnotationWarning,
+)
+
+
+__version__ = "2.0.2"
+
