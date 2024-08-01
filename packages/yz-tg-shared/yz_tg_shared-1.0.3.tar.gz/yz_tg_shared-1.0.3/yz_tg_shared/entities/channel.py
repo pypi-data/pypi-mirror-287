3be23acee3186b8c059fbbf18a8a600566989d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:43 +0000

    Add package yz-tg-shared/1.0.3/yz_tg_shared-1.0.3.tar.gz

---
+++ packages/yz-tg-shared/yz_tg_shared-1.0.3.tar.gz/yz_tg_shared-1.0.3/yz_tg_shared/entities/channel.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from entities.base import Base
+from entities.category import Category
+
+
+class Channel(Base):
+    __tablename__ = 'Channels'
+    ChannelID = Column(Integer, primary_key=True, autoincrement=True)
+    TelegramGrpID = Column(Integer, nullable=False)
+    OffsetMessageID = Column(Integer, nullable=True)
+    ChannelName = Column(String(255), nullable=False)
+    CategoryID = Column(Integer, ForeignKey('Categories.CategoryID'))
+    HasSignature = Column(Boolean, default=True)
+    category = relationship('Category', back_populates='channels')
+
+
+Category.channels = relationship('Channel', order_by=Channel.ChannelID, back_populates='category')
