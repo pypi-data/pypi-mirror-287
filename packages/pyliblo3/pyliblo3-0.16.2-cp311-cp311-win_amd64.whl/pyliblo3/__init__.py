56a33514f51e754b56d404f829ed5cc98324a3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:06:31 +0000

    Add package pyliblo3/0.16.2/pyliblo3-0.16.2-cp311-cp311-win_amd64.whl

---
+++ packages/pyliblo3/pyliblo3-0.16.2-cp311-cp311-win_amd64.whl/pyliblo3/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_7_1():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pyliblo3.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_7_1()
+del _delvewheel_patch_1_7_1
+# end delvewheel patch
+
+import platform
+import os
+
+if platform.system() == "Windows":
+    libspath = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pyliblo3.libs'))
+    if os.path.exists(libspath) and os.path.isdir(libspath):
+        os.add_dll_directory(libspath)
+    else:
+        possible_paths = ['C:/Program Files/liblo/lib', 'C:/Program Files/liblo/bin']
+        for path in ['C:/Program Files/liblo/lib', 'C:/Program Files/liblo/bin']:
+            if os.path.exists(path) and os.path.isdir(path):
+                os.add_dll_directory(path)
+del platform
+del os
+
+
+from ._liblo import *
