97952924efb969d18a7ea97050b04c0c8b2bbf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:06:06 +0000

    Add package cookit/0.7.2/cookit-0.7.2-py3-none-any.whl

---
+++ packages/cookit/cookit-0.7.2-py3-none-any.whl/cookit/pw/screenshot.py
@@ -0,0 +1,45 @@
+from typing import Any, Awaitable, Callable, Literal, Optional, Union
+from typing_extensions import TypeAlias
+
+from playwright.async_api import Page
+
+WaitFunction: TypeAlias = Callable[[Page], Awaitable[Any]]
+WaitForType: TypeAlias = Union[
+    Literal["domcontentloaded", "load", "networkidle"],
+    WaitFunction,
+    None,
+]
+
+
+async def _wait_for(page: Page, wait_type: WaitForType):
+    if callable(wait_type):
+        await wait_type(page)
+    elif wait_type:
+        await page.wait_for_load_state(wait_type)
+
+
+async def screenshot_selector(
+    page: Page,
+    selector: str,
+    wait_type: WaitForType = "load",
+    **kwargs,
+) -> bytes:
+    await _wait_for(page, wait_type)
+    elem = await page.query_selector(selector)
+    if not elem:
+        raise ValueError("Element not found")
+    return await elem.screenshot(**kwargs)
+
+
+async def screenshot_html(
+    page: Page,
+    html: str,
+    selector: Optional[str] = None,
+    wait_type: WaitForType = "load",
+    **kwargs,
+) -> bytes:
+    await page.set_content(html)
+    if selector:
+        return await screenshot_selector(page, selector, wait_type, **kwargs)
+    await _wait_for(page, wait_type)
+    return await page.screenshot(**kwargs)
