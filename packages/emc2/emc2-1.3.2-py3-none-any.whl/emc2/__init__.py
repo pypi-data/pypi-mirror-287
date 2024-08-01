09ff187ca1e90a3cef4c1caee8472e4766e8c778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:05:06 +0000

    Add package emc2/1.3.2/emc2-1.3.2-py3-none-any.whl

---
+++ packages/emc2/emc2-1.3.2-py3-none-any.whl/emc2/__init__.py
@@ -0,0 +1,13 @@
+"""emc2 - An open source framework for atmospheric model and observational column comparison."""
+
+__version__ = '1.3.1'
+__author__ = 'Robert Jackson <rjackson@anl.gov>'
+__all__ = []
+
+from . import core
+from . import io
+from . import simulator
+from . import plotting
+from . import test_files
+from . import scattering
+from . import statistics_LLNL
