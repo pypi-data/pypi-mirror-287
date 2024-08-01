0b383c1f3fe839f544a26bd8daca7ff2d66b5ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:06:34 +0000

    Add package torcheval-nightly/2024.7.31/torcheval_nightly-2024.7.31-py3-none-any.whl

---
+++ packages/torcheval-nightly/torcheval_nightly-2024.7.31-py3-none-any.whl/torcheval/metrics/regression/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from torcheval.metrics.regression.mean_squared_error import MeanSquaredError
+from torcheval.metrics.regression.r2_score import R2Score
+
+__all__ = ["MeanSquaredError", "R2Score"]
+__doc_name__ = "Regression Metrics"
