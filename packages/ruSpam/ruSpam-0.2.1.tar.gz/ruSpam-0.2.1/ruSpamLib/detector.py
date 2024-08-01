5e981d600ddc0c1f0e779d6fcf1ec4a9f4f20c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:06:14 +0000

    Add package ruSpam/0.2.1/ruSpam-0.2.1.tar.gz

---
+++ packages/ruSpam/ruSpam-0.2.1.tar.gz/ruSpam-0.2.1/ruSpamLib/detector.py
@@ -0,0 +1,22 @@
+import requests
+
+def is_spam(message, model_name='spamNS_v1'):
+    api_url = "https://ruspam-ruspamlib-server.hf.space/check_spam"
+    
+    payload = {
+        "message": message,
+        "model_name": model_name
+    }
+    
+    response = requests.post(api_url, json=payload)
+
+    if response.status_code == 200:
+        result = response.json()
+        return result['is_spam'] == 1
+    else:
+        print("Ошибка при запросе к API:", response.status_code)
+        if response.status_code == 400:
+            result = response.json()
+            if 'error' in result:
+                print("Ошибка:", result['error'])
+        return False
