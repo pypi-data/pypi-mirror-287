320ea2106024113caeeb9e4afa0b9682d2ac3b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:04:31 +0000

    Add package okik/0.0.342/okik-0.0.342.tar.gz

---
+++ packages/okik/okik-0.0.342.tar.gz/okik-0.0.342/okik/scripts/dockerfiles/dockerfile_gen.py
@@ -0,0 +1,43 @@
+def create_dockerfile(path, name:str = "Dockerfile"):
+    dockerfile_content = f"""
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Install git
+RUN apt-get update && apt-get install -y git
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install okik and its dependencies
+RUN pip install --upgrade pip
+# create a virtual environment
+RUN python3 -m venv venv
+# activate the virtual environment
+RUN . venv/bin/activate
+# install okik
+RUN echo "Installing okik"
+RUN pip install okik
+# install okik cli
+RUN echo "Installing okik cli"
+RUN export PATH="$HOME/.local/bin:$PATH"
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+# verify installation
+RUN echo "Verifying installation"
+RUN which okik
+# initialise okik
+RUN okik init
+
+# Upgrade pip and install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 3000
+
+# Run okik app
+CMD ["okik", "server", "-r", "-d"]
+    """
+    with open(f"{path}/{name}", 'w') as file:
+        file.write(dockerfile_content)
