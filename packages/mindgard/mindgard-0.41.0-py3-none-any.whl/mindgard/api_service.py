4b91e670675a2223ed08dfc02e69a9ad229ad2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 05:05:00 +0000

    Add package mindgard/0.41.0/mindgard-0.41.0-py3-none-any.whl

---
+++ packages/mindgard/mindgard-0.41.0-py3-none-any.whl/mindgard/api_service.py
@@ -0,0 +1,50 @@
+# Types
+from typing import Callable, Dict, Any
+
+# Requests
+import requests
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+# Constants
+from .constants import (
+    VERSION,
+    API_RETRY_ATTEMPTS,
+    API_RETRY_WAIT_BETWEEN_ATTEMPTS_SECONDS,
+)
+
+
+# Type aliases
+type_post_request_function = Callable[[str, str, Dict[str, Any]], requests.Response]
+type_get_request_function = Callable[[str, str], requests.Response]
+
+
+def _standard_headers(access_token: str) -> Dict[str, str]:
+    return {
+        "Authorization": f"Bearer {access_token}",
+        "User-Agent": f"mindgard-cli/{VERSION}",
+        "X-User-Agent": f"mindgard-cli/{VERSION}",
+    }
+
+
+@retry(
+    stop=stop_after_attempt(API_RETRY_ATTEMPTS),
+    wait=wait_fixed(API_RETRY_WAIT_BETWEEN_ATTEMPTS_SECONDS),
+    reraise=True,
+)
+def api_post(url: str, access_token: str, payload: Dict[str, Any]) -> requests.Response:
+    response = requests.post(
+        url=url, json=payload, headers=_standard_headers(access_token)
+    )
+    response.raise_for_status()
+    return response
+
+
+@retry(
+    stop=stop_after_attempt(API_RETRY_ATTEMPTS),
+    wait=wait_fixed(API_RETRY_WAIT_BETWEEN_ATTEMPTS_SECONDS),
+    reraise=True,
+)
+def api_get(url: str, access_token: str) -> requests.Response:
+    response = requests.get(url=url, headers=_standard_headers(access_token))
+    response.raise_for_status()
+    return response
