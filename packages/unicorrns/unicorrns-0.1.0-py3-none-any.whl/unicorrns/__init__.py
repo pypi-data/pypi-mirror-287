ad3e0d3bb4035bc7aa45bd985daef4e83b8d73ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:04:46 +0000

    Add package unicorrns/0.1.0/unicorrns-0.1.0-py3-none-any.whl

---
+++ packages/unicorrns/unicorrns-0.1.0-py3-none-any.whl/unicorrns/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 14:22:39 2024
+
+@author: Peter
+"""
+
+
+
+
+
+from .unicorn import *
+
+__all__=["get_Rsamp","get_Psamp","get_Psamp_Unc","Pressure","Radius",r"get_maxMtov",r"get_maxrhotov",r"get_MAP"]
