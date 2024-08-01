dea86b8c0f42e5a685ac726437aa2edd125af823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:05:42 +0000

    Add package senfd/0.2.2/senfd-0.2.2-py3-none-any.whl

---
+++ packages/senfd/senfd-0.2.2-py3-none-any.whl/senfd/pipeline.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import List
+
+from senfd.documents.enriched import FromFigureDocument
+from senfd.documents.plain import FromDocx
+from senfd.errors import TableError
+
+CONVERTERS = [FromDocx, FromFigureDocument]
+
+
+def process(input: Path, output: Path) -> List[TableError]:
+    all_errors = []
+
+    for converter in CONVERTERS:
+        if not converter.is_applicable(input):
+            continue
+
+        document, errors = converter.convert(input)
+        all_errors += errors
+
+        document.to_html_file(output)
+        json_path = document.to_json_file(output)
+
+        all_errors += process(json_path, output)
+        break
+
+    return all_errors
