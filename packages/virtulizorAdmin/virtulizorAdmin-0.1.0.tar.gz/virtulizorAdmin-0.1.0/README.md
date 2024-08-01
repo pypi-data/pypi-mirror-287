d94bde2cd2813878eb3ae1478a00ec0b34fe87c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:05:00 +0000

    Add package virtulizorAdmin/0.1.0/virtulizorAdmin-0.1.0.tar.gz

---
+++ packages/virtulizorAdmin/virtulizorAdmin-0.1.0.tar.gz/virtulizorAdmin-0.1.0/README.md
@@ -0,0 +1,21 @@
+# virtulizorAdmin
+
+A brief description of what your library does.
+
+## Installation
+
+You can install the library using pip:
+
+```
+pip install virtulizorAdmin
+```
+
+## Usage
+
+An example of how to use your library:
+
+```python
+from virtulizorAdmin import admin
+
+# Your code here
+```
---
+++ packages/virtulizorAdmin/virtulizorAdmin-0.1.0.tar.gz/virtulizorAdmin-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='virtulizorAdmin',
+    version='0.1.0',
+    description='A brief description of your library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/virtulizorAdmin',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
