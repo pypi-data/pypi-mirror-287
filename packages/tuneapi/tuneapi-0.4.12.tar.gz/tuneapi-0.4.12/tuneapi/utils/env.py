29f0c1fa6119bfb4fb84121979a38ea4c50e7874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 04:04:48 +0000

    Add package tuneapi/0.4.12/tuneapi-0.4.12.tar.gz

---
+++ packages/tuneapi/tuneapi-0.4.12.tar.gz/tuneapi-0.4.12/tuneapi/utils/env.py
@@ -0,0 +1,19 @@
+# Copyright © 2024- Frello Technology Private Limited
+# REMEMBER: nothing from outside tune should be imported in utils
+
+import os
+
+
+class _ENV:
+    def __init__(self):
+        self.vars_called = set()
+
+    TUNE_API_KEY = lambda x="": os.getenv("TUNE_API_KEY", x)
+    TUNE_ORG_ID = lambda x="": os.getenv("TUNE_ORG_ID", x)
+
+    def __getattr__(self, __name: str):
+        self.vars_called.add(__name)
+        return lambda x="": os.getenv(__name, x)
+
+
+ENV = _ENV()
