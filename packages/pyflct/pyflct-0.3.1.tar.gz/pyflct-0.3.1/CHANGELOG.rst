39ccfae08e5c216a4b8a3b42cfe47afef779e62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:05:17 +0000

    Add package pyflct/0.3.1/pyflct-0.3.1.tar.gz

---
+++ packages/pyflct/pyflct-0.3.1.tar.gz/pyflct-0.3.1/CHANGELOG.rst
@@ -0,0 +1,82 @@
+0.3.1 (2024-07-31)
+==================
+
+Bugfixes
+--------
+
+- Tried to fix builds on conda-forge. (`#80 <https://github.com/sunpy/pyflct/pull/80>`__)
+
+0.3.0 (2024-07-23)
+==================
+
+Backwards Incompatible Changes
+------------------------------
+
+- Update to wheels:
+
+  Linux base goes from manylinux2014 to manylinux_2_28
+  macOS base goes from 10.9 to 12.0 (`#74 <https://github.com/sunpy/pyflct/pull/74>`__)
+- Dropped Python 3.9 support. (`#74 <https://github.com/sunpy/pyflct/pull/74>`__)
+
+
+Features
+--------
+
+- Added Python 3.12 wheels. (`#74 <https://github.com/sunpy/pyflct/pull/74>`__)
+- Build with numpy 2.0.0 as the base. (`#74 <https://github.com/sunpy/pyflct/pull/74>`__)
+
+
+0.2.4 (2023-11-27)
+==================
+
+Breaking Changes
+----------------
+
+- Dropped support for Python 3.8
+
+0.2.3 (2022-05-24)
+==================
+
+Breaking Changes
+----------------
+
+- Dropped support for Python 3.6 and 3.7
+
+Features
+--------
+
+- Added wheels for python 3.10
+
+0.2.2 (2020-05-01)
+==================
+
+Features
+--------
+
+- Added wheels for python 3.9
+
+0.2.1 (2020-05-08)
+==================
+
+Features
+--------
+
+- Fix PyPi upload.
+
+
+0.2.0 (2020-05-08)
+==================
+
+Features
+--------
+
+- Updated docs and enabled windows via anaconda.
+
+
+0.1.0 (2020-05-01)
+==================
+
+Features
+--------
+
+- Seeded pyflct with the C, Cython, Python code and examples to create the pyflct package. (`#1 <https://github.com/sunpy/pyflct/pull/1>`__)
---
+++ packages/pyflct/pyflct-0.3.1.tar.gz/pyflct-0.3.1/docs/conf.py
@@ -0,0 +1,183 @@
+"""
+Configuration file for the Sphinx documentation builder.
+"""
+
+# -- stdlib imports ------------------------------------------------------------
+import os
+import datetime
+from packaging.version import Version
+
+# -- Read the Docs Specific Configuration --------------------------------------
+
+# This needs to be done before sunpy is imported
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+if on_rtd:
+    os.environ["SUNPY_CONFIGDIR"] = "/home/docs/"
+    os.environ["HOME"] = "/home/docs/"
+    os.environ["LANG"] = "C"
+    os.environ["LC_ALL"] = "C"
+    os.environ["HIDE_PARFIVE_PROGESS"] = "True"
+
+# -- Non stdlib imports --------------------------------------------------------
+from pyflct import __version__
+
+# -- Project information -------------------------------------------------------
+project = "pyflct"
+author = "The SunPy Community"
+copyright = "{}, {}".format(datetime.datetime.now().year, author)
+
+# The full version, including alpha/beta/rc tags
+release = __version__
+pyflct_version = Version(__version__)
+is_release = not (pyflct_version.is_prerelease or pyflct_version.is_devrelease)
+
+# For the linkcheck
+linkcheck_ignore = [
+    r"https://doi.org/\d+",
+    r"https://element.io/\d+",
+    r"https://github.com/\d+",
+    r"https://docs.sunpy.org/\d+",
+]
+linkcheck_anchors = False
+
+# This is added to the end of RST files - a good place to put substitutions to
+# be used globally.
+rst_epilog = """
+.. SunPy
+.. _SunPy: https://sunpy.org
+.. _`SunPy mailing list`: https://groups.google.com/group/sunpy
+.. _`SunPy dev mailing list`: https://groups.google.com/group/sunpy-dev
+"""
+
+# -- General configuration -----------------------------------------------------
+
+# Suppress warnings about overriding directives as we overload some of the
+# doctest extensions.
+suppress_warnings = [
+    "app.add_directive",
+]
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "matplotlib.sphinxext.plot_directive",
+    "sphinx_automodapi.automodapi",
+    "sphinx_automodapi.smart_resolver",
+    "sphinx_changelog",
+    "sphinx_gallery.gen_gallery",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.inheritance_diagram",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+# templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+
+# Add any extra paths that contain custom files (such as robots.txt or
+# .htaccess) here, relative to this directory. These files are copied
+# directly to the root of the documentation.
+html_extra_path = ["robots.txt"]
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
+
+# The reST default role (used for this markup: `text`) to use for all
+# documents. Set to the "smart" one.
+default_role = "obj"
+
+# Disable having a separate return type row
+napoleon_use_rtype = False
+
+# Disable google style docstrings
+napoleon_google_docstring = False
+
+# -- Options for intersphinx extension -----------------------------------------
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    "python": (
+        "https://docs.python.org/3/",
+        (None, "http://www.astropy.org/astropy-data/intersphinx/python3.inv"),
+    ),
+    "numpy": (
+        "https://numpy.org/doc/stable/",
+        (None, "http://www.astropy.org/astropy-data/intersphinx/numpy.inv"),
+    ),
+    "scipy": (
+        "https://docs.scipy.org/doc/scipy/reference/",
+        (None, "http://www.astropy.org/astropy-data/intersphinx/scipy.inv"),
+    ),
+    "matplotlib": (
+        "https://matplotlib.org/",
+        (None, "http://www.astropy.org/astropy-data/intersphinx/matplotlib.inv"),
+    ),
+    "sunpy": (
+        "https://sunpy.org/",
+        (None, "https://docs.sunpy.org/en/stable/"),
+    ),
+    "astropy": ("https://docs.astropy.org/en/stable/", None),
+    "sqlalchemy": ("https://docs.sqlalchemy.org/en/latest/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "skimage": ("https://scikit-image.org/docs/stable/", None),
+    "drms": ("https://docs.sunpy.org/projects/drms/en/stable/", None),
+    "parfive": ("https://parfive.readthedocs.io/en/stable/", None),
+    "reproject": ("https://reproject.readthedocs.io/en/stable/", None),
+    "aiapy": ("https://aiapy.readthedocs.io/en/stable/", None),
+}
+
+# -- Options for HTML output ---------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = "sunpy"
+
+from sunpy_sphinx_theme import PNG_ICON
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+
+# Render inheritance diagrams in SVG
+graphviz_output_format = "svg"
+
+graphviz_dot_args = [
+    "-Nfontsize=10",
+    "-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Efontsize=10",
+    "-Efontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Gfontsize=10",
+    "-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+]
+
+# -- Sphinx Gallery ------------------------------------------------------------
+sphinx_gallery_conf = {
+    "backreferences_dir": os.path.join("generated", "modules"),
+    "filename_pattern": "^((?!skip_).)*$",
+    "examples_dirs": os.path.join("..", "examples"),
+    "within_subsection_order": "ExampleTitleSortKey",
+    "gallery_dirs": os.path.join("generated", "gallery"),
+    "default_thumb_file": PNG_ICON,
+    "abort_on_example_error": False,
+    "plot_gallery": "True",
+    "remove_config_comments": True,
+    "doc_module": ("sunpy"),
+    "only_warn_on_example_error": True,
+}
