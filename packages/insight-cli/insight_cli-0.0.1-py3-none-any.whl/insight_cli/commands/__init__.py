5fb6535f2bd943c1273b206f882c2ea00f966a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:06:00 +0000

    Add package insight-cli/0.0.1/insight_cli-0.0.1-py3-none-any.whl

---
+++ packages/insight-cli/insight_cli-0.0.1-py3-none-any.whl/insight_cli/commands/__init__.py
@@ -0,0 +1,5 @@
+from .base import Command
+from .initialize_command import InitializeCommand
+from .query_command import QueryCommand
+from .uninitialize_command import UninitializeCommand
+from .version_command import VersionCommand
