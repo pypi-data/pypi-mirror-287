c7a86a68e0e687042e4ed08877dc65ba847dbf2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:05:22 +0000

    Add package durakgame/1.0.2/durakgame-1.0.2.tar.gz

---
+++ packages/durakgame/durakgame-1.0.2.tar.gz/durakgame-1.0.2/README.md
@@ -0,0 +1,5 @@
+# Durak
+
+A simple implementation of the Russian's [Durak](https://en.wikipedia.org/wiki/Durak) card game.
+
+For example usage see [this](tests/Usage.ipynb)
---
+++ packages/durakgame/durakgame-1.0.2.tar.gz/durakgame-1.0.2/tests/utils.py
@@ -0,0 +1,16 @@
+from durakgame import CardValue, Suit
+
+six = CardValue.Six
+seven = CardValue.Seven
+eight = CardValue.Eight
+nine = CardValue.Nine
+ten = CardValue.Ten
+jack = CardValue.Jack
+queen = CardValue.Queen
+king = CardValue.King
+ace = CardValue.Ace
+
+heart = Suit.Heart
+spade = Suit.Spade
+cross = Suit.Cross
+diamond = Suit.Diamond
