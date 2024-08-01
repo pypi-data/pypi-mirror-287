b943118a0d0caa803a89bef33c4fb1fe49b9013b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 03:05:48 +0000

    Add package PyWaves/1.0.5/PyWaves-1.0.5.tar.gz

---
+++ packages/PyWaves/PyWaves-1.0.5.tar.gz/PyWaves-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='PyWaves',
+      version='1.0.5',
+      description='Object-oriented library for the Waves blockchain platform',
+      url='http://github.com/pywaves/pywaves',
+      author='PyWaves Developers',
+      author_email='dev@pywaves.org',
+      license='MIT',
+      packages=['pywaves', 'pywaves/protobuf', 'pywaves/protobuf/waves'],
+      keywords = ['waves', 'blockchain', 'analytics'],
+      install_requires=[
+	    'base58==0.2.5',
+#            'pyblake2',
+            'python-axolotl-curve25519',
+            'requests',
+	    'google-api-python-client',
+	    'protobuf==3.19.6'
+      ]
+      )
+
+
